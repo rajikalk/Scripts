59ef24f5b92e68ac24575bd5dafe1745ae34b155
rajikalk <rajikakuruwita@gmail.com>
2016-11-08 09:49:25 +1100

    wokring version of slice_plot before I try to average centers

--- my_slice_plot.py
+++ my_slice_plot.py
@@ -22,7 +22,7 @@ def parse_inputs():
     parser.add_argument("-pt", "--plot_time", help="would you like to plot a specific time?", default=None)
     parser.add_argument("-dt", "--time_step", help="time step between movie frames", default=2, type=float)
     parser.add_argument("-res", "--resolution", help="how many pixels by how many pixels do you want?", default=512, type=int)
-    parser.add_argument("-qf", "--quiver_frequency", help="How many pixles do you want between quivers?", default=32, type=int)
+    parser.add_argument("-qf", "--quiver_frequency", help="How many quivers along one edge do you want?", default=31, type=int)
     parser.add_argument("-c", "--center", help="would you like to center the profile on the center of mass, or tone of the particles?, 0=CoM, 1=particle 1, 2=particle 2", default=0, type=int)
     parser.add_argument("-fi", "--field", help="What field would you like to plot?", default="Relative_Keplerian_Velocity_Cyl", nargs='+')
     parser.add_argument("-ip", "--image_prefix", help="do you want your saved images to all have the same prefix?", default=None, type=str)
@@ -30,7 +30,8 @@ def parse_inputs():
     parser.add_argument("-clim", "--colour_limits", help="colourbar limits", nargs="+", type=float, default=None)
     parser.add_argument("-tmax", "--time_max", help="what is the max time you would like to go up to?", default=None, type=int)
     parser.add_argument("-cl", "--colour_bar_label", help="what do you want as the colour bar label?", default=None, type=str)
-    parser.add_argument("-pvl", "--plot_velocity_legend", help="would you like to annotate the velocity legend?", default=True)
+    parser.add_argument("-pvl", "--plot_velocity_legend", help="would you like to annotate the velocity legend?", default=False, type=bool)
+    parser.add_argument("-sc", "--smooth_cells", help="how many cells would you like the smooth the velocities over? If not defined it is set to half the annotatation frequency", default=None, type=int)
     args = parser.parse_args()
     return args
 
@@ -60,12 +61,10 @@ def find_files_bin(m_times, files, sink_form_time):
     pit = 0
     while mit < len(m_times):
         it = int(np.round(min + ((max - min)/2.)))
-        #print 'search iterator =', it
         file = files[it]
         part_file=file[:-12] + 'part' + file[-5:]
         ds = yt.load(file, particle_filename=part_file)
         time = ds.current_time.in_units('yr').value-sink_form_time
-        #print "it =", it
         if pit == it or time == m_times[mit]:
             if m_times[mit] == 0.0:
                 if time < 0.0:
@@ -77,15 +76,12 @@ def find_files_bin(m_times, files, sink_form_time):
                         time = ds.current_time.in_units('yr').value-sink_form_time
                         if time > 0.0:
                             usable_files.append(file)
-                            print("found time", time, "for m_time", m_times[mit])
                         else:
                             it = it + 1
                 else:
                     usable_files.append(file)
-                    print("found time", time, "for m_time", m_times[mit])
             else:
                 usable_files.append(file)
-                print("found time", time, "for m_time", m_times[mit])
             mit = mit + 1
             min = it
             max = len(files)-1
@@ -110,7 +106,10 @@ def find_time(plot_time, files, sink_form_time):
         ds = yt.load(files[it])
         time_val = (ds.current_time.in_units('yr').value)-sink_form_time
         if pit == it:
-            print("found time", time_val)
+            if plot_time == 0.0 and time_val < 0.0:
+                it = it + 1
+                ds = yt.load(files[it])
+                time_val = (ds.current_time.in_units('yr').value)-sink_form_time
             found = True
             break
         elif time_val > plot_time:
@@ -122,7 +121,6 @@ def find_time(plot_time, files, sink_form_time):
     return it
 
 def _CoM(field, data):
-    #Center of mass calculation:
     TM = np.sum(data['cell_mass'].in_units('g'))
     x_top = yt.YTArray(0.0, 'cm*g')
     y_top = yt.YTArray(0.0, 'cm*g')
@@ -159,6 +157,45 @@ def _My_Bulk_Velocity(field, data):
 
 yt.add_field("My_Bulk_Velocity", function=_My_Bulk_Velocity, units=r"cm/s")
 
+def get_quiver_arrays(velx_full, vely_full):
+    global args
+    annotate_freq = float(args.resolution)/float(args.quiver_frequency)
+    if args.smooth_cells == None:
+        smoothing_val = int(annotate_freq/2)
+    else:
+        smoothing_val = int(args.smooth_cells)
+    x_pos = []
+    y_pos = []
+    counter = 0
+    while counter < 31:
+        val = int(annotate_freq*counter + annotate_freq/2.)
+        x_pos.append(int(val))
+        y_pos.append(int(val))
+        counter = counter + 1
+    velx = []
+    vely = []
+    for x in x_pos:
+        x = int(x)
+        xarr = []
+        yarr = []
+        for y in y_pos:
+            y = int(y)
+            x_vel =[]
+            y_vel = []
+            for curx in range(x-smoothing_val, x+smoothing_val):
+                for cury in range(y-smoothing_val, y+smoothing_val):
+                    x_vel.append(velx_full[curx][cury])
+                    y_vel.append(vely_full[curx][cury])
+            x_vel = np.mean(x_vel)
+            y_vel = np.mean(y_vel)
+            xarr.append(x_vel)
+            yarr.append(y_vel)
+        velx.append(xarr)
+        vely.append(yarr)
+    velx = yt.YTArray(velx, "cm/s")
+    vely = yt.YTArray(vely, "cm/s")
+    return velx, vely
+
 def my_own_quiver_function(axis, X_pos, Y_pos, X_val, Y_val):
     global field_dictionary
     global args
@@ -168,10 +205,8 @@ def my_own_quiver_function(axis, X_pos, Y_pos, X_val, Y_val):
     xmin = np.min(field_dictionary['X'][0].in_units('AU').value)
     xmax = np.max(field_dictionary['X'][0].in_units('AU').value)
     ymin = np.min(field_dictionary['Y'][0].in_units('AU').value)
-    ymax = np.max(field_dictionary['Y'][0].in_units('AU').value)
     len_scale = standard_vel/(0.04*(xmax - xmin))
     vels = np.hypot(X_val, Y_val)
-    print("got image limits. About to plot arrows")
     for xp in range(len(X_pos[0])):
         for yp in range(len(Y_pos[0])):
             xvel = X_val[xp][yp]/len_scale
@@ -180,11 +215,7 @@ def my_own_quiver_function(axis, X_pos, Y_pos, X_val, Y_val):
             if width_val > 1.0:
                 width_val = 1.0
             axis.add_patch(mpatches.FancyArrowPatch((X_pos[xp][yp], Y_pos[xp][yp]), (X_pos[xp][yp]+xvel, Y_pos[xp][yp]+yvel), color='w', linewidth=1.*width_val, arrowstyle='->', mutation_scale=15.*width_val, shrinkA=0.0, shrinkB=0.0))
-            print("plotted arrow for position", xp, yp)
-    print("done plotting arrows")
     if args.plot_velocity_legend == True:
-        #import pdb
-        #pdb.set_trace()
         print("plotting quiver legend")
         pos_start = [0.77*xmax, 0.87*ymin]
         xvel = standard_vel/len_scale
@@ -192,7 +223,6 @@ def my_own_quiver_function(axis, X_pos, Y_pos, X_val, Y_val):
         width_val = 1.0
         axis.add_patch(mpatches.FancyArrowPatch((pos_start[0], pos_start[1]), (pos_start[0]+xvel, pos_start[1]+yvel), arrowstyle='->', color='w', linewidth=1.*width_val, mutation_scale=15.*width_val))
         axis.annotate("5kms$^{-1}$", xy=(0.98*xmax, 0.95*ymin), va="center", ha="right", color='w', fontsize=16)
-        print("plotted arrow legend")
 
 
 def My_plotting_function(dd, field, colormap=None, save_name=None, image_prefix=None, log_colour=False, clim=None, units=None):
@@ -209,29 +239,21 @@ def My_plotting_function(dd, field, colormap=None, save_name=None, image_prefix=
             field_data = field_dictionary[field].in_units(units)
         if str(field_dictionary[field].unit_quantity).split(' ')[-1] != '':
             cbar_label = cbar_label + ' (' + str(field_dictionary[field].unit_quantity).split(' ')[-1] + ')'
-    print("cbar_label=", cbar_label)
     
     plt.clf()
     fig, ax = plt.subplots()
-    #import pdb
-    #pdb.set_trace()
     if log_colour:
-        plot = plt.pcolormesh(field_dictionary['X'].value, field_dictionary['Y'].value, np.fliplr(field_dictionary[field].value), cmap='brg', norm=LogNorm(), rasterized=True)
-        #plot = plt.imshow(np.fliplr(field_dictionary[field].value), extent=[-bx, bx, -bx, bx], cmap='brg', norm=LogNorm())
+        plot = plt.pcolormesh(field_dictionary['X'].value, field_dictionary['Y'].value, field_dictionary[field].value, cmap='brg', norm=LogNorm(), rasterized=True)
     else:
-        plot = plt.pcolormesh(field_dictionary['X'].value, field_dictionary['Y'].value, np.fliplr(field_dictionary[field].value), cmap='brg', rasterized=True)
-        #plot = plt.imshow(np.fliplr(field_dictionary[field].value), extent=[-bx, bx, -bx, bx], cmap='brg')
+        plot = plt.pcolormesh(field_dictionary['X'].value, field_dictionary['Y'].value, field_dictionary[field].value, cmap='brg', rasterized=True)
     print("plotted image")
     if clim != None:
         plt.clim(clim)
     cbar = plt.colorbar(plot, pad=0.0)
     cbar.set_label(cbar_label, rotation=270, labelpad=12, size=14)
-    print("plotted color bar")
-    #Q = plt.quiver(field_dictionary['quiver_grids'][0].value, field_dictionary['quiver_grids'][1].value, field_dictionary['velx_quiver'].in_units('km/s').value, field_dictionary['vely_quiver'].in_units('km/s').value)
-    #qk = plt.quiverkey(Q, 0.9, 0.1, 5, r'$5 \frac{km}{s}$')
+    #print("plotted color bar")
     print("plotting quiver plot")
     my_own_quiver_function(ax, field_dictionary['X_quiver'].in_units('AU').value, field_dictionary['Y_quiver'].in_units('AU').value, field_dictionary['velx_quiver'].value, field_dictionary['vely_quiver'].value)
-    print("plotting quiver plot")
     if ('all', u'particle_mass') in dd.ds.field_list:
         part_color = ['lime','cyan','r','c','y','w','k']
         line_rad = np.min(dd['dx'].in_units('AU'))*2.5
@@ -252,7 +274,7 @@ def My_plotting_function(dd, field, colormap=None, save_name=None, image_prefix=
         save_name = field +'_sliceplot.eps'
     if image_prefix != None:
         save_name = image_prefix + save_name
-    plt.savefig(save_name, bbox_inches='tight')
+    plt.savefig(save_name, bbox_inches='tight', pad_inches = 0.02)
     print('Saved file', save_name)
     return fig
 
@@ -300,7 +322,8 @@ nx_quiver = args.quiver_frequency #number of points for a quiver plot in the gri
 x = np.linspace(-bx, bx, nx)
 xy = np.meshgrid(x,x)
 delta_quiver = (bx*2)/nx_quiver
-x_quiver = np.linspace(-bx+(delta_quiver/2.), bx-(delta_quiver/2.), (nx_quiver-1))
+annotate_freq = args.resolution/args.quiver_frequency
+x_quiver = np.linspace(-bx+(delta_quiver/2.), bx-(delta_quiver/2.), (nx_quiver))
 xy_quiver = np.meshgrid(x_quiver,x_quiver)
 print("Created plot and quiver meshs")
 
@@ -333,11 +356,9 @@ for file in usable_files:
 
     #create grid of the pixel positions and quiver positions
     xyz_grid = yt.YTArray([xy[0].flatten(), xy[1].flatten(), dz*np.ones_like(xy[0].flatten().value)]).T
-    xyz_grid_quiver = yt.YTArray([xy_quiver[0].flatten(), xy_quiver[1].flatten(), np.zeros_like(xy_quiver[0].flatten())]).T
 
     #This line also takes a while
     print("Finding nearest points...")
-    nearest_points_quiver = tree.query(xyz_grid_quiver, distance_upper_bound=np.max(dd['dx'].in_units('AU')), n_jobs=-1)
     nearest_points = tree.query(xyz_grid, distance_upper_bound=np.max(dd['dx'].in_units('AU')), n_jobs=-1)
     print("Done finding nearest points...")
 
@@ -346,8 +367,10 @@ for file in usable_files:
     vx_grid = dd['velx'][nearest_points[1]].reshape(xy[0].shape).in_units('cm/s') - center_vel[0]
     vy_grid = dd['vely'][nearest_points[1]].reshape(xy[0].shape).in_units('cm/s') - center_vel[1]
     vz_grid = dd['velz'][nearest_points[1]].reshape(xy[0].shape).in_units('cm/s') - center_vel[2]
-    vx_grid_quiver = dd['velx'][nearest_points_quiver[1]].reshape(xy_quiver[0].shape)
-    vy_grid_quiver = dd['vely'][nearest_points_quiver[1]].reshape(xy_quiver[0].shape)
+    vx_grid_quiver_full = dd['velx'][nearest_points[1]].reshape(xy[0].shape).in_units('cm/s')
+    vy_grid_quiver_full = dd['vely'][nearest_points[1]].reshape(xy[0].shape).in_units('cm/s')
+    vx_grid_quiver, vy_grid_quiver = get_quiver_arrays(vx_grid_quiver_full, vy_grid_quiver_full)
+
 
     #Try computing mass versus radius
     radii_cyl = np.sqrt((xyz.T[0].in_units('AU')-center[0])**2 + (xyz.T[1].in_units('AU')-center[1])**2)
@@ -372,6 +395,7 @@ for file in usable_files:
         a = yt.YTArray(0.0, 'cm')
 
     #Calculate enclosed mass in cylindrical coordinates
+    print("Calculating cylindrical enclosed mass")
     enclosed_mass_grid_cyl = yt.YTArray(np.zeros(np.shape(r_corr_grid)), 'g')
     prev_r = rs[0]
     prev_enc_mass_cyl = yt.YTArray(0.0, 'g')
@@ -379,24 +403,19 @@ for file in usable_files:
     for r in rs[1:]:
         ind = np.where((radii_cyl >= prev_r) & (radii_cyl < r))
         grid_ind = np.where((r_corr_grid >= prev_r) & (r_corr_grid < r))
-        #print("enclosed_mass(1) =", enclosed_mass_cyl)
         if len(ind) != 0:
             enclosed_mass_cyl = prev_enc_mass_cyl + np.sum(masses[ind])
-            print("enclosed_mass(1) =", enclosed_mass_cyl.in_units('Msun'))
             if r == rs[1] and args.center != 0:
                 enclosed_mass_cyl = enclosed_mass_cyl + dd['particle_mass'][args.center-1]
-                print("enclosed_mass(2) =", enclosed_mass_cyl.in_units('Msun'))
             if included_particles == False:
                 if args.center != 0 and r > a.in_units('AU'):
                     if args.center == 1:
                         enclosed_mass_cyl = enclosed_mass_cyl + dd['particle_mass'][1]
                     else:
                         enclosed_mass_cyl = enclosed_mass_cyl + dd['particle_mass'][0]
-                    print("enclosed_mass(3) =", enclosed_mass_cyl.in_units('Msun'))
                     included_particles = True
                 elif args.center == 0 and r > a.in_units('AU')/2. and ('all', u'particle_mass') in ds.field_list:
                     enclosed_mass_cyl = enclosed_mass_cyl + np.sum(dd['particle_mass'])
-                    print("enclosed_mass(4) =", enclosed_mass_cyl.in_units('Msun'))
                     included_particles = True
             enclosed_mass_grid_cyl[grid_ind] = enclosed_mass_cyl
             prev_enc_mass_cyl = enclosed_mass_cyl
@@ -404,6 +423,7 @@ for file in usable_files:
 
     #Then again in spherical coordinates
     #Calculate enclosed mass in cylindrical coordinates
+    print("Calculating spherical enclosed mass")
     enclosed_mass_grid_sph = yt.YTArray(np.zeros(np.shape(r_corr_grid)), 'g')
     prev_r = rs[0]
     prev_enc_mass_sph = yt.YTArray(0.0, 'g')
@@ -411,24 +431,19 @@ for file in usable_files:
     for r in rs[1:]:
         ind = np.where((radii_sph >= prev_r) & (radii_sph < r))
         grid_ind = np.where((r_corr_grid >= prev_r) & (r_corr_grid < r))
-        #print("enclosed_mass(1) =", enclosed_mass_cyl)
         if len(ind) != 0:
             enclosed_mass_sph = prev_enc_mass_sph + np.sum(masses[ind])
-            print("enclosed_mass(1) =", enclosed_mass_sph.in_units('Msun'))
             if r == rs[1] and args.center != 0:
                 enclosed_mass_sph = enclosed_mass_sph + dd['particle_mass'][args.center-1]
-                print("enclosed_mass(2) =", enclosed_mass_sph.in_units('Msun'))
             if included_particles == False:
                 if args.center != 0 and r > a.in_units('AU'):
                     if args.center == 1:
                         enclosed_mass_sph = enclosed_mass_sph + dd['particle_mass'][1]
                     else:
                         enclosed_mass_sph = enclosed_mass_sph + dd['particle_mass'][0]
-                    print("enclosed_mass(3) =", enclosed_mass_sph.in_units('Msun'))
                     included_particles = True
                 elif args.center == 0 and r > a.in_units('AU')/2. and ('all', u'particle_mass') in ds.field_list:
                     enclosed_mass_sph = enclosed_mass_sph + np.sum(dd['particle_mass'])
-                    print("enclosed_mass(4) =", enclosed_mass_sph.in_units('Msun'))
                     included_particles = True
             enclosed_mass_grid_sph[grid_ind] = enclosed_mass_sph
             prev_enc_mass_sph = enclosed_mass_sph
