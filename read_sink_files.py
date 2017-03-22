d81cf74a70e471455850d0a54107ad8e332bd22a
rajikalk <rajikakuruwita@gmail.com>
2017-03-22 16:31:21 +1100

    fixed to read sink formation time and not the accretion rate

--- read_sink_files.py
+++ read_sink_files.py
@@ -34,12 +34,14 @@ with open(file, 'r') as f:
                 accretion.append([])
             pit = particle_tags.index(part_tag)
 
-            t = float(row[1]) - float(row[15])
-            pos = [float(row[2]), float(row[3]), float(row[4])]
+            t = (float(row[1]) - float(row[16]))/yt.units.year.in_units('s').value
+            pos = [float(row[2])/yt.units.AU.in_units('cm').value, float(row[3])/yt.units.AU.in_units('cm').value, float(row[4])/yt.units.AU.in_units('cm').value]
             vel = [float(row[5]), float(row[6]), float(row[7])]
             acc = [float(row[8]), float(row[9]), float(row[10])]
             ang = np.sqrt(float(row[11])**2. + float(row[12])**2. + float(row[13])**2.)
-            m = float(row[14])
+            ang = yt.YTArray(ang, 'g*cm**2/s')
+            ang = ang.in_units('Msun*km**2/yr')
+            m = float(row[14])/yt.units.Msun.in_units('g').value
             mdot = float(row[15])
 
             time[pit].append(t)
@@ -50,6 +52,8 @@ with open(file, 'r') as f:
             mass[pit].append(m)
             accretion.append(mdot)
 
+print "Read sink file"
+
 particle_tags = np.array(particle_tags)
 time = np.array(time)
 position = np.array(position)
@@ -59,9 +63,11 @@ angular_momentum = np.array(angular_momentum)
 mass = np.array(mass)
 accretion = np.array(accretion)
 
-for particle in range(len(particle_tag)):
-    plt.plot(time[particle]/yt.units.year.in_units('s').value, angular_momentum[particle]/mass[particle])
-plt.x
+for particle in range(len(particle_tags)):
+    plt.plot(time[particle], np.array(angular_momentum[particle])/np.array(mass[particle]))
+plt.xlabel("Time since protostar formation")
+plt.ylabel("Specific angular momentum (cm$^2$s$^{-1}$)")
+plt.savefig('specific_angular_momentum.png')
 
 
 
