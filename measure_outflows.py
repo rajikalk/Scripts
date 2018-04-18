9ed16462569c028fed0e5868e70cc713eb9a217e
rajikalk <rajikakuruwita@gmail.com>
2018-04-18 14:34:34 +1000

    changed a lot... included velocity threshold for measuring outflows and also added some code to try and measure keplerian mass

--- measure_outflows.py
+++ measure_outflows.py
@@ -25,6 +25,9 @@ def parse_inputs():
     parser.add_argument("-disk", "--measure_disks", help="Do you want to measure disk quantities?", type=bool, default=False)
     parser.add_argument("-lb", "--lower_bounds", help="lower bounds of box", type=float, default=50.0)
     parser.add_argument("-rad", "--radius", help="What is the radius of the measuring cylindar", type=float, default=6000.0)
+    parser.add_argument("-vthres", "--velocity_threshold", help="what velocity threshold do you want to use to outflowing material?", type=float, default=0.0)
+    parser.add_argument("-center", "--disk_center", help="do you want two disks centered on the particles or a volume centered on the CoM?", default='CoM', type=str)
+    parser.add_argument("-st", "--start_time", help="default is -500yr", default=-500.0, type=float)
     #parser.add_argument("files", nargs='*')
     args = parser.parse_args()
     return args
@@ -54,10 +57,15 @@ files = sorted(glob.glob(dir + file_name))
 
 #define analysis cylinder:
 if args.measure_disks != False:
-    radius = 20.0
-    height = 20.0
-    tube_center_1 = None
-    tube_center_2 = None
+    if args.disk_center == 'CoM':
+        radius = 100.0
+        height = 100.0
+        tube_center_1 = None
+    else:
+        radius = 20.0
+        height = 20.0
+        tube_center_1 = None
+        tube_center_2 = None
 else:
     '''
     radius = 500.0
@@ -66,7 +74,7 @@ else:
     lower_cylinder_bound = -250.0
     '''
     radius = args.radius
-    height = 4011.0
+    height = 3300.0
     upper_cylinder_bound = args.lower_bounds
     lower_cylinder_bound = -1 * args.lower_bounds
     center_1 = [0.0, 0.0, (upper_cylinder_bound+(height/2.))*yt.units.AU.in_units('cm')]
@@ -80,7 +88,7 @@ sink_form = np.min(dd['particle_creation_time']/yt.units.yr.in_units('s')).value
 
 #find times:
 if rank == 0:
-    m_times = mym.generate_frame_times(files, args.time_step, presink_frames=0, end_time=None, start_time=-500.)
+    m_times = mym.generate_frame_times(files, args.time_step, presink_frames=0, end_time=None, start_time=args.start_time)
     usable_files = mym.find_files(m_times, files)
     for other_rank in range(1, size):
         comm.send(usable_files, dest=other_rank, tag=1)
@@ -98,47 +106,66 @@ if rank == 0:
         f.close()
     f = open(save_dir + output_file, 'w')
     if args.measure_disks == False:
-        f.write('Lref ' + dir.split('_')[-1] + ': Time, Mass, Momentum, Angular Momentum, Max speed, Unbound Mass \n')
+        f.write('Lref ' + dir.split('_')[-1] + ': Time, Mass, Momentum, Angular Momentum, Max speed, Unbound Mass, CoM dist \n')
     else:
         f.write('Lref ' + dir.split('_')[-1] + ': Time, Lz_1, Lz_2 \n')
     f.close()
 
 file_no = 0
-rit = 1
+rit = 0
 for file in usable_files:
     if rank == rit:
         part_file = file[:-12] + 'part' + file[-5:]
         ds = yt.load(file, particle_filename=part_file)
         dd = ds.all_data()
+        dist = np.sqrt(dd['CoM'][0]**2. + dd['CoM'][1]**2. + dd['CoM'][2]**2.).in_units('AU').value
+        print "CoM dist =", dist, "(AU)"
         
         if args.measure_disks != False:
-            center_1 = [dd['particle_posx'][0].in_units('cm'), dd['particle_posy'][0].in_units('cm'), dd['particle_posz'][0].in_units('cm')]
-            center_2 = [dd['particle_posx'][1].in_units('cm'), dd['particle_posy'][1].in_units('cm'), dd['particle_posz'][1].in_units('cm')]
+            if args.disk_center == 'CoM':
+                center_1 = [dd['CoM'][0].in_units('cm'), dd['CoM'][1].in_units('cm'), dd['CoM'][2].in_units('cm')]
+                center_2 = None
+            else:
+                center_1 = [dd['particle_posx'][0].in_units('cm'), dd['particle_posy'][0].in_units('cm'), dd['particle_posz'][0].in_units('cm')]
+                center_2 = [dd['particle_posx'][1].in_units('cm'), dd['particle_posy'][1].in_units('cm'), dd['particle_posz'][1].in_units('cm')]
         
         time_val = ds.current_time.in_units('yr').value - sink_form
 
         #define cylinders:
         tube_1 = ds.disk(center_1, [0.0, 0.0, 1.0], (radius, 'au'), (height/2., 'au'))
-        tube_2 = ds.disk(center_2, [0.0, 0.0, 1.0], (radius, 'au'), (height/2., 'au'))
+        if center_2 != None:
+            tube_2 = ds.disk(center_2, [0.0, 0.0, 1.0], (radius, 'au'), (height/2., 'au'))
         
         #calculate mass in cylinders
         # for region 1
         if args.measure_disks != False:
-            myf.set_center(1)
-            #print "doing center 1, with center_vel =", dd['Center_Velocity']
-            Lz_1 = np.sum(tube_1['Angular_Momentum_z'].in_units("Msun * km**2 / s").value)
-            myf.set_center(2)
-            del dd
-            dd = ds.all_data()
-            #print "doing center 2, with center_vel =", dd['Center_Velocity']
-            Lz_2 = np.sum(tube_2['Angular_Momentum_z'].in_units("Msun * km**2 / s").value)
-            del dd
-            print "OUTPUT=", time_val, Lz_1, Lz_2, "on rank", rit
+            if center_2 != None:
+                myf.set_center(1)
+                #print "doing center 1, with center_vel =", dd['Center_Velocity']
+                Lz_1 = np.sum(tube_1['Angular_Momentum_z'].in_units("Msun * km**2 / s").value)
+                myf.set_center(2)
+                del dd
+                dd = ds.all_data()
+                #print "doing center 2, with center_vel =", dd['Center_Velocity']
+                Lz_2 = np.sum(tube_2['Angular_Momentum_z'].in_units("Msun * km**2 / s").value)
+                del dd
+                print "OUTPUT=", time_val, Lz_1, Lz_2, "on rank", rit
                     
-            #send data to rank 0 to append to write out.
-            write_data = [time_val, Lz_1, Lz_2]
+                #send data to rank 0 to append to write out.
+                write_data = [time_val, Lz_1, Lz_2]
+            else:
+                myf.set_center(0)
+                myf.set_coordinate_system('sph')
+                v_kep = tube_1['Relative_Keplerian_Velocity']
+                keplerian_inds = np.where((v_kep>0.8) & (v_kep<1.2))[0]
+                disk_mass = np.sum(tube_1['cell_mass'][keplerian_inds].in_units('msun'))
+                print "OUTPUT=", time_val, disk_mass, "on rank", rit
+                write_data = [time_val, disk_mass]
         else:
-            pos_pos = np.where(tube_1['velocity_z'] > 0.0)[0]
+            del dd
+            myf.set_center(0)
+            pos_pos = np.where(tube_1['velz'].in_units('km/s') > args.velocity_threshold)[0]
+            #pos_pos = np.where(tube_1['Is_Unbound']==True)[0]
             if len(pos_pos) == 0:
                 mass_1 = 0.0
                 speed_1 = 0.0
@@ -152,13 +179,14 @@ for file in usable_files:
                 mom_1 = speed_1*mass_1
                 kin = (tube_1['kinetic_energy'][pos_pos] * tube_1['cell_volume'][pos_pos]).in_units('erg')
                 pot = (tube_1['gpot'][pos_pos]*tube_1['cell_mass'][pos_pos]).in_units('erg')
-                eint = (tube_1['eint'][pos_pos]*tube_1['cell_mass'][pos_pos]).in_units('erg')
-                tot_e = kin + pot + eint
+                #eint = (tube_1['eint'][pos_pos]*tube_1['cell_mass'][pos_pos]).in_units('erg')
+                tot_e = kin + pot# + eint
                 un_ints = np.where(tot_e > 0.0)
                 unbound_1 =tube_1['cell_mass'][pos_pos][un_ints].in_units('Msun').value
             
             #for region 2
-            neg_pos = np.where(tube_2['velocity_z'] < 0.0)[0]
+            neg_pos = np.where(tube_2['velz'].in_units('km/s') < -1*args.velocity_threshold)[0]
+            #neg_pos = np.where(tube_2['Is_Unbound']==True)[0]
             if len(neg_pos) == 0:
                 mass_2 = 0.0
                 speed_2 = 0.0
@@ -172,8 +200,8 @@ for file in usable_files:
                 mom_2 = speed_2*mass_2
                 kin = (tube_2['kinetic_energy'][neg_pos] * tube_2['cell_volume'][neg_pos]).in_units('erg')
                 pot = (tube_2['gpot'][neg_pos]*tube_2['cell_mass'][neg_pos]).in_units('erg')
-                eint = (tube_2['eint'][neg_pos]*tube_2['cell_mass'][neg_pos]).in_units('erg')
-                tot_e = kin + pot + eint
+                #eint = (tube_2['eint'][neg_pos]*tube_2['cell_mass'][neg_pos]).in_units('erg')
+                tot_e = kin + pot# + eint
                 un_ints = np.where(tot_e > 0.0)
                 unbound_2 =tube_2['cell_mass'][neg_pos][un_ints].in_units('Msun').value
             
@@ -191,10 +219,11 @@ for file in usable_files:
             mom = np.sum(mom_1) + np.sum(mom_2)
 
             #Calculate outflow angular momentum:
-            L_x = np.sum(tube_1['angular_momentum_x'][pos_pos].in_units("Msun * km**2 / s").value) + np.sum(tube_2['angular_momentum_x'][neg_pos].in_units("Msun * km**2 / s").value)
-            L_y = np.sum(tube_1['angular_momentum_y'][pos_pos].in_units("Msun * km**2 / s").value) + np.sum(tube_2['angular_momentum_y'][neg_pos].in_units("Msun * km**2 / s").value)
-            L_z = np.sum(tube_1['angular_momentum_z'][pos_pos].in_units("Msun * km**2 / s").value) + np.sum(tube_2['angular_momentum_z'][neg_pos].in_units("Msun * km**2 / s").value)
-            L = np.sqrt((L_x)**2. + (L_y)**2. + (L_z)**2.)
+            L_x = np.sum(tube_1['Angular_Momentum_x'][pos_pos].in_units("Msun * km**2 / s").value) + np.sum(tube_2['Angular_Momentum_x'][neg_pos].in_units("Msun * km**2 / s").value)
+            L_y = np.sum(tube_1['Angular_Momentum_y'][pos_pos].in_units("Msun * km**2 / s").value) + np.sum(tube_2['Angular_Momentum_y'][neg_pos].in_units("Msun * km**2 / s").value)
+            L_z = np.sum(tube_1['Angular_Momentum_z'][pos_pos].in_units("Msun * km**2 / s").value) + np.sum(tube_2['Angular_Momentum_z'][neg_pos].in_units("Msun * km**2 / s").value)
+            L = L_z
+            #L = np.sqrt((L_x)**2. + (L_y)**2. + (L_z)**2.)
             
             #Append quantities
             if outflow_mass == 0.0:
@@ -204,10 +233,11 @@ for file in usable_files:
                 L = np.nan
                 unbound_mass = np.nan
             
-            print "OUTPUT=", time_val, outflow_mass, mom, L, max_speed, unbound_mass, "on rank", rit
+            
+            print "OUTPUT=", time_val, outflow_mass, mom, L, max_speed, unbound_mass, dist, "on rank", rit
 
             #send data to rank 0 to append to write out.
-            write_data = [time_val, outflow_mass, mom, L, max_speed, unbound_mass]
+            write_data = [time_val, outflow_mass, mom, L, max_speed, unbound_mass, dist]
         comm.send(write_data, dest=0, tag=2)
         print "Sent data", write_data, "from rank", rank
 
@@ -248,5 +278,25 @@ for file in usable_files:
     if len(usable_files) < size:
         if rank >= size:
             break
+
+if rank == 0:
+    print "GETTING THE FINAL STUFF TO WRITE OUT"
+    for other_rank in range(1,size):
+        write_data = comm.recv(source=other_rank, tag=2)
+        print "received data", write_data, "from rank", other_rank
+        f = open(save_dir + output_file, 'a')
+        write_string = ''
+        for dit in range(len(write_data)):
+            if dit != len(write_data)-1:
+                write_string = write_string + str(write_data[dit]) + ','
+            else:
+                write_string = write_string + str(write_data[dit]) + '\n'
+                print "finished creating write line"
+        print "write_string =", write_string
+        f.write(write_string)
+        f.close()
+            
+        file_no = file_no + 1
+        print "Progress:", file_no, "/", len(usable_files)
 
 print "Completed job on rank", rank
