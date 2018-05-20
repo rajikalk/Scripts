a8b11db796c1a55ea99a8fce081cfc9a0624e2ff
rajikalk <rajikakuruwita@gmail.com>
2018-05-21 01:47:04 +1000

    added error bars on magnitude

--- Observational_Scripts/make_color_color_plots.py
+++ Observational_Scripts/make_color_color_plots.py
@@ -16,13 +16,20 @@ args = parse_inputs()
 
 #set up lists and arrays
 Object = [[],[]]
+Disk_tag = [[],[]]
+SpT = [[],[]]
 H_alpha = [[],[]]
 H_alpha_variation = [[],[]]
 J_mag = [[],[]]
+J_mag_err = [[],[]]
 K_mag = [[],[]]
+K_mag_err = [[],[]]
 W2_mag = [[],[]]
+W2_mag_err = [[],[]]
 W3_mag = [[],[]]
+W3_mag_err = [[],[]]
 W4_mag = [[],[]]
+W4_mag_err = [[],[]]
 
 #read in current data
 print "Reading in current spreadsheet"
@@ -37,11 +44,18 @@ with open(args.input_file, 'rU') as f:
                 else:
                     reg_ind = 1
                 Object[reg_ind].append(row[0])
-                J_mag[reg_ind].append(float(row[21]))
-                K_mag[reg_ind].append(float(row[22]))
-                W2_mag[reg_ind].append(float(row[16]))
-                W3_mag[reg_ind].append(float(row[17]))
-                W4_mag[reg_ind].append(float(row[18]))
+                Disk_tag[reg_ind].append(row[-4])
+                SpT[reg_ind].append(row[-1])
+                J_mag[reg_ind].append(float(row[25]))
+                J_mag_err[reg_ind].append(float(row[26]))
+                K_mag[reg_ind].append(float(row[28]))
+                K_mag_err[reg_ind].append(float(row[29]))
+                W2_mag[reg_ind].append(float(row[17]))
+                W2_mag_err[reg_ind].append(float(row[18]))
+                W3_mag[reg_ind].append(float(row[19]))
+                W3_mag_err[reg_ind].append(float(row[20]))
+                W4_mag[reg_ind].append(float(row[21]))
+                W4_mag_err[reg_ind].append(float(row[22]))
         if header == 0:
             header = 1
     f.close()
@@ -70,29 +84,78 @@ xlabels = ['G0','G2','G5','G8','K0','K2','K4','K5','K7','M0','M1','M2','M3','M4'
 J_K_color = [0.33,0.34,0.38,0.45,0.49,0.56,0.65,0.69,0.76,0.86,0.86,0.87,0.87,0.88,0.92,0.99,1.07]
 
 J_mag = np.array([np.array(J_mag[0]),np.array(J_mag[1])])
+J_mag_err = np.array([np.array(J_mag_err[0]),np.array(J_mag_err[1])])
 K_mag = np.array([np.array(K_mag[0]),np.array(K_mag[1])])
+K_mag_err = np.array([np.array(K_mag_err[0]),np.array(K_mag_err[1])])
 W3_mag = np.array([np.array(W3_mag[0]),np.array(W3_mag[1])])
+W3_mag_err = np.array([np.array(W3_mag_err[0]),np.array(W3_mag_err[1])])
 W4_mag = np.array([np.array(W4_mag[0]),np.array(W4_mag[1])])
+W4_mag_err = np.array([np.array(W4_mag_err[0]),np.array(W4_mag_err[1])])
 J_K_min = 0.3
-J_K_max = 1.0
+J_K_max = 1.3
 J_K = np.arange(J_K_min, J_K_max, 0.01)
 J_K_W2_1 = np.arange(J_K_min, 0.86, 0.01)
 J_K_W2_2 = np.arange(0.86, J_K_max, 0.01)
 
 plt.clf()
 fig, (ax) = plt.subplots(1, 1)
+fig.set_size_inches(6,5)
 plt.axhline(y=0.0, color='k', ls='--')
-#plt.errorbar((J_mag[0]-K_mag[0]), H_alpha[0], yerr=H_alpha_variation[0]/2., fmt='bo', label='Upper Scorpius')
-#plt.errorbar((J_mag[1]-K_mag[1]), H_alpha[1], yerr=H_alpha_variation[1]/2., fmt='r^', label='Upper Centaurus-Lupus')
-plt.scatter((J_mag[0]-K_mag[0]), H_alpha[0], color='b', marker='o', label='Upper Scorpius')
-plt.scatter((J_mag[1]-K_mag[1]), H_alpha[1], color='r', marker='^', label='Upper Centaurus-Lupus')
-plt.ylabel('EW(H)')
+plt.errorbar((J_mag[0]-K_mag[0]), H_alpha[0], xerr=np.sqrt(np.square(J_mag_err[0])+np.square(K_mag_err[0])), yerr=H_alpha_variation[0], fmt='bo', label='Upper Scorpius')
+plt.errorbar((J_mag[1]-K_mag[1]), H_alpha[1], xerr=np.sqrt(np.square(J_mag_err[1])+np.square(K_mag_err[1])),yerr=H_alpha_variation[1], fmt='r^', label='Upper Centaurus-Lupus')
+#plt.scatter((J_mag[0]-K_mag[0]), H_alpha[0], color='b', marker='o', label='Upper Scorpius')
+#plt.scatter((J_mag[1]-K_mag[1]), H_alpha[1], color='r', marker='^', label='Upper Centaurus-Lupus')
+plt.ylabel('EW(H'+r'$ \alpha $'+')')
 plt.xlabel('J-K')
+#plt.ylim([-45.,5.])
 #ax.set_xticks(J_K_color)
 #ax.set_xticklabels(xlabels)
 #ax.xaxis.set_major_locator(FixedLocator(J_K_color))
 plt.legend(loc='lower left')
-plt.savefig('/Users/rajikak/Dropbox/Reggie_PhD/Papers/Multiplicity_US_UCL_2018/Resubmission_1/H_alpha_EW.eps')
+plt.savefig('/Users/rajikak/Dropbox/Reggie_PhD/Papers/Multiplicity_US_UCL_2018/Resubmission_1/H_alpha_EW_variation.eps', bbox_inches='tight', pad_inches = 0.02)
+
+plt.clf()
+fig, (ax) = plt.subplots(1, 1)
+fig.set_size_inches(6,5)
+plt.axhline(y=0.0, color='k', ls='--')
+plt.errorbar((K_mag[0]-W3_mag[0]), H_alpha[0], xerr=np.sqrt(np.square(W3_mag_err[0])+np.square(K_mag_err[0])), yerr=H_alpha_variation[0], fmt='bo', label='Upper Scorpius')
+plt.errorbar((K_mag[1]-W3_mag[1]), H_alpha[1], xerr=np.sqrt(np.square(W3_mag_err[1])+np.square(K_mag_err[1])),yerr=H_alpha_variation[1], fmt='r^', label='Upper Centaurus-Lupus')
+#plt.scatter((J_mag[0]-K_mag[0]), H_alpha[0], color='b', marker='o', label='Upper Scorpius')
+#plt.scatter((J_mag[1]-K_mag[1]), H_alpha[1], color='r', marker='^', label='Upper Centaurus-Lupus')
+plt.ylabel('EW(H'+r'$ \alpha $'+')')
+plt.xlabel('K-W3')
+#plt.ylim([-45.,5.])
+#ax.set_xticks(J_K_color)
+#ax.set_xticklabels(xlabels)
+#ax.xaxis.set_major_locator(FixedLocator(J_K_color))
+plt.legend(loc='lower left')
+plt.savefig('/Users/rajikak/Dropbox/Reggie_PhD/Papers/Multiplicity_US_UCL_2018/Resubmission_1/H_alpha_EW_variation_W3.eps', bbox_inches='tight', pad_inches = 0.02)
+
+plt.clf()
+fig, (ax1, ax2) = plt.subplots(2, 1,sharex=True)
+fig.set_size_inches(7,8)
+ax1.plot(J_K, ((1.1*J_K)-0.3),'k--')
+ax1.errorbar((J_mag[0]-K_mag[0]), (K_mag[0]-W3_mag[0]), xerr=np.sqrt(np.square(J_mag_err[0])+np.square(K_mag_err[0])), yerr=np.sqrt(np.square(K_mag_err[0])+np.square(W3_mag_err[0])), fmt='bo', label='Upper Scorpius')
+ax1.errorbar((J_mag[1]-K_mag[1]), (K_mag[1]-W3_mag[1]), xerr=np.sqrt(np.square(J_mag_err[1])+np.square(K_mag_err[1])), yerr=np.sqrt(np.square(K_mag_err[1])+np.square(W3_mag_err[1])), fmt='r^', label='Upper Centaurus-Lupus')
+#ax1.scatter((J_mag[0]-K_mag[0]), (K_mag[0]-W3_mag[0]), color='b', marker='+', label='Upper Scorpius')
+#ax1.scatter((J_mag[1]-K_mag[1]), (K_mag[1]-W3_mag[1]), color='r', marker='^', label='Upper Centaurus-Lupus')
+ax1.set_ylabel('K-W3')
+ax1.set_ylim([-0.5,4.])
+ax1.set_xlim([J_K_min,J_K_max])
+ax1.legend(loc='upper left')
+plt.setp([ax1.get_yticklabels()[::2]], visible=False)
+ax2.plot(J_K, ((2.08*J_K)-0.48),'k--')
+ax2.errorbar((J_mag[0]-K_mag[0]), (K_mag[0]-W4_mag[0]), xerr=np.sqrt(np.square(J_mag_err[0])+np.square(K_mag_err[0])), yerr=np.sqrt(np.square(K_mag_err[0])+np.square(W4_mag_err[0])), fmt='bo', label='Upper Scorpius')
+ax2.errorbar((J_mag[1]-K_mag[1]), (K_mag[1]-W4_mag[1]), xerr=np.sqrt(np.square(J_mag_err[1])+np.square(K_mag_err[1])), yerr=np.sqrt(np.square(K_mag_err[1])+np.square(W4_mag_err[1])), fmt='r^', label='Upper Centaurus-Lupus')
+#ax2.scatter((J_mag[0]-K_mag[0]), (K_mag[0]-W4_mag[0]), color='b', marker='+', label='Upper Scorpius')
+#ax2.scatter((J_mag[1]-K_mag[1]), (K_mag[1]-W4_mag[1]), color='r', marker='^', label='Upper Centaurus-Lupus')
+ax2.set_ylabel('K-w4')
+ax2.set_xlabel('J-K')
+ax2.set_xlim([J_K_min,J_K_max])
+plt.setp([ax2.get_yticklabels()[-1]], visible=False)
+plt.setp([a.get_xticklabels() for a in fig.axes[:-1]], visible=False)
+fig.subplots_adjust(hspace=0)
+plt.savefig('/Users/rajikak/Dropbox/Reggie_PhD/Papers/Multiplicity_US_UCL_2018/Resubmission_1/color_color_plot_2.eps', bbox_inches='tight', pad_inches = 0.02)
 
 plt.clf()
 fig, (ax1, ax2, ax3) = plt.subplots(3, 1,sharex=True)
@@ -119,4 +182,4 @@ plt.setp([a.get_xticklabels() for a in fig.axes[:-1]], visible=False)
 plt.setp([ax3.get_yticklabels()[-1]], visible=False)
 plt.setp([a.get_xticklabels() for a in fig.axes[:-1]], visible=False)
 fig.subplots_adjust(hspace=0)
-plt.savefig('/Users/rajikak/Dropbox/Reggie_PhD/Papers/Multiplicity_US_UCL_2018/Resubmission_1/color_color_plot.eps')
+plt.savefig('/Users/rajikak/Dropbox/Reggie_PhD/Papers/Multiplicity_US_UCL_2018/Resubmission_1/color_color_plot_3.eps', bbox_inches='tight', pad_inches = 0.02)
