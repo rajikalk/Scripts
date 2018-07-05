056f6fef616568dfd40bec5989ec6b203273f4af
rajikalk <rajikakuruwita@gmail.com>
2018-07-05 13:02:33 +1000

    just changed the column which quantities are read. Really shoudl automatically detect this

--- Observational_Scripts/bayesian_analysis.py
+++ Observational_Scripts/bayesian_analysis.py
@@ -5,6 +5,8 @@ import csv
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+print "THIS IS THE LATEST VERSION OF BAYESIAN ANALYSIS"
+
 def parse_inputs():
     import argparse
     parser = argparse.ArgumentParser()
@@ -50,54 +52,61 @@ RV_standard_info = {}
 
 #Read in RV standard list
 header = 0
-with open('/Users/rajikak/Observational_Data/RV_standard_list.csv', 'rU') as f:
+with open('/home/100/rlk100/RV_standard_list.csv', 'rU') as f:
     reader = csv.reader(f)
     for row in reader:
         if header != 0:
-            RV_standard_info[row[0]] = (float(row[5]), float(row[6]), float(row[7]))
+            RV_standard_info[row[2]] = (float(row[5]), float(row[6]), float(row[7]))
         else:
             header = 1
 
 print "Reading in current spreadsheet"
 header = 0
+reshape_len = -1
 with open(args.input_file, 'rU') as f:
     reader = csv.reader(f)
     for row in reader:
         if header != 0:
             if 'U4' in row[0]:
                 row[0] = 'UCAC4' + row[0].split('U4')[-1]
-            Object.append(row[0])
-            Region.append(row[1])
-            Coords.append((row[2], row[3]))
-            Membership_probability.append(int(row[4]))
-            IR_excess.append(row[5])
-            Disk_visual.append(row[6])
-            Magnitudes.append([float(row[7]), float(row[8]), float(row[9])])
-            No_obs.append(int(row[10]))
-            if row[11] == 'FALSE' or row[11] == 'False':
-                SB1_flag.append(False)
-            else:
-                SB1_flag.append(True)
-            if row[12] == 'FALSE' or row[12] == 'False':
-                SB2_flag.append(False)
-            else:
-                SB2_flag.append(True)
-            RV_variation.append(row[13])
-            Pref_template.append(row[14])
-            Temp_sptype.append(row[15])
-            if len(row) > 16:
-                Obs = np.array(row[16:])
-                Obs = np.delete(Obs, np.where(Obs==''))
-                #if len(Obs) > 5:
-                #    Obs = np.reshape(Obs, (len(Obs)/5, 5))
-                Obs = np.reshape(Obs, (len(Obs)/6, 6))
-                for ind_obs in Obs:
-                    if '/' in ind_obs[0]:
-                        new_format = '20' + ind_obs[0].split('/')[-1] + '-' + ind_obs[0].split('/')[-2] + '-' + ("%02d" % int(ind_obs[0].split('/')[-3]))
-                        ind_obs[0] = new_format
-            else:
-                Obs = np.array([])
-            Obs_info.append(Obs)
+            if row[0] != 'UCAC4-1253626396':
+                Object.append(row[0])
+                Region.append(row[1])
+                Coords.append((row[2], row[3]))
+                Membership_probability.append(int(row[4]))
+                IR_excess.append(row[5])
+                Disk_visual.append(row[6])
+                Magnitudes.append([float(row[7]), float(row[8]), float(row[9])])
+                No_obs.append(int(row[10]))
+                if row[11] == 'FALSE' or row[13] == 'False':
+                    SB1_flag.append(False)
+                else:
+                    SB1_flag.append(True)
+                if row[12] == 'FALSE' or row[14] == 'False':
+                    SB2_flag.append(False)
+                else:
+                    SB2_flag.append(True)
+                RV_variation.append(row[13])
+                Pref_template.append(row[14])
+                Temp_sptype.append(row[15])
+                if len(row) > 16:
+                    Obs = np.array(row[16:])
+                    Obs = np.delete(Obs, np.where(Obs==''))
+                    if reshape_len == -1:
+                        for ob in Obs:
+                            reshape_len = reshape_len + 1
+                            if '/' in ob and ob != Obs[0]:
+                                break
+                    #if len(Obs) > 5:
+                    #    Obs = np.reshape(Obs, (len(Obs)/5, 5))
+                    Obs = np.reshape(Obs, (len(Obs)/reshape_len, reshape_len))
+                    for ind_obs in Obs:
+                        if '/' in ind_obs[0]:
+                            new_format = '20' + ind_obs[0].split('/')[-1] + '-' + ind_obs[0].split('/')[-2] + '-' + ("%02d" % int(ind_obs[0].split('/')[-3]))
+                            ind_obs[0] = new_format
+                else:
+                    Obs = np.array([])
+                Obs_info.append(Obs)
         if header == 0:
             header = 1
 
@@ -109,7 +118,7 @@ header = 0
 with open(args.bayes_file, 'rU') as f:
     reader = csv.reader(f)
     for row in reader:
-        if header != 0:
+        if row[0] in Object:
             ind = Object.index(row[0])
             Obj_bayes[ind] = float(row[2])
             if row[1] == 'US':
@@ -118,29 +127,31 @@ with open(args.bayes_file, 'rU') as f:
             else:
                 all_bayes[1].append(float(row[2]))
                 Obj_bayes_SpT[1].append(row[3])
-        else:
-            header = 1
+
 
 if args.calculate_bayes == 'False':
     all_bayes[0] = np.nan_to_num(np.array(all_bayes[0]))
     all_bayes[1] = np.nan_to_num(np.array(all_bayes[1]))
-    all_bayes[0] = np.minimum(all_bayes[0],1e6)
-    all_bayes[1] = np.minimum(all_bayes[1],1e6)
+    #all_bayes[0] = np.minimum(all_bayes[0],1e6)
+    #all_bayes[1] = np.minimum(all_bayes[1],1e6)
 
     ####### CALCULATED BAYES FACTORS HAVE BEEN READ IN
 
     gamma = np.linspace(0.0, 1.0, 1000)
-    P_gamma = 1./(gamma*(1-gamma))
-    gamma_prior = 1.0 #A constant for now.
-    p_gamma_US = 1.0
-    p_gamma_UCL = 1.0
+    gamma = gamma[1:-1]
+    P_gamma = 1./(gamma*(1.-gamma))
+    gamma_prior = P_gamma #1.0 #A constant for now.
+    p_gamma_US = gamma_prior
+    p_gamma_UCL = gamma_prior
 
     #US
     for bayes in all_bayes[0]:
         p_gamma_US *= gamma*bayes + (1.0-gamma)
+    p_gamma_US = p_gamma_US/np.max(p_gamma_US)
     p_gamma_US = P_gamma*p_gamma_US
     p_gamma_US_norm = p_gamma_US/(np.trapz(p_gamma_US, x=gamma, dx=(gamma[1]-gamma[0])))
     mean_US =  np.trapz(p_gamma_US_norm*gamma, x=gamma, dx=(gamma[1]-gamma[0]))
+    max_US = gamma[np.argmax(p_gamma_US_norm)]
     mean_squared_US = np.trapz(p_gamma_US_norm*gamma*gamma, x=gamma, dx=(gamma[1]-gamma[0]))
     sigma_US = np.sqrt(mean_squared_US - mean_US*mean_US)
 
@@ -161,22 +172,25 @@ if args.calculate_bayes == 'False':
             US_84th = gamma[x_gamma-1]
             break
         prev_delta = delta
-    US_pos_error = US_84th - mean_US
-    US_neg_error = mean_US - US_16th
+    US_pos_error = US_84th - max_US
+    US_neg_error = max_US - US_16th
 
     plt.clf()
     plt.plot(gamma, p_gamma_US_norm)
     #plt.title('US:' + "{0:5.1f}".format(mean_US*100.)+'$^{' + "{0:5.1f}".format(US_pos_error*100.)+'}_{' + "{0:5.1f}".format(US_neg_error*100.)+'}$%')
-    print "US:", mean_US, "(+", US_pos_error, ", -",US_neg_error,")"
-    print "US_max:", gamma[np.argmax(p_gamma_US_norm)]
+    print "US:", max_US, "(+", US_pos_error, ", -",US_neg_error,")"
+    print "US_mean:", mean_US
     plt.savefig('Binary_fraction_US'+args.image_suffix+'.png')
 
     #UCL
     for bayes in all_bayes[1]:
         p_gamma_UCL *= gamma*bayes + (1.0-gamma)
+    #p_gamma_UCL = np.nan_to_num(p_gamma_UCL)
+    p_gamma_UCL = p_gamma_UCL/np.max(p_gamma_UCL)
     p_gamma_UCL = P_gamma*p_gamma_UCL
     p_gamma_UCL_norm = p_gamma_UCL/(np.trapz(p_gamma_UCL, x=gamma, dx=(gamma[1]-gamma[0])))
     mean_UCL =  np.trapz(p_gamma_UCL_norm*gamma, x=gamma, dx=(gamma[1]-gamma[0]))
+    max_UCL = gamma[np.argmax(p_gamma_UCL_norm)]
     mean_squared_UCL = np.trapz(p_gamma_UCL_norm*gamma*gamma, x=gamma, dx=(gamma[1]-gamma[0]))
     sigma_UCL = np.sqrt(mean_squared_UCL - mean_UCL*mean_UCL)
 
@@ -197,42 +211,49 @@ if args.calculate_bayes == 'False':
             UCL_84th = gamma[x_gamma-1]
             break
         prev_delta = delta
-    UCL_pos_error = UCL_84th - mean_UCL
-    UCL_neg_error = mean_UCL - UCL_16th
+    UCL_pos_error = UCL_84th - max_UCL
+    UCL_neg_error = max_UCL - UCL_16th
 
     plt.clf()
     plt.plot(gamma, p_gamma_UCL_norm)
     #plt.title('UCL:' + "{0:5.1f}".format(mean_UCL*100.)+'$^{' + "{0:5.1f}".format(UCL_pos_error*100.)+'}_{' + "{0:5.1f}".format(UCL_neg_error*100.)+'}$%')
-    print "UCL:", mean_UCL, "(+", UCL_pos_error, ", -",UCL_neg_error,")"
-    print "UCL_max:", gamma[np.argmax(p_gamma_UCL_norm)]
+    print "UCL:", max_UCL, "(+", UCL_pos_error, ", -",UCL_neg_error,")"
+    print "UCL_mean:", mean_UCL
     plt.savefig('Binary_fraction_UCL'+args.image_suffix+'.png')
 
+    US_inds = np.where((gamma > (max_US - US_neg_error))&(gamma < (max_US + US_pos_error)))[0]
+    UCL_inds = np.where((gamma > (max_UCL - UCL_neg_error))&(gamma < (max_UCL + UCL_pos_error)))[0]
     plt.clf()
-    plt.plot(gamma, p_gamma_US_norm, label='Upper Scorpius')
-    plt.plot(gamma, p_gamma_UCL_norm, label='Upper Centaurus-Lupus')
-    plt.axvline(x=0.12)
-    #plt.fill_between([11,13], np.zeros(len(gamma)), np.ones(len(gamma)), where=y2 >= y1, facecolor='green', interpolate=True, alpha=0.5)
+    plt.axvline(x=0.12, color='k')
+    #plt.fill_between([11,13], np.zeros(len(gamma)), np.ones(len(gamma)), where=y >= y1, facecolor='green', interpolate=True, alpha=0.5)
+    plt.axvspan(0.11, 0.14, alpha=0.5, color='grey')
+    plt.plot(gamma, p_gamma_US_norm, label='Upper Scorpius', color='b')
+    plt.fill_between(gamma[US_inds], np.zeros(len(gamma[US_inds])), p_gamma_US_norm[US_inds], facecolor='blue', alpha=0.5, edgecolors='none')
+    plt.plot(gamma, p_gamma_UCL_norm, label='Upper Centaurus-Lupus', color='g')
+    plt.fill_between(gamma[UCL_inds], np.zeros(len(gamma[UCL_inds])), p_gamma_UCL_norm[UCL_inds], facecolor='green', alpha=0.5, edgecolors='none')
     plt.legend(loc='best')
+    plt.xlim([0.0, 0.5])
     plt.xlabel('Binary Fraction')
     plt.ylabel('Likelihood')
 
     #plt.title('US:' + "{0:5.1f}".format(mean_US*100.)+'$^{' + "{0:5.1f}".format(US_pos_error*100.)+'}_{' + "{0:5.1f}".format(US_neg_error*100.)+'}$%, UCL:' + "{0:5.1f}".format(mean_UCL*100.)+'$^{' + "{0:5.1f}".format(UCL_pos_error*100.)+'}_{' + "{0:5.1f}".format(UCL_neg_error*100.)+'}$%')
-    plt.savefig('Composite_fractions' + args.image_suffix + '.png')
+    plt.savefig('Composite_fractions' + args.image_suffix + '.eps', bbox_inches='tight', pad_inches = 0.02)
+    plt.savefig('Composite_fractions' + args.image_suffix + '.pdf', bbox_inches='tight', pad_inches = 0.02)
 
     ## per spectral type
     US_per_SpT = []
     UCL_per_SpT = []
     US_per_SpT_err = []
     UCL_per_SpT_err = []
-    gamma_prior = 1.0 #A constant for now.
-    p_gamma_US_F = 1.0
-    p_gamma_UCL_F = 1.0
-    p_gamma_US_G = 1.0
-    p_gamma_UCL_G = 1.0
-    p_gamma_US_K = 1.0
-    p_gamma_UCL_K = 1.0
-    p_gamma_US_M = 1.0
-    p_gamma_UCL_M = 1.0
+    gamma_prior = P_gamma #A constant for now.
+    p_gamma_US_F = gamma_prior
+    p_gamma_UCL_F = gamma_prior
+    p_gamma_US_G = gamma_prior
+    p_gamma_UCL_G = gamma_prior
+    p_gamma_US_K = gamma_prior
+    p_gamma_UCL_K = gamma_prior
+    p_gamma_US_M = gamma_prior
+    p_gamma_UCL_M = gamma_prior
 
     for bayes in range(len(all_bayes[0])):
         if Obj_bayes_SpT[0][bayes] == 'F':
