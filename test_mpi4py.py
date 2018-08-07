b0496f728163f40392627ac0a17f2f3820b20dab
rajikalk <rajikakuruwita@gmail.com>
2018-08-07 16:16:24 +1000

    simple script to test parallelism of yt

---
+++ test_mpi4py.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+from __future__ import division
+from mpi4py import MPI
+from mpi4py.MPI import COMM_WORLD as CW
+import yt
+yt.enable_parallelism()
+import my_fields as myf
+import sys
+import time
+
+file = sys.argv[1]
+part_file = part_file = file[:-12] + 'part' + file[-5:]
+ds = yt.load(file, particle_filename=part_file)
+
+dd = ds.all_data()
+start = time.time()
+ds.derived_field_list
+dd['Gravitational_Force_on_particles_Rad']
+print('Finished deriving field list after', time.time()-start)
+
