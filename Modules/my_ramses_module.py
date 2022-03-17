510816fdce63d549f61f932d54918f31a2c65c31
Rajika Lakmali Kuruwita <reggie@reggies-macbook-pro.home>
2022-03-18 00:47:40 +0100

    arrowheads are missing in quiver collection. I'll try this

--- Modules/my_ramses_module.py
+++ Modules/my_ramses_module.py
@@ -395,7 +395,7 @@ def my_own_quiver_function(axis, X_pos, Y_pos, X_val, Y_val, plot_velocity_legen
                 color = rv_cmap(cit)
             quiver.append(mpatches.FancyArrowPatch((X_pos[xp][yp], Y_pos[xp][yp]), (X_pos[xp][yp]+xvel, Y_pos[xp][yp]+yvel), color=color, linewidth=width_val, arrowstyle='->', mutation_scale=10.*width_val, shrinkA=0.0, shrinkB=0.0))
             #axis.add_patch(mpatches.FancyArrowPatch((X_pos[xp][yp], Y_pos[xp][yp]), (X_pos[xp][yp]+xvel, Y_pos[xp][yp]+yvel), color=color, linewidth=width_val, arrowstyle='->', mutation_scale=10.*width_val, shrinkA=0.0, shrinkB=0.0), zorder=zorder)#, alpha=width_val/width_ceil))
-        coll=PatchCollection(quiver, zorder=zorder, match_original=True)
+        coll=PatchCollection(quiver, zorder=zorder, match_original=True, arrowstyle='->')
         axis.add_collection(coll)
     if plot_velocity_legend:
         #print("plotting quiver legend")
