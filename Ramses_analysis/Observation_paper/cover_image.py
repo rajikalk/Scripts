539ef39742045294d913219f008d65aed9c5631f
Rajika Lakmali Kuruwita <reggie@reggies-macbook-pro.home>
2022-03-18 12:58:36 +0100

    changed parallel cpus per file

--- Ramses_analysis/Observation_paper/cover_image.py
+++ Ramses_analysis/Observation_paper/cover_image.py
@@ -305,7 +305,7 @@ CW.Barrier()
 if args.make_frames_only == 'False':
     #Trying yt parallelism
     file_int = -1
-    for fn in yt.parallel_objects(usable_files, njobs=int(size/6)):
+    for fn in yt.parallel_objects(usable_files, njobs=int(size/7)):
         if size > 1:
             file_int = usable_files.index(fn)
         else:
