c1f55e41e39fe605437aca3546082933888c554e
Rajika Lakmali Kuruwita <reggie@reggies-mbp-2.home>
2022-03-23 12:37:24 +0100

    updating pathway evolution to save mean gradients for 1.e3, 1.e4, and 1.e5 yrs after system formation

--- Ramses_analysis/CF_analysis/pathway_evolution.py
+++ Ramses_analysis/CF_analysis/pathway_evolution.py
@@ -21,7 +21,7 @@ read_pickle = True
 #plot_key = sys.argv[2]
 plot_keys = ['System_semimajor']#, 'System_ecc', 'System_energies']
 
-if read_pickle == False:
+if read_pickle == True:
     for plot_key in plot_keys:
         savename = 'pathway_evolution_'+plot_key
         if plot_gradient:
@@ -48,8 +48,10 @@ if read_pickle == False:
         sim_start_time = np.nan
         plotted_sinks = []
         set_start_time = True
-        Initial_gradients_1000 = [[],[],[],[]]
         Initial_gradients = [[],[],[],[]]
+        Initial_gradients_1000 = [[],[],[],[]]
+        Initial_gradients_10000 = [[],[],[],[]]
+        Initial_gradients_100000 = [[],[],[],[]]
         for time_key in superplot_dict['System_times'].keys():
             print("processing system:", time_key)
             key_inds = flatten(eval(time_key))
@@ -104,10 +106,27 @@ if read_pickle == False:
                                 ds = Sep_arr[sub_1000_inds[-1]] - Sep_arr[sub_1000_inds[0]]
                                 mean_grad = ds/dt
                                 Initial_gradients_1000[axis_ind].append([mean_grad])
-                                #time_1000_ind = np.where(mean_x<1000)[0][-1] + 1
-                                #Initial_gradients_1000[axis_ind].append(grad[:time_1000_ind])
                             except:
                                 print('system has not mean times < 1000yr')
+                                
+                            try:
+                                sub_10000_inds = np.where(Time_arr<10000)[0]
+                                dt = Time_arr[sub_10000_inds[-1]] - Time_arr[sub_10000_inds[0]]
+                                ds = Sep_arr[sub_10000_inds[-1]] - Sep_arr[sub_10000_inds[0]]
+                                mean_grad = ds/dt
+                                Initial_gradients_10000[axis_ind].append([mean_grad])
+                            except:
+                                print('system has not mean times < 10000yr')
+                                
+                            try:
+                                sub_100000_inds = np.where(Time_arr<100000)[0]
+                                dt = Time_arr[sub_100000_inds[-1]] - Time_arr[sub_100000_inds[0]]
+                                ds = Sep_arr[sub_100000_inds[-1]] - Sep_arr[sub_100000_inds[0]]
+                                mean_grad = ds/dt
+                                Initial_gradients_100000[axis_ind].append([mean_grad])
+                            except:
+                                print('system has not mean times < 100000yr')
+                            
                             if len(grad) > 0:
                                 Initial_gradients[axis_ind].append(grad[0])
                             #lets trying smoothing over a 1000year window, but not centred
@@ -162,7 +181,7 @@ if read_pickle == False:
 
     grad_pickle = 'grad_pickle.pkl'
     file = open(grad_pickle, 'wb')
-    pickle.dump((Initial_gradients, Initial_gradients_1000), file)
+    pickle.dump((Initial_gradients, Initial_gradients_1000, Initial_gradients_10000, Initial_gradients_100000), file)
     file.close()
     print('saved_gradients')
 
