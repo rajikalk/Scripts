58a3429877403c524d47fd65822a4ab272e00a2c
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-03-24 14:12:07 +0100

    removed erorbars just to test plotting

--- Ramses_analysis/CF_analysis/pathway_evolution_multiplot.py
+++ Ramses_analysis/CF_analysis/pathway_evolution_multiplot.py
@@ -81,7 +81,7 @@ for grad_it in range(len(grad_pickles)):
         axs_list[0][grad_it].set_xticklabels(ticklabels[::2])
         axs_list[0][grad_it].set_xlabel('Inspiral rate (au/yr)')
         axs_list[0][grad_it].set_ylim(bottom=0)
-    fig_list[0].savefig('Initial_grad_hist.png')
+    fig_list[0].savefig('Initial_grad_hist.png', bbox_inches='tight', pad_inches=0.02)
 
     mean_grads = [Initial_gradients_1000, Initial_gradients_10000, Initial_gradients_100000]
 
@@ -172,7 +172,7 @@ for grad_it in range(len(grad_pickles)):
             axs_list[time_means_counter+1][grad_it].set_xticklabels(ticklabels[::2])
             axs_list[time_means_counter+1][grad_it].set_xlabel('Inspiral rate (au/yr)')
             axs_list[time_means_counter+1][grad_it].set_ylim(bottom=0)
-        fig_list[time_means_counter+1].savefig('Initial_mean_grad_'+str(time_means[time_means_counter])+'.png')
+        fig_list[time_means_counter+1].savefig('Initial_mean_grad_'+str(time_means[time_means_counter])+'.png', bbox_inches='tight', pad_inches=0.02)
         time_means_counter = time_means_counter + 1
 
 """
