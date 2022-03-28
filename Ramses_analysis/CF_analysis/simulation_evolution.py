7c13b5d6db2978abdcbb65152d35994f305f57a2
Rajika Lakmali Kuruwita <reggie@Reggies-MBP.unicph.domain>
2022-03-28 15:02:07 +0200

    updated x axis to be from the formation of the first sink

--- Ramses_analysis/CF_analysis/simulation_evolution.py
+++ Ramses_analysis/CF_analysis/simulation_evolution.py
@@ -56,19 +56,21 @@ for pick_it in iter_range:
     superplot_dict, Sink_bound_birth, Sink_formation_times, means_dict, Lifetimes_sys, Sep_maxs, Sep_mins, Initial_Seps, Final_seps = pickle.load(file)
     file.close()
     
-    axs.flatten()[0].plot(np.array(superplot_dict['Times'])/1.e6, superplot_dict['M_tot'], label=subplot_titles[pick_it])
-    axs.flatten()[1].plot(np.array(superplot_dict['Times'])/1.e6, superplot_dict['N_stars'])
-    axs.flatten()[2].plot(np.array(superplot_dict['Times'])/1.e6, superplot_dict['SFE'])
+    Time_arr = (np.array(superplot_dict['Times'])-superplot_dict['Times'][0])/1.e6
+    
+    axs.flatten()[0].plot(Time_arr, superplot_dict['M_tot'], label=subplot_titles[pick_it])
+    axs.flatten()[1].plot(Time_arr, superplot_dict['N_stars'])
+    axs.flatten()[2].plot(Time_arr, superplot_dict['SFE'])
     
     SFE_5_ind = np.argmin(abs(np.array(superplot_dict['SFE'])-0.05))
     
-    axs.flatten()[0].scatter(superplot_dict['Times'][SFE_5_ind]/1.e6, superplot_dict['M_tot'][SFE_5_ind], marker='o')
-    axs.flatten()[1].scatter(superplot_dict['Times'][SFE_5_ind]/1.e6, superplot_dict['N_stars'][SFE_5_ind], marker='o')
-    axs.flatten()[2].scatter(superplot_dict['Times'][SFE_5_ind]/1.e6, superplot_dict['SFE'][SFE_5_ind], marker='o')
+    axs.flatten()[0].scatter(Time_arr[SFE_5_ind], superplot_dict['M_tot'][SFE_5_ind], marker='o')
+    axs.flatten()[1].scatter(Time_arr[SFE_5_ind], superplot_dict['N_stars'][SFE_5_ind], marker='o')
+    axs.flatten()[2].scatter(Time_arr[SFE_5_ind], superplot_dict['SFE'][SFE_5_ind], marker='o')
     
     print('plotted data from pickle', pickle_files[file_it])
 
-axs.flatten()[2].set_xlabel('Time in Simulation (Myr)', fontsize=font_size)
+axs.flatten()[2].set_xlabel('Time since formation of first sink (Myr)', fontsize=font_size)
 axs.flatten()[0].set_ylabel('$M_{stars}$ (M$_\odot$)', labelpad=1, fontsize=font_size)
 axs.flatten()[0].legend(loc='upper right', fontsize=font_size, labelspacing=0.1, handletextpad=0.2, borderaxespad=0.2, borderpad=0.3)
 axs.flatten()[1].set_ylabel('$N_{stars}$', labelpad=1, fontsize=font_size)
