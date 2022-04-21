5b03d68846ac7ee49f4f404f617ceec586af9c8c
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-04-21 16:07:48 +0200

    saving initial separation for bound sinks

--- Ramses_analysis/CF_analysis/get_birth_conditions.py
+++ Ramses_analysis/CF_analysis/get_birth_conditions.py
@@ -129,3 +129,7 @@ for sink_id in formation_inds[1]:
         most_bound_sep = np.nan
     Sink_bound_birth.append([born_bound, most_bound_sink_id, most_bound_sep])
     print("Found birth conditions of sink", sink_id)
+
+file = open("sink_birth_conditions.pkl", 'wb')
+pickle.dump((Sink_bound_birth),file)
+file.close()
