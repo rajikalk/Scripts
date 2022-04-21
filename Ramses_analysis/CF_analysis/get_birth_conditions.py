10b2902f9fa811cdfe329ace091b5e92d567eba8
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-04-21 16:21:43 +0200

    saving initial separation for bound sinks

--- Ramses_analysis/CF_analysis/get_birth_conditions.py
+++ Ramses_analysis/CF_analysis/get_birth_conditions.py
@@ -20,9 +20,6 @@ def parse_inputs():
 
 args = parse_inputs()
 
-rank = CW.Get_rank()
-size = CW.Get_size()
-
 #==========================================================================================
 
 units_override = {"length_unit":(4.0,"pc"), "velocity_unit":(0.18, "km/s"), "time_unit":(685706129102738.9, "s")}
