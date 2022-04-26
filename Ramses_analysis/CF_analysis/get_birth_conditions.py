e1d5bd4cd3fc444685515fad5ef94c32a5a1156f
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-04-26 17:33:08 +0200

    forgot to check if sink is within 10000au

--- Ramses_analysis/CF_analysis/get_birth_conditions.py
+++ Ramses_analysis/CF_analysis/get_birth_conditions.py
@@ -102,11 +102,12 @@ zero_inds = np.where(diff_arr == 0)
 diff_arr[zero_inds] = 1
 formation_inds = np.where(diff_arr == global_data['m'])
 if len(Sink_bound_birth) > 0:
-    sink_id = len(Sink_bound_birth)
+    sink_id = len(Sink_bound_birth)-1
 else:
-    sink_id = 0
+    sink_id = -1
 n_stars = 0
 while sink_id < len(formation_inds[1]):
+    sink_id = sink_id + 1
     form_time_it = formation_inds[0][sink_id]
     new_sink_pos = np.array([global_data['x'][form_time_it][sink_id], global_data['y'][form_time_it][sink_id], global_data['z'][form_time_it][sink_id]]).T
     new_sink_vel = np.array([global_data['ux'][form_time_it][sink_id], global_data['uy'][form_time_it][sink_id], global_data['uz'][form_time_it][sink_id]]).T
