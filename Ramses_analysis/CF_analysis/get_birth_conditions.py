Fixed cyclic separation calculation

Ramses_analysis/CF_analysis/get_birth_conditions.py
@@ -422,7 +422,7 @@ while sink_id < len(formation_inds[1]):
 
 #compile pickles
 import glob
-birth_pickles = sorted(glob.glob("sink_birth_conditions_*.pkl"))[1:]
+birth_pickles = sorted(glob.glob("sink_birth_conditions_*.pkl"))
 Sink_birth_all = []
 for birth_pick in birth_pickles:
     file = open(birth_pick, 'rb')
@@ -437,7 +437,7 @@ Sink_birth_all = Sink_birth_all[sink_sorted_inds]
 mismatched_inds = []
 true_delay = []
 true_first_sys = []
-
+'''
 for sink_id in range(np.shape(Sink_birth_fast)[0]):
     if sink_id in Sink_birth_all[:,0]:
         match_ind = np.argwhere(Sink_birth_all[:,0]==sink_id)[0]
@@ -457,3 +457,4 @@ for sink_id in range(np.shape(Sink_birth_fast)[0]):
         except:
             mismatched_inds.append(sink_id)
             true_delay.append(Sink_match[-1])
+'''