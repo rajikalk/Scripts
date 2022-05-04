867cfb95c3bc2684d0af69fcb3160d6a05605efd
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-05-04 11:27:39 +0200

    testing accuracy

--- Ramses_analysis/CF_analysis/get_birth_conditions_para.py
+++ Ramses_analysis/CF_analysis/get_birth_conditions_para.py
@@ -493,12 +493,10 @@ while sink_id < len(formation_inds):
                             break
                         del res
 
-        try:
+        if str(sink_id) in True_sink_birth_conditions.keys():
             if True_sink_birth_conditions[str(sink_id)][3] != most_bound_sep or True_sink_birth_conditions[str(sink_id)][5] != delay_time:
                 mismatched_inds.append(sink_id)
                 print("SHORT CUT DOESN'T WORK FOR SINK_ID", sink_id)
-        except:
-            pass
         Sink_bound_birth.append([sink_id, born_bound, most_bound_sink_id, str(first_bound_sink), most_bound_sep, lowest_Etot, delay_time])
         print("Birth conditions of sink", sink_id, "is", Sink_bound_birth[-1])
 
