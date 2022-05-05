f3e89c491d1b2167f6e9fd876e6449f1294e29eb
Rajika Lakmali Kuruwita <reggie@reggies-macbook-pro.home>
2022-05-05 16:50:10 +0200

    Hopefully I implemented some way to restart the analysis if I need to

--- Ramses_analysis/CF_analysis/get_birth_conditions_para.py
+++ Ramses_analysis/CF_analysis/get_birth_conditions_para.py
@@ -511,7 +511,7 @@ for sink_id in sink_ids:
                         gc.collect()
 
         Sink_bound_birth.append([sink_id, born_bound, most_bound_sink_id, str(first_bound_sink), most_bound_sep, lowest_Etot, delay_time])
-        print("Rank:", rank, "Birth conditions of sink", sink_id, "(of ", sink_ids[-],") is", Sink_bound_birth[-1], flush=True)
+        print("Rank:", rank, "Birth conditions of sink", sink_id, "(of ", sink_ids[-1],") is", Sink_bound_birth[-1], flush=True)
 
         file = open("sink_birth_conditions_"+("%03d" % rank)+".pkl", 'wb')
         pickle.dump((Sink_bound_birth),file)
