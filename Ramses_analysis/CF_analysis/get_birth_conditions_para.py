61d6838275f09991a05ef34f11a76adc66fa47f6
Rajika Lakmali Kuruwita <reggie@reggies-macbook-pro.home>
2022-05-05 17:21:04 +0200

    Hopefully I implemented some way to restart the analysis if I need to

--- Ramses_analysis/CF_analysis/get_birth_conditions_para.py
+++ Ramses_analysis/CF_analysis/get_birth_conditions_para.py
@@ -90,6 +90,8 @@ if rank == 0:
     if len(Sink_bound_birth) == 0:
         sink_ids = np.arange(np.shape(global_data['m'].T)[0])
     else:
+        import pdb
+        pdb.set_trace()
         sink_ids = list(set(found_sinks).symmetric_difference(set(all_inds)))
     
     #print("Memory_useage:", virtual_memory().percent, "on line", getframeinfo(currentframe()).lineno)
