4e2c60fe098a007dd0f6b177c7c182e5267e6911
Rajika Lakmali Kuruwita <reggie@reggies-macbook-pro.home>
2022-05-09 14:44:52 +0200

    Teasing out restart

--- Ramses_analysis/CF_analysis/get_birth_conditions_para.py
+++ Ramses_analysis/CF_analysis/get_birth_conditions_para.py
@@ -122,10 +122,14 @@ if rank == 0:
     print("Found formation times", flush=True)
     #print("Memory_useage:", virtual_memory().percent, "on line", getframeinfo(currentframe()).lineno)
 
+    import pdb
+    pdb.set_trace()
     for trunc_it in range(size):
         try:
             form_time_it = np.where(global_data['time']==formation_times[sink_ids[trunc_it]])[0][0]
         except:
+            import pdb
+            pdb.set_trace()
             print("FAILED ON  trun_it", trunc_it, "sink_is:", sink_ids[trunc_it])
         
         #truncate global data
