17e693df3c99f15825312d2f07b3172c051141a9
Rajika Lakmali Kuruwita <reggie@reggies-macbook-pro.home>
2022-05-18 14:24:54 +0200

    Trying to use to actually other sink from the 100yr cadence data instead of the saved most bound sink to determine formation pathway

--- Ramses_analysis/CF_analysis/superplot_multi.py
+++ Ramses_analysis/CF_analysis/superplot_multi.py
@@ -328,12 +328,16 @@ if plot_truncated_super_mult == True:
                                         marker_color = 'm'
                                         marker_shape = '^'
                                     else:
+                                        if Sink_birth_all[str(np.max(sub_sys))][1] in flatten(eval(Sink_birth_all[str(np.max(sub_sys))][2])):
+                                            marker_color = 'm'
+                                            marker_shape = '^'
+                                        else:
+                                            marker_color = 'r'
+                                            marker_shape = 'o'
                                         if np.isnan(Sink_birth_all[str(np.max(sub_sys))][3]):
                                             print("misclassified birth conditions!")
                                             import pdb
                                             pdb.set_trace()
-                                        marker_color = 'r'
-                                        marker_shape = 'o'
                                 else:
                                     print("sink", np.max(sub_sys), "Not found in birth conditions")
                                     Not_plotted_sinks[pick_it].append(np.max(sub_sys))
@@ -345,9 +349,6 @@ if plot_truncated_super_mult == True:
                                     if args.x_field == 'Time':
                                         axs.flatten()[pick_it].scatter(Time_adjusted_formation[:t_max][0], superplot_dict[args.plot_key][time_key][:t_max][0][sep_ind], color=marker_color, marker=marker_shape)
                                     elif args.x_field == 'SFE':
-                                        if sub_sys == [254, 5370]:
-                                            import pdb
-                                            pdb.set_trace()
                                         #axs.flatten()[pick_it].scatter(SFE_arr[0], superplot_dict[args.plot_key][time_key][:sep_end_ind+1][0][sep_ind], color=marker_color, marker=marker_shape)
                                         if marker_color == 'b':
                                             print("Core_frag: Bound==True")
@@ -402,12 +403,16 @@ if plot_truncated_super_mult == True:
                                             marker_color = 'm'
                                             marker_shape = '^'
                                         else:
+                                            if Sink_birth_all[str(np.max(real_sinks))][1] in flatten(eval(Sink_birth_all[str(np.max(real_sinks))][2])):
+                                                marker_color = 'm'
+                                                marker_shape = '^'
+                                            else:
+                                                marker_color = 'r'
+                                                marker_shape = 'o'
                                             if np.isnan(Sink_birth_all[str(np.max(real_sinks))][3]):
                                                 print("misclassified birth conditions!")
                                                 import pdb
                                                 pdb.set_trace()
-                                            marker_color = 'r'
-                                            marker_shape = 'o'
                                     else:
                                         print("sink", np.max(real_sinks), "Not found in birth conditions")
                                         Not_plotted_sinks[pick_it].append(np.max(real_sinks))
@@ -419,9 +424,6 @@ if plot_truncated_super_mult == True:
                                         if args.x_field == 'Time':
                                             axs.flatten()[pick_it].scatter(Time_adjusted_formation[:t_max][0], superplot_dict[args.plot_key][time_key][:t_max][0][sep_ind], color=marker_color, marker=marker_shape)
                                         elif args.x_field == 'SFE':
-                                            if sub_sys == [254, 5370]:
-                                                import pdb
-                                                pdb.set_trace()
                                             #axs.flatten()[pick_it].scatter(SFE_arr[0], superplot_dict[args.plot_key][time_key][:sep_end_ind+1][0][sep_ind], color=marker_color, marker=marker_shape)
                                             if marker_color == 'b':
                                                 print("Core_frag: Bound==True")
