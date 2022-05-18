ff68a5b2d51de5c7833b76be7c5b6284114f006f
Rajika Lakmali Kuruwita <reggie@reggies-macbook-pro.home>
2022-05-18 16:22:46 +0200

    Trying to use to actually other sink from the 100yr cadence data instead of the saved most bound sink to determine formation pathway

--- Ramses_analysis/CF_analysis/superplot_multi.py
+++ Ramses_analysis/CF_analysis/superplot_multi.py
@@ -320,17 +320,17 @@ if plot_truncated_super_mult == True:
                             if np.mean(np.array(sub_sys)<superplot_dict['N_stars'][-1]) == 1:
                                 if str(np.max(sub_sys)) in Sink_birth_all.keys():
                                     other_sys = np.min(sub_sys)
-                                    if Sink_birth_all[str(np.max(sub_sys))][0] == True and other_sys == Sink_birth_all[str(np.max(sub_sys))][1]:
+                                    if Sink_birth_all[str(np.max(sub_sys))][0] == True and str(other_sys) == str(Sink_birth_all[str(np.max(sub_sys))][1]):
                                         marker_color = 'b'
                                         marker_shape = 's'
                                         #elif Sink_birth_all[str(np.max(sub_sys))][1] in flatten(eval(Sink_birth_all[str(np.max(sub_sys))][2])):
                                         #elif other_sys in flatten(eval(str(Sink_birth_all[str(np.max(sub_sys))][1]))):
-                                    elif str(other_sys) == Sink_birth_all[str(np.max(sub_sys))][1]:
+                                    elif str(other_sys) == str(Sink_birth_all[str(np.max(sub_sys))][1]):
                                         marker_color = 'm'
                                         marker_shape = '^'
                                     else:
                                         try:
-                                            if other_sys != int(Sink_birth_all[str(np.max(sub_sys))][1]):
+                                            if str(other_sys) != str(Sink_birth_all[str(np.max(sub_sys))][1]):
                                                 marker_color = 'r'
                                                 marker_shape = 'o'
                                             else:
@@ -399,17 +399,17 @@ if plot_truncated_super_mult == True:
                                                 insert_str = str(sub_sys_dict[str(flatten(other_sys)[greater_ind])])
                                                 other_sys_str = other_split[0] + insert_str + other_split[1]
                                                 other_sys = eval(other_sys_str)
-                                        if Sink_birth_all[str(np.max(real_sinks))][0] == True and other_sys == Sink_birth_all[str(np.max(real_sinks))][1]:
+                                        if Sink_birth_all[str(np.max(real_sinks))][0] == True and other_sys_str == str(Sink_birth_all[str(np.max(real_sinks))][1]):
                                             marker_color = 'b'
                                             marker_shape = 's'
                                             #elif Sink_birth_all[str(np.max(sub_sys))][1] in flatten(eval(Sink_birth_all[str(np.max(sub_sys))][2])):
                                             #elif other_sys in flatten(eval(str(Sink_birth_all[str(np.max(sub_sys))][1]))):
-                                        elif str(other_sys) == Sink_birth_all[str(np.max(real_sinks))][1]:
+                                        elif other_sys_str == str(Sink_birth_all[str(np.max(real_sinks))][1]):
                                             marker_color = 'm'
                                             marker_shape = '^'
                                         else:
                                             try:
-                                                if other_sys != int(Sink_birth_all[str(np.max(real_sinks))][1]):
+                                                if other_sys_str != str(Sink_birth_all[str(np.max(real_sinks))][1]):
                                                     marker_color = 'r'
                                                     marker_shape = 'o'
                                                 else:
