0123d86a558f2b157ceb6230b4848006616ae5ad
Rajika Lakmali Kuruwita <reggie@reggies-macbook-pro.home>
2022-05-18 14:43:26 +0200

    Trying to use to actually other sink from the 100yr cadence data instead of the saved most bound sink to determine formation pathway

--- Ramses_analysis/CF_analysis/superplot_multi.py
+++ Ramses_analysis/CF_analysis/superplot_multi.py
@@ -328,7 +328,7 @@ if plot_truncated_super_mult == True:
                                         marker_color = 'm'
                                         marker_shape = '^'
                                     else:
-                                        if Sink_birth_all[str(np.max(sub_sys))][1] in flatten(eval(Sink_birth_all[str(np.max(sub_sys))][2])):
+                                        if Sink_birth_all[str(np.max(sub_sys))][1] in flatten(eval(Sink_birth_all[str(np.max(sub_sys))][2])) and other_sys in flatten(eval(Sink_birth_all[str(np.max(sub_sys))][2])):
                                             import pdb
                                             pdb.set_trace()
                                             marker_color = 'm'
@@ -405,7 +405,7 @@ if plot_truncated_super_mult == True:
                                             marker_color = 'm'
                                             marker_shape = '^'
                                         else:
-                                            if Sink_birth_all[str(np.max(real_sinks))][1] in flatten(eval(Sink_birth_all[str(np.max(real_sinks))][2])):
+                                            if Sink_birth_all[str(np.max(real_sinks))][1] in flatten(eval(Sink_birth_all[str(np.max(real_sinks))][2])) and other_sys in flatten(eval(Sink_birth_all[str(np.max(real_sinks))][2]))::
                                                 import pdb
                                                 pdb.set_trace()
                                                 marker_color = 'm'
