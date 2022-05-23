e3b17b8619fceeaeaa949d56a1de4c648b709544
Rajika Lakmali Kuruwita <reggie@reggies-macbook-pro.home>
2022-05-23 15:20:52 +0200

    refining plotting high inspiral systems

--- Ramses_analysis/CF_analysis/pathway_evolution.py
+++ Ramses_analysis/CF_analysis/pathway_evolution.py
@@ -174,6 +174,9 @@ if read_pickle == True:
                                     try:
                                         sub_10000_inds = np.where(Time_arr<10000)[0]
                                         non_nan_ind = np.where(np.isnan(Sep_arr)==False)[0]
+                                        if np.max(non_nan_ind[1:] - non_nan_ind[:-1])>1:
+                                            import pdb
+                                            pdb.set_trace()
                                         if len(sub_10000_inds)>9 and Time_arr[non_nan_ind][-1]>10000:
                                             dt = Time_arr[sub_10000_inds[-1]] - Time_arr[sub_10000_inds[0]]
                                             ds = Sep_arr[sub_10000_inds[-1]] - Sep_arr[sub_10000_inds[0]]
