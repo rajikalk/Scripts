eb9b673e8028dd5df604d63783a606cbf6bffa23
Rajika Lakmali Kuruwita <reggie@reggies-macbook-pro.home>
2022-06-08 18:38:27 +0200

    trying to find why the fast analysis gives wrong result

--- Ramses_analysis/CF_analysis/get_birth_conditions_para.py
+++ Ramses_analysis/CF_analysis/get_birth_conditions_para.py
@@ -54,8 +54,6 @@ Grho = int(global_data_pickle_file.split('/G')[-1].split('/')[0])
 
 if Grho == 50:
     scale_m = 1500*1.98841586e+33
-    if size == 1:
-        diff_conds = [46, 48, 77, 83]
 elif Grho == 100:
     scale_m = 3000*1.98841586e+33
 elif Grho == 125:
@@ -236,10 +234,6 @@ for sink_id in loop_inds:
         absvel = np.array([global_data['ux'][0][:sink_id+1], global_data['uy'][0][:sink_id+1], global_data['uz'][0][:sink_id+1]]).T
         mass = np.array(global_data['m'][0][:sink_id+1])
         n_stars = np.argwhere(global_data['m'][0]>0).T[0]
-        if len(n_stars) < sink_id:
-            print("NUMBER OF STARS", len(n_stars), "THE LESS THAN SINK_ID:", sink_id)
-            import pdb
-            pdb.set_trace()
         del global_data
         gc.collect()
         
@@ -298,11 +292,6 @@ for sink_id in loop_inds:
         born_bound = True
         delay_time = 0
         
-        if Grho == 50 and size == 1:
-            if sink_id in diff_conds:
-                import pdb
-                pdb.set_trace()
-        
         sys_id = np.nan
         if len(n_stars)>1:
             file_open = open("global_data_rank_"+str(rank)+".pkl", 'rb')
@@ -341,10 +330,6 @@ for sink_id in loop_inds:
             gc.collect()
             #print("Memory_useage on rank", rank,":", virtual_memory().percent, "on line", getframeinfo(currentframe()).lineno)
             res = m.multipleAnalysis(S,cutoff=10000, bound_check=True, nmax=6, cyclic=True, Grho=Grho, verbose=False)
-            if Grho == 50 and size == 1:
-                if sink_id in diff_conds:
-                    import pdb
-                    pdb.set_trace()
             #print("Memory_useage on rank", rank,":", virtual_memory().percent, "on line", getframeinfo(currentframe()).lineno)
             if sink_id in res['index1']:
                 sys_id = np.argwhere(res['index1'] == sink_id)[0][0]
@@ -355,10 +340,6 @@ for sink_id in loop_inds:
             else:
                 sys_id = np.nan
             if np.isnan(sys_id) == False:
-                if Grho == 50 and size == 1:
-                    if sink_id in diff_conds:
-                        import pdb
-                        pdb.set_trace()
                 first_bound_sink = losi(first_bound_sink, res)
                 lowest_Etot = res['epot'][sys_id] + res['ekin'][sys_id]
                 most_bound_sep = res['separation'][sys_id]
@@ -411,11 +392,6 @@ for sink_id in loop_inds:
             lowest_Etot = np.nan
             delay_time = np.nan
             sys_form_time = np.nan
-            
-            if Grho == 50 and size == 1:
-                if sink_id in diff_conds:
-                    import pdb
-                    pdb.set_trace()
             
             if len([s for s in system_keys if ' '+str(sink_id)+']' in s]) == 0 or len([s for s in system_keys if '['+str(sink_id)+',' in s]) == 0:
                 try:
@@ -658,10 +634,6 @@ for sink_id in loop_inds:
                             gc.collect()
                             #print("Memory_useage on rank", rank,":", virtual_memory().percent, "on line", getframeinfo(currentframe()).lineno)
                             res = m.multipleAnalysis(S,cutoff=10000, bound_check=True, nmax=6, cyclic=True, Grho=Grho)
-                            if Grho == 50 and size == 1:
-                                if sink_id in diff_conds:
-                                    import pdb
-                                    pdb.set_trace()
                             #print("Memory_useage on rank", rank,":", virtual_memory().percent, "on line", getframeinfo(currentframe()).lineno)
                             if sink_id in res['index1']:
                                 sys_id = np.argwhere(res['index1'] == sink_id)[0][0]
@@ -682,10 +654,6 @@ for sink_id in loop_inds:
                                 if delay_time == 0:
                                     born_bound = True
                                     most_bound_sink_id = str(first_bound_sink)
-                                if Grho == 50 and size == 1:
-                                    if sink_id in diff_conds:
-                                        import pdb
-                                        pdb.set_trace()
                                 break
                             del res
                             gc.collect()
