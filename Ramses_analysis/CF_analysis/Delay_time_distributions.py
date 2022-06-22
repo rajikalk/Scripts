90e7b0772a1655901c0e5be915f9caf29c4903f6
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-06-22 12:58:38 +0200

    plotting sims on different panels, and annotating different systems

--- Ramses_analysis/CF_analysis/Delay_time_distributions.py
+++ Ramses_analysis/CF_analysis/Delay_time_distributions.py
@@ -61,47 +61,46 @@ for pickle_it in range(len(birth_con_pickles)):
     
 plt.savefig('delay_vs_SFE.pdf', bbox_inches='tight', pad_inches=0.02)
 
-'''
 plt.clf()
-fig, axs = plt.subplots(ncols=1, nrows=1, figsize=(single_col_width, 0.8*single_col_width), sharex=True)
-ax = plt.gca()
+fig, axs = plt.subplots(ncols=1, nrows=len(birth_con_pickles), figsize=(two_col_width, single_col_width*2.5), sharex=True, sharey=True)
+iter_range = range(0, len(birth_con_pickles))
+plt.subplots_adjust(wspace=0.0)
+plt.subplots_adjust(hspace=0.0)
+markers = ['o', '^', 's', 'p', 'h']
+marker_labels = ['Binary', 'Trinary', 'Quadruple', 'Quintuple', 'Sextuple']
 
-for sim in range(len(subplot_titles)):
-    plt.scatter(Initial_seps[sim], T_delay[sim], label=subplot_titles[sim], marker='.')
-
-ax.set_yscale('log')
-plt.legend()
-
-plt.xlabel('Initial Separation (au)', fontsize=font_size)
-plt.ylabel('T$_{delay}$ (yr)', fontsize=font_size)
-plt.xlim([0, 10000])
-
-axs.tick_params(axis='both', which='major', labelsize=font_size, right=True)
-axs.tick_params(axis='both', which='minor', labelsize=font_size)
-axs.tick_params(axis='x', direction='in')
-axs.tick_params(axis='y', direction='in')
-
-plt.savefig('delay_vs_sep.pdf', bbox_inches='tight', pad_inches=0.02)
-
-plt.clf()
-fig, axs = plt.subplots(ncols=1, nrows=1, figsize=(single_col_width, 0.8*single_col_width), sharex=True)
-ax = plt.gca()
-
-for sim in range(len(subplot_titles)):
-    plt.scatter(np.array(SFE[sim])*100, T_delay[sim], label=subplot_titles[sim], marker='.')
-ax.set_yscale('log')
-plt.legend()
-
-plt.xlabel('SFE (%)', fontsize=font_size)
-plt.ylabel('T$_{delay}$ (yr)', fontsize=font_size)
-plt.xlim([0, 5])
-
-axs.tick_params(axis='both', which='major', labelsize=font_size, right=True)
-axs.tick_params(axis='both', which='minor', labelsize=font_size)
-axs.tick_params(axis='x', direction='in')
-axs.tick_params(axis='y', direction='in')
-
-plt.savefig('delay_vs_SFE.pdf', bbox_inches='tight', pad_inches=0.02)
-'''
-
+for pickle_it in range(len(birth_con_pickles)):
+    SFE = [[], [], [], [], []]
+    T_delay = [[], [], [], [], []]
+    Initial_seps = [[], [], [], [], []]
+    
+    file = open(birth_con_pickles[pickle_it], 'rb')
+    Sink_birth_all = pickle.load(file)
+    file.close()
+    
+    for sink_key in Sink_birth_all.keys():
+        if Sink_birth_all[sink_key][0] == False and Sink_birth_all[sink_key][1] == Sink_birth_all[sink_key][2]:
+            n_stars = len(flatten(eval(Sink_birth_all[sink_key][2])))
+            SFE[n_stars-1].append(Sink_birth_all[sink_key][-1])
+            T_delay[n_stars-1].append(Sink_birth_all[sink_key][-2])
+            Initial_seps[n_stars-1].append(Sink_birth_all[sink_key][-4])
             
+    for T_del_it in range(len(T_delay)):
+        axs[pickle_it].scatter(Initial_seps, T_delay[T_del_it], marker=markers[T_del_it], label=marker_labels[T_del_it])
+    if pickle_it == 0:
+        axs[pickle_it].legend(ncol=3, loc='lower left')
+    axs[pickle_it].set_yscale('log')
+    
+    if pickle_it == len(birth_con_pickles)-1:
+        axs[pickle_it].set_xlabel('Initial Separation (au)', fontsize=font_size)
+    axs[pickle_it].set_ylabel('T$_{delay}$ (yr)', fontsize=font_size)
+    axs[pickle_it].set_xlim([0, 10000])
+
+    axs[pickle_it].tick_params(axis='both', which='major', labelsize=font_size, right=True)
+    axs[pickle_it].tick_params(axis='both', which='minor', labelsize=font_size)
+    axs[pickle_it].tick_params(axis='x', direction='in')
+    axs[pickle_it].tick_params(axis='y', direction='in')
+    
+    axs[pickle_it].text((9000), 2, subplot_titles[pickle_it], zorder=11, fontsize=font_size)
+    
+plt.savefig('delay_vs_sep.pdf', bbox_inches='tight', pad_inches=0.02)
