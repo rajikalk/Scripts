2a0ade9df6d38947265ebea6afeb12b3b668d046
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-06-22 13:01:32 +0200

    plotting sims on different panels, and annotating different systems

--- Ramses_analysis/CF_analysis/Delay_time_distributions.py
+++ Ramses_analysis/CF_analysis/Delay_time_distributions.py
@@ -101,6 +101,6 @@ for pickle_it in range(len(birth_con_pickles)):
     axs[pickle_it].tick_params(axis='x', direction='in')
     axs[pickle_it].tick_params(axis='y', direction='in')
     
-    axs[pickle_it].text((4000), 2, subplot_titles[pickle_it], zorder=11, fontsize=font_size)
+    axs[pickle_it].text((8100), 10, subplot_titles[pickle_it], zorder=11, fontsize=font_size)
     
 plt.savefig('delay_vs_sep.pdf', bbox_inches='tight', pad_inches=0.02)
