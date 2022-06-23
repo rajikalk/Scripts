f61a4d3fc5519040739a92a63227d6fd1ae7c195
Rajika Lakmali Kuruwita <reggie@reggies-macbook-pro.home>
2022-06-23 10:57:58 +0200

    Checking if the time for the 'bound at birth' check is the same as the formation time

--- Ramses_analysis/CF_analysis/get_birth_conditions_para.py
+++ Ramses_analysis/CF_analysis/get_birth_conditions_para.py
@@ -451,6 +451,10 @@ for sink_id in loop_inds:
                 sink_ids, formation_times, global_data = pickle.load(file_open)
                 file_open.close()
                 
+                if sink_id == 33 or sink_id = 44:
+                    import pbd
+                    pdb.set_trace()
+                
                 first_test_ind = np.argmin(abs(global_data['time']*scale_t_yr - sys_start_time))
                 start_test_ind = np.argmin(abs(global_data['time']*scale_t_yr - (sys_start_time-10000)))
                 test_time_inds = np.arange(start_test_ind, first_test_ind+1)
@@ -571,8 +575,6 @@ for sink_id in loop_inds:
                 global_test_inds.update({'uy':global_data['uy'][test_time_inds]})
                 global_test_inds.update({'uz':global_data['uz'][test_time_inds]})
                 '''
-                import pdb
-                pdb.set_trace()
                 global_test_inds.update({'time':global_data['time'][test_time_inds][::-1]})
                 global_test_inds.update({'m':global_data['m'][test_time_inds][::-1]})
                 global_test_inds.update({'x':global_data['x'][test_time_inds][::-1]})
@@ -685,8 +687,9 @@ for sink_id in loop_inds:
                                     most_bound_sep = res['separation'][sys_id]
                                     bound_time = res['time']*scale_t_yr
                                     delay_time = float(bound_time - formation_time)
-                                    import pdb
-                                    pdb.set_trace()
+                                    if sink_id == 33 or sink_id = 44:
+                                        import pbd
+                                        pdb.set_trace()
                                     sys_form_time = sfe
                                     #Find initial separation
                                     if delay_time == 0:
