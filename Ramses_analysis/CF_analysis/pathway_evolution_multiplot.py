e9add08a6c0929d47a4caea13cea8d2cea53e202
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-06-30 15:45:04 +0200

    cleaning up inspiral rates

--- Ramses_analysis/CF_analysis/pathway_evolution_multiplot.py
+++ Ramses_analysis/CF_analysis/pathway_evolution_multiplot.py
@@ -169,8 +169,8 @@ for grad_it in range(len(grad_pickles)):
             scale_guess = np.max(grad_hist_core_mean_norm[:-2])
             mean_guess = np.nanmean(np.log10(np.array(core_mean)*-1))
             median_guess = np.nanmedian(np.log10(np.array(core_mean)*-1))
-            skew_guess = (3*(mean_guess - median_guess))/std_guess
             std_guess = np.nanstd(np.log10(np.array(core_mean)*-1))
+            skew_guess = (3*(mean_guess - median_guess))/std_guess
             try:
                 popt, pcov = curve_fit(Skewed_Gaussian, bin_centres[::-1], (grad_hist_core_mean_norm[:-2][::-1]), [scale_guess, mean_guess, std_guess, skew_guess], bounds=([0.0, bin_centres[-1], 0.0, 0.0], [1.0, bin_centres[0], (bin_centres[0]-bin_centres[-1]), (bin_centres[0]-bin_centres[-1])]))
                 #popt, pcov = curve_fit(Gaussian, bin_centres[::-1], (grad_hist_core_mean_norm[:-2][::-1]), [scale_guess, mean_guess, std_guess], bounds=([0.0, bin_centres[-1], 0.0], [1.0, bin_centres[0], (bin_centres[0]-bin_centres[-1])]))
@@ -185,8 +185,8 @@ for grad_it in range(len(grad_pickles)):
             scale_guess = np.max(grad_hist_core_delayed_mean_norm[:-2])
             mean_guess = np.nanmean(np.log10(np.array(core_delayed_mean)*-1))
             median_guess = np.nanmedian(np.log10(np.array(core_mean)*-1))
-            skew_guess = (3*(mean_guess - median_guess))/std_guess
             std_guess = np.nanstd(np.log10(np.array(core_delayed_mean)*-1))
+            skew_guess = (3*(mean_guess - median_guess))/std_guess
             try:
                 popt, pcov = curve_fit(Gaussian, bin_centres[::-1], (grad_hist_core_delayed_mean_norm[:-2][::-1]), [scale_guess, mean_guess, std_guess, skew_guess], bounds=([0.0, bin_centres[-1], 0.0, 0.0], [1.0, bin_centres[0], (bin_centres[0]-bin_centres[-1]), (bin_centres[0]-bin_centres[-1])]))
                 x_fit = np.linspace(0, len(grad_hist_core))
@@ -201,8 +201,8 @@ for grad_it in range(len(grad_pickles)):
             scale_guess = np.max(grad_hist_capt_mean_norm[:-2])
             mean_guess = np.nanmean(np.log10(np.array(capt_mean)*-1))
             median_guess = np.nanmedian(np.log10(np.array(core_mean)*-1))
+            std_guess = np.nanstd(np.log10(np.array(core_delayed_mean)*-1))
             skew_guess = (3*(mean_guess - median_guess))/std_guess
-            std_guess = np.nanstd(np.log10(np.array(capt_mean)*-1))
             try:
                 popt, pcov = curve_fit(Gaussian, bin_centres[::-1], (grad_hist_capt_mean_norm[:-2][::-1]), [scale_guess, mean_guess, std_guess, skew_guess], bounds=([0.0, bin_centres[-1], 0.0, 0.0], [1.0, bin_centres[0], (bin_centres[0]-bin_centres[-1]), (bin_centres[0]-bin_centres[-1])]))
                 x_fit = np.linspace(0, len(grad_hist_core))
@@ -217,8 +217,8 @@ for grad_it in range(len(grad_pickles)):
             scale_guess = np.max(grad_hist_misc_mean_norm[:-2])
             mean_guess = np.nanmean(np.log10(np.array(misc_mean)*-1))
             median_guess = np.nanmedian(np.log10(np.array(core_mean)*-1))
+            std_guess = np.nanstd(np.log10(np.array(core_delayed_mean)*-1))
             skew_guess = (3*(mean_guess - median_guess))/std_guess
-            std_guess = np.nanstd(np.log10(np.array(misc_mean)*-1))
             try:
                 popt, pcov = curve_fit(Gaussian, bin_centres[::-1], (grad_hist_misc_mean_norm[:-2][::-1]), [scale_guess, mean_guess, std_guess, skew_guess], bounds=([0.0, bin_centres[-1], 0.0, 0.0], [1.0, bin_centres[0], (bin_centres[0]-bin_centres[-1]), (bin_centres[0]-bin_centres[-1])]))
                 x_fit = np.linspace(0, len(grad_hist_core))
