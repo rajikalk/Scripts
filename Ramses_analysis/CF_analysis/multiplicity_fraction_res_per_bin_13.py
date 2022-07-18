a2e34c3e6c27808778a9653ccc0b685094e9a4ea
Rajika Lakmali Kuruwita <reggie@reggies-macbook-pro.home>
2022-07-18 13:38:54 +0200

    testing to see if I get NaN accretion rates when calculated the old way

--- Ramses_analysis/CF_analysis/multiplicity_fraction_res_per_bin_13.py
+++ Ramses_analysis/CF_analysis/multiplicity_fraction_res_per_bin_13.py
@@ -587,9 +587,9 @@ if update == True and args.make_plots_only == 'False':
             sink_inds = np.where(global_data['m'][time_it]>0)[0]
             L_tot = luminosity(global_data, sink_inds, time_it)
             M_dot = accretion(sink_inds, time_it)
-            if True in np.isnan(M_dot):
-                import pdb
-                pdb.set_trace()
+            #if True in np.isnan(M_dot):
+            import pdb
+            pdb.set_trace()
             vis_inds_tot = np.where((L_tot>=luminosity_lower_limit)&(M_dot>accretion_limit)&(L_tot<=args.upper_L_limit))[0]
             
             S = pr.Sink()
