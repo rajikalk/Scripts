f312969bd476b5000fd4ddf63c70308218ddde8e
Rajika Lakmali Kuruwita <reggie@reggies-macbook-pro.home>
2022-07-18 08:55:03 +0200

    investigating why I get high CF

--- Ramses_analysis/CF_analysis/multiplicity_fraction_res_per_bin_13.py
+++ Ramses_analysis/CF_analysis/multiplicity_fraction_res_per_bin_13.py
@@ -925,7 +925,7 @@ if update == True and args.make_plots_only == 'False':
                         print("CF =", cf)
                     else:
                         print_lines.append(print_line)
-                if cf > 0.2:
+                if cf > 0.5:
                     import pdb
                     pdb.set_trace()
                 CF_per_bin.append(cf)
