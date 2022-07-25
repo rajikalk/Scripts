175a14848a46d3b7ced6105fc202efb38f411377
Rajika Lakmali Kuruwita <reggie@reggies-macbook-pro.home>
2022-07-25 18:03:05 +0200

    Revisiting fitting CH histograms over time

--- Ramses_analysis/CF_analysis/fit_over_time.py
+++ Ramses_analysis/CF_analysis/fit_over_time.py
@@ -135,7 +135,7 @@ for CF_it in range(len(CF_Array_Full)):
     chi_red_tobin = (np.sum(((CF_hist[1:]-gauss_total)**2)/(CF_errs[1:]**2)))/len(CF_hist[1:])
     #chi_red_tobin = (np.sum(((CF_hist[usable_bin_inds]-CF_per_bin_Tobin_Per[usable_bin_inds])**2)/(CF_errs[usable_bin_inds]**2)))/len(CF_hist[usable_bin_inds])
     reduced_chi_square_tobin.append(chi_red_tobin)
-    if chi_red_tobin < 0.5:
+    if chi_red_tobin < 0.25:
         plt.clf()
         plt.bar(bin_centers, CF_hist, edgecolor='k', label="CF Simulations", width=0.25, alpha=0.5)
         plt.bar(bin_centers, CF_per_bin_Tobin_Per, yerr=CF_errs, width=0.25, edgecolor='black', alpha=0.5, label="Tobin et al")
