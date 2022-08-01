ab7d81b49a4785f2bd8da909eacc0dd3fee3ca52
Rajika Lakmali Kuruwita <reggie@reggies-macbook-pro.home>
2022-08-01 17:15:54 +0200

    cleaning up fit over time

--- Ramses_analysis/CF_analysis/fit_over_time.py
+++ Ramses_analysis/CF_analysis/fit_over_time.py
@@ -98,7 +98,7 @@ except:
 
 #do chi squared
 reduced_chi_square_tobin = []
-usable_bin_inds = np.array([2, 4, 6, 7, 8, 9, 10, 11, 12])
+usable_bin_inds = np.array([2, 4, 6, 8, 10, 11, 12])
 
 import scipy.stats as stats
 
