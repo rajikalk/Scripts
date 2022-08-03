f80ae432f357089d66fcc8e88b9869e4aaae4b56
Rajika Lakmali Kuruwita <reggie@reggies-macbook-pro.home>
2022-08-03 11:22:09 +0200

    going to save all the spearations to plot CDFs

--- Ramses_analysis/CF_analysis/multi_fit_plot.py
+++ Ramses_analysis/CF_analysis/multi_fit_plot.py
@@ -38,8 +38,10 @@ subplot_titles = ["1500M$_\odot$", "3000M$_\odot$", "3750M$_\odot$", "4500M$_\od
 colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown']
 line_styles = [':', (0, (3, 1, 1, 1, 1, 1, 1, 1)), (0, (3, 1, 1, 1, 1, 1)), (0, (3, 1, 1, 1)), '--', '-']
 
-plt.clf()
-plt.figure(figsize=(single_col_width,0.7*single_col_width))
+fig, axs = plt.subplots(ncols=1, nrows=2, figsize=(single_col_width,1.5*single_col_width), sharex=True, sharey=True)
+iter_range = range(0, len(pickle_files)*2)
+plt.subplots_adjust(wspace=0.0)
+plt.subplots_adjust(hspace=0.07)
 
 for fit_pick in range(len(fit_pickles)):
     file = open(fit_pickles[fit_pick], 'rb')
@@ -67,15 +69,42 @@ for fit_pick in range(len(fit_pickles)):
         smoothed_chi.append(median_chi)
         smoothed_upp.append(err_upper)
         smoothed_low.append(err_lower)
-    plt.semilogy(SFE, smoothed_chi, label=subplot_titles[fit_pick], color=colors[fit_pick], ls=line_styles[fit_pick])
-    plt.fill_between(SFE, smoothed_low, smoothed_upp, alpha=0.2)
+    axs[0].semilogy(SFE, smoothed_chi, label=subplot_titles[fit_pick], color=colors[fit_pick], ls=line_styles[fit_pick])
+    axs[0].fill_between(SFE, smoothed_low, smoothed_upp, alpha=0.2)
     
-plt.legend(loc='upper right', ncol=2, fontsize=font_size, labelspacing=0.1, handletextpad=0.2, borderaxespad=0.2, borderpad=0.2, columnspacing=0.3)
-plt.xlabel('SFE')
-plt.ylabel("Fit (<$\chi^2$>)", labelpad=-0.2)
-plt.xlim([0.01, 0.05])
-plt.ylim(top=100)
-plt.savefig("fit_tobin_multi.pdf", format='pdf', bbox_inches='tight', pad_inches=0.02)
+    smoothed_chi = []
+    smoothed_upp = []
+    smoothed_low = []
+    smooth_window = 0.0001
+    for SFE_it in range(len(SFE)):
+        low_SFE = SFE[SFE_it] - smooth_window
+        if low_SFE < 0:
+            low_SFE = 0
+        high_SFE = SFE[SFE_it] + smooth_window
+        if high_SFE > 0.05:
+            high_SFE = 5
+        low_SFE_it = np.argmin(abs(SFE-low_SFE))
+        high_SFE_it = np.argmin(abs(SFE-high_SFE))
+        mean_chi = np.mean(reduced_chi_square_selected[low_SFE_it:high_SFE_it])
+        median_chi = np.median(reduced_chi_square_selected[low_SFE_it:high_SFE_it])
+        std_chi = np.std(reduced_chi_square_selected[low_SFE_it:high_SFE_it])
+        err_upper = mean_chi+std_chi
+        err_lower = mean_chi-std_chi
+        smoothed_chi.append(median_chi)
+        smoothed_upp.append(err_upper)
+        smoothed_low.append(err_lower)
+    axs[1].semilogy(SFE, smoothed_chi, label=subplot_titles[fit_pick], color=colors[fit_pick], ls=line_styles[fit_pick])
+    axs[1].fill_between(SFE, smoothed_low, smoothed_upp, alpha=0.2)
+    
+axs[0].legend(loc='upper right', ncol=2, fontsize=font_size, labelspacing=0.1, handletextpad=0.2, borderaxespad=0.2, borderpad=0.2, columnspacing=0.3)
+axs[1].xlabel('SFE')
+axs[0].ylabel("Fit (<$\chi^2$>)", labelpad=-0.2)
+axs[1].ylabel("Fit (<$\chi^2$>)", labelpad=-0.2)
+axs[0].text((0.02), (0.6), "All bins", zorder=11, size=args.text_font)
+axs[1].text((0.02), (0.6), "Selected bins", zorder=11, size=args.text_font)
+axs[0].xlim([0.01, 0.05])
+axs[0].ylim(top=100)
+plt.savefig("fit_multi.pdf", format='pdf', bbox_inches='tight', pad_inches=0.02)
 
 plt.clf()
 plt.figure(figsize=(single_col_width,0.7*single_col_width))
