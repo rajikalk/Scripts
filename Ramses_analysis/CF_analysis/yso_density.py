78ea2430f95ccd992d6a6519159a18a3a5098831
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-08-11 11:47:18 +0200

    makei YSO dens for different times

--- Ramses_analysis/CF_analysis/yso_density.py
+++ Ramses_analysis/CF_analysis/yso_density.py
@@ -288,16 +288,9 @@ file = open('yso_dens.pkl', 'wb')
 pickle.dump((Time, SFE, All_YSO_dens), file)
 file.close()
 '''
-sfe_42_ind = np.argmin(abs(SFE-0.042))
-ysos_42 = np.log10(np.sort(All_YSO_dens[sfe_42_ind]))
-yso_CDF = abs(np.cumsum(ysos_42))/abs(np.cumsum(ysos_42))[-1]
-
-plt.clf()
-plt.step(np.sort(ysos_42), yso_CDF, label='YSO_density')
-plt.xlabel("YSO density (pc$^{-2}$)")
-plt.ylabel("Frequency")
-plt.ylim([0,1])
-plt.savefig('yso_dens_42.png')
+SFE_label = ['0.5\%', '1\%', '2\%', '3\%', '4\%', '5\%']
+import matplotlib.pylab as pl
+colors = pl.cm.viridis(np.linspace(0,1,6))
 
 YSO_per = []
 import csv
@@ -312,17 +305,23 @@ file.close()
 ysos_per = np.log10(np.sort(YSO_per))
 yso_CDF_per = abs(np.cumsum(ysos_per))/abs(np.cumsum(ysos_per))[-1]
 
-from scipy import stats
-KS_test_result = stats.ks_2samp(yso_CDF_per, yso_CDF)[0]
-alpha=0.99
-m = len(yso_CDF)
-n = len(yso_CDF_per)
-D_crit = np.sqrt((-1*np.log(alpha/2))*((1+(m/n))/(2*m)))
-print("KS stat =", KS_test_result, "D_crit =", D_crit)
-
 plt.clf()
-plt.step(np.sort(ysos_42), yso_CDF, label='simulation')
-plt.step(np.sort(ysos_per), yso_CDF_per, label='Perseus')
+from scipy import stats
+for SFE_it in range(len(SFE)):
+    ysos_log = np.log10(np.sort(All_YSO_dens[SFE_it]))
+    yso_CDF = abs(np.cumsum(ysos_log))/abs(np.cumsum(ysos_log))[-1]
+    
+    plt.step(np.sort(ysos_log), yso_CDF, label=SFE_label[SFE_it], color=colors[SFE_it])
+    
+    KS_test_result = stats.ks_2samp(yso_CDF_per, yso_CDF)[0]
+    alpha=0.99
+    m = len(yso_CDF)
+    n = len(yso_CDF_per)
+    D_crit = np.sqrt((-1*np.log(alpha/2))*((1+(m/n))/(2*m)))
+    print("KS stat =", KS_test_result, "D_crit =", D_crit)
+
+
+plt.step(np.sort(ysos_per), yso_CDF_per, label='Perseus', color='k', ls="--")
 plt.legend(loc='upper left')
 plt.xlabel("YSO density (pc$^{-2}$)")
 plt.ylabel("Frequency")
