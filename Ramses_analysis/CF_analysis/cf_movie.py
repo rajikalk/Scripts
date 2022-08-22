382d15dda148c8b103b4421dd0d72b86ae0641b0
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-08-22 12:04:46 +0200

    making movies consistent with the plots

--- Ramses_analysis/CF_analysis/cf_movie.py
+++ Ramses_analysis/CF_analysis/cf_movie.py
@@ -140,8 +140,8 @@ for time_it in range(start_time_it, end_time_it):
             except:
                 #plt.bar(bin_centers[1:], CF_median, yerr=CF_err, edgecolor='k', label="CF Simulations", width=0.25, alpha=0.5)
                 plt.bar(bin_centers[1:], CF_median, edgecolor='k', label="CF Simulations", width=0.25, alpha=0.5)
-            plt.bar(bin_centers, CF_per_bin_66, yerr=CF_errs, width=0.25, edgecolor='black', alpha=0.5, label="Perseus", fill=None, ls='-')
-            plt.bar(bin_centers, CF_per_bin_all, width=0.25, edgecolor='black', alpha=0.5, fill=None, ls='--')
+            plt.bar(bin_centers, CF_per_bin_Tobin_Per, yerr=CF_errs, width=0.25, edgecolor='black', alpha=0.5, label="Perseus", fill=None, ls='-')
+            #plt.bar(bin_centers, CF_per_bin_all, width=0.25, edgecolor='black', alpha=0.5, fill=None, ls='--')
             #plt.bar(bin_centers, CF_per_bin_Tobin_Ori, width=0.25, edgecolor='black', alpha=0.5, label="Orion", fill=None, ls='-.')
             #plt.plot(x,gauss_total)
             plt.legend(loc='upper left')
