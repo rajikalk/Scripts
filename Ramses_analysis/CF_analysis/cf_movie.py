aaacd426f41804f9c5e066e37aaf2cafc5b82dd1
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-08-22 12:59:36 +0200

    making movies consistent with the plots

--- Ramses_analysis/CF_analysis/cf_movie.py
+++ Ramses_analysis/CF_analysis/cf_movie.py
@@ -151,9 +151,9 @@ for time_it in range(start_time_it, end_time_it):
             plt.ylim([0, args.y_limit])
             plt.ylim(bottom=0.0)
             if args.time_spread != None:
-                plt.title("SFE:"+str(np.round(SFE[time_it]*100, decimals=1))+"\% ("+str(int((time_val - Times[0])/1000))+"kyr), Integration window:" + str(args.time_spread) + "yr, N_stars:" + str(N_stars))
+                plt.title("SFE:"+str(np.round(SFE[time_it]*100, decimals=1))+"% ("+str(int((time_val - Times[0])/1000))+"kyr), Integration window:" + str(args.time_spread) + "yr, N_stars:" + str(N_stars))
             else:
-                plt.title("SFE:"+str(np.round(SFE[time_it]*100, decimals=1))+"\% ("+str(int((time_val - Times[0])/1000))+"kyr), Integration window:" + str(args.SFE_spread_val*100) + "\% SFE, Nstars:" + str(N_stars))
+                plt.title("SFE:"+str(np.round(SFE[time_it]*100, decimals=1))+"% ("+str(int((time_val - Times[0])/1000))+"kyr), Integration window:" + str(args.SFE_spread_val*100) + "% SFE, Nstars:" + str(N_stars))
             if size > 1:
                 try:
                     plt.savefig(file_name+'.jpg', format='jpg', bbox_inches='tight')
