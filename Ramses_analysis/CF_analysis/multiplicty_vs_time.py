4a6e564ea28bba747a0c80ee183825fd8bb74429
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-08-23 15:17:38 +0200

    Working on calculating multiplicity vs primary mass

--- Ramses_analysis/CF_analysis/multiplicty_vs_time.py
+++ Ramses_analysis/CF_analysis/multiplicty_vs_time.py
@@ -264,8 +264,8 @@ if rank == 0:
     for SFE_it in range(len(SFE_vals)):
         plt.errorbar(bin_centres, MF_plot[SFE_it], np.array(MF_plot_err[SFE_it]), label='SFE='+str(SFE_vals[SFE_it]), color=colors[SFE_it])
     plt.legend(loc='upper left')
-    plt.xlabel('Log Mass')
-    plt.ylabel('Multiplicity Fraction')
+    #plt.xlabel('Log Mass')
+    #plt.ylabel('Multiplicity Fraction')
     plt.ylim(bottom=0.0)
     plt.tick_params(which='both', direction='in')
     plt.tick_params(axis='both', which='major', labelsize=10, )
