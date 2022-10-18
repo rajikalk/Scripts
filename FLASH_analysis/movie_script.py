8fde4d455e64bb42822c3afe4a0113eb2a9cc074
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2022-10-18 16:58:24 +0200

    Making movie script to avoid amking frames in parallel

--- FLASH_analysis/movie_script.py
+++ FLASH_analysis/movie_script.py
@@ -6,7 +6,7 @@ import sys
 import argparse
 from mpi4py.MPI import COMM_WORLD as CW
 import numpy as np
-import pickle
+import pickle5 as pickle
 import os
 
 #------------------------------------------------------
