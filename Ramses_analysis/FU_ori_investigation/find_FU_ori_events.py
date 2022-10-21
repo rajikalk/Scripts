9101211b9dd31eebb12ed1b0c8e2c50ab0141e6e
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2022-10-21 16:36:59 +0200

    back to finding ways to match FU ori events

--- Ramses_analysis/FU_ori_investigation/find_FU_ori_events.py
+++ Ramses_analysis/FU_ori_investigation/find_FU_ori_events.py
@@ -13,7 +13,6 @@ p=mesaPlot.plot()
 lsun = 3.828e26*1e7 # solar luminosity in erg
 FU_temp = np.concatenate((np.zeros(20), np.ones(80)))
 time_window = 100
-window_linspace = np.linspace(0, time_window, 101)
 
 rank = CW.Get_rank()
 size = CW.Get_size()
@@ -28,36 +27,36 @@ for sink_file in sink_files:
     if rit == size:
         rit = 0
     if rank == rit:
-        try:
-            m.log_fold=sink_file
-            m.loadHistory()
-            mass = m.hist.star_mass
-            age = m.hist.star_age
-            idx = np.where(age <= max_age)
-            age = age[idx]
-            lum = 10.**m.hist.log_L
-            lacc = m.hist.extra_lum / lsun
-            ltot = lum + lacc
-            
-            #have moving window:
-            for time_it in range(len(age)):
-                end_time = age[time_it] + time_window
-                end_it = np.argmin(abs(age - end_time))
-                useable_times = age[time_it:end_it]
-                useable_L = ltot[time_it:end_it]
-                L_diff = np.max(np.log10(useable_L)) - np.min(np.log10(useable_L))
-                scaled_T = useable_times - useable_times[0]
-                scaled_L = useable_L - np.min(useable_L)
-                scaled_L = scaled_L/np.max(scaled_L)
-                cor = np.correlate(scaled_L,FU_temp,'same')
-                if np.max(cor)>0.9 and L_diff>1:
-                    plt.clf()
-                    plt.plot(useable_times, scaled_L, label="scaled Luminosity")
-                    plt.plot(useable_times, cor, label="correlation")
-                    plt.xlabel('Time (yr)')
-                    plt.ylabel('scaled L and correlation')
-                    plt.legend()
-                    plt.savefig('Sink_' + sink_file.split('sink_')[-1].split('/')[0] + '_time_'+str(age[time_it])+'png')
-                    print("Found potential match for sink", sink_file.split('sink_')[-1].split('/')[0], "at age", age[time_it])
-        except:
-            print("can't read file")
+        #try:
+        m.log_fold=sink_file
+        m.loadHistory()
+        mass = m.hist.star_mass
+        age = m.hist.star_age
+        idx = np.where(age <= max_age)
+        age = age[idx]
+        lum = 10.**m.hist.log_L
+        lacc = m.hist.extra_lum / lsun
+        ltot = lum + lacc
+        
+        #have moving window:
+        for time_it in range(len(age)):
+            end_time = age[time_it] + time_window
+            end_it = np.argmin(abs(age - end_time))
+            useable_times = age[time_it:end_it]
+            useable_L = ltot[time_it:end_it]
+            L_diff = np.max(np.log10(useable_L)) - np.min(np.log10(useable_L))
+            scaled_T = useable_times - useable_times[0]
+            scaled_L = useable_L - np.min(useable_L)
+            scaled_L = scaled_L/np.max(scaled_L)
+            cor = np.correlate(scaled_L,FU_temp,'same')
+            if np.max(cor)>0.8 and L_diff>1:
+                plt.clf()
+                plt.plot(useable_times, scaled_L, label="scaled Luminosity")
+                plt.plot(useable_times, cor, label="correlation")
+                plt.xlabel('Time (yr)')
+                plt.ylabel('scaled L and correlation')
+                plt.legend()
+                plt.savefig('Sink_' + sink_file.split('sink_')[-1].split('/')[0] + '_time_'+str(age[time_it])+'png')
+                print("Found potential match for sink", sink_file.split('sink_')[-1].split('/')[0], "at age", age[time_it])
+        #except:
+        #    print("can't read file")
