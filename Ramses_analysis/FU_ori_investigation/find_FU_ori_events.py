c3fe3f2b17ba2956ae676e0593e5deba54d61187
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2022-10-21 16:59:43 +0200

    back to finding ways to match FU ori events

--- Ramses_analysis/FU_ori_investigation/find_FU_ori_events.py
+++ Ramses_analysis/FU_ori_investigation/find_FU_ori_events.py
@@ -27,45 +27,43 @@ for sink_file in sink_files:
     if rit == size:
         rit = 0
     if rank == rit:
-        try:
-            m.log_fold=sink_file
-            m.loadHistory()
-            mass = m.hist.star_mass
-            age = m.hist.star_age
-            idx = np.where(age <= max_age)
-            age = age[idx]
-            lum = 10.**m.hist.log_L
-            lacc = m.hist.extra_lum / lsun
-            ltot = lum + lacc
-            L_diff_arr = []
-            
-            #have moving window:
-            for time_it in range(len(age)):
-                end_time = age[time_it] + time_window
-                end_it = np.argmin(abs(age - end_time))
-                useable_times = age[time_it:end_it]
-                useable_L = ltot[time_it:end_it]
-                if len(useable_L) > 0:
-                    L_diff = np.log10(useable_L[-1]) - np.log10(useable_L[0])
-                    L_diff_arr.append(L_diff)
-                    #L_diff = np.max(np.log10(useable_L)) - np.min(np.log10(useable_L))
-                    scaled_T = useable_times - useable_times[0]
-                    scaled_L = useable_L - np.min(useable_L)
-                    scaled_L = scaled_L/np.max(scaled_L)
-                    cor = np.correlate(scaled_L,FU_temp,'same')
-                    if L_diff>1: # and np.max(cor)>0.5
-                        plt.clf()
-                        plt.plot(useable_times, scaled_L, label="scaled Luminosity")
-                        plt.plot(useable_times, cor, label="correlation")
-                        plt.xlabel('Time (yr)')
-                        plt.ylabel('scaled L and correlation')
-                        plt.legend()
-                        plt.savefig('Sink_' + sink_file.split('sink_')[-1].split('/')[0] + '_time_'+str(age[time_it])+'png')
-                        print("Found potential match for sink", sink_file.split('sink_')[-1].split('/')[0], "at age", age[time_it])
-            plt.clf()
-            plt.plot(age, L_diff_arr)
-            plt.xlabel('age (yr)')
-            plt.ylabel('max L diff over 100 yr (log)')
-            plt.savefig('L_diff_Sink_'+sink_file.split('sink_')[-1].split('/')[0]+'.png')
-        except:
-            print("can't read file")
+        m.log_fold=sink_file
+        m.loadHistory()
+        mass = m.hist.star_mass
+        age = m.hist.star_age
+        idx = np.where(age <= max_age)
+        age = age[idx]
+        lum = 10.**m.hist.log_L
+        lacc = m.hist.extra_lum / lsun
+        ltot = lum + lacc
+        L_diff_arr = []
+        
+        #have moving window:
+        for time_it in range(len(age)):
+            end_time = age[time_it] + time_window
+            end_it = np.argmin(abs(age - end_time))
+            useable_times = age[time_it:end_it]
+            useable_L = ltot[time_it:end_it]
+            if len(useable_L) > 0:
+                L_diff = np.log10(useable_L[-1]) - np.log10(useable_L[0])
+                L_diff_arr.append(L_diff)
+                #L_diff = np.max(np.log10(useable_L)) - np.min(np.log10(useable_L))
+                scaled_T = useable_times - useable_times[0]
+                scaled_L = useable_L - np.min(useable_L)
+                scaled_L = scaled_L/np.max(scaled_L)
+                cor = np.correlate(scaled_L,FU_temp,'same')
+                if L_diff>1: # and np.max(cor)>0.5
+                    plt.clf()
+                    plt.plot(useable_times, scaled_L, label="scaled Luminosity")
+                    plt.plot(useable_times, cor, label="correlation")
+                    plt.xlabel('Time (yr)')
+                    plt.ylabel('scaled L and correlation')
+                    plt.legend()
+                    plt.savefig('Sink_' + sink_file.split('sink_')[-1].split('/')[0] + '_time_'+str(age[time_it])+'png')
+                    print("Found potential match for sink", sink_file.split('sink_')[-1].split('/')[0], "at age", age[time_it])
+        plt.clf()
+        plt.plot(age, L_diff_arr)
+        plt.xlabel('age (yr)')
+        plt.ylabel('max L diff over 100 yr (log)')
+        plt.savefig('L_diff_Sink_'+sink_file.split('sink_')[-1].split('/')[0]+'.png')
+        print("plotted L dif history for sink", sink_file.split('sink_')[-1].split('/')[0])
