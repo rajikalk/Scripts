c36c4d576bdc580f837be955cd083e69deb02199
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2022-10-24 13:10:00 +0200

    adding yt fields for FLASH

---
+++ Modules/my_flash_fields.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#Created by Rajika Kuruwita, 2019
+import yt
+yt.enable_parallelism()
+
+def _Neg_z(field, data):
+    """
+    returns the negative of the z-positions
+    """
+    return -1*data['z']
+
+yt.add_field("Neg_z", function=_Neg_z, units=r"cm")
+    
+def _Neg_dz(field, data):
+    """
+    returns the negative of the dz
+    """
+    return -1*data['dz']
+    
+yt.add_field("Neg_dz", function=_Neg_z, units=r"cm")
