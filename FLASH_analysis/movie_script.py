3de1038366b54f2846337d4bcef6a6ee64f19c9a
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2022-10-24 15:56:12 +0200

    seems to be writing empy image arrays again

--- FLASH_analysis/movie_script.py
+++ FLASH_analysis/movie_script.py
@@ -125,9 +125,14 @@ if args.make_movie_pickles == 'True':
             sys.stdout.flush()
             CW.Barrier()
             
-            if rank == proj_root_rank or size == 1:
+            if rank == proj_root_rank and size > 1:
                 for key, vals in sorted(my_storage.items()):
                     proj_dict[key] = vals
+                file = open(pickle_file, 'wb')
+                pickle.dump((X_image, Y_image, proj_dict['dens'], proj_dict['magx'], proj_dict['magy'], X_image_vel, Y_image_vel, proj_dict['velx'], proj_dict['vely'], part_info, time_val), file)
+                file.close()
+                print("created pickle for frame", file_counter)
+            elif size == 1:
                 file = open(pickle_file, 'wb')
                 pickle.dump((X_image, Y_image, proj_dict['dens'], proj_dict['magx'], proj_dict['magy'], X_image_vel, Y_image_vel, proj_dict['velx'], proj_dict['vely'], part_info, time_val), file)
                 file.close()
