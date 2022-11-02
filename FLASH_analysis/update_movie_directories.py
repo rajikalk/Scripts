7b9cd5d3983e881cd0a7799bed7eed8842cc60b4
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-11-02 09:15:17 +0100

    fixing parallelisation

--- FLASH_analysis/update_movie_directories.py
+++ FLASH_analysis/update_movie_directories.py
@@ -39,7 +39,7 @@ for sim_dir in sim_dirs:
         proj_dirs = ['/XY/', '/XZ/']
         for proj_dir in proj_dirs:
             save_dir = movie_dir + proj_dir
-            if os.path.exists(save_dir) == False:
+            if os.path.exists(save_dir) == False and rank == 0:
                 os.makedirs(save_dir)
                 
             if clean_pickles and rank == 0:
