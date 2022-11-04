7eb035b48180f10c9df64b45c25edd74a37dbc6f
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-11-04 17:08:21 +0100

    Calculated angular momentums. Now to save time adn test parallelisation

--- FLASH_analysis/angular_momentum_evolution.py
+++ FLASH_analysis/angular_momentum_evolution.py
@@ -52,13 +52,18 @@ start_file = mym.find_files([0], files)[0]
 files = files[files.index(start_file):]
 
 L_dict = {}
+Time_array = []
 L_primary = []
 L_secondary = []
 L_orbit = []
-L_gas = []
+L_in_gas = []
 for fn in yt.parallel_objects(files, njobs=size, storage=L_dict):
     part_file = 'part'.join(fn[-1].split('plt_cnt'))
     ds = yt.load(fn[-1], particle_filename=part_file)
+    
+    import pdb
+    pdb.set_trace()
+    #SAVE TIME INTO TIME ARRAY
 
     #Calculate CoM
     dd = ds.all_data()
@@ -69,11 +74,32 @@ for fn in yt.parallel_objects(files, njobs=size, storage=L_dict):
     dx = dd['particle_posx'].in_units('cm') - dd['CoM'][0]
     dy = dd['particle_posy'].in_units('cm') - dd['CoM'][1]
     dz = dd['particle_posz'].in_units('cm') - dd['CoM'][2]
+    d_pos = yt.YTArray([dx, dy, dz]).T
     
     dvx = dd['particle_velx'].in_units('cm/s') - dd['CoM_Velocity'][0]
     dvy = dd['particle_vely'].in_units('cm/s') - dd['CoM_Velocity'][1]
     dvz = dd['particle_velz'].in_units('cm/s') - dd['CoM_Velocity'][2]
+    d_vel = yt.YTArray([dvx, dvy, dvz]).T
     
-    import pdb
-    pdb.set_trace()
+    L_orb = dd['particle_mass'].value * np.cross(d_vel, d_pos).T
+    L_orb_tot = yt.YTQuantity(np.sum(np.sqrt(np.sum(L_orb**2, axis=0))), 'g*cm**2/s')
+    
+    #Calculate angular momentum in gas
+    dx_gas = dd['x'] - dd['CoM'][0]
+    dy_gas = dd['y'] - dd['CoM'][1]
+    dz_gas = dd['z'] - dd['CoM'][2]
+    d_pos_gas = yt.YTArray([dx_gas, dy_gas, dz_gas]).T
+    
+    dvx_gas = dd['velx'].in_units('cm/s') - dd['CoM_Velocity'][0]
+    dvy_gas = dd['vely'].in_units('cm/s') - dd['CoM_Velocity'][1]
+    dvz_gas = dd['velz'].in_units('cm/s') - dd['CoM_Velocity'][2]
+    d_vel_gas = yt.YTArray([dvx_gas, dvy_gas, dvz_gas]).T
+    
+    L_gas = dd['mass'].value * np.cross(d_vel_gas, d_pos_gas).T
+    L_gas_tot = yt.YTQuantity(np.sum(np.sqrt(np.sum(L_gas**2, axis=0))), 'g*cm**2/s')
+    
+    L_primary.append(particle_spin[0])
+    L_secondary.append(particle_spin[1])
+    L_orbit.append(L_orb_tot)
+    L_in_gas.append(L_gas_tot)
     
