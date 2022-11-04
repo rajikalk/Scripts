b7a93a1f0b0098740962d72e6bcc4d76c70a1eea
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-11-04 15:40:05 +0100

    fixed CoM calcualtions

--- FLASH_analysis/angular_momentum_evolution.py
+++ FLASH_analysis/angular_momentum_evolution.py
@@ -50,7 +50,7 @@ files = sorted(glob.glob(input_dir + '*plt_cnt*'))
 part_file = 'part'.join(files[-1].split('plt_cnt'))
 ds = yt.load(files[-1], particle_filename=part_file)
 dd = ds.all_data()
-sink_form_time = np.min(dd['particle_creation_time']).in_units('yr')
+sink_form_time = yt.YTQuantity(np.min(dd['particle_creation_time'])value, 's')
 
 L_dict = {}
 L_primary = []
@@ -60,7 +60,7 @@ L_gas = []
 for fn in yt.parallel_objects(files, njobs=size, storage=L_dict):
     part_file = 'part'.join(fn[-1].split('plt_cnt'))
     ds = yt.load(fn[-1], particle_filename=part_file)
-    if ds.current_time.in_units('yr') >= sink_form_time:
+    if ds.current_time.in_units('yr') >= sink_form_time.in_units('yr'):
         #Calculate CoM
         dd = ds.all_data()
         CoM = dd['CoM']
