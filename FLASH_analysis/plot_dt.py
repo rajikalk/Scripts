8f2b4165abb3965b3a9cd427cdc2b3cd77035f11
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-11-10 09:50:29 +0100

    testing dt plotting

--- FLASH_analysis/plot_dt.py
+++ FLASH_analysis/plot_dt.py
@@ -16,17 +16,18 @@ with open(shell_out_file, 'r') as f:
     found_start = False
     line_counter = 0
     for row in reader:
-        if row[0][:7] == '       ' and row[0][7] != 'n':
-            import pdb
-            pdb.set_trace()
-            curr_step = int(row_list[0])
-            curr_dt = float(row_list[2])
-            step_number.append(curr_step)
-            dt.append(curr_dt)
-        if '*** Wrote checkpoint file to SPIN_hdf5_chk_' in row:
-            curr_chk_number = int(row.split('*** Wrote checkpoint file to SPIN_hdf5_chk_')[-1].split('****')[0])
-            step_chk.append(curr_step)
-            chk_number.append(curr_chk_number)
+        if len(row) > 0:
+            if row[0][:7] == '       ' and row[0][7] != 'n':
+                import pdb
+                pdb.set_trace()
+                curr_step = int(row_list[0])
+                curr_dt = float(row_list[2])
+                step_number.append(curr_step)
+                dt.append(curr_dt)
+            if '*** Wrote checkpoint file to SPIN_hdf5_chk_' in row:
+                curr_chk_number = int(row.split('*** Wrote checkpoint file to SPIN_hdf5_chk_')[-1].split('****')[0])
+                step_chk.append(curr_step)
+                chk_number.append(curr_chk_number)
 
 import matplotlib.pyplot as plt
 
