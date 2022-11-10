330db60e221fd64dafa4a5dc895b6fef130af5fa
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-11-10 09:19:47 +0100

    make diagnostic script to plot dt evolution in simulations

---
+++ FLASH_analysis/plot_dt.py
@@ -0,0 +1,36 @@
+import csv
+import sys
+import numpy as np
+import pickle
+
+shell_out_file = sys.argv[1]
+
+step_number = []
+dt = []
+step_chk = []
+chk_number = []
+
+with open(shell_out_file, 'r') as f:
+    reader = csv.reader(f)
+    line_counter = 0
+    found_start = False
+    for row in reader:
+        if row[:7] == '       ' and row[7] != 'n':
+            curr_step = int(row_list[0])
+            curr_dt = float(row_list[2])
+            step_number.append(curr_step)
+            dt.append(curr_dt)
+        if '*** Wrote checkpoint file to SPIN_hdf5_chk_' in row:
+            curr_chk_number = int(row.split('*** Wrote checkpoint file to SPIN_hdf5_chk_')[-1].split('****')[0])
+            step_chk.append(curr_step)
+            chk_number.append(curr_chk_number)
+
+import matplotlib.pyplot as plt
+
+plt.semilogy(step_number, dt)
+for chk_it in range(len(step_chk)):
+    plt.axvline(step_chk[chk_it])
+    plt.text(step_chk[chk_it]+1, np.mean(dt), str(chk_number[chk]), rotation=90)
+plt.xlabel('step number')
+plt.ylabel('dt (s)')
+plt.savefig(shell_out_file+'.png')
