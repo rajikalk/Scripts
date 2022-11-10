51fc36e864ea1a9a5dffa9d4ee32b0385c35c961
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro.local>
2022-11-10 10:13:22 +0100

    testing dt plotting

--- FLASH_analysis/plot_dt.py
+++ FLASH_analysis/plot_dt.py
@@ -19,9 +19,11 @@ with open(shell_out_file, 'r') as f:
         line_counter = line_counter + 1
         if len(row) > 0:
             if found_start == False and len(step_number) > 0:
-                curr_step = curr_step + 1
-                import pdb
-                pdb.set_trace()
+                values = list(filter(None, row[0].split(' ')))
+                if values[0] == next_step:
+                    step_number.append(next_step)
+                    dt.append(float(values[2]))
+                    next_step =  next_step + 1
             if found_start:
                 values = list(filter(None, row[0].split(' ')))
                 curr_step = int(values[0])
@@ -29,6 +31,7 @@ with open(shell_out_file, 'r') as f:
                 step_number.append(curr_step)
                 dt.append(curr_dt)
                 found_start = False
+                next_step = curr_step + 1
             if row[0][:7] == '       ' and row[0][7] == 'n':
                 found_start = True
             if '*** Wrote checkpoint file to SPIN_hdf5_chk_' in row[0]:
