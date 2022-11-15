a8c710ff3e36e30a03b8f2f58cc39c9ff10155ef
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2022-11-15 12:23:15 +0100

    looking for massive triple candidates for Abby Frost

--- Ramses_analysis/Massive_triples/Find_candidates.py
+++ Ramses_analysis/Massive_triples/Find_candidates.py
@@ -67,7 +67,7 @@ size = CW.Get_size()
 #Set units
 units_override = {"length_unit":(4.0,"pc"), "velocity_unit":(0.18, "km/s"), "time_unit":(685706129102738.9, "s")}
 
-simulation_density_id = args.global_data_pickle_file.split('/')[-1].split('.')[0]
+simulation_density_id = args.global_data_pickle_file.split('/G')[-1].split('.')[0]
 
 if simulation_density_id == '50':
     Grho=50
