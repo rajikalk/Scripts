ddfadfc77e07920e176052a3726d0ad4d30d55ac
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2022-11-21 11:32:58 +0100

    fixing annotation in movie script

--- Modules/my_ramses_module.py
+++ Modules/my_ramses_module.py
@@ -408,7 +408,7 @@ def my_own_quiver_function(axis, X_pos, Y_pos, X_val, Y_val, plot_velocity_legen
         xvel = len_scale*(standard_vel/standard_vel)
         yvel = 0.0
         width_val = width_ceil
-        annotate_text = axis.text((xmax - 0.01*(xmax-xmin)), (ymin + 0.03*(ymax-ymin)), legend_text, va="center", ha="right", color='w', fontsize=fontsize_global)
+        annotate_text = axis.text((xmax - 0.005*(xmax-xmin)), (ymin + 0.015*(ymax-ymin)), legend_text, va="center", ha="right", color='w', fontsize=fontsize_global)
         annotate_text.set_path_effects([path_effects.Stroke(linewidth=3, foreground='black'), path_effects.Normal()])
         axis.add_patch(mpatches.FancyArrowPatch((pos_start[0], pos_start[1]), (pos_start[0]+xvel, pos_start[1]+yvel), arrowstyle='->', color='w', linewidth=width_val, mutation_scale=10.*width_val, alpha=width_val/width_ceil))
     return axis
