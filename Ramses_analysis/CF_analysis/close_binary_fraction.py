27e0743e340ddbc7028afd6fafb2edcffdd1ea07
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2022-12-14 16:19:59 +0100

    goig to try adn measure the close binary fraction over time

--- Ramses_analysis/CF_analysis/close_binary_fraction.py
+++ Ramses_analysis/CF_analysis/close_binary_fraction.py
@@ -1,48 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pyramses as pr
+from pyramses import rsink
+import multiplicity as m
+import yt
 import glob
+from mpi4py.MPI import COMM_WORLD as CW
+import sys
+import collections
 import os
 import pickle
-import matplotlib.patches
-import collections
-import matplotlib
-import matplotlib.ticker
-#from mpi4py.MPI import COMM_WORLD as CW
+import gc
 
-matplotlib.rcParams['mathtext.fontset'] = 'stixsans'
-matplotlib.rcParams['mathtext.it'] = 'Arial:italic'
-matplotlib.rcParams['mathtext.rm'] = 'Arial'
-matplotlib.rcParams['mathtext.bf'] = 'Arial:bold'
-matplotlib.rcParams['mathtext.it'] = 'Arial:italic'
-matplotlib.rcParams['mathtext.rm'] = 'Arial'
-matplotlib.rcParams['mathtext.sf'] = 'Arial'
-matplotlib.rcParams['mathtext.default'] = 'regular'
-matplotlib.rcParams['font.sans-serif'] = 'Arial'
-matplotlib.rcParams['font.family'] = 'sans-serif'
-matplotlib.rcParams['text.latex.preamble'] = [
-       r'\usepackage{siunitx}',   # i need upright \micro symbols, but you need...
-       r'\sisetup{detect-all}',   # ...this to force siunitx to actually use your fonts
-       r'\usepackage{helvet}',    # set the normal font here
-       r'\usepackage{sansmath}',  # load up the sansmath so that math -> helvet
-       r'\sansmath'               # <- tricky! -- gotta actually tell tex to use!
-]
+f_acc= 0.5
 
-def parse_inputs():
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-lifetime_thres", "--sys_lifetime_threshold", help="What lifetime threshold do you want to define a stable system", type=float, default=100000.)
-    parser.add_argument("-ts", "--timescale", help="Do you want to plot in terms of the of the actual simulation time, or the the time since the formation of the first sink?", type=str, default="first_sink")
-    parser.add_argument("-fig_suffix", "--figure_suffix", help="Do you want add a suffix to the figures?", type=str, default="")
-    parser.add_argument("-add_hist", "--add_histograms", help="Do you want to add the histograms at the end of the super plots?", type=str, default="False")
-    parser.add_argument("-all_sep_evol", "--plot_all_separation_evolution", help="do you want to plot all separation evolution?", type=str, default="True")
-    parser.add_argument("-x_field", "--x_field", help="Default for x-axis in the multiplot is time", type=str, default="Time")
-    parser.add_argument("-tf", "--text_font", help="what font do you want the text to have?", type=int, default=10)
-    parser.add_argument("-plt_key", "--plot_key", help="What dictionary key from superplot_dict do you want to plot?", type=str, default='System_seps')
-    parser.add_argument("-smooth", "--smooth_bool", help="Do you want to smooth what you are plotting?", type=str, default='False')
-    parser.add_argument("-smooth_window", "--smooth_window_val", help="What big (in yrs) do you want the smoothing window to be?", type=float, default=1000)
-    parser.add_argument("files", nargs='*')
-    args = parser.parse_args()
-    return args
+def losi(i, res):
+    if (res['n'][i]==1) or (res['n'][i]==0):
+        return i
+    else:
+        i1 = losi(res['index1'][i],res)
+        i2 = losi(res['index2'][i],res)
+        return [i1,i2]
 
 def flatten(x):
     if isinstance(x, collections.Iterable):
@@ -50,79 +28,225 @@ def flatten(x):
     else:
         return [x]
 
-#rank = CW.Get_rank()
-#size = CW.Get_size()
+def accretion(sink_inds, time_ind):
+    """
+    Calculates the accretion of the given indeexes
+    """
+    global Accretion_array
+    M_dot = Accretion_array[time_ind, sink_inds]
+    return M_dot
+    
+def luminosity(global_data, sink_inds, global_ind):
+    """
+    Calculates the luminosity of the given indexes
+    """
+    global f_acc
+    radius = yt.YTQuantity(2.0, 'rsun')
+    M_dot = accretion(sink_inds, global_ind)
+    M = yt.YTArray(global_data['m'][global_ind,sink_inds]*units['mass_unit'].in_units('msun'), 'Msun')
+    L_acc = f_acc * (yt.units.G * M.in_units('g') * M_dot.in_units('g/s'))/radius.in_units('cm')
+    L_tot = L_acc.in_units('Lsun')
+    return L_tot
 
-two_col_width = 7.20472 #inches
-single_col_width = 3.50394 #inches
-page_height = 10.62472 #inches
-font_size = 10
-plot_booleans = [[False, True], [False, False], [True, True], [True, False]]
+def parse_inputs():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-global_data", "--global_data_pickle_file", help="Where is the directory of the global pickle data?", default='/groups/astro/rlk/Analysis_plots/Ramses/Global/G100/512/stars_red_512.pkl', type=str)
+    parser.add_argument("-pickle", "--pickled_file", help="Define if you want to read this instead", type=str)
+    parser.add_argument("-update", "--update_pickles", help="Do you want to remake the pickles?", type=str, default='True')
+    parser.add_argument("-acc_lim", "--accretion_limit", help="What do you want to set the accretion limit to?", type=float, default=1.e-7)
+    parser.add_argument("-upper_L", "--upper_L_limit", help="What is the upper Luminosity limit?", type=float, default=55.29)
+    parser.add_argument("-lower_L", "--lower_L_limit", help="What is the upper Luminosity limit?", type=float, default=0.09)
+    parser.add_argument("-bound", "--bound_check", help="Do you actually want to analyse bound systems?", type=str, default='True')
+    parser.add_argument("-max_iter", "--max_iterations", help="How many iterations for multiplicity analysis", type=int, default=20)
+    parser.add_argument("-start_time_it", "--start_time_index", help="What time index do you want to start at? mostly for diagnostic reasons.", type=int, default=0)
+    parser.add_argument("-lifetime_thres", "--sys_lifetime_threshold", help="What lifeimteimte threshold do you want to define a stable system", type=float, default=100000.)
+    parser.add_argument("-plot_super", "--plot_superplot", help="do you want to plot the superplot?", type=str, default='True')
+    parser.add_argument("-use_com_sep", "--use_separation_of_the_coms", help="Do you want to same the separation as the separation of the center of masses instead of relative to a particular sink?", type=str, default='True')
+    parser.add_argument("files", nargs='*')
+    args = parser.parse_args()
+    return args
 
 args = parse_inputs()
 
-#Server pickles
-pickle_files = ["/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G50/Max_iter_100/means_superplot.pkl", "/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G100/Max_iter_100/means_superplot.pkl", "/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G125/Max_iter_100/means_superplot.pkl", "/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G150/Max_iter_100/means_superplot.pkl", "/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G200/Max_iter_100/means_superplot.pkl", "/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G400/Max_iter_100/means_superplot.pkl"]
-
-birth_con_pickles = ["/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G50/Full_sink_data/Fast_analysis/sink_birth_all_delayed_core_frag_cleaned.pkl", "/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G100/Full_sink_data/Fast_analysis/sink_birth_all_delayed_core_frag_cleaned.pkl", "/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G125/Full_sink_data/Fast_analysis/sink_birth_all_delayed_core_frag_cleaned.pkl", "/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G150/Full_sink_data/Fast_analysis/sink_birth_all_delayed_core_frag_cleaned.pkl", "/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G200/Full_sink_data/Fast_analysis/sink_birth_all_delayed_core_frag_cleaned.pkl", "/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G400/Full_sink_data/Fast_analysis/sink_birth_all_delayed_core_frag_cleaned.pkl"]#"/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G400/Full_sink_data/Fast_analysis/sink_birth_all_delayed_core_frag_cleaned.pkl"]
-
-low_cadence_birth_con_pickles = ["/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G50/Low_Cadence_birth_con/sink_birth_all_delayed_core_frag_cleaned.pkl", "/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G100/Low_Cadence_birth_con/sink_birth_all_delayed_core_frag_cleaned.pkl", "/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G125/Low_Cadence_birth_con/sink_birth_all_delayed_core_frag_cleaned.pkl", "/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G150/Low_Cadence_birth_con/sink_birth_all_delayed_core_frag_cleaned.pkl", "/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G200/Low_Cadence_birth_con/sink_birth_all_delayed_core_frag_cleaned.pkl", "/groups/astro/rlk/rlk/Analysis_plots/Superplot_pickles_entire_sim/G400/Low_Cadence_birth_con/sink_birth_all_delayed_core_frag_cleaned.pkl"]
-#Local pickles
+rank = CW.Get_rank()
+size = CW.Get_size()
 
 
-#if plot_booleans[rank][1] == True:
-#    args.figure_suffix = args.figure_suffix + '_thres'
+#Set units
+units_override = {"length_unit":(4.0,"pc"), "velocity_unit":(0.18, "km/s"), "time_unit":(685706129102738.9, "s")}
 
-subplot_titles = ["1500M$_\odot$", "3000M$_\odot$", "3750M$_\odot$", "4500M$_\odot$", "6000M$_\odot$", "12000M$_\odot$"]
-GMC_mass_arr = [1500, 3000, 3750, 4500, 6000, 12000]
-S_bins = np.logspace(1,4,13)
-CF_hist = np.zeros((len(pickle_files),12)).tolist()
-Times_all = []
-M_tot_all = []
-M_tot_multi_all = []
-N_stars_all = []
-N_multi_stars_all = []
-SFE_all = []
-t_max = np.nan
-G50_t_max = 0
+simulation_density_id = args.global_data_pickle_file.split('/')[-1].split('_')[0][1:]
+try:
+    simulation_density_id_int = int(simulation_density_id)
+except:
+    simulation_density_id = args.global_data_pickle_file.split('_G')[-1].split('.pkl')[0]
 
-#================================================================================================
-#Plot simulation SFE eovlution
-plot_truncated_super_mult = True
-if plot_truncated_super_mult == True:
-    plt.clf()
-    fig, axs = plt.subplots(ncols=1, nrows=len(pickle_files), figsize=(two_col_width, page_height), sharex=True)#, sharey=True)
-    iter_range = range(0, len(pickle_files))
-    plt.subplots_adjust(wspace=0.0)
-    plt.subplots_adjust(hspace=0.02)
+if simulation_density_id == '50':
+    Grho=50
+    units_override.update({"mass_unit":(1500,"Msun")})
+elif simulation_density_id == '100':
+    Grho=100
+    units_override.update({"mass_unit":(3000,"Msun")})
+elif simulation_density_id == '125':
+    Grho=125
+    units_override.update({"mass_unit":(3750,"Msun")})
+elif simulation_density_id == '150':
+    Grho=150
+    units_override.update({"mass_unit":(4500,"Msun")})
+elif simulation_density_id == '200':
+    Grho=200
+    units_override.update({"mass_unit":(6000,"Msun")})
+elif simulation_density_id == '400':
+    Grho=400
+    units_override.update({"mass_unit":(12000,"Msun")})
+else:
+    print("MASS UNIT NOT SET")
+    import pdb
+    pdb.set_trace()
+    
 
-    for pick_it in iter_range:
-        file_it = pick_it
-        file = open(pickle_files[file_it], 'rb')
-        superplot_dict, Sink_bound_birth, Sink_formation_times, means_dict, Lifetimes_sys, Sep_maxs, Sep_mins, Initial_Seps, Final_seps = pickle.load(file)
-        file.close()
-        
-        Start_times = []
-        Sort_keys = []
-        for time_key in superplot_dict['System_times'].keys():
-            Start_times.append(superplot_dict['System_times'][time_key][0])
-            Sort_keys.append(time_key)
-        if np.min(np.array(Start_times)[1:] - np.array(Start_times)[:-1]) < 0:
-            import pdb
-            pdb.set_trace()
+units_override.update({"density_unit":(units_override['mass_unit'][0]/(units_override['length_unit'][0]**3), "Msun/pc**3")})
+    
+scale_l = yt.YTQuantity(units_override['length_unit'][0], units_override['length_unit'][1]).in_units('cm') # 4 pc
+scale_v = yt.YTQuantity(units_override['velocity_unit'][0], units_override['velocity_unit'][1]).in_units('cm/s')         # 0.18 km/s == sound speed
+scale_t = scale_l/scale_v # 4 pc / 0.18 km/s
+scale_d = yt.YTQuantity(units_override['density_unit'][0], units_override['density_unit'][1]).in_units('g/cm**3')  # 2998 Msun / (4 pc)^3
 
-        SFE_5_ind = np.argmin(abs(np.array(superplot_dict['SFE'])-0.05))
-        SFE_5_time = superplot_dict['Times'][SFE_5_ind]
-        if pick_it == 0:
-            if args.x_field == 'Time':
-                G50_t_max = superplot_dict['Times'][SFE_5_ind] - superplot_dict['System_times'][list(superplot_dict['System_times'].keys())[0]][0]
-            elif args.x_field == 'SFE':
-                G50_t_max = 0.05
-        new_sys_id = superplot_dict['N_stars'][-1]
+units={}
+for key in units_override.keys():
+    units.update({key:yt.YTQuantity(units_override[key][0], units_override[key][1])})
+del units_override
+gc.collect()
 
-        sim_start_time = np.nan
-        plotted_sinks = []
-        sub_sys_dict = {}
-        for time_key in superplot_dict['System_times'].keys():
-            import pdb
-            pdb.set_trace()
+sys.stdout.flush()
+CW.Barrier()
+
+#Define arrays of quantities to be saved.
+Times = []
+SFE = []
+SFE_n = []
+M_tot = []
+M_tot_vis = []
+M_tot_multi = []
+N_stars = []
+N_vis_stars = []
+N_multi_stars = []
+Single_star_inds = []
+Sink_bound_birth = []
+System_seps = {}
+System_midpoint_seps = {}
+System_semimajor = {}
+System_times = {}
+System_ecc = {}
+System_energies = {}
+
+sys.stdout.flush()
+CW.Barrier()
+
+#loading global data
+file_open = open(args.global_data_pickle_file, 'rb')
+try:
+    global_data = pickle.load(file_open,encoding="latin1")
+except:
+    file_open.close()
+    import pickle5 as pickle
+    file_open = open(args.global_data_pickle_file, 'rb')
+    global_data = pickle.load(file_open,encoding="latin1")
+file_open.close()
+dm = global_data['dm']*units['mass_unit'].in_units('Msun')
+dt = (global_data['time'] - global_data['tflush'])*units['time_unit'].in_units('yr')
+Accretion_array = dm/dt
+print('loaded global data')
+
+sys.stdout.flush()
+CW.Barrier()
+
+#Get birth conditions
+file_open = open(args.global_data_pickle_file, 'rb')
+#Sink_bound_birth.append([born_bound, most_bound_sink_id, rel_sep])
+
+sys.stdout.flush()
+CW.Barrier()
+
+if args.bound_check == 'True':
+    bound_check = True
+else:
+    bound_check = False
+
+sys.stdout.flush()
+CW.Barrier()
+
+#Find sink formation times
+Sink_formation_times = []
+time_it = 0
+sink_it = 0
+while time_it < np.shape(global_data['m'])[0]:
+     while sink_it < np.shape(global_data['m'])[1]:
+         if global_data['m'][time_it][sink_it]>0:
+             Sink_formation_times.append(global_data['time'][time_it][sink_it])
+             sink_it = sink_it + 1
+         elif global_data['m'][time_it][sink_it] == 0:
+             time_it = time_it + 1
+             break
+     if sink_it == np.shape(global_data['m'])[1]:
+         break
+Sink_formation_times = (Sink_formation_times*scale_t).in_units('yr')
+
+sys.stdout.flush()
+CW.Barrier()
+
+#Define pickle name
+pickle_file = args.pickled_file
+            
+sys.stdout.flush()
+CW.Barrier()
+
+start_time_it = args.start_time_index
+
+if args.update_pickles == 'True':
+    rit = -1
+    prev_n_stars = 1
+    for time_it in range(start_time_it, len(global_data['time'].T[0]), 100):
+        rit = rit + 1
+        if rit == size:
+            rit = 0
+        if rank == rit:
+            #preamble
+            n_stars = np.where(global_data['m'][time_it]>0)[0]
+            if len(n_stars) > 1:
+                abspos = np.array([global_data['x'][time_it][n_stars], global_data['y'][time_it][n_stars], global_data['z'][time_it][n_stars]]).T#*scale_l
+                absvel = np.array([global_data['ux'][time_it][n_stars], global_data['uy'][time_it][n_stars], global_data['uz'][time_it][n_stars]]).T#*scale_v
+                mass = np.array(global_data['m'][time_it][n_stars])
+                time = global_data['time'][time_it][n_stars][0]
+                time_yr = yt.YTQuantity(scale_t*time, 's').in_units('yr').value
+                Times.append(time_yr)
+                
+                S = pr.Sink()
+                S._jet_factor = 1.
+                S._scale_l = scale_l.value
+                S._scale_v = scale_v.value
+                S._scale_t = scale_t.value
+                S._scale_d = scale_d.value
+                S._time = yt.YTArray(time, '')
+                S._abspos = yt.YTArray(abspos, '')
+                S._absvel = yt.YTArray(absvel, '')
+                S._mass = yt.YTArray(mass, '')
+                
+                L_tot = luminosity(global_data, n_stars, time_it)
+                M_dot = accretion(n_stars, time_it)
+                vis_inds = np.where((L_tot>=args.lower_L_limit)&(M_dot>=args.accretion_limit)&(L_tot<=args.upper_L_limit))[0]
+                real_vis = np.where((L_tot>=0.07)&(M_dot>=1.e-7)&(L_tot<=55.29))[0]#Remember to update if you adjust criteria
+                N_vis_stars.append(len(real_vis))
+                
+                N_stars.append(len(n_stars))
+                M_tot_msun = np.sum(mass*units['mass_unit'].in_units('Msun'))
+                M_tot.append(M_tot_msun)
+                SFE_val = M_tot_msun/units['mass_unit'].in_units('Msun')
+                SFE.append(SFE_val)
+                SFE_n.append(SFE_val/time_yr)
+                
+                res = m.multipleAnalysis(S,cutoff=10000, bound_check=bound_check, nmax=6, cyclic=True, Grho=Grho, max_iter=args.max_iterations)
+                
+                import pdb
+                pdb.set_trace()
