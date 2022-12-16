f1aaa363d8a03f14407eabb39db9c46fb0d5bc1d
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2022-12-16 15:40:10 +0100

    Attmepting to plot the formation pathways

---
+++ Ramses_analysis/CF_analysis/Formation_pathway_frames.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+import yt
+yt.enable_parallelism()
+import glob
+import numpy as np
+import sys
+import os
+from mpi4py.MPI import COMM_WORLD as CW
+import pickle
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+#mpl.rcParams['pdf.fonttype'] = 42
+#mpl.rcParams['ps.fonttype'] = 42
+import matplotlib.pyplot as plt
+#plt.rcParams['figure.dpi'] = 300
+from matplotlib.colors import LogNorm
+import matplotlib.patheffects as path_effects
+import my_ramses_module as mym
+import my_ramses_fields as myf
+
+def parse_inputs():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-tf", "--text_font", help="What font text do you want to use?", type=int, default=10)
+    parser.add_argument("-cmin", "--colourbar_min", help="Input a list with the colour bar ranges", type=str, default='1.e-22')
+    parser.add_argument("-cmax", "--colourbar_max", help="Input a list with the colour bar ranges", type=float, default=1.e-19)
+    parser.add_argument("-G_mass", "--Gas_mass", type=float)
+    parser.add_argument("files", nargs='*')
+    args = parser.parse_args()
+    return args
+
+#=======MAIN=======
+units_override = {"length_unit":(4.0,"pc"), "velocity_unit":(0.18, "km/s"), "time_unit":(685706129102738.9, "s")}
+
+simulation_density_id = args.global_data_pickle_file.split('/G')[-1].split('/')[0]
+
+if simulation_density_id == '50':
+    Grho=50
+    units_override.update({"mass_unit":(1500,"Msun")})
+elif simulation_density_id == '100':
+    Grho=100
+    units_override.update({"mass_unit":(3000,"Msun")})
+elif simulation_density_id == '125':
+    Grho=125
+    units_override.update({"mass_unit":(3750,"Msun")})
+elif simulation_density_id == '150':
+    Grho=150
+    units_override.update({"mass_unit":(4500,"Msun")})
+elif simulation_density_id == '200':
+    Grho=200
+    units_override.update({"mass_unit":(6000,"Msun")})
+elif simulation_density_id == '400':
+    Grho=400
+    units_override.update({"mass_unit":(12000,"Msun")})
+else:
+    print("MASS UNIT NOT SET")
+    import pdb
+    pdb.set_trace()
+    
+
+units_override.update({"density_unit":(units_override['mass_unit'][0]/units_override['length_unit'][0]**3, "Msun/pc**3")})
+    
+scale_l = yt.YTQuantity(units_override['length_unit'][0], units_override['length_unit'][1]).in_units('cm') # 4 pc
+scale_v = yt.YTQuantity(units_override['velocity_unit'][0], units_override['velocity_unit'][1]).in_units('cm/s')         # 0.18 km/s == sound speed
+scale_t = scale_l/scale_v # 4 pc / 0.18 km/s
+scale_d = yt.YTQuantity(units_override['density_unit'][0], units_override['density_unit'][1]).in_units('g/cm**3')  # 2998 Msun / (4 pc)^3
+
+units={}
+for key in units_override.keys():
+    units.update({key:yt.YTQuantity(units_override[key][0], units_override[key][1])})
+mym.set_units(units_override)
+
+#------------------------------
+Interested_sinks = [10, 5, 6]
+Other_sink = [8, 4, [3, 9]]
+
+#Bound core fragmentation pathway
+Primary_form_time = 1.0880111822574787
+Secondary_form_time = 1.0911019317015047
+
+m_times = np.array([Primary_form_time, Secondary_form_time])*scale_t.in_units('yr')
+
+usable_files = mym.find_files(m_times, files, sink_form_time,sink_id, verbatim=False)
+import pdb
+pdb.set_trace()
+
+
+#Delay core frag pathway
+Primary_form_time = 1.0821092151794647
+Secondary_form_time = 1.083394503290905
+System_bound_time = 1.0843248950292361
+
+m_times = [Primary_form_time, Secondary_form_time, System_bound_time]
+
+#Dynamical capture
+Star_form_time = 1.0863474233686798
+Capture_time = 1.092208857287293
+
+m_times = [Star_form_time, Capture_time]
