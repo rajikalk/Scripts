fa49762cb236b5dda0b813794119f1ee47de2e7a
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2022-12-20 16:21:23 +0100

    Going to try adn get sink data first then do projections

--- Ramses_analysis/CF_analysis/Formation_pathway_frames.py
+++ Ramses_analysis/CF_analysis/Formation_pathway_frames.py
@@ -213,7 +213,7 @@ for pickle_file in pickle_files:
     #plt.streamplot(X, Y, magx, magy, density=4, linewidth=0.25, arrowstyle='-', minlength=0.5)
     cbar = plt.colorbar(plot, pad=0.0)
     #mym.my_own_quiver_function(ax, X_vel, Y_vel, velx, vely, plot_velocity_legend=args.plot_velocity_legend, limits=[xlim, ylim], standard_vel=args.standard_vel, Z_val=velz)
-    ax.scatter((particle_x_pos - center_pos[0]), (particle_y_pos - center_pos[1]), color='c', s=1)
+    ax.scatter((particle_x_pos - center_pos[0]).value, (particle_y_pos - center_pos[1]).value, color='c', s=1)
     #mym.annotate_particles(ax, part_info['particle_position'], part_info['accretion_rad'], limits=[xlim, ylim], annotate_field=part_info['particle_mass'], particle_tags=part_info['particle_tag'])
     
     cbar.set_label(r"Density (g$\,$cm$^{-3}$)", rotation=270, labelpad=14, size=10)
