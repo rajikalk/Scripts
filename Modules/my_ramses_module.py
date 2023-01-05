d213b7f91f9b17f0ee2ce09c96cc4a610591d71b
Rajika Lakmali Kuruwita <reggie@Reggies-MBP-2.fritz.box>
2023-01-05 16:52:01 +0100

    changing spacing for mass annotation again. I need to think of a way to automate this.

--- Modules/my_ramses_module.py
+++ Modules/my_ramses_module.py
@@ -57,10 +57,10 @@ def rainbow_text(x,y,ls,lc,**kw):
         ex = text.get_window_extent(renderer=figlocal.canvas.get_renderer())
         if "odot" in string:
             #t = transforms.offset_copy(text._transform, x=ex.width, units='dots')
-            t = transforms.offset_copy(text._transform, x=(ex.width+10), units='dots')
+            t = transforms.offset_copy(text._transform, x=(ex.width+10), units='points')
         else:
             #t = transforms.offset_copy(text._transform, x=space_size, units='dots')
-            t = transforms.offset_copy(text._transform, x=(ex.width+10), units='dots')
+            t = transforms.offset_copy(text._transform, x=(ex.width+10), units='points')
         
 def set_global_font_size(x):
     global fontsize_global
