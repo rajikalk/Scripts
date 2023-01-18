af24bfcc3cfee500c92fe8f3a4d72377a2aeba49
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-01-18 10:33:40 +0100

    Need to convert units from code units

--- Ramses_analysis/Velocity_dispersion_with_age/v_spread.py
+++ Ramses_analysis/Velocity_dispersion_with_age/v_spread.py
@@ -12,6 +12,52 @@ def parse_inputs():
 
 args = parse_inputs()
 
+#Set units
+units_override = {"length_unit":(4.0,"pc"), "velocity_unit":(0.18, "km/s"), "time_unit":(685706129102738.9, "s")}
+
+simulation_density_id = args.global_data_pickle_file.split('/')[-1].split('_')[0][1:]
+try:
+    simulation_density_id_int = int(simulation_density_id)
+except:
+    simulation_density_id = args.global_data_pickle_file.split('_G')[-1].split('.pkl')[0]
+
+if simulation_density_id == '50':
+    Grho=50
+    units_override.update({"mass_unit":(1500,"Msun")})
+elif simulation_density_id == '100':
+    Grho=100
+    units_override.update({"mass_unit":(3000,"Msun")})
+elif simulation_density_id == '125':
+    Grho=125
+    units_override.update({"mass_unit":(3750,"Msun")})
+elif simulation_density_id == '150':
+    Grho=150
+    units_override.update({"mass_unit":(4500,"Msun")})
+elif simulation_density_id == '200':
+    Grho=200
+    units_override.update({"mass_unit":(6000,"Msun")})
+elif simulation_density_id == '400':
+    Grho=400
+    units_override.update({"mass_unit":(12000,"Msun")})
+else:
+    print("MASS UNIT NOT SET")
+    import pdb
+    pdb.set_trace()
+    
+
+units_override.update({"density_unit":(units_override['mass_unit'][0]/(units_override['length_unit'][0]**3), "Msun/pc**3")})
+    
+scale_l = yt.YTQuantity(units_override['length_unit'][0], units_override['length_unit'][1]).in_units('cm') # 4 pc
+scale_v = yt.YTQuantity(units_override['velocity_unit'][0], units_override['velocity_unit'][1]).in_units('cm/s')         # 0.18 km/s == sound speed
+scale_t = scale_l/scale_v # 4 pc / 0.18 km/s
+scale_d = yt.YTQuantity(units_override['density_unit'][0], units_override['density_unit'][1]).in_units('g/cm**3')  # 2998 Msun / (4 pc)^3
+
+units={}
+for key in units_override.keys():
+    units.update({key:yt.YTQuantity(units_override[key][0], units_override[key][1])})
+del units_override
+gc.collect()
+
 file_open = open(args.global_data_pickle_file, 'rb')
 try:
     global_data = pickle.load(file_open,encoding="latin1")
@@ -22,5 +68,11 @@ except:
     global_data = pickle.load(file_open,encoding="latin1")
 file_open.close()
 
-import pdb
-pdb.set_trace()
+V_spread_array = []
+for time_it in range(len(np.std(global_data['ux'][time_it][usable_inds])['ux'])):
+    #get indices fo stars that exist
+    usable_inds = np.argwhere(global_data['m'][time_it]>0)[0]
+    velocity_std = np.std(global_data['ux'][time_it][usable_inds])
+    
+    import pdb
+    pdb.set_trace()
