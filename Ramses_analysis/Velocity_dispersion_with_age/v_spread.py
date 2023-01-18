ffbc3950fc1af484cd589218a439d151cb313d84
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-01-18 10:16:06 +0100

    updated to make some more diagnostic plots

---
+++ Ramses_analysis/Velocity_dispersion_with_age/v_spread.py
@@ -0,0 +1,26 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+def parse_inputs():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-global_data", "--global_data_pickle_file", help="Where is the directory of the global pickle data?", default='/groups/astro/rlk/Analysis_plots/Ramses/Global/G100/512/stars_red_512.pkl', type=str)
+    parser.add_argument("files", nargs='*')
+    args = parser.parse_args()
+    return args
+
+args = parse_inputs()
+
+file_open = open(args.global_data_pickle_file, 'rb')
+try:
+    global_data = pickle.load(file_open,encoding="latin1")
+except:
+    file_open.close()
+    import pickle5 as pickle
+    file_open = open(args.global_data_pickle_file, 'rb')
+    global_data = pickle.load(file_open,encoding="latin1")
+file_open.close()
+
+import pdb
+pdb.set_trace()
