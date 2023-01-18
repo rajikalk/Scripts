fixed age calculation

Ramses_analysis/Velocity_dispersion_with_age/v_spread.py
@@ -76,10 +76,20 @@ for time_it in range(len(global_data['ux'])):
     #get indices fo stars that exist
     usable_inds = np.argwhere(global_data['m'][time_it]>0)[0]
     velocity_std = np.std(global_data['ux'][time_it][usable_inds]*scale_v.in_units('km/s'))
-    age = (global_data['time'][0][0] - global_data['time'][time_it][0])*scale_t.in_units('Myr')
+    age = (global_data['time'][time_it][0] - global_data['time'][0][0])*scale_t.in_units('Myr')
     
     V_spread_array.append(velocity_std)
     Cluster_age.append(age)
     
     if np.remainder(time_it, 1000) == 0:
         print('calculated v_spread for time_it', time_it, 'of', len(global_data['ux']))
+        
+
+plt.clf()
+plt.plot(Cluster_age, V_spread_array)
+plt.xlabel('Time since first star formation (Myr)')
+plt.ylabel('Velocity spread (km/s)')
+plt.xlim(left=0)
+plt.ylim(bottom=0)
+plt.savefig('v_spread_vs_time.png')
+