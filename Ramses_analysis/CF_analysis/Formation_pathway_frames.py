f4bbd44117c78543b8c9ff3d47b9e3e74dd5b6f8
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-01-24 17:10:12 +0100

    attempting to parallelise

--- Ramses_analysis/CF_analysis/Formation_pathway_frames.py
+++ Ramses_analysis/CF_analysis/Formation_pathway_frames.py
@@ -143,6 +143,7 @@ print('removed dynamical capture candidates that had birth separations >20000 au
 #Dynamical_capture_system = Dynamical_capture_candidates[0]
 
 for system in yt.parallel_objects(Bound_core_frag_candidates, njobs=int(size/(3))):# Bound_core_frag_candidates: #3 projections
+    print('Processing system', system)
     #Bound core fragmentation
     Bound_primary_form_time = global_data['time'].T[system[1]][np.where(global_data['m'].T[system[1]]>0)[0][0]]
     Bound_secondary_form_time = global_data['time'].T[system[0]][np.where(global_data['m'].T[system[0]]>0)[0][0]]
@@ -379,6 +380,7 @@ for system in yt.parallel_objects(Bound_core_frag_candidates, njobs=int(size/(3)
 
 for system in yt.parallel_objects(Unbound_core_frag_candidates, njobs=int(size/(3))):# Bound_core_frag_candidates: #3 projections
     #Bound core fragmentation
+    print('Processing system', system)
     Bound_primary_form_time = global_data['time'].T[system[1]][np.where(global_data['m'].T[system[1]]>0)[0][0]]
     Bound_secondary_form_time = global_data['time'].T[system[0]][np.where(global_data['m'].T[system[0]]>0)[0][0]]
     Bound_m_times = [Bound_secondary_form_time, Bound_primary_form_time]
@@ -614,6 +616,7 @@ for system in yt.parallel_objects(Unbound_core_frag_candidates, njobs=int(size/(
 
 for system in yt.parallel_objects(Dynamical_capture_candidates, njobs=int(size/(3))):# Bound_core_frag_candidates: #3 projections
     #Bound core fragmentation
+    print('Processing system', system)
     Bound_primary_form_time = global_data['time'].T[system[1]][np.where(global_data['m'].T[system[1]]>0)[0][0]]
     Bound_secondary_form_time = global_data['time'].T[system[0]][np.where(global_data['m'].T[system[0]]>0)[0][0]]
     Bound_m_times = [Bound_secondary_form_time, Bound_primary_form_time]
