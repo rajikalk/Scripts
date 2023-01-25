39de8d9c21a1f6216611863c0b79f17063a7c27f
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-01-25 13:01:18 +0100

    trying converting tuple to list

--- Ramses_analysis/CF_analysis/Formation_pathway_frames.py
+++ Ramses_analysis/CF_analysis/Formation_pathway_frames.py
@@ -245,6 +245,7 @@ for system in yt.parallel_objects(Bound_core_frag_candidates, njobs=int(size/(3)
         datadir = fn.split('output_')[0]
         loaded_sink_data = rsink(file_no, datadir=datadir)
         #try:
+        print('center_sink = ', center_sink)
         center_pos = yt.YTArray([loaded_sink_data['x'][center_sink]*units['length_unit'].in_units('au'), loaded_sink_data['y'][center_sink]*units['length_unit'].in_units('au'), loaded_sink_data['z'][center_sink]*units['length_unit'].in_units('au')])
         sink_creation_time = loaded_sink_data['tcreate'][center_sink]*units['time_unit'].in_units('yr')
         center_positions.append(center_pos)
