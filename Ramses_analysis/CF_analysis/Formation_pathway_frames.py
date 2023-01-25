dbccaec0fa146261833213a2c7d8d17cb243d19c
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-01-25 10:13:31 +0100

    trying to delete global data early for memory

--- Ramses_analysis/CF_analysis/Formation_pathway_frames.py
+++ Ramses_analysis/CF_analysis/Formation_pathway_frames.py
@@ -104,7 +104,7 @@ if rank == 0:
             else:
                 other_ind = int(pair[1])
             #Save times formation
-            Bound_primary_form_time = global_data['time'].T[other_ind)][np.where(global_data['m'].T[other_ind]>0)[0][0]]
+            Bound_primary_form_time = global_data['time'].T[other_ind][np.where(global_data['m'].T[other_ind]>0)[0][0]]
             Bound_secondary_form_time = global_data['time'].T[pair[0]][np.where(global_data['m'].T[pair[0]]>0)[0][0]]
             Bound_m_times = [Bound_secondary_form_time, Bound_primary_form_time]
             Bound_core_frag_candidates_reduced.append([pair, Bound_m_times])
