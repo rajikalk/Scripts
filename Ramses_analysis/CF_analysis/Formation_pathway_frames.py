383132f6002874cf1f390da3c25bf42b3fde1c4a
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-02-03 13:40:39 +0100

    making markers larger

--- Ramses_analysis/CF_analysis/Formation_pathway_frames.py
+++ Ramses_analysis/CF_analysis/Formation_pathway_frames.py
@@ -478,7 +478,7 @@ if args.make_bound_frames == 'True':
                     ax.plot(particle_x_pos, particle_y_pos, linestyle='-', color='grey')
                     
                 #part_color = ['cyan','magenta','r','b','y','w','k']
-                ax.scatter(particle_x_pos, particle_y_pos, c='y', marker='*', s=10, linewidth=1.5, edgecolor="k")
+                ax.scatter(particle_x_pos, particle_y_pos, c='y', marker='*', s=20, linewidth=1.5, edgecolor="k", zorder=11)
 
                 plt.savefig(file_name + ".png", format='png', bbox_inches='tight')
                 #plt.savefig(file_name + ".pdf", format='pdf', bbox_inches='tight')
@@ -830,7 +830,7 @@ if args.make_unbound_frames == 'True':
                                 linestyle = ':'
                             ax.plot(particle_x_pos[np.array([target_sink_ind, -1])], particle_y_pos[np.array([target_sink_ind, -1])], linestyle=linestyle, color='grey')
                 
-                ax.scatter(particle_x_pos, particle_y_pos, c='y', marker='*', s=10, linewidth=1.5, edgecolor="k")
+                ax.scatter(particle_x_pos, particle_y_pos, c='y', marker='*', s=20, linewidth=1.5, edgecolor="k", zorder=11)
                 
                 try:
                     plt.savefig(file_name + ".png", format='png', bbox_inches='tight')
