ba933e852e91310ccebbddfaef21ada4d0d5a345
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-02-06 16:36:42 +0100

    attempting to plot the mean accretion rate for all sims

--- Ramses_analysis/CF_analysis/mean_accretion_vs_time.py
+++ Ramses_analysis/CF_analysis/mean_accretion_vs_time.py
@@ -180,5 +180,5 @@ plt.legend(ncol=2, fontsize=font_size, labelspacing=0.1, handletextpad=0.2, bord
 plt.xlim([0, 0.05])
 plt.xlabel("SFE")
 plt.ylabel("Accretion rate")
-savename = "mean_acc.pdf"
+savename = "mean_acc.png"
 plt.savefig(savename, bbox_inches='tight', pad_inches=0.02)
