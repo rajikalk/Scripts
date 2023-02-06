5303383d202bcbe6a45077d44327edf8b6ed254d
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-02-06 11:57:12 +0100

    cleaning up close binary fraction plot

--- Ramses_analysis/CF_analysis/close_binary_fraction_multi.py
+++ Ramses_analysis/CF_analysis/close_binary_fraction_multi.py
@@ -6,7 +6,13 @@ pickle_files = ['/groups/astro/rlk/rlk/Analysis_plots/Close_binary_fraction/G50.
 
 labels = ["1500M$_\odot$", "3000M$_\odot$", "3750M$_\odot$", "4500M$_\odot$", "6000M$_\odot$", "12000M$_\odot$"]
 
+two_col_width = 7.20472 #inches
+single_col_width = 3.50394 #inches
+page_height = 10.62472 #inches
+font_size = 10
+
 plt.clf()
+fig, axs = plt.subplots(ncols=1, nrows=1, figsize=(single_col_width, single_col_width))
 pit = -1
 for pickle_file in pickle_files:
     pit = pit + 1
@@ -16,10 +22,10 @@ for pickle_file in pickle_files:
     
     plt.plot(SFE, Close_Fractions, label=labels[pit])
     
-plt.legend()
+plt.legend(ncol=2)
 plt.xlim([0, 0.05])
 plt.ylim([0, 1])
 plt.xlabel('SFE')
 plt.ylabel('Close binary fraction (<100au)')
-plt.savefig('close_frac_comp.png')
+plt.savefig('close_frac_comp.pdf', bbox_inches='tight', pad_inches=0.02)
     
