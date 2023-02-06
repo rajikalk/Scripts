e02dc11cd05e6d4975e8696142e42a13315fdfb7
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-02-06 16:44:42 +0100

    attempting to plot the mean accretion rate for all sims

--- Ramses_analysis/CF_analysis/mean_accretion_vs_time.py
+++ Ramses_analysis/CF_analysis/mean_accretion_vs_time.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 import matplotlib
+import yt
 
 matplotlib.rcParams['mathtext.fontset'] = 'stixsans'
 matplotlib.rcParams['mathtext.it'] = 'Arial:italic'
