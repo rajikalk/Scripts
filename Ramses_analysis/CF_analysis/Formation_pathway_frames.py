fixed units

Ramses_analysis/CF_analysis/Formation_pathway_frames.py
@@ -362,11 +362,7 @@ if args.make_bound_frames == 'True':
                 axis_ind = 2
                 try:
                     proj = yt.ProjectionPlot(ds, axis_ind, ("ramses", "Density"), width=thickness, data_source=region, method='integrate', center=(center_pos, 'AU'))
-                    proj_array = np.array(proj.frb.data[("ramses", "Density")])/thickness.in_units('cm')
-                    image = proj_array*units['density_unit'].in_units('g/cm**3')
-                    if str(image.units) == 'g/cm**4':
-                        import pdb
-                        pdb.set_trace()
+                    image = (proj.frb.data[("ramses", "Density")]/thickness.in_units('cm')).value*units['density_unit'].in_units('g/cm**3')
                     del proj
                     del proj_array
                 except:
@@ -675,11 +671,7 @@ if args.make_unbound_frames == 'True':
                 
                 axis_ind = 2
                 proj = yt.ProjectionPlot(ds, axis_ind, ("ramses", "Density"), width=thickness, data_source=region, method='integrate', center=(center_pos, 'AU'))
-                proj_array = np.array(proj.frb.data[("ramses", "Density")])/thickness.in_units('cm')
-                image = proj_array*units['density_unit'].in_units('g/cm**3')
-                if str(image.units) == 'g/cm**4':
-                    import pdb
-                    pdb.set_trace()
+                image = (proj.frb.data[("ramses", "Density")]/thickness.in_units('cm')).value*units['density_unit'].in_units('g/cm**3')
                 del proj
                 del proj_array
                 gc.collect()
@@ -1017,8 +1009,7 @@ if args.make_dynamical_frames == 'True':
                 
                 axis_ind = 2
                 proj = yt.ProjectionPlot(ds, axis_ind, ("ramses", "Density"), width=thickness, data_source=region, method='integrate', center=(center_pos, 'AU'))
-                proj_array = np.array(proj.frb.data[("ramses", "Density")])/thickness.in_units('cm')
-                image = proj_array*units['density_unit'].in_units('g/cm**3')
+                image = (proj.frb.data[("ramses", "Density")]/thickness.in_units('cm')).value*units['density_unit'].in_units('g/cm**3')
                 if str(image.units) == 'g/cm**4':
                     import pdb
                     pdb.set_trace()