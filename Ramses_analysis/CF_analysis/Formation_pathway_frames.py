43543d0ceaab00e72993904c90189ae146c9d35b
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-02-09 09:02:04 +0100

    colourbar shoudl adjust for the image

--- Ramses_analysis/CF_analysis/Formation_pathway_frames.py
+++ Ramses_analysis/CF_analysis/Formation_pathway_frames.py
@@ -742,8 +742,9 @@ if args.make_unbound_frames == 'True':
                 ax.set_xlim(xlim)
                 ax.set_ylim(ylim)
 
-                
-                plot = ax.pcolormesh(X, Y, image, cmap=plt.cm.gist_heat, norm=LogNorm(vmin=1.e-19, vmax=1.e-16), rasterized=True)
+                cmin = 10**(np.log10(np.mean(image))-1.5)
+                cmax = 10**(np.log10(np.mean(image))+1.5)
+                plot = ax.pcolormesh(X, Y, image, cmap=plt.cm.gist_heat, norm=LogNorm(cmin=1.e-19, cmax=1.e-16), rasterized=True)
                 plt.gca().set_aspect('equal')
                 #plt.streamplot(X, Y, magx, magy, density=4, linewidth=0.25, arrowstyle='-', minlength=0.5)
                 cbar = plt.colorbar(plot, pad=0.0)
