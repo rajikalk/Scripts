20415552757f93eacdfe0cd602ff4946e851d653
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-02-10 11:45:21 +0100

    making formation pathway plot

---
+++ Ramses_analysis/CF_analysis/Formation_pathway_multi.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import numpy as np
+import sys
+import pickle
+import csv
+import matplotlib.pyplot as plt
+
+plot_pickles = []
+
+two_col_width = 7.20472 #inches
+single_col_width = 3.50394 #inches
+page_height = 10.62472 #inches
+font_size = 10
+
+fig, axs = plt.subplots(ncols=3, nrows=3, figsize=(two_col_width,two_col_width))
+import pdb
+pdb.set_trace()
+
