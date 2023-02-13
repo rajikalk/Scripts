43b69939fc522d2ddea56ba73557b09a6f300ae9
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-02-13 16:43:26 +0100

    I was accidentally adjusting the image position by the center pos twice

--- Ramses_analysis/CF_analysis/Formation_pathway_multi.py
+++ Ramses_analysis/CF_analysis/Formation_pathway_multi.py
@@ -47,7 +47,7 @@ pathway_label = ['Bound Core Fragmentation', 'Unbound Core Fragmentation', 'Dyna
 
 plt.clf()
 fig, axs = plt.subplots(ncols=3, nrows=3, figsize=(two_col_width,two_col_width))
-plt.subplots_adjust(wspace=0.0, hspace=0.4)
+plt.subplots_adjust(wspace=0.1, hspace=0.2)
 for pick_it in range(len(plot_pickles)):
     pickle_file = plot_pickles[pick_it]
     file = open(pickle_file, 'rb')
