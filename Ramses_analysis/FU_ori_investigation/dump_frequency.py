4b986894ca6ba459fd7d8033f80a0422ea2f67d1
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-03-22 15:14:40 +0100

    making script to get better idea of how it's progressing

--- Ramses_analysis/FU_ori_investigation/dump_frequency.py
+++ Ramses_analysis/FU_ori_investigation/dump_frequency.py
@@ -1,10 +1,25 @@
 import csv
+import numpy
+import matplotlib.pyplot as plt
 
 file_data = "file_data.txt"
+dump_time = []
 
 with open(file_data, 'r') as data_file:
     reader = csv.reader(data_file, delimiter=' ')
     for row in reader:
-        import pdb
-        pdb.set_trace()
-    
+        if 'output' in row[11]:
+            time_split = row[10].split(':')
+            time_dec = float(time_split[0] + str(int(time_split[1])/60)[1:])
+            if len(dump_time) == 0:
+                dump_time.append(time_dec)
+            if dump_time[-1] != time_dec:
+                dump_time.append(time_dec)
+            
+f.close()
+
+dt = np.array(dump_time)[1:] - np.array(dump_time)[:-1]
+plt.clf()
+plt.plot(dt)
+plt.ylabel("time between dumps")
+plt.savefig("dump_frequency.png")
