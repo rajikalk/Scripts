d9764e243d69274ae4a1ea642df763e8a9008f55
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-03-22 15:15:44 +0100

    making script to get better idea of how it's progressing

--- Ramses_analysis/FU_ori_investigation/dump_frequency.py
+++ Ramses_analysis/FU_ori_investigation/dump_frequency.py
@@ -8,13 +8,14 @@ dump_time = []
 with open(file_data, 'r') as data_file:
     reader = csv.reader(data_file, delimiter=' ')
     for row in reader:
-        if 'output' in row[11]:
-            time_split = row[10].split(':')
-            time_dec = float(time_split[0] + str(int(time_split[1])/60)[1:])
-            if len(dump_time) == 0:
-                dump_time.append(time_dec)
-            if dump_time[-1] != time_dec:
-                dump_time.append(time_dec)
+        if len(row) > 11:
+            if 'output' in row[11]:
+                time_split = row[10].split(':')
+                time_dec = float(time_split[0] + str(int(time_split[1])/60)[1:])
+                if len(dump_time) == 0:
+                    dump_time.append(time_dec)
+                if dump_time[-1] != time_dec:
+                    dump_time.append(time_dec)
             
 f.close()
 
