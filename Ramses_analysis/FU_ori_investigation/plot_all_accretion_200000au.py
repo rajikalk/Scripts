002bf8728f345c3025c98a79c9c6edb6ac3574a7
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-03-24 14:40:24 +0100

    Finding closest sinks

--- Ramses_analysis/FU_ori_investigation/plot_all_accretion_200000au.py
+++ Ramses_analysis/FU_ori_investigation/plot_all_accretion_200000au.py
@@ -57,186 +57,55 @@ units={}
 for key in units_override.keys():
     units.update({key:yt.YTQuantity(units_override[key][0], units_override[key][1])})
 
-if args.update_pickle == 'True':
-    print("Reading particle data")
-    loaded_sink_data = rsink(datadir=path, all=True)
-    updating = False
+print("Reading particle data")
+loaded_sink_data = rsink(datadir=path, all=True)
+updating = False
+
+if args.sink_number == None:
+    last_n = int(sorted(glob.glob(path+"output*"))[-1].split("_")[-1])
+    stars_output_file = path + 'output_'+("%05d" % last_n)+'/stars_output.dat'
+    while os.path.exists(stars_output_file) == False:
+        last_n = last_n - 1
+        stars_output_file = path + 'output_'+("%05d" % last_n)+'/stars_output.dat'
+    loaded_sink_data_last = rsink(last_n, datadir=path)
+    sink_ind = np.argmin(loaded_sink_data_last['u'])
+else:
+    sink_ind = args.sink_number
     
-    if os.path.isfile('nearby_accretion.pkl'):
-        try:
-            file_open = open(save_dir+'nearby_accretion.pkl', 'rb')
-            nearby_accretion, counter, sink_ind, sink_form_time = pickle.load(file_open)
-            file_open.close()
-            counter = int(counter)
-            if counter < len(loaded_sink_data):
-                updating = True
-                print('pickle data is not up to date! Updating...')
-        except:
-            os.system('cp '+save_dir+'nearby_accretion_tmp.pkl '+save_dir+'nearby_accretion.pkl ')
-            file_open = open(save_dir+'nearby_accretion.pkl', 'rb')
-            nearby_accretion, counter, sink_ind, sink_form_time = pickle.load(file_open)
-            file_open.close()
-            counter = int(counter)
-            if counter < len(loaded_sink_data):
-                updating = True
-                print('pickle data is not up to date! Updating...')
-    else:
-        updating = True
-        if args.sink_number == None:
-            last_n = int(sorted(glob.glob(path+"output*"))[-1].split("_")[-1])
-            stars_output_file = path + 'output_'+("%05d" % last_n)+'/stars_output.dat'
-            while os.path.exists(stars_output_file) == False:
-                last_n = last_n - 1
-                stars_output_file = path + 'output_'+("%05d" % last_n)+'/stars_output.dat'
-            loaded_sink_data_last = rsink(last_n, datadir=path)
-            sink_ind = np.argmin(loaded_sink_data_last['u'])
-        else:
-            sink_ind = args.sink_number
-            
-        nearby_accretion = {}
-        nearby_accretion.update({'particle_tag':[]})
-        nearby_accretion.update({'time':[]})
-        nearby_accretion.update({'mass':[]})
-        nearby_accretion.update({'mdot':[]})
-        
-        counter = 0
-        sink_form_time = 0
-        
-    for sink_data in loaded_sink_data:
-        if len(sink_data['u']) > sink_ind:
-            import pdb
-            pdb.set_trace()
-    #target_sink_formation_location =
-        
-    if updating == True:
+for sink_data in loaded_sink_data:
+    if len(sink_data['u']) > sink_ind:
+       target_sink_formation_location = yt.YTArray(np.array([sink_data['x'][sink_ind], sink_data['y'][sink_ind], sink_data['z'][sink_ind]])*units['length_unit'].in_units('au'), 'au')
+       break
+    
+plotted_sinks = []
+for sink_data in loaded_sink_data:
+    if len(sink_data['u']) < sink_ind:
         import pdb
         pdb.set_trace()
-        loaded_sink_data = loaded_sink_data[counter:]
-        for sink_data in loaded_sink_data:
-            counter = counter + 1
-            if np.remainder(counter, 1000) == 0:
-                try:
-                    os.remove(save_dir+'nearby_accretion.pkl')
-                except:
-                    print("pickle files doesn't exist yet")
-                file = open(save_dir+'nearby_accretion.pkl', 'wb')
-                pickle.dump((nearby_accretion, counter, sink_ind, sink_form_time), file)
-                file.close()
-                os.system('cp '+save_dir+'nearby_accretion.pkl '+save_dir+'nearby_accretion_tmp.pkl ')
-                print('read', counter, 'snapshots of sink particle data, and saved pickle')
-            if len(sink_data['u']) < sink_ind:
-                import pdb
-                pdb.set_trace()
-            if len(sink_data['u']) > sink_ind:
-                tags = np.arange(len(sink_data['u']))[sink_ind-1:sink_ind+1]
-                for tag in tags:
-                    if tag not in nearby_accretion['particle_tag']:
-                        nearby_accretion['particle_tag'].append(tag)
-                pos_prim = yt.YTArray(np.array([sink_data['x'][sink_ind-1], sink_data['y'][sink_ind-1], sink_data['z'][sink_ind-1]])*units['length_unit'].in_units('au'), 'au')
-                pos_second = yt.YTArray(np.array([sink_data['x'][sink_ind], sink_data['y'][sink_ind], sink_data['z'][sink_ind]])*units['length_unit'].in_units('au'), 'au')
-                separation = np.sqrt(np.sum((pos_second - pos_prim)**2))
-                nearby_accretion['separation'].append(separation)
-                if sink_form_time == 0:
-                    sink_form_time = sink_data['tcreate'][sink_ind]*units['time_unit'].in_units('yr')
-                time_val = sink_data['snapshot_time']*units['time_unit'].in_units('yr') - sink_form_time
-                #if len(particle_tags) == 1:
-                nearby_accretion['time'].append(time_val)
-                nearby_accretion['mass'].append(yt.YTArray(sink_data['m'][sink_ind-1:sink_ind+1]*units['mass_unit'].in_units('msun'), 'msun'))
-                
-                d_mass = sink_data['dm'][sink_ind-1:sink_ind+1]*units['mass_unit'].in_units('msun')
-                d_time = (sink_data['snapshot_time'] - sink_data['tflush'])*units['time_unit'].in_units('yr')
-                acc_val = d_mass/d_time
-                acc_val[np.where(acc_val == 0)[0]]=1.e-12
-                nearby_accretion['mdot'].append(yt.YTArray(acc_val, 'msun/yr'))
-        #write lastest pickle
-        file = open(save_dir+'nearby_accretion.pkl', 'wb')
-        pickle.dump((nearby_accretion, counter, sink_ind, sink_form_time), file)
-        file.close()
-        os.system('cp '+save_dir+'nearby_accretion.pkl '+save_dir+'nearby_accretion_tmp.pkl ')
-        print('read', counter, 'snapshots of sink particle data, and saved pickle')
-                
-
-f_acc = 0.5
-radius = yt.YTQuantity(2.0, 'rsun')
-#M_dot = accretion(sink_inds, global_ind)
-#M = yt.YTArray(global_data['m'][global_ind,sink_inds]*units['mass_unit'].in_units('msun'), 'Msun')
-m_dot = yt.YTArray(nearby_accretion['mdot']).in_units('g/s')
-mass = yt.YTArray(nearby_accretion['mass']).in_units('g')
-L_acc = f_acc * (yt.units.G * mass * m_dot)/radius.in_units('cm')
-L_tot = L_acc.in_units('Lsun')
-
-Mag = -2.5*np.log10(L_tot)
-
-plt.clf()
-for part_it in range(len(nearby_accretion['particle_tag'])):
-    plt.plot(nearby_accretion['time'], Mag.T[part_it], label="Sink "+str(nearby_accretion['particle_tag'][part_it]+1))
-plt.gca().invert_yaxis()
-plt.xlabel('Time (yr)')
-plt.xlim()
-plt.ylabel('Magnitude (M$_{bol}$)')
-plt.legend()
-plt.title('Sink no ' + str(sink_ind))
-plt.savefig('magnitude_vs_time_sink_'+str(sink_ind)+'.png')
-
-plt.clf()
-for part_it in range(len(nearby_accretion['particle_tag'])):
-    plt.semilogy(nearby_accretion['time'], L_tot.T[part_it], label="Sink "+str(nearby_accretion['particle_tag'][part_it]+1))
-plt.xlabel('Time (yr)')
-plt.xlim()
-plt.legend()
-plt.ylabel('Luminosity (Lsun)')
-plt.title('Sink no ' + str(sink_ind))
-plt.savefig('luminosity_vs_time_sink_'+str(sink_ind)+'.png')
-
-
-plt.clf()
-for part_it in range(len(nearby_accretion['particle_tag'])):
-    plt.semilogy(nearby_accretion['time'], np.array(nearby_accretion['mdot']).T[part_it], label="Sink "+str(nearby_accretion['particle_tag'][part_it]+1))
-plt.xlabel('Time (yr)')
-plt.xlim()
-plt.legend()
-plt.ylabel('Accretion rate (Msun/yr)')
-plt.title('Sink no ' + str(sink_ind))
-plt.savefig('accretion_vs_time_sink_'+str(sink_ind)+'.png')
-
-plt.clf()
-for part_it in range(len(nearby_accretion['particle_tag'])):
-    plt.plot(nearby_accretion['time'], np.array(nearby_accretion['mass']).T[part_it], label="Sink "+str(nearby_accretion['particle_tag'][part_it]+1))
-plt.xlabel('Time (yr)')
-plt.xlim()
-plt.legend()
-plt.ylabel('Mass (Msun)')
-plt.title('Sink no ' + str(sink_ind))
-plt.savefig('mass_vs_time_sink_'+str(sink_ind)+'.png')
-
-L = yt.YTQuantity(4, 'pc')
-curr_dir = os.getcwd()
-if 'Level' not in curr_dir:
-    refinement = 18
-else:
-    refinement = int(curr_dir.split('Level_')[-1][:2])
-d_min = L.in_units('au')/(2**refinement)
-plt.clf()
-plt.semilogy(nearby_accretion['time'], nearby_accretion['separation'])
-plt.axhline(y=8*d_min, color='r', linestyle=':')
-plt.axhline(y=4*d_min, color='r', linestyle='--')
-plt.axhline(y=d_min, color='r')
-plt.xlabel('Time (yr)')
-plt.xlim()
-plt.ylabel('Separation (AU)')
-plt.title('Sink no ' + str(sink_ind))
-plt.savefig('separation_vs_time_sink_'+str(sink_ind)+'.png')
-
-if sink_ind == 45:
-    start_time = 3500
-    start_ind = np.argmin(abs(np.array(nearby_accretion['time']) - start_time))
-    plt.clf()
-    plt.semilogy(nearby_accretion['time'][start_ind:], nearby_accretion['mdot'][start_ind:])
-    plt.xlabel('Time (yr)')
-    plt.xlim()
-    plt.ylim(bottom=1.e-7)
-    plt.ylabel('Accretion rate (Msun/yr)')
-    plt.title('Sink no ' + str(sink_ind))
-    plt.tick_params(axis='both', which='major', right=True, direction='in')
-    plt.tick_params(axis='both', which='minor', right=True, direction='in')
-    plt.savefig('accretion_vs_time_sink_'+str(sink_ind)+'_trunc_3500.png')
+    if len(sink_data['u']) > sink_ind:
+        tags = np.arange(len(sink_data['u']))[sink_ind-1:sink_ind+1]
+        for tag in tags:
+            if tag not in nearby_accretion['particle_tag']:
+                nearby_accretion['particle_tag'].append(tag)
+        pos_prim = yt.YTArray(np.array([sink_data['x'][sink_ind-1], sink_data['y'][sink_ind-1], sink_data['z'][sink_ind-1]])*units['length_unit'].in_units('au'), 'au')
+        pos_second = yt.YTArray(np.array([sink_data['x'][sink_ind], sink_data['y'][sink_ind], sink_data['z'][sink_ind]])*units['length_unit'].in_units('au'), 'au')
+        separation = np.sqrt(np.sum((pos_second - pos_prim)**2))
+        nearby_accretion['separation'].append(separation)
+        if sink_form_time == 0:
+            sink_form_time = sink_data['tcreate'][sink_ind]*units['time_unit'].in_units('yr')
+        time_val = sink_data['snapshot_time']*units['time_unit'].in_units('yr') - sink_form_time
+        #if len(particle_tags) == 1:
+        nearby_accretion['time'].append(time_val)
+        nearby_accretion['mass'].append(yt.YTArray(sink_data['m'][sink_ind-1:sink_ind+1]*units['mass_unit'].in_units('msun'), 'msun'))
+        
+        d_mass = sink_data['dm'][sink_ind-1:sink_ind+1]*units['mass_unit'].in_units('msun')
+        d_time = (sink_data['snapshot_time'] - sink_data['tflush'])*units['time_unit'].in_units('yr')
+        acc_val = d_mass/d_time
+        acc_val[np.where(acc_val == 0)[0]]=1.e-12
+        nearby_accretion['mdot'].append(yt.YTArray(acc_val, 'msun/yr'))
+#write lastest pickle
+file = open(save_dir+'nearby_accretion.pkl', 'wb')
+pickle.dump((nearby_accretion, counter, sink_ind, sink_form_time), file)
+file.close()
+os.system('cp '+save_dir+'nearby_accretion.pkl '+save_dir+'nearby_accretion_tmp.pkl ')
+print('read', counter, 'snapshots of sink particle data, and saved pickle')
