994d6277d7c7e50a82412364caf9a7b769390190
Rajika Lakmali Kuruwita <reggie@Reggies-MBP-2.fritz.box>
2023-03-28 13:08:44 +0200

    trying to save the system eccentricities

--- Ramses_analysis/CF_analysis/pathway_evolution_multiplot.py
+++ Ramses_analysis/CF_analysis/pathway_evolution_multiplot.py
@@ -310,14 +310,14 @@ axs[1].errorbar(np.array(masses)+130, Median_grads_10000[2], yerr=Capt_err_10000
 axs[0].tick_params(which='both', direction='in')
 axs[0].tick_params(axis='both', which='major', labelsize=font_size, right=True, top=True)
 axs[0].tick_params(axis='both', which='minor', labelsize=font_size, right=True, top=True)
-axs[0].text(6500, -2.0, "Baseline=$1\,000\,\mathrm{yr}$", zorder=11, size=font_size)
+axs[0].text(6500, -0.1, "Baseline=$1\,000\,\mathrm{yr}$", zorder=11, size=font_size)
 
 axs[1].tick_params(which='both', direction='in')
 axs[1].tick_params(axis='both', which='major', labelsize=font_size, right=True, top=True)
 axs[1].tick_params(axis='both', which='minor', labelsize=font_size, right=True, top=True)
-axs[1].text(6500, 0.5, "Baseline=$10\,000\,\mathrm{yr}$", zorder=11, size=font_size)
+axs[1].text(6500, -0.1, "Baseline=$10\,000\,\mathrm{yr}$", zorder=11, size=font_size)
 
-axs[0].legend(loc='upper right', fontsize=font_size)
+axs[0].legend(loc='lower right', fontsize=font_size)
 axs[1].set_xlabel('Molecular cloud mass (M$_\odot$)', size=font_size)
 axs[0].set_ylabel('Inspiral rate (Log$_{10}$(AU/yr))', size=font_size)
 axs[1].set_ylabel('Inspiral rate (Log$_{10}$(AU/yr))', size=font_size)
