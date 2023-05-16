918c619f95a48da52581f7704acfb60257ae6365
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-05-16 09:07:26 +0200

    created a script that Hopefully automatically resubmits jobs

--- FLASH_analysis/auto_restart.py
+++ FLASH_analysis/auto_restart.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+import mpi4py
 import glob
 import subprocess
 
-chk_files = sorted(glob.glob("*chk*"))
-last_chk = chk_files[-1]
+rank = CW.Get_rank()
+size = CW.Get_size()
 
-run_line = "python ~/Scripts/Automation_Scripts/prep_restart.py " + last_chk
-subprocess.run(proj_run_line, shell=True)
-subprocess.run("sb job.sh", shell=True)
+if rank == 0:
+    chk_files = sorted(glob.glob("*chk*"))
+    last_chk = chk_files[-1]
+
+    run_line = "python ~/Scripts/Automation_Scripts/prep_restart.py " + last_chk
+    subprocess.run(proj_run_line, shell=True)
+    subprocess.run("sb job.sh", shell=True)
