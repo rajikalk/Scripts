3c8db36d3b8bd2dfd910f50daae0a90d71dd895d
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-05-22 14:30:27 +0200

    building a script to compare different initial spins

---
+++ FLASH_analysis/spin_comparison.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pickle
+import yt
+yt.enable_parallelism()
+import glob
+import sys
+import matplotlib.pyplot as plt
+import matplotlib
+from mpi4py.MPI import COMM_WORLD as CW
+import my_flash_fields as myf
+import my_flash_module as mym
+import pickle
+import argparse
+import os
+
+#------------------------------------------------------
+#get mpi size and ranks
+rank = CW.Get_rank()
+size = CW.Get_size()
+
+#------------------------------------------------------
+#Ploting parameters
+matplotlib.rcParams['mathtext.fontset'] = 'stixsans'
+matplotlib.rcParams['mathtext.it'] = 'Arial:italic'
+matplotlib.rcParams['mathtext.rm'] = 'Arial'
+matplotlib.rcParams['mathtext.bf'] = 'Arial:bold'
+matplotlib.rcParams['mathtext.it'] = 'Arial:italic'
+matplotlib.rcParams['mathtext.rm'] = 'Arial'
+matplotlib.rcParams['mathtext.sf'] = 'Arial'
+matplotlib.rcParams['mathtext.default'] = 'regular'
+matplotlib.rcParams['font.sans-serif'] = 'Arial'
+matplotlib.rcParams['font.family'] = 'sans-serif'
+matplotlib.rcParams['text.latex.preamble'] = [
+       r'\usepackage{siunitx}',   # i need upright \micro symbols, but you need...
+       r'\sisetup{detect-all}',   # ...this to force siunitx to actually use your fonts
+       r'\usepackage{helvet}',    # set the normal font here
+       r'\usepackage{sansmath}',  # load up the sansmath so that math -> helvet
+       r'\sansmath'               # <- tricky! -- gotta actually tell tex to use!
+]
+
+two_col_width = 7.20472 #inches
+single_col_width = 3.50394 #inches
+page_height = 10.62472 #inches
+font_size = 10
+
+#---------------------------------------------------
+#Define arguments
+def parse_inputs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-update", "--update_pickles", help="do you want to read the Flash output and update the pickles?", type=str, default='True')
+    parser.add_argument("files", nargs='*')
+    args = parser.parse_args()
+    return args
+
+#---------------------------------------------------
+#Get simulation files
+input_dir = sys.argv[1]
+args = parse_inputs()
+files = sorted(glob.glob(input_dir))
+
+import pdb
+pdb.set_trace()
+for pickle_file in pickle_files:
+    file = open(pickle_file, 'rb')
+    Time_array, L_primary, L_secondary, L_orbit, L_in_gas = pickle.load(file)
+    file.close()
