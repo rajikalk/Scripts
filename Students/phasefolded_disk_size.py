ab148719d18a9509b48486e1771f25359d70b951
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-05-24 14:45:36 +0200

    attempting to phasefold data

---
+++ Students/phasefolded_disk_size.py
@@ -0,0 +1,61 @@
+import yt
+import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+from matplotlib.offsetbox import AnchoredText
+import matplotlib.patches as patches
+class binary_timeseries:
+  def __init__(self,roche,separation,age,disk_size,disk_mass,mass_in_100,mass_in_5000,output,accretion,sink_mass):
+    self.age = age
+    self.disk_size = disk_size
+    self.roche = roche
+    self.separation = separation
+    self.disk_mass = disk_mass
+    self.mass_in_100 = mass_in_100
+    self.mass_in_5000 = mass_in_5000
+    self.output = output
+    self.accretion = accretion
+    self.sink_mass = sink_mass
+    self.units =  {'distances': 'au' , 'masses':'Msun','age':'kyr','accretion':'Lsun'}
+
+class single_timeseries:
+  def __init__(self, age,disk_size,disk_mass,mass_in_100,mass_in_5000,output,accretion,sink_mass):
+    self.age = age
+    self.disk_size = disk_size
+    self.disk_mass = disk_mass
+    self.mass_in_100 = mass_in_100
+    self.mass_in_5000 = mass_in_5000
+    self.outout = output
+    self.accretion = accretion
+    self.sink_mass = sink_mass
+    self.units =  {'distances': 'au' , 'masses':'Msun','age':'kyr','accretion':'Lsun'}
+
+path_to_pickle='/lustre/astro/vitot/viscosity_analysis/collected_metadata/HR_collected_metadata.pkl'
+
+information=open(path_to_pickle,"rb")
+metadata=pickle.load(information,encoding='bytes')
+information.close()
+print(metadata.keys())
+print(vars(metadata['90']))
+print(len(metadata['90'].separation ))
+print(len(metadata['90'].disk_size ))
+print(len(metadata['91'].separation ))
+print(len(metadata['91'].disk_size ))
+
+#Phasefold
+end_ind = np.argmin(abs(metadata['91'].age-yt.YTQuantity(120, 'kyr')))
+time = metadata['91'].age[:end_ind]
+separation = metadata['91'].separation[:end_ind]
+disk_secondary = metadata['91'].disk_size[:end_ind]
+ds_left = (separation[1:-1] - separation[:-2])/(time[1:-1] - time[:-2])
+ds_right = (separation[2:] - separation[1:-1])/(time[2:] - time[1:-1])
+periastron_inds = np.argwhere((ds_left<0)&(ds_right>0)).T[0]
+apastron_inds = np.argwhere((ds_left>0)&(ds_right<0)).T[0]
+
+for peri_ind in range(1, len(periastron_inds)):
+    t_orb = time[peri_ind-1:peri_ind]
+    disk_secondary = disk_secondary[peri_ind-1:peri_ind]
+    
+    #scale time
+    import pdb
+    pdb.set_trace()
