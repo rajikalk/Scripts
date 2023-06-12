fc09e9167bd8959ca586d2a6b2144a8fb7962e7e
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-06-12 16:05:25 +0200

    clean gup measuring and plotting L budget

---
+++ FLASH_analysis/L_budget_comparison.py
@@ -0,0 +1,190 @@
+import numpy as np
+import pickle
+import yt
+yt.enable_parallelism()
+import glob
+import sys
+import matplotlib.pyplot as plt
+import matplotlib
+from mpi4py.MPI import COMM_WORLD as CW
+import my_flash_fields as myf
+import my_flash_module as mym
+import pickle
+import argparse
+import os
+
+#------------------------------------------------------
+#get mpi size and ranks
+rank = CW.Get_rank()
+size = CW.Get_size()
+
+#------------------------------------------------------
+#Ploting parameters
+matplotlib.rcParams['mathtext.fontset'] = 'stixsans'
+matplotlib.rcParams['mathtext.it'] = 'Arial:italic'
+matplotlib.rcParams['mathtext.rm'] = 'Arial'
+matplotlib.rcParams['mathtext.bf'] = 'Arial:bold'
+matplotlib.rcParams['mathtext.it'] = 'Arial:italic'
+matplotlib.rcParams['mathtext.rm'] = 'Arial'
+matplotlib.rcParams['mathtext.sf'] = 'Arial'
+matplotlib.rcParams['mathtext.default'] = 'regular'
+matplotlib.rcParams['font.sans-serif'] = 'Arial'
+matplotlib.rcParams['font.family'] = 'sans-serif'
+matplotlib.rcParams['text.latex.preamble'] = [
+       r'\usepackage{siunitx}',   # i need upright \micro symbols, but you need...
+       r'\sisetup{detect-all}',   # ...this to force siunitx to actually use your fonts
+       r'\usepackage{helvet}',    # set the normal font here
+       r'\usepackage{sansmath}',  # load up the sansmath so that math -> helvet
+       r'\sansmath'               # <- tricky! -- gotta actually tell tex to use!
+]
+
+two_col_width = 7.20472 #inches
+single_col_width = 3.50394 #inches
+page_height = 10.62472 #inches
+font_size = 10
+
+Mach_labels = ['0.0', '0.1', '0.2']
+Spin_labels = ['0.20', '0.25', '0.30', '0.35']
+
+#---------------------------------------------------
+#Define arguments
+def parse_inputs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-update", "--update_pickles", help="do you want to read the Flash output and update the pickles?", type=str, default='True')
+    parser.add_argument("files", nargs='*')
+    args = parser.parse_args()
+    return args
+
+#---------------------------------------------------
+#Get simulation files
+args = parse_inputs()
+#files = sorted(glob.glob(input_dir + '*plt_cnt*'))
+
+plt.clf()
+fig, axs = plt.subplots(ncols=2, nrows=len(Spin_labels), figsize=(two_col_width, single_col_width*2.5), sharex=True, sharey=True)
+iter_range = range(0, len(Spin_labels))
+plt.subplots_adjust(wspace=0.0)
+plt.subplots_adjust(hspace=0.0)
+
+plot_it = -1
+xmax= 0
+ymax = 0
+for spin_lab in Spin_labels:
+    plot_it = plot_it + 1
+    for mach_lab in Mach_labels:
+        if np.remainder(plot_it, 2) == 0:
+    
+            single_pickle = '/home/kuruwira/fast/Analysis/Angular_momentum_budget/Flash_2023/Spin_'+spin_lab+'/Single/Mach_'+mach_lab+'/Lref_9/Spin_'+spin_lab+'_Single_Mach_'+mach_lab+'_Lref_9_gathered_ang_mom.pkl'
+            
+            if os.path.exists(single_pickle):
+                file = open(single_pickle, 'rb')
+                Time_array, L_primary, L_secondary, L_orbit, L_in_gas = pickle.load(file)
+                file.close()
+                
+                axs.flatten()[plot_it].plot(Time_array - Time_array[0], L_orbit, label='Orbit')
+                axs.flatten()[plot_it].plot(Time_array - Time_array[0], L_in_gas, label='Gas')
+                axs.flatten()[plot_it].plot(Time_array - Time_array[0], L_primary, label='Single')
+                axs.flatten()[plot_it].set_ylabel('$\Omega t_{ff}='+spin_lab+'$: L ($g\,cm^2/s$)')
+            else:
+                print("Couldn't open", single_pickle)
+            
+        else:
+            binary_pickle = '/home/kuruwira/fast/Analysis/Angular_momentum_budget/Flash_2023/Spin_'+spin_lab+'/Binary/Mach_'+mach_lab+'/Lref_9/Spin_'+spin_lab+'_Binary_Mach_'+mach_lab+'_Lref_9_gathered_ang_mom.pkl'
+
+            if os.path.exists(binary_pickle):
+                file = open(binary_pickle, 'rb')
+                Time_array, L_primary, L_secondary, L_orbit, L_in_gas = pickle.load(file)
+                file.close()
+                
+                axs.flatten()[plot_it].plot(Time_array - Time_array[0], L_orbit, label='Orbit')
+                axs.flatten()[plot_it].plot(Time_array - Time_array[0], L_in_gas, label='Gas')
+                axs.flatten()[plot_it].plot(Time_array - Time_array[0], L_primary, label='Primary')
+                axs.flatten()[plot_it].plot(Time_array - Time_array[0], L_primary, label='Secondary')
+            else:
+                print("Couldn't open", binary_pickle)
+            
+        if spin_lab == '0.2':
+            axs.flatten()[plot_it].legend()
+            if plot_it == 0:
+                axs.flatten()[plot_it].set_title('Single')
+            else:
+                axs.flatten()[plot_it].set_title('Binary')
+        if spin_lab == '0.35':
+            axs.flatten()[plot_it].set_xlabel('Time ($yr$)')
+        axs.flatten()[plot_it].tick_params(axis='both', which='major', labelsize=font_size, right=True)
+        axs.flatten()[plot_it].tick_params(axis='both', which='minor', labelsize=font_size, right=True)
+        axs.flatten()[plot_it].tick_params(axis='x', direction='in')
+        axs.flatten()[plot_it].tick_params(axis='y', direction='in')
+    
+axs.flatten()[plot_it].ylim([0,1])
+axs.flatten()[plot_it].xlim(left=0)
+plt.savefig('L_evolution.png', bbox_inches='tight')
+
+plt.clf()
+fig, axs = plt.subplots(ncols=2, nrows=len(Spin_labels), figsize=(two_col_width, single_col_width*2.5), sharex=True, sharey=True)
+iter_range = range(0, len(Spin_labels))
+plt.subplots_adjust(wspace=0.0)
+plt.subplots_adjust(hspace=0.0)
+
+plot_it = -1
+xmax= 0
+ymax = 0
+for spin_lab in Spin_labels:
+    for mach_lab in Mach_labels:
+        plot_it = plot_it + 1
+        
+        if np.remainder(plot_it, 2) == 0:
+    
+            single_pickle = '/home/kuruwira/fast/Analysis/Angular_momentum_budget/Flash_2023/Spin_'+spin_lab+'/Single/Mach_'+mach_lab+'/Lref_9/Spin_'+spin_lab+'_Single_Mach_'+mach_lab+'_Lref_9_gathered_ang_mom.pkl'
+            
+            if os.path.exists(single_pickle):
+                file = open(single_pickle, 'rb')
+                Time_array, L_primary, L_secondary, L_orbit, L_in_gas = pickle.load(file)
+                file.close()
+                
+                L_tot = L_primary + np.nan_to_num(L_secondary) + L_orbit + L_in_gas
+                
+                axs.flatten()[plot_it].plot(Time_array - Time_array[0], L_orbit/L_tot, label='Orbit')
+                axs.flatten()[plot_it].plot(Time_array - Time_array[0], L_in_gas/L_tot, label='Gas')
+                axs.flatten()[plot_it].plot(Time_array - Time_array[0], L_primary/L_tot, label='Single')
+                axs.flatten()[plot_it].set_ylabel('$\Omega t_{ff}='+spin_lab+'$: L ($g\,cm^2/s$)')
+            else:
+                print("Couldn't open", single_pickle)
+            
+            
+        else:
+            binary_pickle = '/home/kuruwira/fast/Analysis/Angular_momentum_budget/Flash_2023/Spin_'+spin_lab+'/Binary/Mach_'+mach_lab+'/Lref_9/Spin_'+spin_lab+'_Binary_Mach_'+mach_lab+'_Lref_9_gathered_ang_mom.pkl'
+
+            if os.path.exists(binary_pickle):
+                file = open(binary_pickle, 'rb')
+                Time_array, L_primary, L_secondary, L_orbit, L_in_gas = pickle.load(file)
+                file.close()
+                
+                L_tot = L_primary + np.nan_to_num(L_secondary) + L_orbit + L_in_gas
+                
+                axs.flatten()[plot_it].plot(Time_array - Time_array[0], L_orbit/L_tot, label='Orbit')
+                axs.flatten()[plot_it].plot(Time_array - Time_array[0], L_in_gas/L_tot, label='Gas')
+                axs.flatten()[plot_it].plot(Time_array - Time_array[0], L_primary/L_tot, label='Primary')
+                axs.flatten()[plot_it].plot(Time_array - Time_array[0], L_primary/L_tot, label='Secondary')
+            else:
+                print("Couldn't open", binary_pickle)
+            
+        if spin_lab == '0.2':
+            axs.flatten()[plot_it].legend()
+            if plot_it == 0:
+                axs.flatten()[plot_it].set_title('Single')
+            else:
+                axs.flatten()[plot_it].set_title('Binary')
+        if spin_lab == '0.35':
+            axs.flatten()[plot_it].set_xlabel('Time ($yr$)')
+        axs.flatten()[plot_it].tick_params(axis='both', which='major', labelsize=font_size, right=True)
+        axs.flatten()[plot_it].tick_params(axis='both', which='minor', labelsize=font_size, right=True)
+        axs.flatten()[plot_it].tick_params(axis='x', direction='in')
+        axs.flatten()[plot_it].tick_params(axis='y', direction='in')
+    
+axs.flatten()[plot_it].ylim([0,1])
+axs.flatten()[plot_it].xlim(left=0)
+plt.savefig('L_evolution_frac.png', bbox_inches='tight')
+
+sys.stdout.flush()
+CW.Barrier()
