c6dfc1eb428607b3b131ad6683623a334f854f24
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-06-12 17:19:59 +0200

    clean gup measuring and plotting L budget

--- FLASH_analysis/L_budget_comparison.py
+++ FLASH_analysis/L_budget_comparison.py
@@ -121,18 +121,18 @@ for spin_lab in Spin_labels:
                 else:
                     print("Couldn't open", binary_pickle)
                 
-            if spin_lab == '0.2':
-                axs.flatten()[plot_it].legend()
-                if plot_it == 0:
-                    axs.flatten()[plot_it].set_title('Single')
-                else:
-                    axs.flatten()[plot_it].set_title('Binary')
-            if spin_lab == '0.35':
-                axs.flatten()[plot_it].set_xlabel('Time ($yr$)')
-            axs.flatten()[plot_it].tick_params(axis='both', which='major', labelsize=font_size, right=True)
-            axs.flatten()[plot_it].tick_params(axis='both', which='minor', labelsize=font_size, right=True)
-            axs.flatten()[plot_it].tick_params(axis='x', direction='in')
-            axs.flatten()[plot_it].tick_params(axis='y', direction='in')
+        if spin_lab == '0.2':
+            axs.flatten()[plot_it].legend()
+            if plot_it == 0:
+                axs.flatten()[plot_it].set_title('Single')
+            else:
+                axs.flatten()[plot_it].set_title('Binary')
+        if spin_lab == '0.35':
+            axs.flatten()[plot_it].set_xlabel('Time ($yr$)')
+        axs.flatten()[plot_it].tick_params(axis='both', which='major', labelsize=font_size, right=True)
+        axs.flatten()[plot_it].tick_params(axis='both', which='minor', labelsize=font_size, right=True)
+        axs.flatten()[plot_it].tick_params(axis='x', direction='in')
+        axs.flatten()[plot_it].tick_params(axis='y', direction='in')
 
 axs.flatten()[plot_it].set_xlim(left=0)
 plt.savefig('L_evolution.png', bbox_inches='tight')
@@ -227,7 +227,7 @@ for spin_lab in Spin_labels:
     axs.flatten()[plot_it].tick_params(axis='x', direction='in')
     axs.flatten()[plot_it].tick_params(axis='y', direction='in')
     
-axs.flatten()[plot_it].set_ylim(top=1)
+axs.flatten()[plot_it].set_ylim([0,1])
 axs.flatten()[plot_it].set_xlim(left=0)
 plt.savefig('L_evolution_frac.png', bbox_inches='tight')
 
