fe3b9afdff911d247347c8aa4ef46b5ef481dd95
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-06-12 16:57:33 +0200

    clean gup measuring and plotting L budget

--- FLASH_analysis/L_budget_comparison.py
+++ FLASH_analysis/L_budget_comparison.py
@@ -130,7 +130,7 @@ for spin_lab in Spin_labels:
         axs.flatten()[plot_it].tick_params(axis='x', direction='in')
         axs.flatten()[plot_it].tick_params(axis='y', direction='in')
 
-axs.flatten()[plot_it].set_ylim(top=1])
+axs.flatten()[plot_it].set_ylim(top=1)
 axs.flatten()[plot_it].set_xlim(left=0)
 plt.savefig('L_evolution.png', bbox_inches='tight')
 
@@ -225,7 +225,7 @@ for spin_lab in Spin_labels:
         axs.flatten()[plot_it].tick_params(axis='x', direction='in')
         axs.flatten()[plot_it].tick_params(axis='y', direction='in')
     
-axs.flatten()[plot_it].set_ylim(top=1])
+axs.flatten()[plot_it].set_ylim(top=1)
 axs.flatten()[plot_it].set_xlim(left=0)
 plt.savefig('L_evolution_frac.png', bbox_inches='tight')
 
