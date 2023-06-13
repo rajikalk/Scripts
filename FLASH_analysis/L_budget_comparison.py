fe2c038669c1521b530b698d50e692036b92fde9
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-06-13 14:50:15 +0200

    clean gup measuring and plotting L budget

--- FLASH_analysis/L_budget_comparison.py
+++ FLASH_analysis/L_budget_comparison.py
@@ -85,10 +85,6 @@ for spin_lab in Spin_labels:
                     Time_array, L_primary, L_secondary, L_orbit, L_in_gas = pickle.load(file)
                     file.close()
                     
-                    if np.min(L_primary[-1*int(len(L_primary)/2):])<1.e-4:
-                        import pdb
-                        pdb.set_trace()
-                    
                     for time_it in range(len(L_orbit)):
                         try:
                             if len(L_orbit[time_it]) == 3:
@@ -110,10 +106,6 @@ for spin_lab in Spin_labels:
                     file = open(binary_pickle, 'rb')
                     Time_array, L_primary, L_secondary, L_orbit, L_in_gas = pickle.load(file)
                     file.close()
-                    
-                    if np.min(L_primary[-1*int(len(L_primary)/2):])<1.e-4:
-                        import pdb
-                        pdb.set_trace()
                     
                     for time_it in range(len(L_orbit)):
                         try:
@@ -170,10 +162,6 @@ for spin_lab in Spin_labels:
                     Time_array, L_primary, L_secondary, L_orbit, L_in_gas = pickle.load(file)
                     file.close()
                     
-                    if np.min(L_primary[-1*int(len(L_primary)/2):])<1.e-4:
-                        import pdb
-                        pdb.set_trace()
-                    
                     L_orb_fixed = []
                     for time_it in range(len(L_orbit)):
                         try:
@@ -191,6 +179,9 @@ for spin_lab in Spin_labels:
                     #L_tot = yt.YTArray(np.nan_to_num(L_primary) + np.nan_to_num(L_secondary) + L_in_gas, 'g*cm**2/s')
                     #L_tot = L_tot + L_orbit
                     
+                    if np.min((L_primary/L_tot)[-1*int(len(L_primary)/2):])<1.e-4:
+                        import pdb
+                        pdb.set_trace()
                     
                     axs.flatten()[plot_it].semilogy(Time_array - Time_array[0], L_orb_fixed/L_tot, label='Orbit', linestyle = mach_ls[mach_it], color=colors[0])
                     axs.flatten()[plot_it].semilogy(Time_array - Time_array[0], L_in_gas/L_tot, label='Gas', linestyle = mach_ls[mach_it], color=colors[1])
@@ -208,10 +199,6 @@ for spin_lab in Spin_labels:
                     Time_array, L_primary, L_secondary, L_orbit, L_in_gas = pickle.load(file)
                     file.close()
                     
-                    if np.min(L_primary[-1*int(len(L_primary)/2):])<1.e-4:
-                        import pdb
-                        pdb.set_trace()
-                    
                     L_orb_fixed = []
                     for time_it in range(len(L_orbit)):
                         try:
@@ -226,6 +213,10 @@ for spin_lab in Spin_labels:
                     L_orb_fixed = yt.YTArray(L_orb_fixed, 'g*cm**2/s')
                     
                     L_tot = np.nan_to_num(L_primary) + np.nan_to_num(L_secondary) + np.nan_to_num(L_orb_fixed).value + L_in_gas
+                    
+                    if np.min((L_primary/L_tot)[-1*int(len(L_primary)/2):])<1.e-4:
+                        import pdb
+                        pdb.set_trace()
                     
                     axs.flatten()[plot_it].semilogy(Time_array - Time_array[0], L_orb_fixed/L_tot, label='Orbit', linestyle = mach_ls[mach_it], color=colors[0])
                     axs.flatten()[plot_it].semilogy(Time_array - Time_array[0], L_in_gas/L_tot, label='Gas', linestyle = mach_ls[mach_it], color=colors[1])
