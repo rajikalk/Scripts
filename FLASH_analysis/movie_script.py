3bab8f7c0917fa591093c7deea8d30438a10f0d1
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-06-23 10:59:03 +0200

    Trying to get around plotting empty images

--- FLASH_analysis/movie_script.py
+++ FLASH_analysis/movie_script.py
@@ -269,7 +269,8 @@ if args.make_movie_frames == 'True':
                 else:
                     plt.streamplot(X_image.value, Y_image.value, magx.value, magy.value, density=4, linewidth=0.25, minlength=0.5, zorder=2)
                     #plt.streamplot(X_image, Y_image, magx, magy, density=4, linewidth=0.25, minlength=0.5, zorder=2)
-                cbar = plt.colorbar(plot, pad=0.0)
+                if np.isnan(np.nanmean(image)) == False:
+                    cbar = plt.colorbar(plot, pad=0.0)
                 mym.my_own_quiver_function(ax, X_vel, Y_vel, velx.value, vely.value, plot_velocity_legend=True, limits=[xlim, ylim], Z_val=None, standard_vel=stdvel)
 
                 if len(part_info.keys())>0:
