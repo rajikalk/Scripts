946c59e312014d57cf15a28a77d8ee6fb1540657
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-06-23 11:08:12 +0200

    Trying to get around plotting empty images

--- Modules/my_flash_fields.py
+++ Modules/my_flash_fields.py
@@ -226,7 +226,7 @@ def _L_gas_wrt_nearest_sink(field, data):
         L_gas = data['gas', 'mass'].value * np.cross(d_vel_gas, d_pos_gas).T
         L_wrt_nearest = yt.YTArray(np.sqrt(np.sum(L_gas**2, axis=0)), 'g*cm**2/s')
     else:
-        L_wrt_nearest = yt.YTArray(np.nan*np.ones(np.shape(data['x'])), 'g*cm**2/s')
+        L_wrt_nearest = data['gas', 'L_gas_wrt_CoM']
     return L_wrt_nearest
 
 yt.add_field("L_gas_wrt_nearest_sink", function=_L_gas_wrt_nearest_sink, units=r"g*cm**2/s", sampling_type="local")
