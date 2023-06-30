4d8dff796865732ad0450ae9cef3eb572f9306b5
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-06-30 15:38:02 +0200

    Finding triple stars for Shanghuo

--- Ramses_analysis/CF_analysis/triple_quad_birth_cons.py
+++ Ramses_analysis/CF_analysis/triple_quad_birth_cons.py
@@ -106,6 +106,64 @@ t_max = np.nan
 G50_t_max = 0
 '''
 
+sink_files = sorted(glob.glob("/lustre/astro/troels/IMF_G400/data/output*/*.dat"))
+files = sorted(glob.glob("/lustre/astro/troels/IMF_G400/data/*/info*.txt"))
+rm_files = []
+for info_name in files:
+    sink_file = info_name.split('info')[0]+'stars_output.dat'
+    if sink_file not in sink_files:
+        rm_files.append(info_name)
+for rm_file in rm_files:
+    files.remove(rm_file)
+del sink_files
+gc.collect()
+
+#Define units to override:
+units_override = {"length_unit":(4.0,"pc"), "velocity_unit":(0.18, "km/s"), "time_unit":(685706129102738.9, "s")}
+
+simulation_density_id = global_pickle.split('/G')[-1].split('/')[0] #save_dir.split('G')[-1].split('/')[0]
+
+if simulation_density_id == '50':
+    Grho=50
+    units_override.update({"mass_unit":(1500,"Msun")})
+elif simulation_density_id == '100':
+    Grho=100
+    units_override.update({"mass_unit":(3000,"Msun")})
+elif simulation_density_id == '125':
+    Grho=125
+    units_override.update({"mass_unit":(3750,"Msun")})
+elif simulation_density_id == '150':
+    Grho=150
+    units_override.update({"mass_unit":(4500,"Msun")})
+elif simulation_density_id == '200':
+    Grho=200
+    units_override.update({"mass_unit":(6000,"Msun")})
+elif simulation_density_id == '400':
+    Grho=400
+    units_override.update({"mass_unit":(12000,"Msun")})
+else:
+    print("MASS UNIT NOT SET")
+    import pdb
+    pdb.set_trace()
+    
+del simulation_density_id
+gc.collect()
+
+units_override.update({"density_unit":(units_override['mass_unit'][0]/units_override['length_unit'][0]**3, "Msun/pc**3")})
+    
+scale_l = yt.YTQuantity(units_override['length_unit'][0], units_override['length_unit'][1]).in_units('cm') # 4 pc
+scale_v = yt.YTQuantity(units_override['velocity_unit'][0], units_override['velocity_unit'][1]).in_units('cm/s')         # 0.18 km/s == sound speed
+scale_t = scale_l/scale_v # 4 pc / 0.18 km/s
+scale_d = yt.YTQuantity(units_override['density_unit'][0], units_override['density_unit'][1]).in_units('g/cm**3')  # 2998 Msun / (4 pc)^3
+
+units={}
+for key in units_override.keys():
+    units.update({key:yt.YTQuantity(units_override[key][0], units_override[key][1])})
+
+print('Calculated the units on rank', rank)
+sys.stdout.flush()
+CW.Barrier()
+
 #================================================================================================
 #plot fractions of core fragmentation and dynamical capture
 
@@ -127,6 +185,10 @@ for pick_it in range(len(pickle_files)):
                     if superplot_dict['System_seps'][sys_key][0][0] < 1000 and superplot_dict['System_seps'][sys_key][0][0] > 100 and superplot_dict['System_seps'][sys_key][0][1] > 300 and superplot_dict['System_seps'][sys_key][0][1] < 3000:
                         if youngest_birth_con[0] == True:
                             print("Found Triple candidate:", sys_key)
+                            #make frame!
+                            import pdb
+                            pdb.set_trace()
+                            
                 except:
                     pass
             if len(flatten(eval(sys_key))) == 4:
