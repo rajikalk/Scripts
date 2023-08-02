942418d384013a80822c7f472cabfec62ecb4707
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-08-02 13:46:02 +0200

    figuring out why particle mass annotations are messing up

--- Modules/my_flash_module.py
+++ Modules/my_flash_module.py
@@ -354,6 +354,8 @@ def my_own_quiver_function(axis, X_pos, Y_pos, X_val, Y_val, plot_velocity_legen
         width_val = width_ceil
         annotate_text = axis.text((xmax - 0.01*(xmax-xmin)), (ymin + 0.05*(ymax-ymin)), legend_text, va="center", ha="right", color='w', fontsize=fontsize_global)
         annotate_text.set_path_effects([path_effects.Stroke(linewidth=3, foreground='black'), path_effects.Normal()])
+        import pdb
+        pdb.set_trace()
         axis.add_patch(mpatches.FancyArrowPatch((pos_start[0], pos_start[1]), (pos_start[0]+xvel, pos_start[1]+yvel), arrowstyle='->', color='w', linewidth=width_val, mutation_scale=10.*width_val, alpha=width_val/width_ceil))
     return axis
 
