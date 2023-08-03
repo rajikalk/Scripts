b1c391a4b5a8634d2572eda948d96d45d3fb064f
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-08-03 11:06:13 +0200

    Making sure particles are coded by formation time

--- FLASH_analysis/Fig_1_frames.py
+++ FLASH_analysis/Fig_1_frames.py
@@ -124,11 +124,11 @@ for spin_val in spin_values:
         if spin_val == '0.20':
             #add mach labels:
             mach_label = "$\mathcal{M}$="+str(mach_val)
-            title_text = axs.flatten()[plot_it].text((np.mean(xlim)), (ylim[1]-0.04*(ylim[1]-ylim[0])), mach_label, va="center", ha="center", color='w', fontsize=(font_size))
+            title_text = axs.flatten()[plot_it].text((np.mean(xlim)), (ylim[1]-0.04*(ylim[1]-ylim[0])), mach_label, va="center", ha="center", color='w', fontsize=(font_size), bbox=dict(facecolor='none', edgecolor='white', boxstyle='round'))
             title_text.set_path_effects([path_effects.Stroke(linewidth=3, foreground='black'), path_effects.Normal()])
         if mach_val =='0.0':
             spin_label = "$\Omega t_{\mathrm{ff}}$="+str(spin_val)
-            title_text = axs.flatten()[plot_it].text((xlim[0]+0.04*(xlim[1]-xlim[0])), np.mean(ylim), spin_label, va="center", ha="center", color='w', fontsize=(font_size), rotation = 90)
+            title_text = axs.flatten()[plot_it].text((xlim[0]+0.04*(xlim[1]-xlim[0])), np.mean(ylim), spin_label, va="center", ha="center", color='w', fontsize=(font_size), rotation = 90, bbox=dict(facecolor='none', edgecolor='white', boxstyle='round'))
             title_text.set_path_effects([path_effects.Stroke(linewidth=3, foreground='black'), path_effects.Normal()])
             
         
