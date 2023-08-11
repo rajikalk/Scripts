bca1b89d29f02e62ce93ec7794d41ef6a6a07523
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-08-11 17:01:43 +0200

    cleaning up Fig_1

--- FLASH_analysis/Fig_1_frames.py
+++ FLASH_analysis/Fig_1_frames.py
@@ -107,7 +107,7 @@ for spin_val in spin_values:
                     part_info['particle_mass'] = part_info['particle_mass'][sort_inds]
                     part_info['particle_tag'] = part_info['particle_tag'][sort_inds]
                     part_info['particle_form_time'] = part_info['particle_form_time'][sort_inds]
-            mym.annotate_particles(axs.flatten()[plot_it], part_info['particle_position'], part_info['accretion_rad'], limits=[xlim, ylim], annotate_field=part_info['particle_mass'], particle_tags=part_info['particle_tag'], zorder=7, split_threshold=10)
+        mym.annotate_particles(axs.flatten()[plot_it], part_info['particle_position'], part_info['accretion_rad'], limits=[xlim, ylim], annotate_field=part_info['particle_mass'], particle_tags=part_info['particle_tag'], zorder=7, split_threshold=10)
         
         axs.flatten()[plot_it].tick_params(axis='both', which='major', labelsize=font_size)
         for line in axs.flatten()[plot_it].xaxis.get_ticklines():
