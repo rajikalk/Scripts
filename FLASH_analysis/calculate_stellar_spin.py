061cfb6ef745b73ba1b0e73f4b156a4266e92aaa
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-08-16 14:29:35 +0200

    calculating rotation rate

--- FLASH_analysis/calculate_stellar_spin.py
+++ FLASH_analysis/calculate_stellar_spin.py
@@ -214,6 +214,6 @@ for spin_lab in Spin_labels:
         
         plot_it = plot_it + 1
 
-axs.flatten()[plot_it-1].set_ylim([0, 30])
+axs.flatten()[plot_it-1].set_ylim([0, 15])
 axs.flatten()[plot_it-1].set_xlim([0, 10000])
 plt.savefig('rotation_rate_lin.pdf', bbox_inches='tight')
