e31b5471dbb2ef480caf7fdeb060aefb4ec3436e
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-08-22 13:52:41 +0200

    updating resolution study

--- Ramses_analysis/FU_ori_investigation/resolution_study.py
+++ Ramses_analysis/FU_ori_investigation/resolution_study.py
@@ -84,6 +84,7 @@ for pick_file in pickle_files:
     '''
     axs.flatten()[1].semilogy(particle_data['time'][t_start:t_end], np.array(particle_data['separation'][t_start:t_end]), color=proj_colours[cit])
     axs.flatten()[1].set_ylabel('Separation (au)')
+    axs.flatten()[1].axhline(y=[1.5, 3, 6])
     #axs.flatten()[1].set_ylim()
     
 
