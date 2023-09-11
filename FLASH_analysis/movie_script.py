83adf5d70168ad0229240e06c70c0061c19184ad
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-09-11 09:03:04 +0200

    making sure to plot plot frame for specific time if specificied

--- FLASH_analysis/movie_script.py
+++ FLASH_analysis/movie_script.py
@@ -198,7 +198,7 @@ if args.make_movie_pickles == 'True':
             
             #Make projections of each field
             proj_depth = yt.ProjectionPlot(ds, args.axis, ['z', 'Neg_z', 'dz', 'Neg_dz'], width=(args.plot_width,'au'), weight_field=None, data_source=region, method='mip', center=(center_pos, 'AU'))
-            thickness = ((proj_depth.frb.data[('gas', 'Neg_z')].in_units('cm') + proj_depth.frb.data[('gas', 'Neg_dz')].in_units('cm')/2.) + (proj_depth.frb.data[('gas', 'z')].in_units('cm') + proj_depth.frb.data[('gas', 'dz')].in_units('cm')/2.))
+            thickness = ((proj_depth.frb.data[('gas', 'Neg_z')].in_units('cm') + proj_depth.frb.data[('gas', 'Neg_dz')].in_units('cm')/2.) + (proj_depth.frb.data[('flash', 'z')].in_units('cm') + proj_depth.frb.data[('flash', 'dz')].in_units('cm')/2.))
             proj_dict = {}
             for sto, field in yt.parallel_objects(proj_field_list, storage=proj_dict):
                 #print("Projecting field", field, "on rank", rank)
