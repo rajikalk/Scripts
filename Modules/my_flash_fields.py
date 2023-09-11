ec97d40c1e3341124e6d8be09a1d0efea87a876e
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-09-11 08:56:05 +0200

    making sure to plot plot frame for specific time if specificied

--- Modules/my_flash_fields.py
+++ Modules/my_flash_fields.py
@@ -503,4 +503,18 @@ def _Relative_keplerian_velocity_wrt_primary(field, data):
 
 yt.add_field("Relative_keplerian_velocity_wrt_primary", function=_Relative_keplerian_velocity_wrt_primary, units=r"", sampling_type="local")
 
+def _Neg_z(field, data):
+    """
+    returns the negative of the z-positions
+    """
+    return -1*data['z']
 
+yt.add_field("Neg_z", function=_Neg_z, units=r"cm")
+    
+def _Neg_dz(field, data):
+    """
+    returns the negative of the dz
+    """
+    return -1*data['dz']
+
+yt.add_field("Neg_dz", function=_Neg_z, units=r"cm")
