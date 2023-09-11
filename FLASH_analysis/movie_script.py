bd77c00386213f71939fede5821afc8cfdd4211d
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-09-11 09:14:23 +0200

    making sure to plot plot frame for specific time if specificied

--- FLASH_analysis/movie_script.py
+++ FLASH_analysis/movie_script.py
@@ -203,7 +203,7 @@ if args.make_movie_pickles == 'True':
             for sto, field in yt.parallel_objects(proj_field_list, storage=proj_dict):
                 #print("Projecting field", field, "on rank", rank)
                 proj = yt.ProjectionPlot(ds, args.axis, field, method='integrate', data_source=region, width=(args.plot_width,'au'), weight_field=None, center=(center_pos, 'AU'))
-                thickness = (proj.bounds[1] - proj.bounds[0]).in_cgs() #MIGHT HAVE TO UPDATE THIS LATER
+                #thickness = (proj.bounds[1] - proj.bounds[0]).in_cgs() #MIGHT HAVE TO UPDATE THIS LATER
                 proj_array = proj.frb.data[field].in_cgs()/thickness
                 if args.axis == 'y':
                     proj_array = proj_array.T
