7d9ec3fa1ecaae2aad06250c48de424cd24bf830
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-09-12 14:43:52 +0200

    making histogram for Shanghuo

--- Ramses_analysis/CF_analysis/Shanghuo/Initial_separation_histogram.py
+++ Ramses_analysis/CF_analysis/Shanghuo/Initial_separation_histogram.py
@@ -255,7 +255,7 @@ for pick_it in range(len(Initial_Seps_all)):
         
         plt.clf()
         plt.cla()
-        fig, axs = plt.subplots(ncols=1, nrows=1, figsize=(single_col_width, 0.7*single_col_width))
+        fig, axs = plt.subplots(ncols=1, nrows=1, figsize=(single_col_width, 0.75*single_col_width))
         plt.bar(bin_centers, core_sep_hist_normalised, width=0.25, color='b', label='Bound core frag. (KH23)', alpha=0.5)
         plt.bar(bin_centers, core_delayed_sep_hist_normalised, width=0.25, bottom=core_sep_hist_normalised, color='m', label='Unbound core frag. (KH23)', alpha=0.5)
         plt.bar(bin_centers, Shanghuo_hist_normalised, width=0.25, color='grey', label='This Paper', alpha=0.5)
