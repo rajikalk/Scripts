c74a1b3d0bc34effc979f1c3430b3e51715fd8fe
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-09-12 16:20:19 +0200

    Attempting to correlate accretion bursts and outflow bursts

---
+++ Ramses_analysis/Outflow_analysis/Burst_Analysis.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+import yt
+yt.enable_parallelism()
+import glob
+import numpy as np
+import sys
+import os
+import my_ramses_module as mym
+import my_ramses_fields as myf
+from mpi4py.MPI import COMM_WORLD as CW
+import pickle
+
+def parse_inputs():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-sim_dens_id", "--simulation_density_id", help="G50, G100, G200 or G400?", type=str, default="G100")
+    parser.add_argument("files", nargs='*')
+    args = parser.parse_args()
+    return args
+
+#=======MAIN=======
+
+rank = CW.Get_rank()
+size = CW.Get_size()
+if rank == 0:
+    print("size =", size)
+
+#Get input and output directories
+args = parse_inputs()
+
+#Define relevant directories
+input_dir = sys.argv[1]
+save_dir = sys.argv[2]
+if os.path.exists(save_dir) == False:
+    os.makedirs(save_dir)
+
+sys.stdout.flush()
+CW.Barrier()
+
+#Set some plot variables independant on data files
+sys.stdout.flush()
+CW.Barrier()
+
+#File files
+files = sorted(glob.glob(input_dir+"*/info*.txt"))
+
+sys.stdout.flush()
+CW.Barrier()
+
+#Define units to override:
+units_override = {"length_unit":(4.0,"pc"), "velocity_unit":(0.18, "km/s"), "time_unit":(685706129102738.9, "s")}
+
+if args.simulation_density_id == 'G50':
+    units_override.update({"mass_unit":(1500,"Msun")})
+elif args.simulation_density_id == 'G200':
+    units_override.update({"mass_unit":(6000,"Msun")})
+elif args.simulation_density_id == 'G400':
+    units_override.update({"mass_unit":(12000,"Msun")})
+else:
+    units_override.update({"mass_unit":(2998,"Msun")})
+
+units_override.update({"density_unit":(units_override['mass_unit'][0]/units_override['length_unit'][0]**3, "Msun/pc**3")})
+    
+scale_l = yt.YTQuantity(units_override['length_unit'][0], units_override['length_unit'][1]).in_units('cm').value # 4 pc
+scale_v = yt.YTQuantity(units_override['velocity_unit'][0], units_override['velocity_unit'][1]).in_units('cm/s').value         # 0.18 km/s == sound speed
+scale_t = scale_l/scale_v # 4 pc / 0.18 km/s
+scale_d = yt.YTQuantity(units_override['density_unit'][0], units_override['density_unit'][1]).in_units('g/cm**3').value  # 2998 Msun / (4 pc)^3
+mym.set_units(units_override)
+
+
+#find sink particle to center on and formation time
+ds = yt.load(files[-1], units_override=units_override)
+#try:
+dd = ds.all_data()
+if args.sink_number == None:
+    sink_id = np.argmin(dd['sink_particle_speed'])
+else:
+    sink_id = args.sink_number
+if rank == 0:
+    print("CENTERED SINK ID:", sink_id)
+myf.set_centred_sink_id(sink_id)
+sink_form_time = dd['sink_particle_form_time'][sink_id]
+del dd
+
+if args.make_pickles == 'True':
+    file_int = -1
+    for fn in yt.parallel_objects(files, njobs=int(size/6)):
+        ds = yt.load(fn, units_override=units_override)
+        dd = ds.all_data()
+            
+        #Define box:
+        import pdb
+        pdb.set_trace()
+        center_pos = dd['Center_Position'].in_units('au').value
+        
