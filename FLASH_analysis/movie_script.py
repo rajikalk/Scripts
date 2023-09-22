5a5b3dc6e5502e3a114285e7e8497d933e6838e1
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-09-22 17:11:11 +0200

    plotting distance from primary

--- FLASH_analysis/movie_script.py
+++ FLASH_analysis/movie_script.py
@@ -174,9 +174,12 @@ if args.make_movie_pickles == 'True':
             if args.field == 'dens':
                 proj_field_list = [('flash', 'dens')]
             else:
-                proj_field_list = [field for field in ds.derived_field_list if (args.field in field[1])]
-                if len(proj_field_list) > 1:
-                    proj_field_list = [proj_field_list[0]]
+                try:
+                    proj_field_list = [field for field in ds.derived_field_list if (args.field == field[1])]
+                else:
+                    proj_field_list = [field for field in ds.derived_field_list if (args.field in field[1])]
+                    if len(proj_field_list) > 1:
+                        proj_field_list = [proj_field_list[0]]
                 
             proj_field_list = proj_field_list + \
                 [field for field in ds.field_list if ('vel'in field[1])&(field[0]=='flash')&('vel'+args.axis not in field[1])] + \
