75243ee824187c077c90a65acc1418fe56eaa87a
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-09-25 14:24:49 +0200

    attempting to use the thickness calculated from dz

--- FLASH_analysis/movie_script.py
+++ FLASH_analysis/movie_script.py
@@ -215,7 +215,7 @@ if args.make_movie_pickles == 'True':
             
             thickness = z_proj_max.frb.data[('gas', 'z')].in_cgs() + abs(z_proj_min.frb.data[('gas', 'z')].in_cgs())
             
-            thickness_proj = yt.ProjectionPlot(ds, args.axis, ('gas', 'dz'), method='integrate', data_source=region, width=plot_width, weight_field=args.weight_field, center=center_pos)
+            thickness_proj = yt.ProjectionPlot(ds, args.axis, ('gas', 'dz'), method='integrate', data_source=region, width=plot_width, weight_field='volume', center=center_pos)
             thickness = thickness_proj.frb.data[('gas', 'dz')].in_cgs()
             
             proj_dict = {}
@@ -224,7 +224,7 @@ if args.make_movie_pickles == 'True':
                 proj = yt.ProjectionPlot(ds, args.axis, field, method='integrate', data_source=region, width=plot_width, weight_field=args.weight_field, center=center_pos)
                 if args.weight_field == None:
                     #thickness = (proj.bounds[1] - proj.bounds[0]).in_cgs() #MIGHT HAVE TO UPDATE THIS LATER
-                    proj_array = proj.frb.data[field].in_cgs()/thickness
+                    proj_array = proj.frb.data[field].in_cgs()#/thickness
                 else:
                     proj_array = proj.frb.data[field].in_cgs()
                 if args.axis == 'y':
