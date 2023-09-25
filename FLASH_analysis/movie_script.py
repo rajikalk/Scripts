21a91d704746af84776684f91b8decc897d1e1c6
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-09-25 11:58:39 +0200

    attempting to use the thickness calculated from dz

--- FLASH_analysis/movie_script.py
+++ FLASH_analysis/movie_script.py
@@ -209,12 +209,16 @@ if args.make_movie_pickles == 'True':
             #Make projections of each field
             #proj_depth = yt.ProjectionPlot(ds, args.axis, [('flash', 'z'), ('gas', 'Neg_z'), ('flash', 'dz'), ('gas', 'Neg_dz')], width=(args.plot_width,'au'), weight_field=None, data_source=region, method='mip', center=(center_pos, 'AU'))
             #thickness = ((proj_depth.frb.data[('gas', 'Neg_z')].in_units('cm') + proj_depth.frb.data[('gas', 'Neg_dz')].in_units('cm')/2.) + (proj_depth.frb.data[('flash', 'z')].in_units('cm') + proj_depth.frb.data[('flash', 'dz')].in_units('cm')/2.))
+            
+            proj_thickness = proj = yt.ProjectionPlot(ds, args.axis, 'dz', method='integrate', data_source=region, width=plot_width, weight_field=None, center=center_pos)
+            thickness = proj_thickness.frb.data['dz'].in_cgs()
+            
             proj_dict = {}
             for sto, field in yt.parallel_objects(proj_field_list, storage=proj_dict):
                 #print("Projecting field", field, "on rank", rank)
                 proj = yt.ProjectionPlot(ds, args.axis, field, method='integrate', data_source=region, width=plot_width, weight_field=args.weight_field, center=center_pos)
                 if args.weight_field == None:
-                    thickness = (proj.bounds[1] - proj.bounds[0]).in_cgs() #MIGHT HAVE TO UPDATE THIS LATER
+                    #thickness = (proj.bounds[1] - proj.bounds[0]).in_cgs() #MIGHT HAVE TO UPDATE THIS LATER
                     proj_array = proj.frb.data[field].in_cgs()/thickness
                 else:
                     proj_array = proj.frb.data[field].in_cgs()
