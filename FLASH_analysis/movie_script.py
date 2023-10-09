2ff53a9e4aac7b9c55dffafb9aa04fd852346f89
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-10-09 17:08:24 +0200

    velocity update looks right

--- FLASH_analysis/movie_script.py
+++ FLASH_analysis/movie_script.py
@@ -324,8 +324,6 @@ if args.make_movie_frames == 'True':
                     primary_ind = np.argmin(part_info['particle_form_time'])
                     part_velx = part_info['particle_velocities'][0][primary_ind]
                     part_vely = part_info['particle_velocities'][1][primary_ind]
-                    import pdb
-                    pdb.set_trace()
                     velx = velx - part_velx.value
                     vely = vely - part_vely.value
 
