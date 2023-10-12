8d97450095785edcb17ec48b85bfe863120af9b9
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2023-10-12 10:23:33 +0200

    Hoping I can calculating keplerian velocity correctly

--- Modules/my_flash_fields.py
+++ Modules/my_flash_fields.py
@@ -828,8 +828,8 @@ def _Relative_keplerian_velocity_wrt_primary_full_v(field, data):
     Calculates the angular momentum w.r.t to the CoM
     """
     if ('all', 'particle_mass') in data.ds.field_list:
-        #v_kep = np.sqrt(abs(data['flash', 'gpot'].in_cgs())).in_units('cm/s')
-        v_kep = data['Keplerian_velocity_wrt_primary']
+        v_kep = np.sqrt(abs(data['flash', 'gpot'].in_cgs())).in_units('cm/s')
+        #v_kep = data['Keplerian_velocity_wrt_primary']
         vel_mag = np.sqrt(data['Velocity_wrt_primary'][0].in_units('cm/s')**2 + data['Velocity_wrt_primary'][1].in_units('cm/s')**2 + data['Velocity_wrt_primary'][2].in_units('cm/s')**2)
         rel_kep = vel_mag/v_kep
         del v_kep, vel_mag
