6977f9b4e57e59eea82416820732c02ee64cca15
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-3.local>
2023-10-20 15:54:21 +0200

    adjusting yaxis of disk profiles

--- FLASH_analysis/inner_disk_multi.py
+++ FLASH_analysis/inner_disk_multi.py
@@ -126,7 +126,7 @@ for spin_lab in Spin_labels:
     axs.flatten()[plot_it-1].set_xlim([0, 10000])
     plt.savefig('Total_Inner_disk_L_mach_comp.pdf', bbox_inches='tight')
     
-#axs.flatten()[plot_it-1].set_ylim(top=1.e52)
+axs.flatten()[plot_it-1].set_ylim(top=1.e58)
 #axs.flatten()[plot_it-1].set_ylim([1e+57, 1e+58])
 axs.flatten()[plot_it-1].set_xlim([0, 10000])
 plt.savefig('Total_Inner_disk_L_mach_comp.pdf', bbox_inches='tight')
@@ -197,7 +197,7 @@ for spin_lab in Spin_labels:
     plt.savefig('Total_Inner_disk_L_mach_comp_spec.pdf', bbox_inches='tight')
     
 
-#axs.flatten()[plot_it-1].set_ylim(top=1.e24)
+axs.flatten()[plot_it-1].set_ylim(top=1.e35)
 #axs.flatten()[plot_it-1].set_ylim([1.e35, 5e35])
 axs.flatten()[plot_it-1].set_xlim([0, 10000])
 plt.savefig('Total_Inner_disk_L_mach_comp_spec.pdf', bbox_inches='tight')
