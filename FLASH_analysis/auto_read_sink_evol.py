1bf98b19fe24f36da4de66ddfba9ef5bd23125f0
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-01-09 09:23:03 +0100

    adapting sink_evol auto read to gadi

--- FLASH_analysis/auto_read_sink_evol.py
+++ FLASH_analysis/auto_read_sink_evol.py
@@ -9,25 +9,26 @@ import sys
 rank = CW.Get_rank()
 size = CW.Get_size()
 #size=sys.argv[1]
+try:
+    sim_dirs = [x[0] for x in os.walk('/home/kuruwira/fast/Protostellar_spin/Flash_2023/')]
 
-sim_dirs = [x[0] for x in os.walk('/home/kuruwira/fast/Protostellar_spin/Flash_2023/')]
+    update = True
 
-update = True
-
-rit = -1
-for sim_dir in sim_dirs:
-    if os.path.exists(sim_dir+'/sinks_evol.dat') == True and 'Binary' not in sim_dir:
-        rit = rit + 1
-        if rit == size:
-            rit = 0
-        if rank == rit:
-            pickle_name = "_".join(sim_dir.split('Protostellar_spin/')[-1].split('/')) + '.pkl'
-            if update == False and os.path.exists(pickle_name) == False:
-                proj_run_line = 'python3 /home/kuruwira/Scripts/FLASH_analysis/read_sink_evol.py '+sim_dir+'/sinks_evol.dat /home/kuruwira/fast/Analysis/Sink_evol_pickles/'+pickle_name
-        
-                subprocess.run(proj_run_line, shell=True)
-            elif update == True:
-                proj_run_line = 'python3 /home/kuruwira/Scripts/FLASH_analysis/read_sink_evol.py '+sim_dir+'/sinks_evol.dat /home/kuruwira/fast/Analysis/Sink_evol_pickles/'+pickle_name
-        
-                subprocess.run(proj_run_line, shell=True)
+    rit = -1
+    for sim_dir in sim_dirs:
+        if os.path.exists(sim_dir+'/sinks_evol.dat') == True and 'Binary' not in sim_dir:
+            rit = rit + 1
+            if rit == size:
+                rit = 0
+            if rank == rit:
+                pickle_name = "_".join(sim_dir.split('Protostellar_spin/')[-1].split('/')) + '.pkl'
+                if update == False and os.path.exists(pickle_name) == False:
+                    proj_run_line = 'python3 /home/kuruwira/Scripts/FLASH_analysis/read_sink_evol.py '+sim_dir+'/sinks_evol.dat /home/kuruwira/fast/Analysis/Sink_evol_pickles/'+pickle_name
             
+                    subprocess.run(proj_run_line, shell=True)
+                elif update == True:
+                    proj_run_line = 'python3 /home/kuruwira/Scripts/FLASH_analysis/read_sink_evol.py '+sim_dir+'/sinks_evol.dat /home/kuruwira/fast/Analysis/Sink_evol_pickles/'+pickle_name
+            
+                    subprocess.run(proj_run_line, shell=True)
+except:
+    sim_dirs = [x[0] for x in os.walk('/home/100/rlk100/rlk/Protostellar_spin/')]
