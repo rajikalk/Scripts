6cc6728b7c0b26891f011cb51d1bb1a8766c48b7
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-01-11 16:12:20 +0100

    working on making sliceplots

--- FLASH_analysis/Zoom_mechanism_plots.py
+++ FLASH_analysis/Zoom_mechanism_plots.py
@@ -4,7 +4,7 @@ import yt
 yt.enable_parallelism()
 import glob
 import my_flash_module as mym
-
+import numpy as np
 
 input_dir = sys.argv[1]
 save_dir = sys.argv[2]
@@ -14,6 +14,29 @@ files = sorted(glob.glob(input_dir + '*plt_cnt*'))
 fn = mym.find_files([plot_time.value], files)[0]
 part_file = 'part'.join(fn.split('plt_cnt'))
 ds = yt.load(fn, particle_filename=part_file)
+
+dd = ds.all_data()
+
+#Calculate separation vector
+particle_mass = dd['particle_mass'][:2].in_units('msun')
+com_x = np.sum(dd['particle_mass'][:2].in_units('msun') * dd['particle_posx'][:2].in_units('au'))/np.sum(dd['particle_mass'][:2].in_units('msun'))
+com_y = np.sum(dd['particle_mass'][:2].in_units('msun') * dd['particle_posy'][:2].in_units('au'))/np.sum(dd['particle_mass'][:2].in_units('msun'))
+com_z = np.sum(dd['particle_mass'][:2].in_units('msun') * dd['particle_posz'][:2].in_units('au'))/np.sum(dd['particle_mass'][:2].in_units('msun'))
+com = yt.YTArray([com_x, com_y, com_z])
+
+
+#Calculate orbital angular momentum vector mr x v
+dx = dd['particle_posx'][:2].in_units('cm') - com[0]
+dy = dd['particle_posy'][:2].in_units('cm') - com[1]
+dz = dd['particle_posz'][:2].in_units('cm') - com[2]
+d_pos = yt.YTArray([dx, dy, dz]).T
+
+dvx = dd['particle_velx'][:2].in_units('cm/s') - com[0]
+dvy = dd['particle_vely'][:2].in_units('cm/s') - com[1]
+dvz = dd['particle_velz'][:2].in_units('cm/s') - com[2]
+d_vel = yt.YTArray([dvx, dvy, dvz]).T
+
+L_orb = dd['particle_mass'].value * np.cross(d_pos, d_vel).T
 
 import pdb
 pdb.set_trace()
