88bb4e2679e1bdb2844b3fc40eba4ca0a730d028
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-01-11 16:30:54 +0100

    working on making sliceplots

--- FLASH_analysis/Zoom_mechanism_plots.py
+++ FLASH_analysis/Zoom_mechanism_plots.py
@@ -17,31 +17,18 @@ ds = yt.load(fn, particle_filename=part_file)
 
 dd = ds.all_data()
 
-#Calculate separation vector
-particle_mass = dd['particle_mass'][:2].in_units('msun')
-com_x = np.sum(dd['particle_mass'][:2].in_units('msun') * dd['particle_posx'][:2].in_units('au'))/np.sum(dd['particle_mass'][:2].in_units('msun'))
-com_y = np.sum(dd['particle_mass'][:2].in_units('msun') * dd['particle_posy'][:2].in_units('au'))/np.sum(dd['particle_mass'][:2].in_units('msun'))
-com_z = np.sum(dd['particle_mass'][:2].in_units('msun') * dd['particle_posz'][:2].in_units('au'))/np.sum(dd['particle_mass'][:2].in_units('msun'))
-com = yt.YTArray([com_x, com_y, com_z])
+#Altnerative calculation
+Primary_pos = yt.YTArray([dd['particle_posx'].in_units('au')[0], dd['particle_posy'].in_units('au')[0], dd['particle_posz'].in_units('au')[0]])
+Secondary_pos = yt.YTArray([dd['particle_posx'].in_units('au')[1], dd['particle_posy'].in_units('au')[1], dd['particle_posz'].in_units('au')[1]])
+d_pos = Secondary_pos - Primary_pos
 
-com_vel_x = np.sum(dd['particle_mass'][:2].in_units('msun') * dd['particle_velx'][:2].in_units('km/s'))/np.sum(dd['particle_mass'][:2].in_units('msun'))
-com_vel_y = np.sum(dd['particle_mass'][:2].in_units('msun') * dd['particle_vely'][:2].in_units('km/s'))/np.sum(dd['particle_mass'][:2].in_units('msun'))
-com_vel_z = np.sum(dd['particle_mass'][:2].in_units('msun') * dd['particle_velz'][:2].in_units('km/s'))/np.sum(dd['particle_mass'][:2].in_units('msun'))
-com_vel = yt.YTArray([com_vel_x, com_vel_y, com_vel_z])
+Primary_vel = yt.YTArray([dd['particle_velx'].in_units('km/s')[0], dd['particle_vely'].in_units('km/s')[0], dd['particle_velz'].in_units('km/s')[0]])
+Secondary_vel = yt.YTArray([dd['particle_velx'].in_units('km/s')[1], dd['particle_vely'].in_units('km/s')[1], dd['particle_velz'].in_units('km/s')[1]])
+d_vel = Secondary_vel - Primary_vel
+L_vec = np.cross(d_pos, d_vel).T
+L_vec_norm = L_vec/np.sqrt(np.sum(L_vec**2))
 
-
-#Calculate orbital angular momentum vector mr x v
-dx = dd['particle_posx'][:2].in_units('au') - com[0]
-dy = dd['particle_posy'][:2].in_units('au') - com[1]
-dz = dd['particle_posz'][:2].in_units('au') - com[2]
-d_pos = yt.YTArray([dx, dy, dz]).T
-
-dvx = dd['particle_velx'][:2].in_units('km/s') - com_vel[0]
-dvy = dd['particle_vely'][:2].in_units('km/s') - com_vel[1]
-dvz = dd['particle_velz'][:2].in_units('km/s') - com_vel[2]
-d_vel = yt.YTArray([dvx, dvy, dvz]).T
-
-L_orb = dd['particle_mass'].value * np.cross(d_pos, d_vel).T
+slc = yt.OffAxisSlicePlot(ds, L_vec_norm, 'dens', center=Primary_pos, width=(200, 'au'), north_vector=[0, 1, 0])
 
 import pdb
 pdb.set_trace()
