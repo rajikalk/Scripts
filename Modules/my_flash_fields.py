6f58e3a4515d701fa95fb83897036a1a38b9be02
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-01-15 13:03:11 +0100

    Attempting to make off aixs slice movies

--- Modules/my_flash_fields.py
+++ Modules/my_flash_fields.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #Created by Rajika Kuruwita, 2019
 import yt
-from yt.fields.api import ValidateParameter
+#from yt.fields.api import ValidateParameter
 yt.enable_parallelism()
 import numpy as np
 
