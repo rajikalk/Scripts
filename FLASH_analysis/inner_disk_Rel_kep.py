7854bb9ae9521edf611b232495cd1838b8e74797
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-01-19 13:01:11 +0100

    Plotting disk relative kep

--- FLASH_analysis/inner_disk_Rel_kep.py
+++ FLASH_analysis/inner_disk_Rel_kep.py
@@ -74,7 +74,7 @@ for mach_lab in Mach_labels:
             R_sink, Time_array, Total_L, Total_L_spec, Mean_L, Mean_L_spec, Mean_rad_vel, Min_rad_vel, Mean_rel_vel, Min_rel_vel, Mass_all, Separation = pickle.load(file)
             file.close()
             
-            axs.flatten()[plot_it].plot(Time_array, Mean_L, label='$\Omega t_{ff}$='+spin_lab, linestyle=line_styles[Spin_labels.index(spin_lab)], color=colors[Spin_labels.index(spin_lab)])
+            axs.flatten()[plot_it].plot(Time_array, Mean_L, label='$\Omega t_{ff}$='+spin_lab, linestyle=line_styles[Spin_labels.index(spin_lab)], color=colors[Spin_labels.index(spin_lab)], alpha=0.75)
             
             axs.flatten()[plot_it].set_xlabel('Time ($yr$)', labelpad=-0.2)
             if mach_lab == '0.0':
