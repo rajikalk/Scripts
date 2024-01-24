3a4f7c2c205f897bfb8aa852a022b6c7a7ea8c10
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-01-24 15:31:42 +0100

    added smoothing

--- FLASH_analysis/inner_disk_Rel_kep.py
+++ FLASH_analysis/inner_disk_Rel_kep.py
@@ -92,7 +92,7 @@ for mach_lab in Mach_labels:
                 T_smoothed.append(t_smooth_val)
                 Rel_kep_smoothed.append(rel_kep_smooth_val)
             
-            axs.flatten()[plot_it].plot(T_smoothed, Rel_kep_smoothed, label='$\Omega t_{ff}$='+spin_lab, linestyle=line_styles[Spin_labels.index(spin_lab)], color=colors[Spin_labels.index(spin_lab)], alpha=0.75)
+            axs.flatten()[plot_it].plot(T_smoothed, Rel_kep_smoothed, label='$\Omega t_{ff}$='+spin_lab, linestyle=line_styles[Spin_labels.index(spin_lab)], color=colors[Spin_labels.index(spin_lab)], alpha=0.75, linewidth=1)
             
             axs.flatten()[plot_it].set_xlabel('Time ($yr$)', labelpad=-0.2)
             if mach_lab == '0.0':
@@ -167,7 +167,7 @@ for mach_lab in Mach_labels:
                 T_smoothed.append(t_smooth_val)
                 Rel_kep_smoothed.append(rel_kep_smooth_val)
             
-            axs.flatten()[plot_it].plot(T_smoothed, Rel_kep_smoothed, label='$\Omega t_{ff}$='+spin_lab, linestyle=line_styles[Spin_labels.index(spin_lab)], color=colors[Spin_labels.index(spin_lab)], alpha=0.75)
+            axs.flatten()[plot_it].plot(T_smoothed, Rel_kep_smoothed, label='$\Omega t_{ff}$='+spin_lab, linestyle=line_styles[Spin_labels.index(spin_lab)], color=colors[Spin_labels.index(spin_lab)], alpha=0.75, linewidth=1)
             
             axs.flatten()[plot_it].set_xlabel('Time ($yr$)', labelpad=-0.2)
             if mach_lab == '0.0':
