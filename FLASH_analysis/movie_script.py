fcd090b776a529274e58cf1cbca736c2989959af
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-01-29 20:42:19 +0100

    cleaning up finding times

--- FLASH_analysis/movie_script.py
+++ FLASH_analysis/movie_script.py
@@ -320,7 +320,7 @@ if args.make_movie_frames == 'True':
                 X_image, Y_image, image, magx, magy, X_vel, Y_vel, velx, vely, part_info, time_val = pickle.load(file)
                 file.close()
                 
-                if args.update_velocity_field != 'False':
+                if args.update_velocity_field != 'False' and len(part_info.keys())!=0:
                     primary_ind = np.argmin(part_info['particle_form_time'])
                     part_velx = part_info['particle_velocities'][0][primary_ind]
                     part_vely = part_info['particle_velocities'][1][primary_ind]
