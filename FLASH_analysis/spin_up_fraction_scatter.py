a45599e1a0a7e299b10a55feb631337097455510
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-02-07 11:58:12 +0100

    finding peak spin after spinup event

--- FLASH_analysis/spin_up_fraction_scatter.py
+++ FLASH_analysis/spin_up_fraction_scatter.py
@@ -92,6 +92,9 @@ for mach_lab in Mach_labels:
                 secondary_form_ind = np.argmin(abs(sink_data[prime_id]['time'] - secondary_form_time))
                 time_yr = yt.YTArray((sink_data[prime_id]['time']-sink_data[prime_id]['time'][0]), 's').in_units('yr')
                 end_window_ind = np.argmin(abs(time_yr - yt.YTQuantity(7500, 'yr')))
+                if np.round(time_yr[end_window_ind]) != 7500.0:
+                    import pdb
+                    pdb.set_trace()
                 
                 pre_sec_L = np.sqrt(sink_data[prime_id]['anglx'][secondary_form_ind-1]**2 + sink_data[prime_id]['angly'][secondary_form_ind-1]**2 + sink_data[prime_id]['anglz'][secondary_form_ind-1]**2)
                 pre_sec_L_spec = pre_sec_L/sink_data[prime_id]['mass'][secondary_form_ind-1]
