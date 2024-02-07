d06ebd785006a75179fffb6df7ad7f0088f136a5
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-02-07 11:31:51 +0100

    finding peak spin after spinup event

--- FLASH_analysis/spin_up_fraction_scatter.py
+++ FLASH_analysis/spin_up_fraction_scatter.py
@@ -90,6 +90,7 @@ for mach_lab in Mach_labels:
                 secondary_form_time = sink_data[second_id]['time'][0] - yt.YTQuantity(500, 'yr').in_units('s').value
                 #secondary_form_time = yt.YTQuantity(2000, 'yr').in_units('s').value + sink_data[prime_id]['time'][0]
                 secondary_form_ind = np.argmin(abs(sink_data[prime_id]['time'] - secondary_form_time))
+                end_window_ind = np.argmin(abs(sink_data[prime_id]['time'] - 7500))
                 
                 pre_sec_L = np.sqrt(sink_data[prime_id]['anglx'][secondary_form_ind-1]**2 + sink_data[prime_id]['angly'][secondary_form_ind-1]**2 + sink_data[prime_id]['anglz'][secondary_form_ind-1]**2)
                 pre_sec_L_spec = pre_sec_L/sink_data[prime_id]['mass'][secondary_form_ind-1]
@@ -100,6 +101,9 @@ for mach_lab in Mach_labels:
                 post_sec_L_val = post_sec_L[-1]
                 post_sec_L_spec_last = post_sec_L_spec[-1]
                 post_sec_L_spec_peak = np.max(post_sec_L_spec)
+                peak_spec_L_pos = np.argmax(post_sec_L_spec) + secondary_form_ind
+                peak_time = sink_data[prime_id]['time'][peak_spec_L_pos]
+                print("peak time =", peak_time)
                 
                 DL = post_sec_L_val - pre_sec_L
                 spin_up_percentage = DL/pre_sec_L * 100
