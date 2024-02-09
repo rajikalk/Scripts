31de8f24b9d626ae8d83f374635c0c0e5c88d2d2
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-02-09 13:29:23 +0100

    highlighting spink up phase

--- FLASH_analysis/sink_spin_comparison.py
+++ FLASH_analysis/sink_spin_comparison.py
@@ -440,7 +440,7 @@ for mach_lab in Mach_labels:
                     highlight_end_ind = np.argmin(abs(plot_time.value-highlight_end_time))
                     highlight_min = np.min(plot_L[highlight_start_ind:highlight_end_ind])
                     highlight_max = np.min(plot_L[highlight_start_ind:highlight_end_ind])
-                    axs.flatten()[plot_it].axvspan(highlight_start_time, highlight_end_time, ymin=highlight_min, ymax=highlight_max, alpha=0.30, facecolor=colors[Spin_labels.index(spin_lab)], edgecolor=None)
+                    axs.flatten()[plot_it].axvspan(highlight_start_time, highlight_end_time, ymin=highlight_min.value, ymax=highlight_max.value, alpha=0.30, facecolor=colors[Spin_labels.index(spin_lab)], edgecolor=None)
                     plot_highlight = False
         else:
             print("Couldn't open", single_pickle)
