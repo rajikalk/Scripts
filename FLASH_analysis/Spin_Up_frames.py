cdcd0fa83394a9aedc64bfeef6f0908f049282d5
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-02-23 17:20:37 +0100

    working on plotting interesting part

--- FLASH_analysis/Spin_Up_frames.py
+++ FLASH_analysis/Spin_Up_frames.py
@@ -80,7 +80,6 @@ for spin_val in spin_values:
             plot_times = [plot_times[0]-1000] + list(plot_times) + [plot_times[-1]+1000]
             plot_times = np.array([plot_times, plot_times]).T.flatten()
         
-        fig.subplots_adjust(top=0.88)
         fig.suptitle("$\Omega t_{ff}$="+spin_val+", $\mathcal{M}$="+mach_val, y=0.9)
         
         for plot_time in plot_times:
