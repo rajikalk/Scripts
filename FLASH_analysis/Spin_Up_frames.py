0d965635cced1faf644af11487c34bfa102ae9d8
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-02-23 14:27:29 +0100

    working on plotting interesting part

--- FLASH_analysis/Spin_Up_frames.py
+++ FLASH_analysis/Spin_Up_frames.py
@@ -120,13 +120,13 @@ for spin_val in spin_values:
             
             if plot_it == len(plot_times)-2:
                 #Figure out colorbar
-                fig.subplots_adjust(right=0.95)
-                cbar_ax = fig.add_axes([0.2, 0.05, 0.25, 0.02])
+                fig.subplots_adjust(bottom=0.95)
+                cbar_ax = fig.add_axes([0.21, 0.05, 0.25, 0.02])
                 cbar = fig.colorbar(plot, cax=cbar_ax, orientation='horizontal')
                 cbar.set_label(r"Density (g$\,$cm$^{-3}$)", labelpad=0, size=font_size)
             elif plot_it == len(plot_times)-1:
-                fig.subplots_adjust(right=0.95)
-                cbar_ax = fig.add_axes([0.65, 0.05, 0.25, 0.02])
+                fig.subplots_adjust(bottom=0.95)
+                cbar_ax = fig.add_axes([0.6, 0.05, 0.25, 0.02])
                 cbar = fig.colorbar(plot, cax=cbar_ax, orientation='horizontal')
                 cbar.set_label(r"Magnetic Toomre Q", labelpad=0, size=font_size)
             
