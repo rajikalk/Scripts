2000fe9ec3fbdc5a0d08c9b8e8d56463be091fa5
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-02-28 09:45:49 +0100

    Trying to plot horizontal frames

--- FLASH_analysis/Spin_Up_frames_horizontal.py
+++ FLASH_analysis/Spin_Up_frames_horizontal.py
@@ -126,12 +126,12 @@ for spin_val in spin_values:
             if plot_it == n_frames-1:
                 #Figure out colorbar
                 #fig.subplots_adjust(bottom=0.0)
-                cbar_ax = fig.add_axes([0.951, 0.50, 0.015, 0.385])
+                cbar_ax = fig.add_axes([0.85, 0.50, 0.015, 0.385])
                 cbar = fig.colorbar(plot, cax=cbar_ax)
                 cbar.set_label(r"Density (g$\,$cm$^{-3}$)", labelpad=0, rotation=270, size=font_size)
             elif plot_it == 2*n_frames-1:
                 #fig.subplots_adjust(bottom=0.05)
-                cbar_ax = fig.add_axes([0.951, 0.094, 0.015, 0.385])
+                cbar_ax = fig.add_axes([0.90, 0.094, 0.015, 0.385])
                 cbar = fig.colorbar(plot, cax=cbar_ax)
                 cbar.set_label(r"Magnetic Toomre Q", labelpad=0, rotation=270, size=font_size)
             
