fe0d4d4088d203b0eb96da056780e164db1c7b74
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-03-01 13:43:29 +0100

    rewriting to only plot non-turbulence and turbulent sims

--- FLASH_analysis/sink_spin_comparison.py
+++ FLASH_analysis/sink_spin_comparison.py
@@ -445,7 +445,7 @@ for mach_lab in Mach_labels:
                         highlight_end_ind = np.argmin(abs(plot_time.value-highlight_end_time))
                         highlight_min = np.min(plot_L[highlight_start_ind:highlight_end_ind])
                         highlight_max = np.max(plot_L[highlight_start_ind:highlight_end_ind])
-                        axs.flatten()[plot_it].axvspan(highlight_start_time, highlight_end_time, ymin=highlight_min.value/1.5e15, ymax=highlight_max.value/1.5e15, alpha=0.30, facecolor=colors[Spin_labels.index(spin_lab)])
+                        axs.flatten()[plot_it].axvspan(highlight_start_time, highlight_end_time, ymin=highlight_min.value, ymax=highlight_max.value, alpha=0.30, facecolor=colors[Spin_labels.index(spin_lab)])
                     plot_highlight = False
         else:
             print("Couldn't open", single_pickle)
@@ -455,11 +455,11 @@ for mach_lab in Mach_labels:
     if mach_lab == '0.0':
         mach_string = "No Turbulence \n($\mathcal{M}$="+mach_lab+")"
         mach_string_raw = r"{}".format(mach_string)
-        time_text = axs.flatten()[plot_it].text(250, 1.4, mach_string_raw, va="center", ha="left", color='k', fontsize=font_size)
+        time_text = axs.flatten()[plot_it].text(250, 1.3, mach_string_raw, va="center", ha="left", color='k', fontsize=font_size)
     else:
         mach_string = "With Turbulence \n($\mathcal{M}$="+mach_lab+")"
         mach_string_raw = r"{}".format(mach_string)
-        time_text = axs.flatten()[plot_it].text(250, 1.4, mach_string_raw, va="center", ha="left", color='k', fontsize=font_size)
+        time_text = axs.flatten()[plot_it].text(250, 1.3, mach_string_raw, va="center", ha="left", color='k', fontsize=font_size)
 
 axs.flatten()[0].legend(loc='lower right', ncol=2)#, fontsize=font_size, labelspacing=0.1, handletextpad=0.2, borderaxespad=0.2, borderpad=0.2, columnspacing=0.3)
 axs.flatten()[0].tick_params(axis='x', direction='in', top=True)
@@ -473,7 +473,7 @@ axs.flatten()[1].minorticks_on()
 axs.flatten()[1].tick_params(which='both', direction='in', axis='both', right=True, top=True)
 axs.flatten()[1].set_xlabel('Time ($yr$)', labelpad=-0.2)
 yticklabels = axs.flatten()[1].get_yticklabels()
-plt.setp(yticklabels[-2], visible=False)
+plt.setp(yticklabels[-3], visible=False)
 
 axs.flatten()[plot_it-1].set_xlim([0, 10000])
 axs.flatten()[plot_it-1].set_ylim(bottom=0)
