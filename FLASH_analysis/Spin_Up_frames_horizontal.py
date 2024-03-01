3efa540e71fb0efe5b7d3559df67aa746ab8406a
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-03-01 17:09:14 +0100

    rewriting to only plot non-turbulence and turbulent sims

--- FLASH_analysis/Spin_Up_frames_horizontal.py
+++ FLASH_analysis/Spin_Up_frames_horizontal.py
@@ -142,7 +142,7 @@ for spin_val in spin_values:
                 plot_velocity_legend = True
             else:
                 plot_velocity_legend = False
-            mym.my_own_quiver_function(ax, X_vel, Y_vel, velx, vely, plot_velocity_legend=plot_velocity_legend,limits=[xlim, ylim], Z_val=None, standard_vel=stdvel, width_ceil = 0.1)
+            mym.my_own_quiver_function(ax, X_vel, Y_vel, velx, vely, plot_velocity_legend=plot_velocity_legend,limits=[xlim, ylim], Z_val=None, standard_vel=stdvel, width_ceil = 0.2)
             
             if len(part_info['particle_tag']) > 1:
                 if np.min(part_info['particle_form_time'][1:] - part_info['particle_form_time'][:-1]) < 0:
