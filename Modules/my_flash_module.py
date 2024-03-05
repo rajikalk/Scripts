f0c1a72898cb3bac1faccf415b3aaff0f69a3d3e
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-03-05 17:30:32 +0100

    fixing particle mass annotation

--- Modules/my_flash_module.py
+++ Modules/my_flash_module.py
@@ -466,14 +466,8 @@ def annotate_particles(axis, particle_position, accretion_rad, limits, annotate_
                 sub_strings.append(",".join(p_t.split(',')[split_it-split_threshold:split_it])+",")
                 sub_colors.append(rainbow_text_colors[2*(split_it-split_threshold)+1:2*split_it+1])
                 split_it = split_it + split_threshold
-            if split_it > len(particle_tags):
-                sub_strings.append(",".join(p_t.split(',')[split_it-split_threshold:]))
-                sub_colors.append(rainbow_text_colors[2*(split_it-split_threshold)+1:2])
-            else:
-                import pdb
-                pdb.set_trace()
-                sub_strings[-1] = sub_strings[-1]+'$M$_\\odot$'
-                sub_colors[0] = ['white'] + sub_colors[0]
+            sub_strings.append(",".join(p_t.split(',')[split_it-split_threshold:]))
+            sub_colors.append(rainbow_text_colors[2*(split_it-split_threshold)+1:])
             
             annotate_text = axis.text((xmin + 0.01*(box_size)), (ymin + 0.029*(ymax-ymin)), sub_strings[0], va="bottom", ha="right", color='w', fontsize=fontsize_global)
             annotate_text.set_path_effects([path_effects.Stroke(linewidth=3, foreground='black'), path_effects.Normal()])
