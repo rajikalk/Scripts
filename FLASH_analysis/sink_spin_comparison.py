5a564df1deee9dd100a656461c1dd858eedec30d
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-03-12 09:24:24 +0100

    Attempting to calculate protostar period

--- FLASH_analysis/sink_spin_comparison.py
+++ FLASH_analysis/sink_spin_comparison.py
@@ -62,230 +62,9 @@ def parse_inputs():
 #input_dir = sys.argv[1]
 args = parse_inputs()
 
-'''
-plt.clf()
-sink_ind = 0
-for pickle_file in pickle_files:
-    Spin_label = " ".join(pickle_file.split('/')[-1].split('_')[:2])
-    file = open(pickle_file, 'rb')
-    sink_data = pickle.load(file)
-    file.close()
-    form_time = np.nan
-    
-    for sink_id in sink_data.keys():
-        if np.isnan(form_time):
-            form_time = sink_data[sink_id]['time'][0]
-        #L_tot = np.sqrt(sink_data[sink_id]['anglx']**2 + sink_data[sink_id]['angly']**2 + sink_data[sink_id]['anglz']**2)
-        #L_tot = yt.YTArray(L_tot, 'g*cm**2/s')
-        L_tot = np.sqrt((sink_data[sink_id]['anglx']/sink_data[sink_id]['mass'])**2 + (sink_data[sink_id]['angly']/sink_data[sink_id]['mass'])**2 + (sink_data[sink_id]['anglz']/sink_data[sink_id]['mass'])**2)
-        L_tot = yt.YTArray(L_tot, 'cm**2/s')
-        time = sink_data[sink_id]['time'] - form_time
-        time = yt.YTArray(time, 's')
-        plt.plot(time.in_units('yr'), L_tot, label=sink_id + "_" + pickle_file.split('/')[-1])
-
-plt.xlabel('Time since formation')
-plt.ylabel('Angular momentum')
-plt.xlim(left=0)
-plt.ylim(bottom=0)
-plt.legend()
-plt.savefig('spin_comp_primary.png')
-'''
 #Mach_labels = ['0.0', '0.1', '0.2']
 Mach_labels = ['0.0', '0.2']
 Spin_labels = ['0.20', '0.25', '0.30', '0.35']
-"""
-plt.clf()
-fig, axs = plt.subplots(ncols=1, nrows=len(Mach_labels), figsize=(single_col_width, 1.3*single_col_width), sharex=True, sharey=True)
-iter_range = range(0, len(Spin_labels))
-plt.subplots_adjust(wspace=0.0)
-plt.subplots_adjust(hspace=0.0)
-
-plot_it = 0
-xmax= 0
-ymax = 0
-for mach_lab in Mach_labels:
-    for spin_lab in Spin_labels:
-        axs.flatten()[plot_it].grid()
-        #single_pickle
-        single_pickle = '/home/kuruwira/fast/Analysis/Sink_evol_pickles/Flash_2023_Spin_'+spin_lab+'_Single_Mach_'+mach_lab+'_Lref_'+args.refinment_level+'.pkl'
-        #binary_pickle = '/home/kuruwira/fast/Analysis/Sink_evol_pickles/Flash_2023_Spin_'+spin_lab+'_Binary_Mach_'+mach_lab+'_Lref_9.pkl'
-        
-        if os.path.exists(single_pickle):
-            file = open(single_pickle, 'rb')
-            sink_data, line_counter = pickle.load(file)
-            file.close()
-            form_time = np.nan
-            
-            #for sink_id in sink_data.keys():
-            #sink_id = list(sink_data.keys())[0]
-            for sink_id in sink_data.keys():
-                if np.isnan(form_time):
-                    form_time = sink_data[sink_id]['time'][0]
-                L_tot = np.sqrt((sink_data[sink_id]['anglx']/sink_data[sink_id]['mass'])**2 + (sink_data[sink_id]['angly']/sink_data[sink_id]['mass'])**2 + (sink_data[sink_id]['anglz']/sink_data[sink_id]['mass'])**2)
-                L_tot = yt.YTArray(L_tot, 'cm**2/s')
-                axs.flatten()[plot_it].set_ylim([0.0e19, 1.5e19])
-                if spin_lab != '0.20':
-                    L_tot = L_tot/1.e19
-                    axs.flatten()[plot_it].set_ylim([0.0, 1.5])
-                time = sink_data[sink_id]['time'] - form_time
-                time = yt.YTArray(time, 's')
-                if time[-1] > xmax:
-                    xmax = time[-1]
-                if np.max(L_tot) > ymax:
-                    ymax = np.max(L_tot)
-                    
-                end_time = max_time[Spin_labels.index(spin_lab)][Mach_labels.index(mach_lab)]
-                end_ind = np.argmin(abs(time.in_units('yr').value - end_time))
-                plot_time = time.in_units('yr')[:end_ind+1]
-                plot_L = L_tot[:end_ind+1]
-                axs.flatten()[plot_it].plot(plot_time, plot_L)
-                
-        else:
-            print("Couldn't open", single_pickle)
-        '''
-        if os.path.exists(binary_pickle):
-            file = open(binary_pickle, 'rb')
-            sink_data = pickle.load(file)
-            file.close()
-            form_time = np.nan
-            
-            Binary_labels = ['Primary', 'Secondary']
-            line_styles = ['--', '-.']
-            
-            for sink_id in list(sink_data.keys())[:2]:
-                if np.isnan(form_time):
-                    form_time = sink_data[sink_id]['time'][0]
-                L_tot = np.sqrt((sink_data[sink_id]['anglx']/sink_data[sink_id]['mass'])**2 + (sink_data[sink_id]['angly']/sink_data[sink_id]['mass'])**2 + (sink_data[sink_id]['anglz']/sink_data[sink_id]['mass'])**2)
-                L_tot = yt.YTArray(L_tot, 'cm**2/s')
-                time = sink_data[sink_id]['time'] - form_time
-                time = yt.YTArray(time, 's')
-                if time[-1] > xmax:
-                    xmax = time[-1]
-                if np.max(L_tot) > ymax:
-                    ymax = np.max(L_tot)
-                axs.flatten()[plot_it].plot(time.in_units('yr'), L_tot/1.e19, label=Binary_labels[list(sink_data.keys()).index(sink_id)], ls=line_styles[list(sink_data.keys()).index(sink_id)])
-        else:
-            print("Couldn't open", binary_pickle)
-        '''
-        if spin_lab == '0.20':
-            axs.flatten()[plot_it].set_title('Mach ='+mach_lab)
-        if mach_lab == '0.0':
-            axs.flatten()[plot_it].set_ylabel('$\Omega t_{ff}='+spin_lab+'$: h ($cm^2/s$)')
-            if spin_lab != '0.20':
-                yticklabels = axs.flatten()[plot_it].get_yticklabels()
-                plt.setp(yticklabels[-1], visible=False)
-        if spin_lab == '0.35':
-            axs.flatten()[plot_it].set_xlabel('Time ($yr$)')
-            if mach_lab != '0.2':
-                xticklabels = axs.flatten()[plot_it].get_xticklabels()
-                plt.setp(xticklabels[-1], visible=False)
-        
-        plot_it = plot_it + 1
-    if mach_lab == '0.0':
-        mach_string = "No Turbulence ($\mathcal{M}$="+mach_lab+")"
-        mach_string_raw = r"{}".format(mach_string)
-        time_text = axs.flatten()[plot_it].text(500, 0.66*ymax, mach_string_raw, va="center", ha="left", color='k', fontsize=font_size)
-    else:
-        mach_string = "With Turbulence ($\mathcal{M}$="+mach_lab+")"
-        mach_string_raw = r"{}".format(mach_string)
-        time_text = axs.flatten()[plot_it].text(500, 0.9*ymax, mach_string_raw, va="center", ha="left", color='k', fontsize=font_size)
-
-axs.flatten()[plot_it-1].set_xlim([0, 10000])
-plt.savefig('spin_comp_multi_specific.pdf', bbox_inches='tight')
-
-plt.clf()
-fig, axs = plt.subplots(ncols=len(Mach_labels), nrows=len(Spin_labels), figsize=(two_col_width, single_col_width*2.5), sharex=True, sharey='row')
-iter_range = range(0, len(Spin_labels))
-plt.subplots_adjust(wspace=0.0)
-plt.subplots_adjust(hspace=0.0)
-
-plot_it = 0
-xmax= 0
-ymax = 0
-for spin_lab in Spin_labels:
-    for mach_lab in Mach_labels:
-        axs.flatten()[plot_it].grid()
-        #single_pickle
-        single_pickle = '/home/kuruwira/fast/Analysis/Sink_evol_pickles/Flash_2023_Spin_'+spin_lab+'_Single_Mach_'+mach_lab+'_Lref_'+args.refinment_level+'.pkl'
-        #binary_pickle = '/home/kuruwira/fast/Analysis/Sink_evol_pickles/Flash_2023_Spin_'+spin_lab+'_Binary_Mach_'+mach_lab+'_Lref_9.pkl'
-        
-        if os.path.exists(single_pickle):
-            file = open(single_pickle, 'rb')
-            sink_data, line_counter = pickle.load(file)
-            file.close()
-            form_time = np.inf
-            for sink_id in sink_data.keys():
-                if sink_data[sink_id]['time'][0] < form_time:
-                    form_time = sink_data[sink_id]['time'][0]
-            
-            #for sink_id in list(sink_data.keys())[0]:
-            #sink_id = list(sink_data.keys())[0]
-            for sink_id in sink_data.keys():
-                L_tot = np.sqrt(sink_data[sink_id]['anglx']**2 + sink_data[sink_id]['angly']**2 + sink_data[sink_id]['anglz']**2)
-                L_tot = yt.YTArray(L_tot, 'g*cm**2/s')
-                axs.flatten()[plot_it].set_ylim([0.0e52, 1.35e52])
-                if spin_lab != '0.20':
-                    L_tot = L_tot/1.e52
-                    axs.flatten()[plot_it].set_ylim([0.0, 1.35])
-                time = sink_data[sink_id]['time'] - form_time
-                time = yt.YTArray(time, 's')
-                if time[-1] > xmax:
-                    xmax = time[-1]
-                if np.max(L_tot) > ymax:
-                    ymax = np.max(L_tot)
-                end_time = max_time[Spin_labels.index(spin_lab)][Mach_labels.index(mach_lab)]
-                end_ind = np.argmin(abs(time.in_units('yr').value - end_time))
-                plot_time = time.in_units('yr')[:end_ind+1]
-                plot_L = L_tot[:end_ind+1]
-                axs.flatten()[plot_it].plot(plot_time, plot_L, label='Single')
-        else:
-            print("Couldn't open", single_pickle)
-        
-        '''
-        if os.path.exists(binary_pickle):
-            file = open(binary_pickle, 'rb')
-            sink_data = pickle.load(file)
-            file.close()
-            form_time = np.nan
-            
-            Binary_labels = ['Primary', 'Secondary']
-            line_styles = ['--', '-.']
-            
-            for sink_id in list(sink_data.keys())[:2]:
-                if np.isnan(form_time):
-                    form_time = sink_data[sink_id]['time'][0]
-                L_tot = np.sqrt(sink_data[sink_id]['anglx']**2 + sink_data[sink_id]['angly']**2 + sink_data[sink_id]['anglz']**2)
-                L_tot = yt.YTArray(L_tot, 'g*cm**2/s')
-                time = sink_data[sink_id]['time'] - form_time
-                time = yt.YTArray(time, 's')
-                if time[-1] > xmax:
-                    xmax = time[-1]
-                if np.max(L_tot) > ymax:
-                    ymax = np.max(L_tot)
-                axs.flatten()[plot_it].plot(time.in_units('yr'), L_tot, label=Binary_labels[list(sink_data.keys()).index(sink_id)], ls=line_styles[list(sink_data.keys()).index(sink_id)])
-        else:
-            print("Couldn't open", binary_pickle)
-        '''
-        
-        #if plot_it == 0:
-        #    axs.flatten()[plot_it].legend()
-        if spin_lab == '0.20':
-            axs.flatten()[plot_it].set_title('Mach ='+mach_lab)
-        if mach_lab == '0.0':
-            axs.flatten()[plot_it].set_ylabel('$\Omega t_{ff}='+spin_lab+'$: L ($g\,cm^2/s$)')
-        if spin_lab == '0.35':
-            axs.flatten()[plot_it].set_xlabel('Time ($yr$)')
-            if mach_lab != '0.2':
-                xticklabels = axs.flatten()[plot_it].get_xticklabels()
-                plt.setp(xticklabels[-1], visible=False)
-        
-        plot_it = plot_it + 1
-
-axs.flatten()[plot_it-1].set_xlim([0, 10000])
-#axs.flatten()[plot_it-1].set_ylim([5.e48, 5.e54])
-#axs.flatten()[plot_it-1].set_ylim(bottom=0)
-plt.savefig('spin_comp_multi_'+args.refinment_level+'.pdf', bbox_inches='tight')
-"""
 
 height_multiplier = 1.5
 plt.clf()
@@ -481,77 +260,71 @@ axs.flatten()[plot_it-1].set_ylim(bottom = 0)
 plt.savefig('Spin_init_spin_spec_comp.pdf', bbox_inches='tight', pad_inches=0.02)
 
 #==========================================================================================================================
-"""
+#Star stats
 plt.clf()
-fig, axs = plt.subplots(ncols=len(Mach_labels), nrows=1, figsize=(two_col_width, 0.7*single_col_width), sharex=True, sharey=True)
+fig, axs = plt.subplots(ncols=2, nrows=4, figsize=(two_col_width, 0.8*page_height), sharex=True, sharey='row')
 iter_range = range(0, len(Spin_labels))
 plt.subplots_adjust(wspace=0.0)
 plt.subplots_adjust(hspace=0.0)
 
 line_styles = ['-', '--', '-.', ':']
-plot_it = -1
-xmax= 0
-ymax = 0
+
+plot_quantity = ['mass', 'angular momentum', 'specific angular momentum', 'period']
+radius = yt.YTQuantity(2, 'rsun')
+
 for mach_lab in Mach_labels:
-    plot_it = plot_it + 1
-    axs.flatten()[plot_it].set_title("Mach = " + mach_lab)
+    plot_it = Mach_labels.index(mach_lab) - 2
     for spin_lab in Spin_labels:
-        axs.flatten()[plot_it].grid()
-        #single_pickle
-        single_pickle = '/home/kuruwira/fast/Analysis/Sink_evol_pickles/Flash_2023_Spin_'+spin_lab+'_Single_Mach_'+mach_lab+'_Lref_'+args.refinment_level+'.pkl'
-        #binary_pickle = '/home/kuruwira/fast/Analysis/Sink_evol_pickles/Flash_2023_Spin_'+spin_lab+'_Binary_Mach_'+mach_lab+'_Lref_9.pkl'
+        single_pickle = '/home/kuruwira/fast/Analysis/Sink_evol_pickles/Flash_2023_Spin_'+spin_lab+'_Single_Mach_'+mach_lab+'_Lref_9.pkl'
+        file = open(single_pickle, 'rb')
+        sink_data, line_counter = pickle.load(file)
+        file.close()
+        form_time = np.nan
+
+        primary_ind = list(sink_data.keys())[0]
+        form_time = sink_data[primary_ind]['time'][0]
+        mass = yt.YTArray(sink_data[primary_ind]['mass'], 'g')
+        L_tot = np.sqrt(sink_data[primary_ind]['anglx']**2 + sink_data[primary_ind]['angly']**2 + sink_data[primary_ind]['anglz']**2)
+        time = sink_data[primary_ind]['time'] - form_time
+        time = yt.YTArray(time, 's')
+        end_ind = np.argmin(abs(time.in_units('yr').value - 10000))
+        plot_time = time.in_units('yr')[:end_ind+1]
         
-        if os.path.exists(single_pickle):
-            file = open(single_pickle, 'rb')
-            sink_data, line_counter = pickle.load(file)
-            file.close()
-            form_time = np.nan
+        for plot_q in plot_quantity:
+            plot_it = plot_it + 2
             
-            for sink_id in sink_data.keys():
-                #sink_id = list(sink_data.keys())[0]
-                if np.isnan(form_time):
-                    form_time = sink_data[sink_id]['time'][0]
-                mass = yt.YTArray(sink_data[sink_id]['mass'], 'g')
-                L_tot = np.sqrt(sink_data[sink_id]['anglx']**2 + sink_data[sink_id]['angly']**2 + sink_data[sink_id]['anglz']**2)
-                L_tot = yt.YTArray(L_tot/sink_data[sink_id]['mass'], 'cm**2/s')
-                #L_tot = L_tot.in_units('m**2/s')
-                time = sink_data[sink_id]['time'] - form_time
-                time = yt.YTArray(time, 's')
-                if max_time[Spin_labels.index(spin_lab)][Mach_labels.index(mach_lab)] == None:
-                    plot_time = time.in_units('yr')
-                    plot_mass = mass.in_units('m**2/s')
-                else:
-                    end_time = max_time[Spin_labels.index(spin_lab)][Mach_labels.index(mach_lab)]
-                    end_ind = np.argmin(abs(time.in_units('yr').value - end_time))
-                    plot_time = time.in_units('yr')[:end_ind+1]
-                    plot_L = L_tot.in_units('m**2/s')[:end_ind+1]
-                    dL = plot_L[1:] - plot_L[:-1]
-                    dt = plot_time[1:] - plot_time[:-1]
-                    plot_L = dL/dt
-                if sink_id == list(sink_data.keys())[0]:
-                    axs.flatten()[plot_it].plot(plot_time[:-1], plot_L, label='$\Omega t_{ff}$='+spin_lab, linestyle=line_styles[Spin_labels.index(spin_lab)], color=colors[Spin_labels.index(spin_lab)], alpha=0.25)
-                else:
-                    axs.flatten()[plot_it].plot(plot_time[:-1], plot_L, linestyle=line_styles[Spin_labels.index(spin_lab)], color=colors[Spin_labels.index(spin_lab)], alpha=0.25)
-        else:
-            print("Couldn't open", single_pickle)
-
-        axs.flatten()[plot_it].set_xlabel('Time ($yr$)')
-        if mach_lab == '0.0':
-            axs.flatten()[plot_it].set_ylabel('h ($m^2/s$)')
-        else:
-            yticklabels = axs.flatten()[plot_it].get_yticklabels()
-            plt.setp(yticklabels, visible=False)
-        if mach_lab != '0.2' and spin_lab == Spin_labels[-1]:
-            xticklabels = axs.flatten()[plot_it].get_xticklabels()
-            plt.setp(xticklabels[-1], visible=False)
-        if mach_lab == '0.0' and spin_lab == '0.35':
-            xticklabels = axs.flatten()[plot_it].get_xticklabels()
-            plt.setp(xticklabels[-1], visible=False)
-
-axs.flatten()[0].legend(loc='lower left')
-axs.flatten()[plot_it-1].set_xlim([0, 10000])
-axs.flatten()[plot_it-1].set_ylim([0.e15, 0.1e15])
-plt.savefig('d_spin_init_spin_spec_comp.pdf', bbox_inches='tight')
-"""
-
-
+            if plot_q == 'mass':
+                m_star = 0.75 * mass.in_units('g')[:end_ind+1]
+                m_star_lower = 0.6 * mass.in_units('g')[:end_ind+1]
+                m_star_upper = 0.9 * mass.in_units('g')[:end_ind+1]
+                axs.flatten()[plot_it].plot(plot_time, m_star.in_units('msun'), label='$\Omega t_{ff}$='+spin_lab, linestyle=line_styles[Spin_labels.index(spin_lab)], color=colors[Spin_labels.index(spin_lab)])
+                axs.flatten()[plot_it].fill_between(plot_time, m_star_lower.in_units('msun'), m_star_upper.in_units('msun'), alpha=0.2, color=colors[Spin_labels.index(spin_lab)])
+                if mach_lab == '0.0':
+                    axs.flatten()[plot_it].set_ylabel('$M_\star$ ($M_\odot$)')
+                    axs.flatten()[plot_it].legend(loc='best', ncol=2)
+            if plot_q == 'angular momentum':
+                l_star = 0.25 * L_tot.in_units('g*cm**2/s')[:end_ind+1]
+                l_star_lower = 0.1 * L_tot.in_units('g*cm**2/s')[:end_ind+1]
+                l_star_upper = 0.4 * L_tot.in_units('g*cm**2/s')[:end_ind+1]
+                axs.flatten()[plot_it].plot(plot_time, l_star.in_units('kg*m**2/s'), linestyle=line_styles[Spin_labels.index(spin_lab)], color=colors[Spin_labels.index(spin_lab)])
+                axs.flatten()[plot_it].fill_between(plot_time, l_star_lower.in_units('kg*m**2/s'), l_star_upper.in_units('kg*m**2/s'), alpha=0.2, color=colors[Spin_labels.index(spin_lab)])
+                if mach_lab == '0.0':
+                    axs.flatten()[plot_it].set_ylabel('$L_\star$ ($kgm^2/s$)')
+            if plot_q == 'specific angular momentum':
+                h_star = l_star/m_star
+                h_star_lower = l_star_lower/m_star_upper
+                h_star_upper = l_star_upper/m_star_lower
+                axs.flatten()[plot_it].plot(plot_time, h_star.in_units('m**2/s'), linestyle=line_styles[Spin_labels.index(spin_lab)], color=colors[Spin_labels.index(spin_lab)])
+                axs.flatten()[plot_it].fill_between(plot_time, h_star_lower.in_units('m**2/s'), h_star_upper.in_units('m**2/s'), alpha=0.2, color=colors[Spin_labels.index(spin_lab)])
+                if mach_lab == '0.0':
+                    axs.flatten()[plot_it].set_ylabel('$h_\star$ ($gm^2/s$)')
+            if plot_q == 'period':
+                P_star = ((4*np.pi)/5) * (radius.in_units('m')**2)/h_star
+                P_star_lower = ((4*np.pi)/5) * (radius.in_units('m')**2)/h_star_lower
+                P_star_upper = ((4*np.pi)/5) * (radius.in_units('m')**2)/h_star_upper
+                axs.flatten()[plot_it].plot(plot_time, P_star.in_units('days'), linestyle=line_styles[Spin_labels.index(spin_lab)], color=colors[Spin_labels.index(spin_lab)])
+                axs.flatten()[plot_it].fill_between(plot_time, P_star_lower.in_units('days'), P_star_upper.in_units('days'), alpha=0.2, color=colors[Spin_labels.index(spin_lab)])
+                axs.flatten()[plot_it].set_xlabel('Time (yr)')
+                if mach_lab == '0.0':
+                    axs.flatten()[plot_it].set_ylabel('$P_\star$ (days)')
+            
