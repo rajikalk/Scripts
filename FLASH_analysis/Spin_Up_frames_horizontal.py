5245245734eea3eb19274e8ca44b698fc13a6a0c
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-03-13 16:51:51 +0100

    flipping line styles for spin up percentage

--- FLASH_analysis/Spin_Up_frames_horizontal.py
+++ FLASH_analysis/Spin_Up_frames_horizontal.py
@@ -108,7 +108,7 @@ for spin_val in spin_values:
                 plot_inds.append(plot_ind)
                 
             plot_start_time = np.argmin(abs(time.in_units('yr').value - (plot_times[0]-500)))
-            plot_end_time = np.argmin(abs(time.in_units('yr').value - (plot_times[n_frames-1]-500)))
+            plot_end_time = np.argmin(abs(time.in_units('yr').value - (plot_times[n_frames-1]+500)))
             axes_1.plot(time[plot_start_time:plot_end_time].in_units('yr'), L_tot[plot_start_time:plot_end_time])
             axes_1.scatter(time[np.array(plot_inds)].in_units('yr'), L_tot[np.array(plot_inds)])
             axes_1.set_xlim([time[plot_start_time].in_units('yr'), time[plot_end_time].in_units('yr')])
