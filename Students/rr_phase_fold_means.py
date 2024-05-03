713291a2210823bfc9246334c91c5645e6299d52
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-05-03 11:58:29 +0200

    deriving significance of disk size at periastron

--- Students/rr_phase_fold_means.py
+++ Students/rr_phase_fold_means.py
@@ -175,17 +175,8 @@ for i,sink_inds in enumerate([('91','90'),('48','49'),('165','164')]):
     apastron_signif = abs((apastron_mean - periastron_mean)/periastron_std)
     periastron_signif = abs((apastron_mean - periastron_mean)/apastron_std)
     
-    if np.isinf(apastron_signif) or np.isnan(apastron_signif):
-        import pdb
-        pdb.set_trace()
-    else:
-        print("The secondary disk size at apastron is", apastron_signif, "sigma away from distribution at periastron")
-        
-    if np.isinf(periastron_signif) or np.isnan(periastron_signif):
-        import pdb
-        pdb.set_trace()
-    else:
-        print("The secondary disk size at periastron is", periastron_signif, "sigma away from distribution at apastron")
+    print("The secondary disk size at apastron is", apastron_signif, "sigma away from distribution at periastron")
+    print("The secondary disk size at periastron is", periastron_signif, "sigma away from distribution at apastron")
     
     bin_means = [bin_means[0]] + bin_means + [bin_means[-1]]
     bin_medians = [bin_medians[0]] + bin_medians + [bin_medians[-1]]
