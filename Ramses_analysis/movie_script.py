0f814f9f4570f80a2118387020a81ba31301cfeb
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-08-16 12:34:57 +0200

    cleaning up FU Ori scripts

--- Ramses_analysis/movie_script.py
+++ Ramses_analysis/movie_script.py
@@ -204,6 +204,7 @@ mym.set_units(units_override)
 
 
 #find sink particle to center on and formation time
+del units_override['density_unit']
 ds = yt.load(files[-1], units_override=units_override)
 #try:
 dd = ds.all_data()
