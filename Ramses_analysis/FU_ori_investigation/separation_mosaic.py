506cd5d08bc608bd9b9146ee75514a541cbc8670
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-08-21 13:12:39 +0200

    working on making mosaic frames with yt 4

--- Ramses_analysis/FU_ori_investigation/separation_mosaic.py
+++ Ramses_analysis/FU_ori_investigation/separation_mosaic.py
@@ -45,7 +45,7 @@ while fit < no_frames:
     if os.path.isfile(args.input_dir+'/XY/movie_frame_' + ("%06d" % fit) +'.pkl') and os.path.isfile(args.input_dir+'/XZ/movie_frame_' + ("%06d" % fit) +'.pkl') and os.path.isfile(args.input_dir+'/YZ/movie_frame_' + ("%06d" % fit) +'.pkl'):
         
         fig = plt.figure()
-        gs = fig.add_gridspec(2, 2, wspace=0)
+        gs = fig.add_gridspec(2, 2, wspace=0, hspace=0)
         (ax1, ax2), (ax3, ax4) = gs.subplots()
     
         yz_pickle = args.input_dir+'/YZ/movie_frame_' + ("%06d" % fit) +'.pkl'
@@ -74,32 +74,27 @@ while fit < no_frames:
         cbar_max = args_dict['cbar_max']
         has_particles = args_dict['has_particles']
         
-        ax1.set_xlabel(xabel, labelpad=-1, fontsize=args.text_font)
         ax1.set_ylabel(yabel, fontsize=args.text_font) #, labelpad=-20
         ax1.set_xlim(xlim)
         ax1.set_ylim(ylim)
-        plt.savefig("Mosaic_test_0.jpg", format='jpg', bbox_inches='tight')
         
         cmap=plt.cm.gist_heat
         plot = ax1.pcolormesh(X, Y, image, cmap=cmap, norm=LogNorm(vmin=cbar_min, vmax=cbar_max), rasterized=True, zorder=1)
         ax1.set_aspect('equal')
-        plt.savefig("Mosaic_test_1.jpg", format='jpg', bbox_inches='tight')
         if fit > 0 or time_val > -1.0:
             # plt.streamplot(X, Y, magx, magy, density=4, linewidth=0.25, arrowstyle='-', minlength=0.5)
             ax1.streamplot(X, Y, magx, magy, density=4, linewidth=0.25, arrowstyle='-', minlength=0.5, color='grey', zorder=2)
         else:
             ax1.streamplot(X, Y, magx, magy, density=4, linewidth=0.25, minlength=0.5, zorder=2)
-        plt.savefig("Mosaic_test_2.jpg", format='jpg', bbox_inches='tight')
         mym.my_own_quiver_function(ax1, X_vel, Y_vel, velx, vely, plot_velocity_legend=False, limits=[xlim, ylim], standard_vel=args.standard_vel, Z_val=None)
-        plt.savefig("Mosaic_test_3.jpg", format='jpg', bbox_inches='tight')
         mym.annotate_particles(ax1, part_info['particle_position'], part_info['accretion_rad'], limits=[xlim, ylim], annotate_field=part_info['particle_mass'], particle_tags=part_info['particle_tag'])
-        plt.savefig("Mosaic_test_4.jpg", format='jpg', bbox_inches='tight')
+    
         time_string = "$t$="+str(int(time_val))+"yr"
         time_string_raw = r"{}".format(time_string)
         time_text = ax1.text((xlim[0]+0.01*(xlim[1]-xlim[0])), (ylim[1]-0.03*(ylim[1]-ylim[0])), time_string_raw, va="center", ha="left", color='w', fontsize=args.text_font)
         time_text.set_path_effects([path_effects.Stroke(linewidth=3, foreground='black'), path_effects.Normal()])
         
-        plt.savefig("Mosaic_test_5.jpg", format='jpg', bbox_inches='tight')
+        plt.savefig("Mosaic_test_0.jpg", format='jpg', bbox_inches='tight')
         
         xz_pickle = args.input_dir+'/XZ/movie_frame_' + ("%06d" % fit) +'.pkl'
         file = open(yz_pickle, 'rb')
@@ -130,21 +125,62 @@ while fit < no_frames:
         ax2.set_xlabel(xabel, labelpad=-1, fontsize=args.text_font)
         ax2.set_xlim(xlim)
         ax2.set_ylim(ylim)
-        plt.savefig("Mosaic_test_0.jpg", format='jpg', bbox_inches='tight')
         
         cmap=plt.cm.gist_heat
         plot = ax2.pcolormesh(X, Y, image, cmap=cmap, norm=LogNorm(vmin=cbar_min, vmax=cbar_max), rasterized=True, zorder=1)
         ax2.set_aspect('equal')
-        plt.savefig("Mosaic_test_1.jpg", format='jpg', bbox_inches='tight')
         if fit > 0 or time_val > -1.0:
             # plt.streamplot(X, Y, magx, magy, density=4, linewidth=0.25, arrowstyle='-', minlength=0.5)
             ax2.streamplot(X, Y, magx, magy, density=4, linewidth=0.25, arrowstyle='-', minlength=0.5, color='grey', zorder=2)
         else:
             ax2.streamplot(X, Y, magx, magy, density=4, linewidth=0.25, minlength=0.5, zorder=2)
-        plt.savefig("Mosaic_test_2.jpg", format='jpg', bbox_inches='tight')
-        mym.my_own_quiver_function(ax2, X_vel, Y_vel, velx, vely, plot_velocity_legend=True, limits=[xlim, ylim], standard_vel=args.standard_vel, Z_val=None)
-        plt.savefig("Mosaic_test_3.jpg", format='jpg', bbox_inches='tight')
+        mym.my_own_quiver_function(ax2, X_vel, Y_vel, velx, vely, plot_velocity_legend=False, limits=[xlim, ylim], standard_vel=args.standard_vel, Z_val=None)
         
+        plt.savefig("Mosaic_test_1.jpg", format='jpg', bbox_inches='tight')
+
+        
+        xy_pickle = args.input_dir+'/XZ/movie_frame_' + ("%06d" % fit) +'.pkl'
+        file = open(yz_pickle, 'rb')
+        X, Y, image, magx, magy, X_vel, Y_vel, velx, vely, velz, part_info, args_dict, simfo = pickle.load(file)
+        #X, Y, image, magx, magy, X_vel, Y_vel, velx, vely, xlim, ylim, has_particles, part_info, simfo, time_val, xabel, yabel = pickle.load(file)
+        file.close()
+        
+        time_val = args_dict['time_val']
+        
+        xlim = args_dict['xlim']
+        ylim = args_dict['ylim']
+        if np.round(np.mean(args_dict['xlim'])) != np.round(np.mean(X)):
+            shift_x = np.mean(X)
+            shift_y = np.mean(Y)
+            X = X - shift_x
+            Y = Y - shift_y
+            X_vel = X_vel - shift_x
+            Y_vel = Y_vel - shift_y
+            part_info['particle_position'][0] = part_info['particle_position'][0] - shift_x
+            part_info['particle_position'][1] = part_info['particle_position'][1] - shift_y
+        
+        xabel = args_dict['xabel']
+        yabel = args_dict['yabel']
+        cbar_min = args_dict['cbar_min']
+        cbar_max = args_dict['cbar_max']
+        has_particles = args_dict['has_particles']
+        
+        ax4.set_xlabel(xabel, labelpad=-1, fontsize=args.text_font)
+        ax4.set_xlim(xlim)
+        ax4.set_ylim(ylim)
+       
+        cmap=plt.cm.gist_heat
+        plot = ax4.pcolormesh(X, Y, image, cmap=cmap, norm=LogNorm(vmin=cbar_min, vmax=cbar_max), rasterized=True, zorder=1)
+        ax4.set_aspect('equal')
+        
+        if fit > 0 or time_val > -1.0:
+            # plt.streamplot(X, Y, magx, magy, density=4, linewidth=0.25, arrowstyle='-', minlength=0.5)
+            ax4.streamplot(X, Y, magx, magy, density=4, linewidth=0.25, arrowstyle='-', minlength=0.5, color='grey', zorder=2)
+        else:
+            ax4.streamplot(X, Y, magx, magy, density=4, linewidth=0.25, minlength=0.5, zorder=2)
+        
+        mym.my_own_quiver_function(ax4, X_vel, Y_vel, velx, vely, plot_velocity_legend=True, limits=[xlim, ylim], standard_vel=args.standard_vel, Z_val=None)
+        plt.savefig("Mosaic_test_2.jpg", format='jpg', bbox_inches='tight')
         
         
         import pdb
