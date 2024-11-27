26c869807e841989b25dc2bb28da9ad9ab8f4ac6
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-11-27 10:51:38 +0100

    getting resimulate data

--- FLASH_analysis/Resimulate/get_resimulate_data.py
+++ FLASH_analysis/Resimulate/get_resimulate_data.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 import yt
 sink_evol_pickle = sys.argv[1]
 zoom_in_sink = sys.argv[2]
+sim_dir = '/scratch/ek9/ccf100/sf_outflow/r1024mM5Ma2A1oh'
 
 file = open(sink_evol_pickle, 'rb')
 sink_data, prev_line_counter = pickle.load(file)
 file.close()
 
-form_time = yt.YTArray([sink_data[zoom_in_sink]['time'][0], 's')
+form_time = yt.YTArray(sink_data[zoom_in_sink]['time'][0], 's')
 form_position = yt.YTArray([sink_data[zoom_in_sink]['posx'][0], sink_data[zoom_in_sink]['posy'][1], sink_data[zoom_in_sink]['posy'][2]], 'cm')
 box_length = yt.YTQuantity(0.1, 'pc')
 
@@ -20,4 +21,7 @@ ymin = form_position[1] - box_length/2
 ymax = form_position[1] + box_length/2
 zmin = form_position[2] - box_length/2
 zmax = form_position[2] + box_length/2
+
+import pdb
+pdb.set_trace()
 
