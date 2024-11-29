c069ae3df8717720960759cebf2d7d920f20c2c4
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-11-29 09:13:44 +0100

    fixing the quiver plotting

--- FLASH_analysis/Resimulate/get_resimulate_data.py
+++ FLASH_analysis/Resimulate/get_resimulate_data.py
@@ -171,6 +171,7 @@ time_text.set_path_effects([path_effects.Stroke(linewidth=3, foreground='black')
 
 square = patches.Rectangle((xmin.in_units('pc'), ymin.in_units('pc')), box_length, box_length, edgecolor='green', facecolor='none')
 ax.add_patch(square)
+plt.contour([X_image.value, Y_image.value], image, [1.e-18])
 
 plt.savefig("xy_proj.jpg", format='jpg', bbox_inches='tight', dpi=300)
 
@@ -240,6 +241,7 @@ time_text.set_path_effects([path_effects.Stroke(linewidth=3, foreground='black')
 
 square = patches.Rectangle((xmin.in_units('pc'), zmin.in_units('pc')), box_length, box_length, edgecolor='green', facecolor='none')
 ax.add_patch(square)
+plt.contour([X_image.value, Y_image.value], image, [1.e-18])
 
 plt.savefig("xz_proj.jpg", format='jpg', bbox_inches='tight', dpi=300)
 
@@ -307,5 +309,6 @@ time_text.set_path_effects([path_effects.Stroke(linewidth=3, foreground='black')
 
 square = patches.Rectangle((ymin.in_units('pc'), zmin.in_units('pc')), box_length, box_length, edgecolor='green', facecolor='none')
 ax.add_patch(square)
+plt.contour([X_image.value, Y_image.value], image, [1.e-18])
 
 plt.savefig("yz_proj.jpg", format='jpg', bbox_inches='tight', dpi=300)
