e45d772c86cb52bc4bc0be0b29c62cb8dfb18550
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-12-02 16:08:56 +0100

    Making a nice plot for a job application

--- Ramses_analysis/FU_ori_investigation/resolution_study_application.py
+++ Ramses_analysis/FU_ori_investigation/resolution_study_application.py
@@ -29,7 +29,7 @@ matplotlib.rcParams['text.latex.preamble'] = [
        r'\usepackage{siunitx}',   # i need upright \micro symbols, but you need...
        '\sisetup{detect-all}',   # ...this to force siunitx to actually use your fonts
        r'\usepackage{helvet}',    # set the normal font here
-       '\usepackage{sansmath}',  # load up the sansmath so that math -> helvet
+       r'\usepackage{sansmath}',  # load up the sansmath so that math -> helvet
        '\sansmath'               # <- tricky! -- gotta actually tell tex to use!
 ]
 def parse_inputs():
