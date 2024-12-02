81e8ab4dfe4d3e4355162fb5b7416f7d79e90160
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2024-12-02 16:03:55 +0100

    Making a nice plot for a job application

--- Ramses_analysis/FU_ori_investigation/resolution_study_application.py
+++ Ramses_analysis/FU_ori_investigation/resolution_study_application.py
@@ -25,6 +25,7 @@ matplotlib.rcParams['mathtext.sf'] = 'Arial'
 matplotlib.rcParams['mathtext.default'] = 'regular'
 matplotlib.rcParams['font.sans-serif'] = 'Arial'
 matplotlib.rcParams['font.family'] = 'sans-serif'
+'''
 matplotlib.rcParams['text.latex.preamble'] = [
        r'\usepackage{siunitx}',   # i need upright \micro symbols, but you need...
        r'\sisetup{detect-all}',   # ...this to force siunitx to actually use your fonts
@@ -32,6 +33,7 @@ matplotlib.rcParams['text.latex.preamble'] = [
        r'\usepackage{sansmath}',  # load up the sansmath so that math -> helvet
        r'\sansmath'               # <- tricky! -- gotta actually tell tex to use!
 ]
+'''
 
 def parse_inputs():
     import argparse
