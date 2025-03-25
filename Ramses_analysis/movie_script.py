2c491b4de3776692142053e0dab085634cf56220
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2025-03-25 09:32:09 +0100

    Fixing up plotting fo all files

--- Ramses_analysis/movie_script.py
+++ Ramses_analysis/movie_script.py
@@ -384,7 +384,7 @@ if args.make_frames_only == 'False':
             
             #Define box::
             if args.image_center == 1:
-                center_pos = yt.YTArray([dd['sink_particle_velx'][sink_id].in_units('au').value, dd['sink_particle_vely'][sink_id].in_units('au').value, dd['sink_particle_velz'][sink_id].in_units('au').value], 'au')
+                center_pos = yt.YTArray([dd['sink_particle_posx'][sink_id].in_units('au').value, dd['sink_particle_posy'][sink_id].in_units('au').value, dd['sink_particle_posz'][sink_id].in_units('au').value], 'au')
             else:
                 center_pos = dd['Center_Position'].in_units('au')
             if args.axis == 'xy':
