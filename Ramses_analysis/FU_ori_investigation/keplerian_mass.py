4bb3bcc78fdcf3dc6b7735715600d45bd79377a8
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2025-04-07 11:44:48 +0200

    Trying to measure kep mass using half the separation

--- Ramses_analysis/FU_ori_investigation/keplerian_mass.py
+++ Ramses_analysis/FU_ori_investigation/keplerian_mass.py
@@ -18,7 +18,7 @@ def parse_inputs():
     parser.add_argument("-sink_id", "--sink_number", help="Which sink do you want to measure around? default is the sink with lowest velocity", type=int, default=None)
     parser.add_argument("-make_pickles", "--make_pickle_files", type=str, default="True")
     parser.add_argument("-make_plots", "--make_plot_figures", type=str, default="True")
-    parser.add_argument("-sphere_radius", "--sphere_radius_cells", type=float, default=20)
+    parser.add_argument("-sphere_radius", "--sphere_radius_cells", type=float)
     parser.add_argument("files", nargs='*')
     args = parser.parse_args()
     return args
@@ -88,7 +88,8 @@ if args.make_pickle_files == "True":
         print("Doing initial ds.all_data() load")
     dd = ds.all_data()
     dx_min = np.min(dd['dx'].in_units('au'))
-    sphere_radius = yt.YTQuantity(args.sphere_radius_cells, 'au')
+    if args.sphere_radius_cells != None:
+        sphere_radius = yt.YTQuantity(args.sphere_radius_cells, 'au')
     r_acc = 4*dx_min
     if args.sink_number == None:
         sink_id = np.argmin(dd['sink_particle_speed'])
@@ -118,6 +119,11 @@ if args.make_pickle_files == "True":
         #Get secondary position
         primary_position = yt.YTArray([dd['sink_particle_posx'][sink_id-1], dd['sink_particle_posy'][sink_id-1], dd['sink_particle_posz'][sink_id-1]])
         secondary_position = yt.YTArray([dd['sink_particle_posx'][sink_id], dd['sink_particle_posy'][sink_id], dd['sink_particle_posz'][sink_id]])
+        
+        if arg.sphere_radius_cells == None:
+            separation = np.sqrt(np.sum((primary_position - secondary_position)**2))
+            sphere_radius = 0.5 * separation.in_units('au')
+            
         
         primary_velocity = yt.YTArray([dd['sink_particle_velx'][sink_id-1], dd['sink_particle_vely'][sink_id-1], dd['sink_particle_velz'][sink_id-1]])
         secondary_velocity = yt.YTArray([dd['sink_particle_velx'][sink_id], dd['sink_particle_vely'][sink_id], dd['sink_particle_velz'][sink_id]])
@@ -220,7 +226,6 @@ if args.make_plot_figures == "True":
     single_col_width = 3.50394 #inches
     page_height = 10.62472 #inches
     font_size = 10
-    r_acc = np.round(yt.YTQuantity(4.0,"pc").in_units('au')/(2**20)*4, decimals=2)
     
     sink_pickle = "/lustre/astro/rlk/FU_ori_investigation/Sink_pickles/particle_data_L20.pkl"
     file_open = open(sink_pickle, 'rb')
@@ -229,141 +234,45 @@ if args.make_plot_figures == "True":
 
     #radial fraction
     time_arr = []
-    sep_arr = []
-    acc_arr = []
-    mean_dens_array = []
-    rv_frac_median = []
-    rv_frac_density_weighted_mean = []
-    rv_frac_low = []
-    rv_frac_high = []
-    rv_frac_min = []
-    rv_frac_max = []
+    Kep_mass_primary = []
+    Kep_mass_secondary = []
+    
+    pickle_files = sorted(glob.glob(save_dir + "kepl_mass_*.pkl"))
+    
+    for pick_file in pickle_files:
+        file_open = open(pick_file, 'rb')
+        save_dict = pickle.load(file_open)
+        file_open.close()
+        
+        time_arr = time_arr + save_dict['time']
+        Kep_mass_primary = Kep_mass_primary + save_dict['Kep_mass_primary']
+        Kep_mass_secondary = Kep_mass_secondary + save_dict['Kep_mass_secondary']
+        
+    sort_inds = np.argsort(time_arr)
+    time_arr = np.array(time_arr)[sort_inds]
+    Kep_mass_primary = np.array(Kep_mass_primary)[sort_inds]
+    Kep_mass_secondary = np.array(Kep_mass_secondary)[sort_inds]
+    
+    start_ind = np.argmin(abs(particle_data['time'] - time_arr[0]))
+    end_ind = np.argmin(abs(particle_data['time'] - time_arr[-1]))
 
     #Plotting
-    pickle_files = sorted(glob.glob(save_dir + "movie_frame_*.pkl"))
-    xmin = np.nan
-    xmax = np.nan
-    ymin = np.nan
-    ymax = np.nan
-    lin_thresh = np.nan
-    rit = -1
-    fit = 0
-    while fit < len(pickle_files):
-        rit = rit + 1
-        if rit == size:
-            rit = 0
-        if rank == rit:
-            plot_pickle = pickle_files[fit]
-            file_name = save_dir + plot_pickle[:-3]+'jpg'
-            if os.path.isfile(file_name) == False:
-                plt.clf()
-                fig, axs = plt.subplots(ncols=2, nrows=1, figsize=(two_col_width, 0.8*single_col_width))#, sharey=True)
-                #plt.subplots_adjust(wspace=0.0)
-                #plt.subplots_adjust(hspace=0.0)
-            
-                file = open(plot_pickle, 'rb')
-                time_val, separation, accretion_rate, density, radial_momentum, radial_velocity_fraction = pickle.load(file)
-                file.close()
-                
-                time_arr.append(time_val)
-                sep_arr.append(separation)
-                acc_arr.append(accretion_rate)
-                neg_inds = np.where(radial_momentum<0)[0]
-                mean_dens_array.append(np.nanmean(density[neg_inds]))
-                rv_frac_median.append(np.nanmedian(radial_velocity_fraction[neg_inds]))
-                density_weighted_mean = np.sum(density[neg_inds]*radial_velocity_fraction[neg_inds])/np.sum(density[neg_inds])
-                rv_frac_density_weighted_mean.append(density_weighted_mean)
-                rv_frac_low.append(np.nanmean(radial_velocity_fraction[neg_inds])-np.nanstd(radial_velocity_fraction[neg_inds]))
-                rv_frac_high.append(np.nanmean(radial_velocity_fraction[neg_inds])+np.nanstd(radial_velocity_fraction[neg_inds]))
-                rv_frac_min.append(np.nanmin(radial_velocity_fraction[neg_inds]))
-                rv_frac_max.append(np.nanmax(radial_velocity_fraction[neg_inds]))
-                
-                if np.isnan(xmin):
-                    xmin = np.min(density.value)
-                elif np.min(density.value) < xmin:
-                    xmin = np.min(density.value)
-                
-                if np.isnan(xmax):
-                    xmax = np.max(density.value)
-                elif np.max(density.value) > xmax:
-                    xmax = np.max(density.value)
-                    
-                if np.isnan(ymin):
-                    ymin = np.nanmin(radial_momentum.value)
-                elif np.nanmin(radial_momentum.value) < ymin:
-                    ymin = np.nanmin(radial_momentum.value)
-                    
-                if np.isnan(ymax):
-                    ymax = np.nanmax(radial_momentum.value)
-                elif np.nanmax(radial_momentum.value) > ymax:
-                    ymax = np.nanmax(radial_momentum.value)
-                    
-                if np.isnan(lin_thresh):
-                    lin_thresh = np.nanmin(np.abs(radial_momentum.value))
-                elif np.nanmin(np.abs(radial_momentum.value)) < lin_thresh:
-                    lin_thresh = np.nanmin(np.abs(radial_momentum.value))
-                    
-                #Plot figure
-                axs[0].semilogy(particle_data['time'], particle_data['separation'])
-                axs[0].axhline(y=r_acc*2, alpha=0.5, linestyle='--')
-                time_ind = np.argmin(abs(yt.YTArray(particle_data['time']) - time_val))
-                axs[0].scatter(particle_data['time'][time_ind], particle_data['separation'][time_ind])
-                axs[0].set_xlim([np.min(particle_data['time']), np.max(particle_data['time'])])
-                axs[0].set_ylim([np.min(particle_data['separation']), np.max(particle_data['separation'])])
-                axs[0].set_xlabel('Time (yr)')
-                axs[0].set_ylabel('Separation (au)')
-                
-                axs[1].set_xscale('log')
-                axs[1].set_yscale('symlog', linthresh=lin_thresh)
-                plot = axs[1].scatter(density.value, radial_momentum.value, c=radial_velocity_fraction, cmap=cm, vmin=0, vmax=1, edgecolor='k')
-                cbar = plt.colorbar(plot, pad=0.0)
-                cbar.set_label(r"v$_{radial}$/v$_{magnitude}$", rotation=270, labelpad=14)
-                axs[1].set_xlim([xmin,xmax])
-                axs[1].set_ylim([ymin,ymax])
-                axs[1].set_xlabel('density (g/cm$^3$)')
-                axs[1].set_ylabel('radial momentum (cm$\,$g/s)')
 
-                plt.savefig(file_name, bbox_inches='tight', dpi=300)
-                print("Plotted", file_name, "for pickle", fit, "of", len(pickle_files))
-        fit = fit + 1
-
-    #Plot radial fraction evolution
-    evol_pickle = "radial_vel_evol_"+str(rank)+".pkl"
-    if os.path.isfile(evol_pickle):
-        file = open(evol_pickle, 'rb')
-        time_arr_pick, sep_arr_pick, mean_dens_array_pick, acc_arr_pick, rv_frac_median_pick, rv_frac_density_weighted_mean_pick, rv_frac_low_pick, rv_frac_high_pick = pickle.load(file)
-        file.close()
-        
-        #CONTINUE EDITTING HERE
-        import pdb
-        pdb.set_trace()
-        
-    else:
-        file = open("radial_vel_evol_"+str(rank)+".pkl", 'wb')
-        pickle.dump((time_arr, sep_arr, mean_dens_array, acc_arr, rv_frac_median, rv_frac_density_weighted_mean, rv_frac_low, rv_frac_high), file)
-        file.close()
-    
     plt.clf()
-    fig, axs = plt.subplots(ncols=1, nrows=4, figsize=(single_col_width, 1.5*single_col_width), sharex=True)
-    plt.subplots_adjust(hspace=0.0)
-    axs[0].semilogy(time_arr, sep_arr)
-    axs[0].set_xlim([0, 9000])
+    fig, axs = plt.subplots(ncols=1, nrows=2, figsize=(two_col_width, single_col_width), sharex=True)
+    
+    #Plot figure
+    axs[0].semilogy(particle_data['time'][start_ind:end_ind], particle_data['separation'][start_ind:end_ind])
+    axs[0].set_xlim([particle_data['time'][start_ind], particle_data['time'][end_ind]])
+    axs[0].set_ylim([np.min(particle_data['separation'][start_ind:end_ind]), np.max(particle_data['separation'][start_ind:end_ind])])
     axs[0].set_ylabel('Separation (au)')
     
-    axs[1].semilogy(time_arr, acc_arr)
-    axs[1].set_xlim([0, 9000])
-    axs[1].set_ylabel('Accretion rate (msun/yr)')
-    
-    
-    axs[2].plot(time_arr, rv_frac_median)
-    axs[2].plot(time_arr, rv_frac_density_weighted_mean, 'k--')
-    #axs[1].plot(time_arr, rv_frac_min)
-    #axs[1].plot(time_arr, rv_frac_max)
-    axs[2].fill_between(time_arr, rv_frac_low, rv_frac_high, alpha=0.2)
-    axs[2].set_ylabel('v$_{radial}$/v$_{magnitude}$')
-    
-    axs[3].semilogy(time_arr, mean_dens_array)
-    axs[3].set_xlim([0, 9000])
-    axs[3].set_ylabel('Density (g/cm$^3$)')
-    axs[3].set_xlabel('Time (yr)')
-    plt.savefig('rv_frac_evolution.jpg', bbox_inches='tight', dpi=300)
+    axs[1].semilogy(time_arr,Kep_mass_primary, label="Primary")
+    axs[1].semilogy(time_arr,Kep_mass_secondary, label="Secondary")
+    axs[1].legend()
+    axs[1].set_xlabel('Time (yr)')
+    axs[1].set_ylabel('Keplerian mass (M$_\odot$)')
+
+    file_name = save_dir+"Keplerian_mass.pdf"
+    plt.savefig(file_name, bbox_inches='tight', dpi=300)
+    print("Plotted", file_name)
