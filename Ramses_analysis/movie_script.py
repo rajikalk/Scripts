269bccd7dbeeec0f0051ee646ad82a6e4de4c337
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2025-04-17 10:02:42 +0200

    updating movie script to take multiple times

--- Ramses_analysis/movie_script.py
+++ Ramses_analysis/movie_script.py
@@ -366,8 +366,10 @@ if args.make_frames_only == 'False':
         make_pickle = False
         if args.plot_time is None:
             pickle_file = save_dir + "movie_frame_" + ("%06d" % frames[file_int]) + ".pkl"
-        else:
+        elif args.plot_time != None:
             pickle_file = save_dir + "time_" + str(args.plot_time) +".pkl"
+        elif len(args.movie_times) > 0:
+            pickle_file = save_dir + "time_" + str(m_times[file_int]) +".pkl"
         if os.path.isfile(pickle_file) == False:
             make_pickle = True
         elif os.path.isfile(pickle_file) == True:
