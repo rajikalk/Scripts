a0b02236427a663fd88194585551741963d57de5
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2025-06-05 10:58:05 +0200

    Fine tuning long term evolution plot

--- Ramses_analysis/FU_ori_investigation/find_FU_ori_events_global_pickle.py
+++ Ramses_analysis/FU_ori_investigation/find_FU_ori_events_global_pickle.py
@@ -37,7 +37,7 @@ for time_it in range(len(age)):
         scaled_L = useable_L - np.min(useable_L)
         scaled_L = scaled_L/np.max(scaled_L)
         cor = np.correlate(scaled_L,FU_temp,'same')
-        if np.median(cor)>66.6 and mass[time_it] > 0.1 and L_diff>10:
+        if np.median(cor)>66.6 and L_diff>10: #and mass[time_it] > 0.1
             plt.clf()
             fig, ax1 = plt.subplots()
 
