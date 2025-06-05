a65b668f034977418d49bd1b39507453fefa5309
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2025-06-05 10:16:32 +0200

    Fine tuning long term evolution plot

--- Ramses_analysis/FU_ori_investigation/find_FU_ori_events_global_pickle.py
+++ Ramses_analysis/FU_ori_investigation/find_FU_ori_events_global_pickle.py
@@ -33,7 +33,6 @@ for time_it in range(len(age)):
         L_diff = np.max(np.log10(useable_L)) - np.min(np.log10(useable_L))
         L_diff_arr.append(L_diff)
         time_arr.append(age[time_it])
-        useable_L = np.log10(useable_L)
         scaled_T = useable_times - useable_times[0]
         scaled_L = useable_L - np.min(useable_L)
         scaled_L = scaled_L/np.max(scaled_L)
