044eecbfb6b6514f9aae7c7420997959f5d013fe
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2025-06-06 13:26:19 +0200

    Trying to use magntiude instead of lsun

--- Ramses_analysis/FU_ori_investigation/find_FU_ori_events_global_pickle.py
+++ Ramses_analysis/FU_ori_investigation/find_FU_ori_events_global_pickle.py
@@ -127,6 +127,7 @@ for time_it in range(len(age)):
         cor = np.correlate(scaled_L,FU_temp_inv,'full')
         scaled_cor = (cor/7) * 100
         cor_arr.append(np.nanmedian(scaled_cor))
+        '''
         if L_diff>5 and useable_L[0]>4:
             #import pdb
             #pdb.set_trace()
@@ -138,7 +139,8 @@ for time_it in range(len(age)):
             plt.title('Max cor ='+ str(np.nanmax(cor)))
             plt.show()
             plt.savefig("Scaled_T_"+str(age[time_it])+".png")
-        if np.median(scaled_cor)>66.6 and L_diff>5: #and mass[time_it] > 0.1
+        '''
+        if np.nanmedian(scaled_cor)>20 and L_diff>5: #and mass[time_it] > 0.1
             import pdb
             pdb.set_trace()
             plt.clf()
