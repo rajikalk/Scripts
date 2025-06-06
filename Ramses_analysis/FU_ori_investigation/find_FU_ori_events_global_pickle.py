6b94d957afa54b84079e5af2ccf57357ff944395
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2025-06-06 15:15:31 +0200

    Trying to use magntiude instead of lsun

--- Ramses_analysis/FU_ori_investigation/find_FU_ori_events_global_pickle.py
+++ Ramses_analysis/FU_ori_investigation/find_FU_ori_events_global_pickle.py
@@ -146,11 +146,10 @@ for time_it in range(len(age)):
             fig, ax1 = plt.subplots()
 
             ax2 = ax1.twinx()
-            plt.gca().invert_yaxis()
-            ax1.plot(useable_times, scaled_L, label="scaled L_acc", color='b')
-            ax1.plot(useable_times[0]+np.linspace(0, scaled_T[-1], len(cor)), cor/np.max(cor), label="correlation", color='r')
+            ax1.invert_yaxis()
+            ax1.plot(useable_times, useable_L, label="scaled L_acc", color='b')
             
-            ax2.plot(useable_times, useable_L, color='b')
+            ax2.plot(useable_times[0]+np.linspace(0, scaled_T[-1], len(cor)), cor/np.max(cor), label="correlation", color='r')
 
             ax1.set_xlabel('Time (yr)')
             ax1.set_ylabel('scaled L_acc and correlation')
