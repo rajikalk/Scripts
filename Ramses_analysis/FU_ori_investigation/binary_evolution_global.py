878827fd8b5124cd68f2ede7c8f21e8249abf115
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2025-06-11 14:34:50 +0200

    plotting quantities of longer term binary evolution

--- Ramses_analysis/FU_ori_investigation/binary_evolution_global.py
+++ Ramses_analysis/FU_ori_investigation/binary_evolution_global.py
@@ -40,19 +40,23 @@ axs.flatten()[0].set_xlim([age[0], age[-1]])
 axs.flatten()[0].set_ylim([0, np.max(yt.YTArray(particle_data['mass']).T[0])])
 axs.flatten()[0].legend()
 axs.flatten()[0].set_ylabel('Mass (M$_\odot$)')
+axs.flatten()[0].tick_params(axis='both', direction='in', top=True, right=True)
 
 axs.flatten()[1].plot(age, mass_ratio)
 axs.flatten()[1].set_ylim([0, np.max(mass_ratio)])
 axs.flatten()[1].set_ylabel('Mass ratio ($q$)')
+axs.flatten()[1].tick_params(axis='both', direction='in', top=True, right=True)
 
 axs.flatten()[2].semilogy(age, particle_data['separation'])
 axs.flatten()[2].set_ylim([np.min(particle_data['separation']), np.max(particle_data['separation'])])
 axs.flatten()[2].set_ylabel('Separation (AU)')
+axs.flatten()[2].tick_params(axis='both', direction='in', top=True, right=True)
 
 axs.flatten()[3].plot(age, particle_data['eccentricity'])
 axs.flatten()[3].set_ylim([np.min(particle_data['eccentricity']), np.max(particle_data['eccentricity'])])
 axs.flatten()[3].set_ylabel('Eccentricity')
 axs.flatten()[3].set_xlabel('Time (kyr)')
+axs.flatten()[3].tick_params(axis='both', direction='in', top=True, right=True)
 plt.savefig('long_term_binary_evoluion.pdf', bbox_inches='tight', pad_inches=0.02)
 
 
