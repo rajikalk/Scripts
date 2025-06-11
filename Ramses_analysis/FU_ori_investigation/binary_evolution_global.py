54adc4f8ab406f51e1c7a37ba80a19722ddd6019
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2025-06-11 14:26:27 +0200

    plotting quantities of longer term binary evolution

--- Ramses_analysis/FU_ori_investigation/binary_evolution_global.py
+++ Ramses_analysis/FU_ori_investigation/binary_evolution_global.py
@@ -8,12 +8,22 @@ import pickle
 import yt
 from matplotlib.ticker import FormatStrFormatter
 
+matplotlib.rcParams['mathtext.fontset'] = 'stixsans'
+matplotlib.rcParams['mathtext.it'] = 'Arial:italic'
+matplotlib.rcParams['mathtext.rm'] = 'Arial'
+matplotlib.rcParams['mathtext.bf'] = 'Arial:bold'
+matplotlib.rcParams['mathtext.it'] = 'Arial:italic'
+matplotlib.rcParams['mathtext.rm'] = 'Arial'
+matplotlib.rcParams['mathtext.sf'] = 'Arial'
+matplotlib.rcParams['mathtext.default'] = 'regular'
+matplotlib.rcParams['font.sans-serif'] = 'Arial'
+matplotlib.rcParams['font.family'] = 'sans-serif'
+matplotlib.rcParams['text.latex.preamble'] = r"\usepackage{siunitx}" "\sisetup{detect-all}" r"\usepackage{helvet}" r"\usepackage{sansmath}" "\sansmath"               # <- tricky! -- gotta actually tell tex to use!
+
 two_col_width = 7.20472 #inches
 single_col_width = 3.50394 #inches
 page_height = 10.62472 #inches
 font_size = 10
-
-time_window = yt.YTQuantity(100, 'yr')
 
 global_pickle = "/groups/astro/rlk/rlk/FU_ori_investigation/Sink_pickles/particle_data_global.pkl"
 file_open = open(global_pickle, 'rb')
@@ -24,18 +34,30 @@ age = yt.YTArray(particle_data['time']).in_units('kyr')
 mass_ratio = yt.YTArray(particle_data['mass']).T[1]/yt.YTArray(particle_data['mass']).T[0]
 
 plt.clf()
-fig, axs = plt.subplots(ncols=1, nrows=4, figsize=(two_col_width, 1.5*two_col_width), sharex=True)
+fig, axs = plt.subplots(ncols=1, nrows=4, figsize=(two_col_width, two_col_width), sharex=True)
 plt.subplots_adjust(wspace=0.0)
 plt.subplots_adjust(hspace=0.1)
 
 axs.flatten()[0].plot(age, yt.YTArray(particle_data['mass']).T[0], label='Primary')
 axs.flatten()[0].plot(age, yt.YTArray(particle_data['mass']).T[1], label='Secondary')
-axs.flatten()[0].set_xlim()
+axs.flatten()[0].set_xlim([age[0], age[-1]])
+axs.flatten()[0].set_ylim([0, np.max(yt.YTArray(particle_data['mass']).T[0])])
+axs.flatten()[0].legend()
+axs.flatten()[0].set_ylabel('Mass (M$_\odot$)')
 
 axs.flatten()[1].plot(age, mass_ratio)
+axs.flatten()[1].set_ylim([0, np.max(mass_ratio)])
+axs.flatten()[1].set_ylabel('Mass ratio ($q$)')
 
-axs.flatten()[2].plot(age, particle_data['separation'])
+axs.flatten()[2].semilogy(age, particle_data['separation'])
+axs.flatten()[2].set_ylim([np.min(particle_data['separation']), np.max(particle_data['separation'])])
+axs.flatten()[2].set_ylabel('Separation (AU)')
 
 axs.flatten()[3].plot(age, particle_data['eccentricity'])
+axs.flatten()[3].set_ylim([np.min(particle_data['eccentricity']), np.max(particle_data['eccentricity'])])
+axs.flatten()[3].set_ylabel('Eccentricity')
+axs.flatten()[3].set_xlabel('Time (kyr)')
+plt.savefig('long_term_binary_evoluion.pdf', bbox_inches='tight', pad_inches=0.02)
+
 
 
