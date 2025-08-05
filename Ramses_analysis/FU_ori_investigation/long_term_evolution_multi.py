dc54a7a6368d14880ec2b4e47eb90de65d9dcc20
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2025-08-05 16:37:42 +0200

    cleaning up long term evolution plots

--- Ramses_analysis/FU_ori_investigation/long_term_evolution_multi.py
+++ Ramses_analysis/FU_ori_investigation/long_term_evolution_multi.py
@@ -71,7 +71,7 @@ for sink_ind in sink_inds:
     axs.flatten()[1].set_ylim([0, 1.1])
     axs.flatten()[1].set_xlabel('Time (yr)')
     axs.flatten()[1].tick_params(axis='both', direction='in', top=True, right=True)
-    axs.flatten()[0].ax.legend(loc='center right', bbox_to_anchor=(1.2, 1.0),
+    axs.flatten()[0].legend(loc='center right', bbox_to_anchor=(1.2, 1.0),
           ncol=2)
     plt.savefig("q_and_e_evol_all_candidates.pdf", bbox_inches='tight', pad_inches=0.02)
     print('updated figure')
