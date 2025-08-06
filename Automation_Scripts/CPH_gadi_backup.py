70c8269fd3bf841172246d6c58ed1b03eb8817eb
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2025-08-06 16:36:31 +0200

    cleaning up long term evolution plots

--- Automation_Scripts/CPH_gadi_backup.py
+++ Automation_Scripts/CPH_gadi_backup.py
@@ -3,8 +3,9 @@
 # author: Christoph Federrath
 import subprocess
 import csv
-
+import glob
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-loc", "--location", help="which server location", default='gadi')
@@ -27,17 +28,68 @@ except:
 while file_no < 1560:
     curr_file = ("%05d" % file_no)
     if args.location == 'gadi':
-        shellcmd = 'scp -r astro03-travel:/groups/astro/rlk/rlk/FU_ori_investigation/Zoom_in_simulations/Sink_45/data/output_'+curr_file+' gadi:/g/data/ek9/rlk100/RAMSES/Sink_45/data/.'
+        #make empty file and copy to gadi
+        if os.path.exists('output_'+curr_file) == False:
+            os.makedirs('output_'+curr_file)
+            shellcmd = 'rsync -vaz output_'+curr_file+' gadi:/g/data/ek9/rlk100/RAMSES/Sink_45/data/.'
+            return_code = subprocess.call(shellcmd, shell=True)
+            if return_code != 0:
+                print("error with copying empty file")
+                break
+        
+        #Check if CPH file has already been synced with local
+        synced_done = []
+        with open('Synced_dirs_'+args.location+'.txt', 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                synced_done.append(row[0])
+        if '/groups/astro/rlk/rlk/FU_ori_investigation/Zoom_in_simulations/Sink_45/data/output_'+curr_file not in synced_done[-1]:
+            shellcmd = 'rsync -vaz astro03-travel:/groups/astro/rlk/rlk/FU_ori_investigation/Zoom_in_simulations/Sink_45/data/output_'+curr_file+'/* output_'+curr_file+'/.'
+            return_code = subprocess.call(shellcmd, shell=True)
+            if return_code != 0:
+                print("error on copying file locally")
+                break
+            else:
+                #update sync list
+                with open('Synced_dirs_'+args.location+'.txt', 'a') as f:
+                    f.write('/groups/astro/rlk/rlk/FU_ori_investigation/Zoom_in_simulations/Sink_45/data/output_'+curr_file+'\n')
+                f.close()
+                
+        #move into directory and compare local to Gadi
+        os.chdir('output_'+curr_file)
+        #Check files on gadi:
+        shellcmd='rsync -n -i -a . gadi:/g/data/ek9/rlk100/RAMSES/Sink_45/data/output_'+curr_file+'/'
+        proc = subprocess.Popen(shellcmd, stdout=subprocess.PIPE, shell=True)
+        output = proc.stdout.read()
+        gadi_files = str(output).split('\\n')
+        transfer_flag = '<f+++++++ '
+        rsync_list = list(filter(lambda x: transfer_flag in x, gadi_files))
+        transfer_files = []
+        for trans_file in rsync_list:
+            transfer_files.append(trans_file.split(transfer_flag)[-1])
+        local_files = sorted(glob.glob('*'))
+        rm_files = list(set(local_files).symmetric_difference(set(transfer_files)))
+        
+        #Remove files that are already synced
+        for rm_file in rm_files:
+            os.remove(rm_file)
+            
+        #sync remaining files
+        shellcmd = 'rsync -vaz * gadi:/g/data/ek9/rlk100/RAMSES/Sink_45/data/output_'+curr_file+'/.'
+        return_code = subprocess.call(shellcmd, shell=True)
+        if return_code != 0:
+            print("Error on ryncing to gadi!")
+            break
+        else:
+            files_done.append('/groups/astro/rlk/rlk/FU_ori_investigation/Zoom_in_simulations/Sink_45/data/output_'+curr_file)
+            with open('Copied_dirs_'+args.location+'.txt', 'a') as f:
+                f.write(files_done[-1]+'\n')
+            f.close()
+            os.chdir('../')
+            os.rmdir('output_'+curr_file)
+            file_no = file_no + 10
+
     elif args.location == 'setonix':
         shellcmd = 'scp -r astro03-travel:/groups/astro/rlk/rlk/FU_ori_investigation/Zoom_in_simulations/Sink_45/data/output_'+curr_file+' setonix:/home/rkuruwita1/rlk/RAMSES/Zoom-ins/Sink_45/data/.'
-    return_code = subprocess.call(shellcmd, shell=True)
-    if return_code == 0:
-        files_done.append('/groups/astro/rlk/rlk/FU_ori_investigation/Zoom_in_simulations/Sink_45/data/output_'+curr_file)
-        with open('Copied_dirs_'+args.location+'.txt', 'a') as f:
-            f.write(files_done[-1])
-        f.close()
-        file_no = file_no + 10
-    else:
-        print("Error while tranferring!")
-        break
+        return_code = subprocess.call(shellcmd, shell=True)
 
--- Ramses_analysis/FU_ori_investigation/long_term_evolution_multi.py
+++ Ramses_analysis/FU_ori_investigation/long_term_evolution_multi.py
@@ -24,7 +24,7 @@ page_height = 10.62472 #inches
 font_size = 10
 
 plt.clf()
-fig, axs = plt.subplots(ncols=1, nrows=2, figsize=(two_col_width, 1.5*single_col_width), sharex=True)
+fig, axs = plt.subplots(ncols=1, nrows=3, figsize=(two_col_width, 2*single_col_width), sharex=True)
 plt.subplots_adjust(hspace=0.0)
 smoothing_window = yt.YTArray(100, 'yr')
 
@@ -33,11 +33,12 @@ for sink_ind in sink_inds:
     if os.path.isfile(pickle_file):
         if os.path.isfile('/groups/astro/rlk/rlk/FU_ori_investigation/Sink_pickles/smoothed_particle_data_'+str(sink_ind)+'.pkl'):
             file_open = open('/groups/astro/rlk/rlk/FU_ori_investigation/Sink_pickles/smoothed_particle_data_'+str(sink_ind)+'.pkl', 'rb')
-            smooth_t, smooth_q, smooth_e = pickle.load(file_open)
+            smooth_t, smooth_q, smooth_e, smooth_sep = pickle.load(file_open)
             file_open.close()
             
             axs.flatten()[0].plot(smooth_t, smooth_q, alpha=0.25, label=str(sink_ind))
             axs.flatten()[1].plot(smooth_t, smooth_e, alpha=0.25)
+            axs.flatten()[2].plot(smooth_t, smooth_sep, alpha=0.25)
         else:
             print('reading ', pickle_file)
             file_open = open(pickle_file, 'rb')
@@ -49,6 +50,7 @@ for sink_ind in sink_inds:
             smooth_t = []
             smooth_q = []
             smooth_e = []
+            smooth_sep = []
             for it in range(len(particle_data['time'])):
                 curr_time = particle_data['time'][it]
                 start_time = particle_data['time'][it] - smoothing_window/2
@@ -64,26 +66,30 @@ for sink_ind in sink_inds:
                 mean_t = np.mean(particle_data['time'][start_it:end_it])
                 mean_q = np.mean(mass_ratio[start_it:end_it])
                 mean_e = np.mean(particle_data['eccentricity'][start_it:end_it])
+                mean_sep = np.mean(particle_data['separation'][start_it:end_it])
                 smooth_t.append(mean_t)
                 smooth_q.append(mean_q)
                 smooth_e.append(mean_e)
+                smooth_sep.append(mean_sep)
             
             axs.flatten()[0].plot(smooth_t, smooth_q, alpha=0.25, label=str(sink_ind))
             axs.flatten()[1].plot(smooth_t, smooth_e, alpha=0.25)
+            axs.flatten()[2].semilogy(smooth_t, smooth_sep, alpha=0.25)
             
             print('updating pickle')
             file = open('/groups/astro/rlk/rlk/FU_ori_investigation/Sink_pickles/smoothed_particle_data_'+str(sink_ind)+'.pkl', 'wb')
-            pickle.dump((smooth_t, smooth_q, smooth_e), file)
+            pickle.dump((smooth_t, smooth_q, smooth_e, smooth_sep), file)
             file.close()
             print('Finished updating pickle', '/groups/astro/rlk/rlk/FU_ori_investigation/Sink_pickles/smoothed_particle_data_'+str(sink_ind)+'.pkl')
 
     axs.flatten()[0].set_xlim([0, 75000])
-    axs.flatten()[0].set_ylabel('$q$ w.r.t closest sink')
+    axs.flatten()[0].set_ylabel('$M_{cand}/M_{clos}$')
     axs.flatten()[0].set_ylim([0, 1.5])
     axs.flatten()[0].tick_params(axis='both', direction='in', top=True, right=True)
     axs.flatten()[1].set_ylabel('Eccentricity')
     axs.flatten()[1].set_ylim([0, 1.1])
-    axs.flatten()[1].set_xlabel('Time (yr)')
+    axs.flatten()[2].set_ylabel('Separation (AU)')
+    axs.flatten()[2].set_xlabel('Time (yr)')
     axs.flatten()[1].tick_params(axis='both', direction='in', top=True, right=True)
     axs.flatten()[0].legend(loc='center right', bbox_to_anchor=(1.2, 1.0),
           ncol=2)
