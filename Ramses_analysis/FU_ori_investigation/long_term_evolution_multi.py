815eae54e838052c53b81093ea5d5cb7f6283924
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2025-08-07 17:26:50 +0200

    adjusting legend anchor for p and e evolution

--- Ramses_analysis/FU_ori_investigation/long_term_evolution_multi.py
+++ Ramses_analysis/FU_ori_investigation/long_term_evolution_multi.py
@@ -65,7 +65,7 @@ for sink_ind in sink_inds:
                     axs.flatten()[0].plot(np.array(smooth_t)[start_ind:end_ind], np.array(smooth_q)[start_ind:end_ind], alpha=0.25, label=str(sink_ind), color=plot_colour)
                     axs.flatten()[1].plot(np.array(smooth_t)[start_ind:end_ind], np.array(smooth_e)[start_ind:end_ind], alpha=0.25, color=plot_colour)
                     axs.flatten()[2].semilogy(np.array(smooth_t)[start_ind:end_ind], np.array(smooth_sep)[start_ind:end_ind], alpha=0.25, color=plot_colour)
-            axs.flatten()[0].legend(loc='center right', bbox_to_anchor=(1.3, 0.5), ncol=1)
+            axs.flatten()[1].legend(loc='center right', bbox_to_anchor=(1.2, 0.5), ncol=1)
             plt.savefig("q_and_e_evol_all_candidates.pdf", bbox_inches='tight', pad_inches=0.02)
         else:
             print('reading ', pickle_file)
