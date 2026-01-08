c0b56a1156e6cda26014c7c555f0da10e4ef774a
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-01-08 15:08:46 +1100

    see of there are other candidates in the zoom-in sphere so I don't have to rerun sims

---
+++ Ramses_analysis/FU_ori_investigation/candidates_in_zoom_in_sphere.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+#In this script we are trying to learn how mass is primarily being accreted
+import yt
+yt.enable_parallelism()
+import glob
+import numpy as np
+import sys
+import os
+import my_ramses_module as mym
+import my_ramses_fields_short as myf
+from mpi4py.MPI import COMM_WORLD as CW
+import pickle
+import matplotlib.pyplot as plt
+#plt.rcParams['figure.dpi'] = 300
+from matplotlib.colors import LogNorm
+cm = plt.cm.get_cmap('seismic')
+
+def parse_inputs():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-tf", "--text_font", help="What font text do you want to use?", type=int, default=10)
+    parser.add_argument("-sink_id", "--sink_number", help="Which sink do you want to measure around? default is the sink with lowest velocity", type=int, default=None)
+    parser.add_argument("-make_pickles", "--make_pickle_files", type=str, default="True")
+    parser.add_argument("-make_plots", "--make_plot_figures", type=str, default="True")
+    parser.add_argument("-sphere_radius", "--sphere_radius_cells", type=float)
+    parser.add_argument("files", nargs='*')
+    args = parser.parse_args()
+    return args
+
+#=======MAIN=======
+rank = CW.Get_rank()
+size = CW.Get_size()
+if rank == 0:
+    print("size =", size)
+
+#Get input and output directories
+args = parse_inputs()
+
+#Define relevant directories
+input_dir = sys.argv[1]
+save_dir = sys.argv[2]
+if os.path.exists(save_dir) == False:
+    os.makedirs(save_dir)
+
+sys.stdout.flush()
+CW.Barrier()
+
+if args.make_pickle_files == "True":
+    #File files
+    usable_files = sorted(glob.glob(input_dir+"*/info*.txt"))
+
+    sys.stdout.flush()
+    CW.Barrier()
+
+    #Define units to override:
+    units_override = {"length_unit":(4.0,"pc"), "velocity_unit":(0.18, "km/s"), "time_unit":(685706129102738.9, "s")}
+    units_override.update({"mass_unit":(2998,"Msun")})
+    units_override.update({"density_unit":(units_override['mass_unit'][0]/units_override['length_unit'][0]**3, "Msun/pc**3")})
+        
+    scale_l = yt.YTQuantity(units_override['length_unit'][0], units_override['length_unit'][1]).in_units('cm').value # 4 pc
+    scale_v = yt.YTQuantity(units_override['velocity_unit'][0], units_override['velocity_unit'][1]).in_units('cm/s').value         # 0.18 km/s == sound speed
+    scale_t = scale_l/scale_v # 4 pc / 0.18 km/s
+    scale_d = yt.YTQuantity(units_override['density_unit'][0], units_override['density_unit'][1]).in_units('g/cm**3').value  # 2998 Msun / (4 pc)^3
+    mym.set_units(units_override)
+    if rank == 0:
+        print("set units")
+
+    #find sink particle to center on and formation time
+    del units_override['density_unit']
+    ds = yt.load(usable_files[-1], units_override=units_override)
+    if rank == 0:
+        print("Doing initial ds.all_data() load")
+    dd = ds.all_data()
+    import pdb
+    pdb.set_trace()
