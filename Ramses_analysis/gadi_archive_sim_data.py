bcaf7b656b743f2a1adf6b433211bdf06b189d8b
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-02-05 15:08:22 +1100

    Fixing tar command to ignore errors

--- Ramses_analysis/gadi_archive_sim_data.py
+++ Ramses_analysis/gadi_archive_sim_data.py
@@ -16,10 +16,10 @@ for data_dir in data_dirs:
         print('Compressing output', dir_it, 'on rank', rit)
         subprocess.run('tar -cvzf output_'+("%05d" % dir_it)+'.tar.gz data/output_'+("%05d" % dir_it)+'/', shell=True)
         subprocess.run('mdss put output_'+("%05d" % dir_it)+'.tar.gz rlk100/2023_paper/Simulation_data/', shell=True)
-        subprocess.run('rm output_'+("%05d" % dir_it)+'.tar.gz', shell=True)
         result = subprocess.run('mdss ls rlk100/2023_paper/Simulation_data/', shell=True, capture_output=True, text=True)
         output = result.stdout.strip()
         output = output.split('\n')
         if 'output_'+("%05d" % dir_it)+'.tar.gz' in output:
+            subprocess.run('rm output_'+("%05d" % dir_it)+'.tar.gz', shell=True)
             subprocess.run('rm -rf data/output_'+("%05d" % dir_it)+'/*.out0*', shell=True)
     
