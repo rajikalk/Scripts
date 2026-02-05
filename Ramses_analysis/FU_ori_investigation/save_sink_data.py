a5ba100576b5e6a8edb85684ee945eb8b08055f6
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-02-05 14:23:24 +1100

    Trying to manage memory for saving sink particle data

--- Ramses_analysis/FU_ori_investigation/save_sink_data.py
+++ Ramses_analysis/FU_ori_investigation/save_sink_data.py
@@ -113,9 +113,10 @@ if args.update_pickle == 'True':
         print("Reading particle data")
         loaded_sink_data = rsink(datadir=path, all=True)
         loaded_sink_data = loaded_sink_data[counter:]
-        import pdb
-        pdb.set_trace()
-        for sink_data in loaded_sink_data:
+        gc.collect()
+        while counter < len(loaded_sink_data):
+            sink_data = loaded_sink_data[0]
+            loaded_sink_data = loaded_sink_data[1:]
             counter = counter + 1
             if np.remainder(counter, 1000) == 0:
                 try:
@@ -135,8 +136,10 @@ if args.update_pickle == 'True':
                 dz = sink_data['z']*units['length_unit'].in_units('au') - pos_second[2]
                 sep = np.sqrt(dx**2 + dy**2 + dz**2)
                 del dx, dy, dz
+                gc.collect()
                 nearest_sink = np.argsort(sep)[1]
                 del sep
+                gc.collect()
                 tags.append(nearest_sink)
                 
                 for tag in tags:
@@ -148,21 +151,25 @@ if args.update_pickle == 'True':
                 particle_data['secondary_position'].append(pos_second)
                 particle_data['secondary_velocity'].append(vel_second)
                 del vel_second
+                gc.collect()
                 
                 separation = np.sqrt(np.sum((pos_second - pos_prim)**2))
                 particle_data['separation'].append(separation)
                 del separation
+                gc.collect()
                 if sink_form_time == 0:
                     sink_form_time = sink_data['tcreate'][sink_ind]*units['time_unit'].in_units('yr')
                 time_val = sink_data['snapshot_time']*units['time_unit'].in_units('yr') - sink_form_time
                 particle_data['time'].append(time_val)
                 del time_val
+                gc.collect()
                 particle_data['mass'].append(yt.YTArray(sink_data['m'][np.array([sink_ind, nearest_sink])]*units['mass_unit'].in_units('msun'), 'msun'))
                 
                 d_mass = sink_data['dm'][np.array([sink_ind, nearest_sink])]*units['mass_unit'].in_units('msun')
                 d_time = (sink_data['snapshot_time'] - sink_data['tflush'])*units['time_unit'].in_units('yr')
                 acc_val = d_mass/d_time
                 del d_mass, d_time
+                gc.collect()
                 acc_val[np.where(acc_val == 0)[0]]=1.e-12
                 particle_data['mdot'].append(yt.YTArray(acc_val, 'msun/yr'))
         #write lastest pickle
