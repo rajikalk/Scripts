6df51fb86215532a9cbcd9b59984125255b27482
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-02-18 15:41:49 +1100

    memory management

--- Ramses_analysis/movie_script.py
+++ Ramses_analysis/movie_script.py
@@ -573,6 +573,9 @@ if args.make_frames_only == 'False':
                             else:
                                 proj_array = np.array(proj.frb.data[field].in_units(args.field_unit))
                     else:
+                        if size == 1:
+                            import pdb
+                            pdb.set_trace()
                         if weight_field == None:
                             if args.axis == 'xz':
                                 proj_array = np.array(proj.frb.data[field].T.in_cgs()/thickness.in_units('cm'))
@@ -678,6 +681,9 @@ if args.make_frames_only == 'False':
                     part_info['particle_position'] = positions
             
             if rank == proj_root_rank:
+                if size == 1:
+                    import pdb
+                    pdb.set_trace()
                 velx, vely, velz = mym.get_quiver_arrays(0.0, 0.0, X_image, velx_full, vely_full, center_vel=center_vel, velz_full=velz_full, axis=args.axis)
                 del velx_full
                 del vely_full
