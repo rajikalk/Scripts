0a25a321fadcb3221af9a39ca126f525ef4a65e5
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-03-13 13:43:08 +1100

    made script to find restart files for specific events

--- Ramses_analysis/FU_ori_investigation/find_restart_file.py
+++ Ramses_analysis/FU_ori_investigation/find_restart_file.py
@@ -26,6 +26,7 @@ target_times = {"17": 83816.50575862321, "51": 48419.8594782825, "101": 105290.4
 
 path = sys.argv[1]
 save_dir = sys.argv[2]
+info_files = sorted(glob.glob(path+"*/info*.txt"))
 if save_dir[-1] != '/':
     save_dir = save_dir + '/'
 if os.path.exists(save_dir) == "False":
@@ -55,61 +56,47 @@ for key in units_override.keys():
     
 loaded_sink_data = rsink(datadir=path, all=True)
 for tag in target_times.keys():
+    sink_ind = int(tag)
     sink_form_time = 0
-    if len(sink_data['tcreate']) > ind(tag):
-        if sink_form_time == 0:
-            sink_form_time = sink_data['tcreate'][sink_ind]*units['time_unit'].in_units('yr')
+    for sink_data in loaded_sink_data:
+        if len(sink_data['tcreate']) > sink_ind:
+            if sink_form_time == 0:
+                sink_form_time = sink_data['tcreate'][sink_ind]*units['time_unit'].in_units('yr')
+                    
+            time_val = sink_data['snapshot_time']*units['time_unit'].in_units('yr') - sink_form_time
+            if time_val >= yt.YTQuantity(target_times[tag], 'yr'):
+                prev_flush_time = sink_data['tflush']
                 
-        time_val = sink_data['snapshot_time']*units['time_unit'].in_units('yr') - sink_form_time
-        if time_val >= yt.YTQuantity(target_times[tag], 'yr'):
-            import pdb
-            pdb.set_trace()
-"""
-                    dx = sink_data['x'] - sink_data['x'][sink_ind]
-                    dy = sink_data['y'] - sink_data['y'][sink_ind]
-                    dz = sink_data['z'] - sink_data['z'][sink_ind]
-                    separation = np.sqrt(dx**2 + dy**2 + dz**2)*units['length_unit'].in_units('au')
-                    closest_sink = np.argsort(separation)[1]
-                    particle_data['closest_sink'].append(closest_sink)
-                    particle_data['time'].append(time_val)
-                    particle_data['mass'].append(yt.YTArray(sink_data['m'][np.array([sink_ind,closest_sink])]*units['mass_unit'].in_units('msun'), 'msun'))
-                    d_mass = sink_data['dm'][np.array([sink_ind,closest_sink])]*units['mass_unit'].in_units('msun')
-                    d_time = (sink_data['snapshot_time'] - sink_data['tflush'])*units['time_unit'].in_units('yr')
-                    acc_val = d_mass/d_time
-                    #acc_val[np.where(acc_val == 0)[0]]=1.e-12
-                    particle_data['mdot'].append(yt.YTArray(acc_val, 'msun/yr'))
+                #binary search for file
+                low_ind = 0
+                high_ind = len(info_files) - 1
+                mid_ind = int(len(info_files)/2)
+                
+                found = False
+                while found == False:
+                    with open(info_files[mid_ind], "r") as f:
+                        reader = csv.reader(f, delimiter=' ')
+                        for row in reader:
+                            if row[0] == 'time':
+                                curr_time = eval(row[-1])
+                                break
+                    f.close()
                     
-                    position = yt.YTArray(np.array([sink_data['x'][np.array([sink_ind,closest_sink])], sink_data['y'][np.array([sink_ind,closest_sink])], sink_data['z'][np.array([sink_ind,closest_sink])]])*units['length_unit'].in_units('au'), 'au')
-                    velocity = yt.YTArray(np.array([sink_data['ux'][np.array([sink_ind,closest_sink])], sink_data['uy'][np.array([sink_ind,closest_sink])], sink_data['uz'][np.array([sink_ind,closest_sink])]])*units['velocity_unit'].in_units('km/s'), 'km/s')
+                    if curr_time > prev_flush_time:
+                        #if current time higher than target, then get mid index between the lowest and current ind
+                        high_ind = mid_ind
+                        mid_ind = int(np.mean([low_ind, mid_ind]))
+                    elif curr_time < prev_flush_time:
+                        #if current time lower than target, then get mid index between the current and highest ind
+                        low_ind = mid_ind
+                        mid_ind = int(np.mean([mid_ind, high_ind]))
                     
-                    CoM_pos = np.sum((position*particle_data['mass'][-1]).T, axis=0)/np.sum(particle_data['mass'][-1])
-                    CoM_vel = np.sum((velocity*particle_data['mass'][-1]).T, axis=0)/np.sum(particle_data['mass'][-1])
-                    
-                    vel_rel_to_com = (velocity.T - CoM_vel).T
-                    pos_rel_to_com = (position.T - CoM_pos).T
-                    distance_from_com = np.sqrt(np.sum(pos_rel_to_com**2, axis=0))
-                    relative_speed_to_com = np.sqrt(np.sum(vel_rel_to_com**2, axis=0))
-                    separation = np.sum(distance_from_com)
-                    particle_data['separation'].append(separation)
-                    
-                    reduced_mass = np.product(particle_data['mass'][-1].in_units('g'))/np.sum(particle_data['mass'][-1].in_units('g'))
-                    E_pot = (-1*(yt.units.gravitational_constant_cgs*np.product(particle_data['mass'][-1].in_units('g')))/separation.in_units('cm')).in_units('erg')
-                    E_kin = np.sum((0.5*particle_data['mass'][-1].in_units('g')*relative_speed_to_com.in_units('cm/s')**2).in_units('erg'))
-                    epsilon = (E_pot + E_kin)/reduced_mass.in_units('g')
-                    r_x_v = yt.YTArray(np.cross(pos_rel_to_com.T.in_units('cm'), vel_rel_to_com.T.in_units('cm/s')).T, 'cm**2/s')
-                    L = particle_data['mass'][-1].in_units('g').T*r_x_v
-                    L_tot = np.sqrt(np.sum(np.sum(L, axis=1)**2, axis=0))
-                    h_val = L_tot/reduced_mass.in_units('g')
-                    e = np.sqrt(1 + (2.*epsilon*h_val**2.)/((yt.units.gravitational_constant_cgs*np.sum(particle_data['mass'][-1].in_units('g')))**2.))
-                    particle_data['eccentricity'].append(e)
-                    
-                else:
-                    break
-                        
-        #write lastest pickle
-        file = open(save_dir+'particle_data_'+str(sink_ind)+'.pkl', 'wb')
-        pickle.dump((particle_data, counter, sink_ind, sink_form_time), file)
-        file.close()
-        os.system('cp '+save_dir+'particle_data_'+str(sink_ind)+'.pkl '+save_dir+'particle_data_'+str(sink_ind)+'_tmp.pkl')
-        print('read', counter, 'snapshots of sink particle data, and saved pickle')
-"""
+                    if low_ind == mid_ind:
+                        if curr_time > prev_flush_time:
+                            restart_file = info_files[mid_ind]
+                            found = True
+                    elif high_ind == mid_ind:
+                        import pdb
+                        pdb.set_trace()
+                print("Restart file for sink", sink_ind, "is", restart_file)
+                break
