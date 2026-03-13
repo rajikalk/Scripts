c858e623faa3bcd87aa45429527fc379d6fcf707
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-03-13 14:57:28 +1100

    writing zoom_centre script to find target particle position

--- Ramses_analysis/FU_ori_investigation/zoom_center_event.py
+++ Ramses_analysis/FU_ori_investigation/zoom_center_event.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 
 import glob
-import yt
-import my_ramses_fields_short as myf
 import sys
+import csv
 
 sink_id = sys.argv[1]
 
+fname = '/home/100/rlk100/gdata/RAMSES/Global/stars_red_512.pkl'
+glob_data = open(fname, 'rb')
+sink_data = pickle.load(glob_data)
+glob_data.close()
+
 file = sorted(glob.glob('data/*/info*txt'))[0]
-
-units_override = {"length_unit":(4.0,"pc"), "velocity_unit":(0.18, "km/s"), "time_unit":(685706129102738.9, "s"), "mass_unit":(2998,"Msun")}
-
-ds = yt.load(file, units_override=units_override)
-dd = ds.all_data()
+with open(file, "r") as f:
+    reader = csv.reader(f, delimiter=' ')
+    for row in reader:
+        if row[0] == 'time':
+            curr_time = eval(row[-1])
+            break
 
 import pdb
 pdb.set_trace()
