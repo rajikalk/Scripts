91b126a6b28705f228aab549d6878a21d3265ca4
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-03-23 13:33:21 +1100

    trying to automate submitting movie scripts

--- Automation_Scripts/prep_make_movie.py
+++ Automation_Scripts/prep_make_movie.py
@@ -31,14 +31,15 @@ script_lines = ['#!/bin/bash',
                 '#PBS -m bea',
                 '#PBS -M rajika.kuruwita@anu.edu.au',
                 '',
-                'mpirun -np $PBS_NCPUS ~/Scripts/Ramses_analysis/movie_script.py ',
-                ' ./ -sink ',
-                ' -sf 0 -dt 50 -cmin 1.e-18 -cmax 1.e-15 -at True -pvl True -ax ',
-                ' -al 1000 -tf 12 -stdv 5 -thickness 2000 -use_gas False -ic 1 -update_alim True -frames_only False -apm True 1>',
-                '.out01 2>&1']
+                'mpirun -np $PBS_NCPUS ~/Scripts/Ramses_analysis/movie_script.py ',]
+#                ' ./ -sink ',
+#                ' -sf 0 -dt 50 -cmin 1.e-18 -cmax 1.e-15 -at True -pvl True -ax ',
+#                ' -al 1000 -tf 12 -stdv 5 -thickness 2000 -use_gas False -ic 1 -update_alim True -frames_only False -apm True 1>',
+#                '.out01 2>&1']
 
 for zoom_dir in Cleaned_dirs:
     Sink_id = int(zoom_dir.split('/Sink_')[-1].split('/')[0])
+    job_name = str(Sink_id)
     movie_dir = save_dir + 'Sink_' + str(Sink_id)
     if os.path.exists(movie_dir) == False:
         os.makedirs(movie_dir)
@@ -46,12 +47,14 @@ for zoom_dir in Cleaned_dirs:
     
     if 'Event_restart' in zoom_dir:
         movie_dir = movie_dir + '/Event_Restart'
+        job_name = job_name + 'E'
         if os.path.exists(movie_dir) == False:
             os.makedirs(movie_dir)
         os.chdir(movie_dir)
     
     if 'Level' not in zoom_dir:
         movie_dir = movie_dir +'/Level_18'
+        job_name = job_name + 'L18'
         if os.path.exists(movie_dir) == False:
             os.makedirs(movie_dir)
     else:
@@ -61,50 +64,25 @@ for zoom_dir in Cleaned_dirs:
     os.chdir(movie_dir)
     
     for dir in dirs:
+        sub_movie_dir = movie_dir + '/' + dir
+        if os.path.exists(sub_movie_dir) == False:
+            os.makedirs(sub_movie_dir)
+        os.chdir(sub_movie_dir)
+        
+        #Check that job script exists
+        if os.path.isfile('job.sh') == False:
+            f = open(script_name, 'a')
+            f.close()
+            f = open(script_name, 'w')
+            for line in script_lines:
+                if line == '#PBS -N ':
+                    write_line = line + job_name
+                elif line == 'mpirun -np $PBS_NCPUS ~/Scripts/Ramses_analysis/movie_script.py ':
+                    write_line = 'mpirun -np $PBS_NCPUS ~/Scripts/Ramses_analysis/movie_script.py '+zoom_dir+' ./ -sink '+Sink_id+' -sf 0 -dt 50 -cmin 1.e-18 -cmax 1.e-15 -at True -pvl True -ax '+dir.lower()+' -al 1000 -tf 12 -stdv 5 -thickness 2000 -use_gas False -ic 1 -update_alim True -frames_only False -apm True 1>'+job_name+'.out01 2>&1'
+                else:
+                    write_line = line
+                write_line = write_line + '\n'
+                f.write(write_line)
+            
         import pdb
         pdb.set_trace()
-        
-    
-    
-
-f = open('job.sh', 'r')
-reader = f.readlines()
-sim_name = reader[7].split(' ')[-1].split('\n')[0]
-
-for dir in dirs:
-    job_name = sim_name+dir
-    if not os.path.exists(save_dir + dir + '/'):
-        os.makedirs(save_dir + dir + '/')
-    movie_ls = subprocess.getoutput('ls ' + save_dir+dir + '/').split('\n')
-    if len(movie_ls) == 1:
-        start_frame = '0'
-    else:
-        movie_ls = subprocess.getoutput('ls ' + save_dir+dir+'/*jpg').split('\n')
-        if "No such file or directory" in movie_ls[0]:
-            movie_ls = subprocess.getoutput('ls ' + save_dir+dir+'/*eps').split('\n')
-        start_frame = movie_ls[-1].split('_')[-1].split('.')[0]
-    script_name = 'movie_'+dir+'.sh'
-    if not os.path.isfile(script_name):
-        f = open(script_name, 'a')
-        f.close()
-    f = open(script_name, 'w')
-    for line in script_lines:
-        if line == '#PBS -N ':
-            write_line = line + job_name
-        elif line == 'mpirun -np $PBS_NCPUS python ~/Scripts/movie_script_mod.py ':
-            if dir == 'xz':
-                write_line = line + cur_dir + ' ' + save_dir + dir + '/ -sf ' + start_frame + ' ' + args.additions + '-zt 1.33 -cmin 1.e-16 -cmax 1.e-14 -at True -pvl True -al 1000 -tf 16 ' + '1>' + job_name + '.out 2>&1'
-            if dir == 'xy':
-                write_line = line + cur_dir + ' ' + save_dir + dir + '/ -sf ' + start_frame + ' -ax "xy" ' + args.additions + ' ' + '1>' + job_name + '.out 2>&1'
-            if dir == 'xz_zoom':
-                write_line = line + cur_dir + ' ' + save_dir + dir + '/ -sf ' + start_frame + ' -zt 12.3 ' + args.additions + ' ' + '1>' + job_name + '.out 2>&1'
-            if dir == 'xy_zoom':
-                write_line = line + cur_dir + ' ' + save_dir + dir + '/ -sf ' + start_frame + '-zt 12.3 -cmin 1.e-15 -cmax 1.e-13 -at True -pvl True -ax xy -al 100 -tf 16 ' + args.additions + ' ' + '1>' + job_name + '.out 2>&1'
-        else:
-            write_line = line
-        write_line = write_line + '\n'
-        f.write(write_line)
-
-
-
-
