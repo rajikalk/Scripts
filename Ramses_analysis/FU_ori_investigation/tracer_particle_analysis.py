8286aacf172f4e67b744cb352dcd19e06535619e
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-04-07 14:19:03 +1000

    making tracer particle anaylsis work on gadi

--- Ramses_analysis/FU_ori_investigation/tracer_particle_analysis.py
+++ Ramses_analysis/FU_ori_investigation/tracer_particle_analysis.py
@@ -82,7 +82,7 @@ if args.make_pickle_files == "True":
         del particle_data['particle_tag'], particle_data['mass'], particle_data['mdot'], particle_data['separation']
         gc.collect()
     else:
-        particle_data = {}
+        particle_data = None
         sink_id = None
         sink_form_time = None
     sys.stdout.flush()
@@ -90,7 +90,7 @@ if args.make_pickle_files == "True":
     
     sink_id = CW.bcast(sink_id, root=0)
     sink_form_time = CW.bcast(sink_form_time, root=0)
-    particle_data = CW.Bcast(particle_data, root=0)
+    particle_data = CW.bcast(particle_data, root=0)
     sys.stdout.flush()
     CW.Barrier()
     gc.collect()
