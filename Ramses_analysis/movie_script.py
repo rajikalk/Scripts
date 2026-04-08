6c262096dfc98c49b17eb0d167de385b18e08db6
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-04-08 12:56:10 +1000

    trying to load information on one process and communicate to others

--- Ramses_analysis/movie_script.py
+++ Ramses_analysis/movie_script.py
@@ -237,8 +237,8 @@ if len(dd['sink_particle_form_time']) > sink_id:
 else:
     print("TARGET SINK NOT FORMED YET")
     sys.exit()
-del dd
 ds.index.clear_all_data()
+del dd
 gc.collect()
 if args.plot_time != None:
     m_times = [args.plot_time]
