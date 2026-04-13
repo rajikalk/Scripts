d522ac08979fb3dc6f68826360fea5f2d46fced1
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-04-13 10:37:48 +1000

    figuring out why image densities are weird

--- Ramses_analysis/movie_script.py
+++ Ramses_analysis/movie_script.py
@@ -649,6 +649,8 @@ if args.make_frames_only == 'False':
                 
                 proj_dict = {}
                 for sto, field in yt.parallel_objects(proj_field_list, storage=proj_dict, njobs=len(proj_field_list)):
+                    if 'velocity' in field[1]:
+                        weight_field = 'cell_volume'
                     proj = yt.ProjectionPlot(ds, axis_ind, field, width=(x_width,'au'), weight_field=weight_field, data_source=region, method='integrate', center=center_pos)
                     proj.set_buff_size([args.resolution, args.resolution])
                     if 'mag' in str(field):
