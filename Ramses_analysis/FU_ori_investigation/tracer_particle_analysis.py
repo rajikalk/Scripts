8e87a263f7d6005894d967fdc51071cd3322b1aa
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-04-22 11:34:19 +1000

    Working on tracer particle pickles

--- Ramses_analysis/FU_ori_investigation/tracer_particle_analysis.py
+++ Ramses_analysis/FU_ori_investigation/tracer_particle_analysis.py
@@ -43,6 +43,7 @@ if args.end_burst_time != None:
     end_time = args.end_burst_time
 else:
     event_id = int(input_dir.split('Event_')[-1][0]) - 1
+    print("Getting tracer particles for Event", event_id)
     end_time = time_bounds[event_id][-1]
     del time_bounds
     gc.collect()
