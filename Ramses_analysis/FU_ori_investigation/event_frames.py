d5dbd06becd0071d0aa0c61d9864d06e002a10f5
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-05-05 16:55:25 +1000

    Continue working on plotting movie frames of suppresion event plut the accretion evolution

--- Ramses_analysis/FU_ori_investigation/event_frames.py
+++ Ramses_analysis/FU_ori_investigation/event_frames.py
@@ -4,6 +4,7 @@ import sys
 import argparse
 import numpy as np
 import pickle
+import my_ramses_module as mym
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
@@ -13,24 +14,6 @@ import matplotlib.gridspec as gridspec
 #------------------------------------------------------
 
 #Ploting parameters
-matplotlib.rcParams['mathtext.fontset'] = 'stixsans'
-matplotlib.rcParams['mathtext.it'] = 'Arial:italic'
-matplotlib.rcParams['mathtext.rm'] = 'Arial'
-matplotlib.rcParams['mathtext.bf'] = 'Arial:bold'
-matplotlib.rcParams['mathtext.it'] = 'Arial:italic'
-matplotlib.rcParams['mathtext.rm'] = 'Arial'
-matplotlib.rcParams['mathtext.sf'] = 'Arial'
-matplotlib.rcParams['mathtext.default'] = 'regular'
-matplotlib.rcParams['font.sans-serif'] = 'Arial'
-matplotlib.rcParams['font.family'] = 'sans-serif'
-matplotlib.rcParams['text.latex.preamble'] = [
-       r'\usepackage{siunitx}',   # i need upright \micro symbols, but you need...
-       r'\sisetup{detect-all}',   # ...this to force siunitx to actually use your fonts
-       r'\usepackage{helvet}',    # set the normal font here
-       r'\usepackage{sansmath}',  # load up the sansmath so that math -> helvet
-       r'\sansmath'               # <- tricky! -- gotta actually tell tex to use!
-]
-
 two_col_width = 7.20472 #inches
 single_col_width = 3.50394 #inches
 page_height = 10.62472 #inches
@@ -39,86 +22,65 @@ mym.set_global_font_size(font_size)
 
 
 #------------------------------------------------------
-time_bounds = [[3810, 4950],[5575, 5700], [6580, 6730], [7295, 7340], [7850, 7900]]
+time_bounds = [[3800, 4900],[5575, 5700], [6580, 6730], [7295, 7340], [7850, 7900]]
 burst_bounds = [[], [[5679, 5689]], [[6625, 6635]], [[7309, 7319], [7327, 7337]], [[7858, 7868]]]
 cmap=plt.cm.gist_heat
 
-width = 200
+#Start by loading pickel data and then deleting what we don't need
+try:
+    sink_pickle = "/Users/reggie/Documents/Simulation_analysis/FU_ori_analysis/Particle_data_pickles/particle_data_L20.pkl"
+    print("read pickle", sink_pickle)
+    file_open = open(sink_pickle, 'rb')
+    particle_data, counter, sink_ind, sink_form_time = pickle.load(file_open)
+    file_open.close()
+    print("finished reading in pickle")
+except:
+    print("ADD PICKLE DIRECTORY FOR GADI")
+    
+
+
+width = 15
 stdvel = 3
 n_frames = 5
+make_frame = True
+event_it = 1
+plot_dt = (time_bounds[event_it -1][1]-time_bounds[event_it -1][0])/4
+plot_times = np.arange(time_bounds[event_it -1][0], time_bounds[event_it -1][1]+plot_dt, plot_dt)
 
-for spin_val in spin_values:
-    for mach_val in mach_values:
-        if make_frame[spin_values.index(spin_val)][mach_values.index(mach_val)]:
-            plot_it = -1
+
+plt.clf()
+fig = plt.figure(figsize=(two_col_width, 0.6*two_col_width))
+G = gridspec.GridSpec(2, n_frames)
+axes_1 = plt.subplot(G[0, :])
+#fig, axs = plt.subplots(ncols=n_frames, nrows=2, figsize=(two_col_width, 0.4*two_col_width), sharex=True, sharey=True)
+#for ax_it in axs.flatten():
+#    ax_it.set_aspect('equal')
+plt.subplots_adjust(wspace=0.01)
+plt.subplots_adjust(hspace=-0.00)
             
-            plt.clf()
-            fig = plt.figure(figsize=(two_col_width, 0.6*two_col_width))
-            G = gridspec.GridSpec(3, n_frames)
-            axes_1 = plt.subplot(G[0, :])
-            #fig, axs = plt.subplots(ncols=n_frames, nrows=2, figsize=(two_col_width, 0.4*two_col_width), sharex=True, sharey=True)
-            #for ax_it in axs.flatten():
-            #    ax_it.set_aspect('equal')
-            plt.subplots_adjust(wspace=0.01)
-            plt.subplots_adjust(hspace=-0.00)
+axes_1.set_title("Suppression event "+str(event_it), y=0.94)
+start_ind = np.argmin(abs(particle_data['time']-plot_times[0]))
+end_ind = np.argmin(abs(particle_data['time']-plot_times[1]))
+axes_1.semilogy(particle_data['time'][start_ind:end_ind], particle_data['mdot'][start_ind:end_ind])
+axes_1_twin = axes_1.twinx(particle_data['time'][start_ind:end_ind], )
+axes_1_twin.plot(particle_data['time'][start_ind:end_ind], particle_data['separation'][start_ind:end_ind])
             
-            axes_1.set_title("$\Omega t_{ff}$="+spin_val+", $\mathcal{M}$="+mach_val, y=0.94)
+#Plot accretion and separation. This should be loaded from a pickle
+
+axes_1.set_xlabel('Time ($yr$)', labelpad=-0.2)
+axes_1.set_ylabel('Accretione rate (M$_\odot/yr$)', labelpad=-0.2, fontsize=font_size)
+axes_1_twin.set_ylabel('Separation (au)')
+axes_1.tick_params(axis='x', which='major', direction='in', color='k', top=True)
+axes_1.tick_params(axis='y', which='major', direction='in', color='k', right=True)
+axes_1.xaxis.label.set_color('black')
+axes_1.yaxis.label.set_color('black')
+axes_1.tick_params(axis='both', labelsize=font_size)
             
-            start_t = start_times[spin_values.index(spin_val)][mach_values.index(mach_val)]
-            end_t = end_times[spin_values.index(spin_val)][mach_values.index(mach_val)]
-            if np.isnan(start_t) or np.isnan(end_t):
-                plot_times = []
-            else:
-                plot_times = np.linspace(start_t, end_t, n_frames-2)
-                plot_times = [plot_times[0]-1000] + list(plot_times) + [plot_times[-1]+1000]
-                plot_times = plot_times + plot_times
-            
-            #Plot spin up
-            sink_pickle = '/home/kuruwira/fast/Analysis/Sink_evol_pickles/Flash_2023_Spin_'+spin_val+'_Single_Mach_'+mach_val+'_Lref_9.pkl'
-            file = open(sink_pickle, 'rb')
-            sink_data, line_counter = pickle.load(file)
-            file.close()
-            primary_ind = list(sink_data.keys())[0]
-            mass = yt.YTArray(sink_data[primary_ind]['mass'], 'g')
-            L_tot = np.sqrt(sink_data[primary_ind]['anglx']**2 + sink_data[primary_ind]['angly']**2 + sink_data[primary_ind]['anglz']**2)
-            L_tot = yt.YTArray(L_tot/sink_data[primary_ind]['mass'], 'cm**2/s')
-            L_tot = L_tot.in_units('m**2/s')/1.e15
-            time = sink_data[primary_ind]['time'] - sink_data[primary_ind]['time'][0]
-            time = yt.YTArray(time, 's')
-            plot_inds = []
-            for plot_time in plot_times[:n_frames]:
-                plot_ind = np.argmin(abs(time.in_units('yr').value - plot_time))
-                plot_inds.append(plot_ind)
-                
-            t_pad = time_padding[spin_values.index(spin_val)]
-            plot_start_time = np.argmin(abs(time.in_units('yr').value - (plot_times[0]-t_pad)))
-            plot_end_time = np.argmin(abs(time.in_units('yr').value - (plot_times[n_frames-1]+t_pad)))
-            axes_1.plot(time[plot_start_time:plot_end_time].in_units('yr'), L_tot[plot_start_time:plot_end_time])
-            axes_1.scatter(time[np.array(plot_inds)].in_units('yr'), L_tot[np.array(plot_inds)])
-            DL = np.max(L_tot[plot_start_time:plot_end_time]) - np.min(L_tot[plot_start_time:plot_end_time])
-            pos_offset = DL/10
-            for plot_ind in plot_inds:
-                panel_string = str(plot_inds.index(plot_ind)+1) + "."
-                panel_string_raw = r"{}".format(panel_string)
-                if plot_inds.index(plot_ind) < 2:
-                    y_pos_str = L_tot[plot_ind]+pos_offset
-                else:
-                    y_pos_str = L_tot[plot_ind]-pos_offset
-                panel_text = axes_1.text(time[plot_ind].in_units('yr'), y_pos_str, panel_string_raw, va="center", ha="center", color='k', fontsize=font_size)
-            
-            axes_1.set_xlim([time[plot_start_time].in_units('yr'), time[plot_end_time].in_units('yr')])
-            if spin_val == '0.35':
-                axes_1.set_ylim(bottom=(np.min(L_tot[plot_start_time:plot_end_time])-0.1*DL))
-            axes_1.set_xlabel('Time ($yr$)', labelpad=-0.2)
-            axes_1.set_ylabel('h ($10^{15}m^2/s$)', labelpad=-0.2, fontsize=font_size)
-            axes_1.tick_params(axis='x', which='major', direction='in', color='k', top=True)
-            axes_1.tick_params(axis='y', which='major', direction='in', color='k', right=True)
-            axes_1.xaxis.label.set_color('black')
-            axes_1.yaxis.label.set_color('black')
-            axes_1.tick_params(axis='both', labelsize=font_size)
-            
-            for plot_time in plot_times:
-                plot_it = plot_it + 1
+for plot_time in plot_times:
+    plot_it = plot_it + 1
+    import pdb
+    pdb.set_trace()
+'''
                 if plot_it < n_frames:
                     pickle_file = 'Dens_Spin_' + spin_val + '_Mach_' + mach_val + '_time_'+ str(plot_time) +'.pkl'
             
@@ -229,3 +191,4 @@ for spin_val in spin_values:
                     plt.setp(yticklabels, visible=False)
                 
                 plt.savefig("Spin_"+spin_val+"_Mach_"+mach_val+"_Spin_up_horizontal.pdf", format='pdf', bbox_inches='tight', pad_inches=0.02, dpi=300)
+'''
