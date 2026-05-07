18f6fac1ced0f1458d1d8034ea2106f1186b5b48
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-05-07 10:17:08 +1000

    debugging suppression event plots

--- Ramses_analysis/FU_ori_investigation/event_frames.py
+++ Ramses_analysis/FU_ori_investigation/event_frames.py
@@ -142,8 +142,7 @@ for plot_time in plot_times:
         part_info['particle_mass'] = part_info['particle_mass'][sort_inds]
         part_info['particle_tag'] = part_info['particle_tag'][sort_inds]
         part_info['formation_time'] = part_info['formation_time'][sort_inds]
-    if plot_it >= n_frames:
-        mym.annotate_particles(ax, part_info['particle_position'], part_info['accretion_rad'], limits=[xlim, ylim], annotate_field=part_info['particle_mass'], particle_tags=part_info['particle_tag'], zorder=7, split_threshold=3, annotate_velocity=True, standard_vel=stdvel, width_ceil = 1.0)
+    mym.annotate_particles(ax, part_info['particle_position'], part_info['accretion_rad'], limits=[xlim, ylim], annotate_field=part_info['particle_mass'], particle_tags=part_info['particle_tag'], zorder=7, split_threshold=3, annotate_velocity=True, standard_vel=stdvel, width_ceil = 1.0)
                 
     ax.tick_params(axis='both', which='major', labelsize=font_size)
     for line in ax.xaxis.get_ticklines():
