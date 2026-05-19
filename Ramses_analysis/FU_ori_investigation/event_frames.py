4dcbb8722133a9d2bc0afb1f9d622243ff8cd72a
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-05-19 10:35:01 +1000

    trying to tweaking tick labels

--- Ramses_analysis/FU_ori_investigation/event_frames.py
+++ Ramses_analysis/FU_ori_investigation/event_frames.py
@@ -207,7 +207,7 @@ for plot_time in plot_times:
     ax.tick_params(axis='y', which='major', direction='in', color='w', right=True)
     ax.xaxis.label.set_color('black')
     ax.yaxis.label.set_color('black')
-    ax.tick_params(axis='both', labelsize=font_size, labelfontfamilystr='sans-serif')
+    ax.tick_params(axis='both', labelsize=font_size, labelfontfamily='sans-serif')
     ax.set_xlabel('AU', fontsize=font_size, labelpad=-1)
                     
     if np.remainder(plot_it, n_frames)==0:
