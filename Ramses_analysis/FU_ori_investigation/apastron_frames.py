c8e03310652782311e1f51c397c4d60488f239b9
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-05-21 14:44:43 +1000

    Attempting to make some frames of the simulations at apastron

--- Ramses_analysis/FU_ori_investigation/apastron_frames.py
+++ Ramses_analysis/FU_ori_investigation/apastron_frames.py
@@ -54,7 +54,7 @@ except:
 
 width = 30
 stdvel = 2
-n_frames = len(plot_times)
+n_frames = 4
 cbar_lims = [1.e-16, 1.e-13]
 start_time = plot_times[0]
 end_time = plot_times[-1]
@@ -62,10 +62,10 @@ end_time = plot_times[-1]
 
 plt.clf()
 fig = plt.figure(figsize=(two_col_width, 0.6*two_col_width))
-G = gridspec.GridSpec(3,4, height_ratios=[1, 2])
+G = gridspec.GridSpec(3,4)#, height_ratios=[1, 2])
 axes_1 = plt.subplot(G[0, :])
-plt.subplots_adjust(wspace=0.01)
-plt.subplots_adjust(hspace=-0.2)
+#plt.subplots_adjust(wspace=0.01)
+#plt.subplots_adjust(hspace=-0.2)
             
 start_ind = np.argmin(abs(particle_data['time']-start_time))
 end_ind = np.argmin(abs(particle_data['time']-end_time))
@@ -96,7 +96,7 @@ for plot_time in plot_times:
     movie_plot_pickle = "time_" + str(float(plot_time)) +".pkl"
     if os.path.isfile(movie_plot_pickle) == False:
         #Make movie frame
-        cmd = ['python', '/home/100/rlk100/Scripts/Ramses_analysis/movie_script.py', '/home/100/rlk100/gdata/RAMSES/Zoom-in_CPH_sims/Sink_45/Level_19/Level_20/data/', './', '-sink', '45', '-pt', str(plot_time), '-at', 'True', '-pvl', 'True',  '-ax', 'xy', '-al', '15', '-tf', '12', '-stdv', str(stdvel), '-thickness', '30', '-use_gas', 'False', '-ic', '1', '-update_alim', 'True', '-frames_only', 'False', '-apm', 'True']
+        cmd = ['python', '/home/100/rlk100/Scripts/Ramses_analysis/movie_script.py', '/home/100/rlk100/gdata/RAMSES/Zoom-in_CPH_sims/Sink_45/Level_19/Level_20/data/', './', '-sink', '45', '-pt', str(plot_time), '-at', 'True', '-pvl', 'True',  '-ax', 'xy', '-al', '15', '-tf', '12', '-stdv', str(stdvel), '-thickness', '30', '-use_gas', 'False', '-ic', '1', '-update_alim', 'True', '-frames_only', 'False', '-apm', 'True', '-vaf', '29']
         
         subprocess.Popen(cmd).wait()
     
