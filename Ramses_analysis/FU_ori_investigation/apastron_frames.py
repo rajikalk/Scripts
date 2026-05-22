3868df5f6fde69526dc37c789635cd4dba1be42c
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-05-22 17:11:03 +1000

    updating save_data to save relative velocity

--- Ramses_analysis/FU_ori_investigation/apastron_frames.py
+++ Ramses_analysis/FU_ori_investigation/apastron_frames.py
@@ -55,7 +55,7 @@ end_time = plot_times[-1]
 
 plt.clf()
 fig = plt.figure(figsize=(two_col_width, 0.6*two_col_width))
-G = gridspec.GridSpec(3,4, height_ratios=[1, 2, 2])
+G = gridspec.GridSpec(3,4, height_ratios=[1.5, 2, 2])
 axes_1 = plt.subplot(G[0, :])
 plt.tight_layout()
 plt.subplots_adjust(wspace=0.01)
