e0e6abe62f12e6ae2730673263076624a0ed1f1e
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-05-22 10:20:58 +1000

    updating save_data to save relative velocity

--- Ramses_analysis/movie_script.py
+++ Ramses_analysis/movie_script.py
@@ -658,6 +658,8 @@ if args.make_frames_only == 'False':
                 proj_dict = {}
                 for sto, field in yt.parallel_objects(proj_field_list, storage=proj_dict, njobs=len(proj_field_list)):
                     if 'velocity' in field[1]:
+                        import pdb
+                        pdb.set_trace()
                         weight_field = 'cell_mass'
                     proj = yt.ProjectionPlot(ds, axis_ind, field, width=(x_width,'au'), weight_field=weight_field, data_source=region, method='integrate', center=center_pos)
                     proj.set_buff_size([args.resolution, args.resolution])
