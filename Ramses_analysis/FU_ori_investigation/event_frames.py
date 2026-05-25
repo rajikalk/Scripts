25485864c56c54b5ab157b770604ca00da0faf58
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-05-25 13:09:58 +1000

    tweaking annotation

--- Ramses_analysis/FU_ori_investigation/event_frames.py
+++ Ramses_analysis/FU_ori_investigation/event_frames.py
@@ -43,7 +43,7 @@ mym.set_global_font_size(font_size)
 #------------------------------------------------------
 time_bounds = [[3800, 4900],[5575, 5700], [6580, 6720], [7295, 7340], [7850, 7900]]
 burst_bounds = [[], [5675, 5700], [6655, 6720], [7325, 7340], [7860, 7900]]
-cbar_lims_all = [[], [5.e-15, 1.e-13], [7.5.e-15, 3.e-13], [1.e-15, 5.e-13], [1.e-15, 5.e-13]]
+cbar_lims_all = [[], [5.e-15, 1.e-13], [7.5e-15, 3.e-13], [1.e-15, 5.e-13], [1.e-15, 5.e-13]]
 cmap=plt.cm.gist_heat
 
 #Start by loading pickel data and then deleting what we don't need
