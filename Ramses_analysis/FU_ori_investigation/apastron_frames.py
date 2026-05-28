d66fb06428594e461b9fa14c5f086f22d4689404
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-05-28 11:28:24 +1000

    Fixing apastron frames

--- Ramses_analysis/FU_ori_investigation/apastron_frames.py
+++ Ramses_analysis/FU_ori_investigation/apastron_frames.py
@@ -97,7 +97,7 @@ for plot_time in plot_times:
     velocity_plot_pickle = "time_" + str(float(plot_time)) +"_velocity.pkl"
     if os.path.isfile(velocity_plot_pickle) == False:
         #Make movie frame
-        cmd = ['python', '/home/100/rlk100/Scripts/Ramses_analysis/movie_script.py', '/home/100/rlk100/gdata/RAMSES/Zoom-in_CPH_sims/Sink_45/Level_19/Level_20/data/', './', '-sink', '45', '-pt', str(plot_time), '-at', 'True', '-pvl', 'True',  '-ax', 'xz', '-al', '15', '-tf', '12', '-stdv', str(stdvel), '-thickness', '30', '-use_gas', 'False', '-ic', '1', '-update_alim', 'True', '-frames_only', 'False', '-apm', 'True', '-vaf', '15', '-wf', 'mass']
+        cmd = ['python', '/home/100/rlk100/Scripts/Ramses_analysis/movie_script.py', '/home/100/rlk100/gdata/RAMSES/Zoom-in_CPH_sims/Sink_45/Level_19/Level_20/data/', './', '-sink', '45', '-pt', str(plot_time), '-f', 'x-velocity', '-at', 'True', '-pvl', 'True',  '-ax', 'xz', '-al', '15', '-tf', '12', '-stdv', str(stdvel), '-thickness', '30', '-use_gas', 'False', '-ic', '1', '-update_alim', 'True', '-frames_only', 'False', '-apm', 'True', '-vaf', '15']
         
         subprocess.Popen(cmd).wait()
     
