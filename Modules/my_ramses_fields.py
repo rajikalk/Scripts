2fdf824a413efce8545cf2f2c47a70d57b80cbb0
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-05-30 19:43:16 +1000

    Still working on projection plot work around

--- Modules/my_ramses_fields.py
+++ Modules/my_ramses_fields.py
@@ -1431,3 +1431,25 @@ def _magz_Proj(field,data):
     return Proj_field
     
 yt.add_field(("gas", "magz_Proj"), function=_magz_Proj, units=r"gauss", sampling_type="local", force_override=True)
+
+def _cell_mass_Proj(field,data):
+    """
+    Overwrites density field
+    """
+    #global unusable_dd_inds
+    global left_corner
+    global right_corner
+    if np.shape(data[('gas', 'cell_mass')]) == (16, 16, 16):
+        Proj_field = data[('gas', 'cell_mass')]
+    else:
+        Proj_field = data[('gas', 'cell_mass')]
+        x_pos = data[('gas', 'x')]
+        y_pos = data[('gas', 'y')]
+        z_pos = data[('gas', 'z')]
+        unusable_dd_inds = np.where((x_pos<left_corner[0])|(x_pos>right_corner[0])|(y_pos<left_corner[1])|(y_pos>right_corner[1])|(z_pos<left_corner[2])|(z_pos>right_corner[2]))[0]
+        Proj_field[unusable_dd_inds] = 0
+        Proj_field = Proj_field.in_units('gauss')
+    
+    return Proj_field
+    
+yt.add_field(("gas", "_cell_mass_Proj"), function=_cell_mass_Proj, units=r"g", sampling_type="local", force_override=True)
