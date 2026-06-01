35b901ec5f0398a175ce5f66ad0ada2ad452c08f
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-06-02 09:56:32 +1000

    reducing resoluion of projection plots

--- Ramses_analysis/movie_script.py
+++ Ramses_analysis/movie_script.py
@@ -513,9 +513,9 @@ if args.make_frames_only == 'False':
                 left_corner = yt.YTArray([center_pos[0].value-(0.5*args.slice_thickness), center_pos[1].value-(0.5*x_width), center_pos[2].value-(0.5*y_width)], 'AU')
                 right_corner = yt.YTArray([center_pos[0].value+(0.5*args.slice_thickness), center_pos[1].value+(0.5*x_width), center_pos[2].value+(0.5*y_width)], 'AU')
                 
-            myf.set_left_corner(left_corner)
-            myf.set_right_corner(right_corner)
-            region = ds.region(center_pos, left_corner, right_corner)
+            import pdb
+            pdb.set_trace()
+            region = ds.box(left_corner, right_corner)
             x_pos = ds.r[('gas', 'x')]
             y_pos = ds.r[('gas', 'y')]
             z_pos = ds.r[('gas', 'z')]
