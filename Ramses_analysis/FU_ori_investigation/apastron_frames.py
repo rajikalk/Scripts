d2edab44076e1d3c8f50592316632dadd0a01dd5
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-06-02 14:08:43 +1000

    reducing resoluion of projection plots

--- Ramses_analysis/FU_ori_investigation/apastron_frames.py
+++ Ramses_analysis/FU_ori_investigation/apastron_frames.py
@@ -53,7 +53,7 @@ end_time = plot_times[-1]
 
 plt.clf()
 fig = plt.figure(figsize=(two_col_width, 0.8*two_col_width))
-fig.subplots_adjust(left=0, right=0, bottom=0, top=0)
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 G = gridspec.GridSpec(3,n_frames)#,height_ratios=[1.5, 2, 2])
 axes_1 = plt.subplot(G[0, :])
 plt.subplots_adjust(wspace=0.0)
