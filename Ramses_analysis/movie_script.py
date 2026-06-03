a2173a685e285a60c0bdf9024376eafc93efc6ae
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-06-03 15:06:13 +1000

    Still working on projection plot work around

--- Ramses_analysis/movie_script.py
+++ Ramses_analysis/movie_script.py
@@ -686,6 +686,7 @@ if args.make_frames_only == 'False':
                 if args.use_density_threshold != None:
                     proj_axis_field = [('gas', 'd'+perp_vel+'_Proj')]
                     proj = yt.ProjectionPlot(ds, axis_ind, proj_axis_field, width=(x_width,'au'), weight_field=weight_field, method='sum', center=center_pos)
+                    proj.set_buff_size([args.resolution, args.resolution])
                     thickness_arr = proj.frb.data[proj_axis_field[0]]
                 
                 proj_dict = {}
