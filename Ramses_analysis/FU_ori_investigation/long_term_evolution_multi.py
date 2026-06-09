09f0716f8948e69d1b122df917f4d6cb8af24f1a
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-06-10 00:12:54 +1000

    Updated colour wheel

--- Ramses_analysis/FU_ori_investigation/long_term_evolution_multi.py
+++ Ramses_analysis/FU_ori_investigation/long_term_evolution_multi.py
@@ -64,7 +64,7 @@ plt.subplots_adjust(hspace=0.0)
 smoothing_window = 200
 do_smoothing = False
 
-axs.flatten()[0].set_xlim(left=0)
+axs.flatten()[0].set_xlim([0, 1.2e6])
 axs.flatten()[0].set_ylabel('$M_{cand.}/M_{clos.}$')
 #axs.flatten()[0].set_ylim([0, 1.5])
 axs.flatten()[0].tick_params(axis='both', direction='in', top=True, right=True)
@@ -75,7 +75,7 @@ axs.flatten()[1].tick_params(axis='both', direction='in', top=True, right=True)
 axs.flatten()[2].set_ylabel('Separation (AU)')
 axs.flatten()[2].set_xlabel('Time since candidate formation (yr)')
 axs.flatten()[2].set_ylim([5.e0, 1e4])
-axs.flatten()[2].axhline(y=100, color='k', ls='--')
+axs.flatten()[2].axhline(y=100, color='k', ls='--', label="r$_{\mathrm{acc}}$")
 axs.flatten()[2].axhline(y=16.6, color='k', ls='--', lw=0.5)
 axs.flatten()[2].tick_params(axis='both', direction='in', top=True, right=True)
 
