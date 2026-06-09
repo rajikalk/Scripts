e1863f2012134c75cd30f2dd78032ec3941b05e6
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-06-10 01:13:00 +1000

    Updated colour wheel

--- Ramses_analysis/FU_ori_investigation/long_term_evolution_multi.py
+++ Ramses_analysis/FU_ori_investigation/long_term_evolution_multi.py
@@ -19,7 +19,7 @@ matplotlib.rcParams['text.latex.preamble'] = r"\usepackage{siunitx}" "\sisetup{d
 
 sink_inds = [10, 17, 45, 48, 51, 54, 56, 71, 73, 75, 85, 93, 103, 109, 118, 141, 150, 151, 154, 159, 168, 176, 177, 195, 221, 239, 258, 275, 292]
 
-labels = ['1', '2', '3', '4*', '5', '6*', '7', '8', '9 + 10', '11^', '12+13', '14', '17', '18', '19', '20^', '21', '22', '23', '24', '25', '26+27', '28', '29*', '30', '31', '32^', '33', '35*', '36']
+labelinestyle = ['1', '2', '3', '4*', '5', '6*', '7', '8', '9 + 10', '11^', '12+13', '14', '17', '18', '19', '20^', '21', '22', '23', '24', '25', '26+27', '28', '29*', '30', '31', '32^', '33', '35*', '36']
 
 #
 #plot_window = {'17' : [[19000, 55000], [56000, 75000]], '45' : [[8500, 27300], [30600, 75000]], '51' : [[16000, 30500], [30900, 75000]], '71' : [[7500, 38000]], '75' : [[5900, 6100], [14000, 17000], [18250, 18500], [19000, 24500], [27000, 75000]], '85' : [[2250, 75000]], '101' : [[3000, 75000]], '103' : [[1000, 2000], [21900, 75000]], '176' : [[36500, 39000], [45000, 46000], [48250, 49000]], '177' : [[32000, 75000]], '258' : [[6500, 12500], [13900, 75000]], '272' : [[10100, 29750], [41000, 75000]], '292' : [[3000, 4000], [5900, 75000]]}
@@ -61,26 +61,26 @@ page_height = 10.62472 #inches
 font_size = 10
 
 plt.clf()
-fig, axs = plt.subplots(ncols=1, nrows=3, figsize=(two_col_width, 2*single_col_width), sharex=True)
+fig, axs = plt.subplots(ncolinestyle=1, nrows=3, figsize=(two_col_width, 2*single_col_width), sharex=True)
 plt.subplots_adjust(hspace=0.1)
 smoothing_window = 200
 do_smoothing = True
 plot_whole_binary = True
-plot_windows = False
+plot_windows = Falinestylee
 
 axs.flatten()[2].set_xlim([0, 1.1e6])
 axs.flatten()[2].set_ylabel('$M_{cand.}/M_{clos.}$')
 axs.flatten()[2].set_ylim([0, 1.25])
 axs.flatten()[2].tick_params(axis='both', direction='in', top=True, right=True)
-#axs.flatten()[0].axhline(y=1.6, color='k', ls='--', label="2r$_{\mathrm{acc}}$")
+#axs.flatten()[0].axhline(y=1.6, color='k', linestyle='--', label="2r$_{\mathrm{acc}}$")
 axs.flatten()[1].set_ylabel('Eccentricity')
 axs.flatten()[1].set_ylim([0, 1.5])
 axs.flatten()[1].tick_params(axis='both', direction='in', top=True, right=True)
 axs.flatten()[0].set_ylabel('Separation (AU)')
 axs.flatten()[2].set_xlabel('Time since candidate formation (yr)')
 axs.flatten()[0].set_ylim([5, 1e3])
-axs.flatten()[0].axhline(y=16.6, color='k', ls='--', label="r$_{soft}$")
-#axs.flatten()[0].axhline(y=16.6, color='k', ls='--', lw=0.5)
+axs.flatten()[0].axhline(y=16.6, color='k', linestyle='--', label="r$_{soft}$")
+#axs.flatten()[0].axhline(y=16.6, color='k', linestyle='--', lw=0.5)
 axs.flatten()[0].tick_params(axis='both', direction='in', top=True, right=True)
 
 label_it = -1
@@ -102,44 +102,44 @@ for sink_ind in sink_inds:
                     
                         if sink_ind == 45:
                             alpha_val = 1.0
-                        else:
+                        elinestylee:
                             alpha_val = 0.25
                         
-                        label = "Cand. " + labels[label_it]
-                        if '*' in labels[label_it]:
-                            ls = ':'
-                        elif '^' in labels[label_it]:
-                            ls='--'
-                        else:
-                            ls='-'
+                        label = "Cand. " + labelinestyle[label_it]
+                        if '*' in labelinestyle[label_it]:
+                            linestyle = ':'
+                        elif '^' in labelinestyle[label_it]:
+                            linestyle='--'
+                        elinestylee:
+                            linestyle='-'
                         
                         if plot_colour == None:
-                            p = axs.flatten()[2].plot(np.array(smooth_t)[start_ind:end_ind], np.array(smooth_q)[start_ind:end_ind], alpha=alpha_val, label=label, ls=line_style)
-                            axs.flatten()[1].plot(np.array(smooth_t)[start_ind:end_ind], np.array(smooth_e)[start_ind:end_ind], alpha=alpha_val, ls=line_style)
-                            axs.flatten()[0].semilogy(np.array(smooth_t)[start_ind:end_ind], np.array(smooth_sep)[start_ind:end_ind], alpha=alpha_val, ls=line_style)
+                            p = axs.flatten()[2].plot(np.array(smooth_t)[start_ind:end_ind], np.array(smooth_q)[start_ind:end_ind], alpha=alpha_val, label=label, linestyle=line_style)
+                            axs.flatten()[1].plot(np.array(smooth_t)[start_ind:end_ind], np.array(smooth_e)[start_ind:end_ind], alpha=alpha_val, linestyle=line_style)
+                            axs.flatten()[0].semilogy(np.array(smooth_t)[start_ind:end_ind], np.array(smooth_sep)[start_ind:end_ind], alpha=alpha_val, linestyle=line_style)
                             plot_colour = p[-1].get_color()
-                        else:
-                            axs.flatten()[2].plot(np.array(smooth_t)[start_ind:end_ind], np.array(smooth_q)[start_ind:end_ind], alpha=alpha_val, color=plot_colour, ls=line_style)
-                            axs.flatten()[1].plot(np.array(smooth_t)[start_ind:end_ind], np.array(smooth_e)[start_ind:end_ind], alpha=alpha_val, color=plot_colour, ls=line_style)
-                            axs.flatten()[0].semilogy(np.array(smooth_t)[start_ind:end_ind], np.array(smooth_sep)[start_ind:end_ind], alpha=alpha_val, color=plot_colour, ls=line_style)
-                else:
+                        elinestylee:
+                            axs.flatten()[2].plot(np.array(smooth_t)[start_ind:end_ind], np.array(smooth_q)[start_ind:end_ind], alpha=alpha_val, color=plot_colour, linestyle=line_style)
+                            axs.flatten()[1].plot(np.array(smooth_t)[start_ind:end_ind], np.array(smooth_e)[start_ind:end_ind], alpha=alpha_val, color=plot_colour, linestyle=line_style)
+                            axs.flatten()[0].semilogy(np.array(smooth_t)[start_ind:end_ind], np.array(smooth_sep)[start_ind:end_ind], alpha=alpha_val, color=plot_colour, linestyle=line_style)
+                elinestylee:
                     if sink_ind == 45:
                         alpha_val = 1.0
-                    else:
+                    elinestylee:
                         alpha_val = 0.25
                     
-                    label = "Cand. " + labels[label_it]
-                    if '*' in labels[label_it]:
+                    label = "Cand. " + labelinestyle[label_it]
+                    if '*' in labelinestyle[label_it]:
                         line_style = ':'
-                    elif '^' in labels[label_it]:
+                    elif '^' in labelinestyle[label_it]:
                         line_style='--'
-                    else:
+                    elinestylee:
                         line_style='-'
                     
-                    p = axs.flatten()[2].plot(np.array(smooth_t), np.array(smooth_q), alpha=alpha_val, ls=line_style)
-                    axs.flatten()[1].plot(np.array(smooth_t), np.array(smooth_e)[start_ind:end_ind], alpha=alpha_val, ls=line_style)
-                    axs.flatten()[0].semilogy(np.array(smooth_t), np.array(smooth_sep)[start_ind:end_ind], alpha=alpha_val, ls=line_style, label=label)
-            else:
+                    p = axs.flatten()[2].plot(np.array(smooth_t), np.array(smooth_q), alpha=alpha_val, linestyle=line_style)
+                    axs.flatten()[1].plot(np.array(smooth_t), np.array(smooth_e)[start_ind:end_ind], alpha=alpha_val, linestyle=line_style)
+                    axs.flatten()[0].semilogy(np.array(smooth_t), np.array(smooth_sep)[start_ind:end_ind], alpha=alpha_val, linestyle=line_style, label=label)
+            elinestylee:
                 print('reading ', pickle_file)
                 file_open = open(pickle_file, 'rb')
                 particle_data, counter, sink_ind, sink_form_time = pickle.load(file_open)
@@ -174,30 +174,30 @@ for sink_ind in sink_inds:
                     smooth_e.append(mean_e)
                     smooth_sep.append(mean_sep)
                 
-                label = "Cand. " + labels[label_it]
-                if '*' in labels[label_it]:
-                    ls = ':'
-                elif '^' in labels[label_it]:
-                    ls='--'
-                else:
-                    ls='-'
-                axs.flatten()[2].plot(smooth_t, smooth_q, alpha=0.25, ls=ls)
-                axs.flatten()[1].plot(smooth_t, smooth_e, alpha=0.25, ls=ls)
-                axs.flatten()[0].semilogy(smooth_t, smooth_sep, alpha=0.25, label=label, ls=ls)
+                label = "Cand. " + labelinestyle[label_it]
+                if '*' in labelinestyle[label_it]:
+                    linestyle = ':'
+                elif '^' in labelinestyle[label_it]:
+                    linestyle='--'
+                elinestylee:
+                    linestyle='-'
+                axs.flatten()[2].plot(smooth_t, smooth_q, alpha=0.25, linestyle=linestyle)
+                axs.flatten()[1].plot(smooth_t, smooth_e, alpha=0.25, linestyle=linestyle)
+                axs.flatten()[0].semilogy(smooth_t, smooth_sep, alpha=0.25, label=label, linestyle=linestyle)
                 
                 print('updating pickle')
                 file = open('/scratch/ek9/rlk100/RAMSES/Analysis/Long_term_evolution_pickles/smoothed_particle_data_'+str(sink_ind)+'.pkl', 'wb')
                 pickle.dump((smooth_t, smooth_q, smooth_e, smooth_sep), file)
                 file.close()
                 print('Finished updating pickle', '/scratch/ek9/rlk100/RAMSES/Analysis/Long_term_evolution_pickles/smoothed_particle_data_'+str(sink_ind)+'.pkl')
-        else:
+        elinestylee:
             print('reading ', pickle_file)
             file_open = open(pickle_file, 'rb')
             particle_data, counter, sink_ind, sink_form_time = pickle.load(file_open)
             file_open.close()
             print('Finished reading pickle')
             
-            if plot_whole_binary == False:
+            if plot_whole_binary == Falinestylee:
                 for time_window in plot_window[str(sink_ind)]:
                     start_t = time_window[0]
                     end_t = time_window[-1]
@@ -208,23 +208,23 @@ for sink_ind in sink_inds:
                     smooth_q = mass_ratio[start_it:end_it]
                     smooth_e = particle_data['eccentricity'][start_it:end_it]
                     smooth_sep = particle_data['separation'][start_it:end_it]
-            else:
+            elinestylee:
                 mass_ratio = yt.YTArray(particle_data['mass'])/yt.YTArray(particle_data['closest_mass'])
                 smooth_t = particle_data['time']
                 smooth_q = mass_ratio
                 smooth_e = particle_data['eccentricity']
                 smooth_sep = particle_data['separation']
                 
-                label = "Cand. " + labels[label_it]
-                if '*' in labels[label_it]:
-                    ls = ':'
-                elif '^' in labels[label_it]:
-                    ls='--'
-                else:
-                    ls='-'
-                axs.flatten()[2].plot(smooth_t, smooth_q, alpha=0.25, ls=ls)
-                axs.flatten()[1].plot(smooth_t, smooth_e, alpha=0.25, ls=ls)
-                axs.flatten()[0].semilogy(smooth_t, smooth_sep, alpha=0.25, label=label, ls=ls)
+                label = "Cand. " + labelinestyle[label_it]
+                if '*' in labelinestyle[label_it]:
+                    linestyle = ':'
+                elif '^' in labelinestyle[label_it]:
+                    linestyle='--'
+                elinestylee:
+                    linestyle='-'
+                axs.flatten()[2].plot(smooth_t, smooth_q, alpha=0.25, linestyle=linestyle)
+                axs.flatten()[1].plot(smooth_t, smooth_e, alpha=0.25, linestyle=linestyle)
+                axs.flatten()[0].semilogy(smooth_t, smooth_sep, alpha=0.25, label=label, linestyle=linestyle)
                 
         axs.flatten()[0].legend(loc='upper center', bbox_to_anchor=(0.5, 2.1), ncol=4)
         plt.savefig("q_and_e_evol_all_candidates.pdf", bbox_inches='tight', pad_inches=0.02)
