fixed semimajor axis calculation

Ramses_analysis/FU_ori_investigation/long_term_evolution_multi.py
@@ -212,7 +212,7 @@ for sink_ind in sink_inds:
                     smooth_e = particle_data['eccentricity'][start_it:end_it]
                     smooth_sep = particle_data['semimajor_axis'][start_it:end_it]
             else:
-                closes_inds = np.unique(particle_data['closest_sink'][:end_it], return_index=True)[0][np.argsort(np.unique(particle_data['closest_sink'][:end_it], return_index=True)[1])]
+                closes_inds = np.unique(particle_data['closest_sink'], return_index=True)[0][np.argsort(np.unique(particle_data['closest_sink'], return_index=True)[1])]
                 diff_inds = np.setdiff1d(np.arange(len(particle_data['time'])), curr_inds)
                 
                 smooth_t = particle_data['time']