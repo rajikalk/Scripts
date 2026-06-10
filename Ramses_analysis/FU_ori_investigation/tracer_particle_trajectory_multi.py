7b1d556e0e6d2a166a49c15d49974e2a8f82cdce
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-06-10 22:11:03 +1000

    mutli plot for tracer trajectory

--- Ramses_analysis/FU_ori_investigation/tracer_particle_trajectory_multi.py
+++ Ramses_analysis/FU_ori_investigation/tracer_particle_trajectory_multi.py
@@ -60,7 +60,7 @@ for Traj_pickle in Traj_pickles:
         sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
         sm.set_array([])
         plt.savefig("XY_tracer_traj.pdf", bbox_inches='tight', pad_inches=0.02)
-cax = fig1.add_axes([0.905, 0.13, 0.03, 0.72])
+cax = fig.add_axes([0.905, 0.13, 0.03, 0.72])
 cbar = plt.colorbar(sm, cax=cax)
 cbar.set_label(r"Time Norm (yr)", rotation=270, labelpad=14)
 plt.savefig("XY_tracer_traj.pdf", bbox_inches='tight', pad_inches=0.02)
