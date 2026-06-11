ceabbfdadd4c1d83922a0697fe5981462f88bddb
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-06-11 22:20:22 +1000

    mutli plot for tracer trajectory

--- Modules/my_ramses_module.py
+++ Modules/my_ramses_module.py
@@ -496,7 +496,7 @@ def my_own_quiver_function(axis, X_pos, Y_pos, X_val, Y_val, plot_velocity_legen
         #print("plotting quiver legend")
         #pos_start = [xmax - 0.15*(xmax-xmin), ymin + (fontsize_global/100)*(ymax-ymin)]
         
-        pos_start = [xmax - 0.1*(xmax-xmin), ymin + 0.7*(fontsize_global/100)*(ymax-ymin)]
+        pos_start = [xmax - 0.11*(xmax-xmin), ymin + 0.7*(fontsize_global/100)*(ymax-ymin)]
         text_y_pos = ymin + 0.07*(ymax-ymin)
         xvel = len_scale*(standard_vel/standard_vel)
         yvel = 0.0
