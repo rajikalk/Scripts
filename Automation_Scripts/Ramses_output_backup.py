19ba2d12020001b770279f6de27f0432dca89bf2
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-07-06 14:49:18 +1000

    making script to backup simulated output

--- Automation_Scripts/Ramses_output_backup.py
+++ Automation_Scripts/Ramses_output_backup.py
@@ -16,19 +16,21 @@ with open('/home/100/rlk100/expiring_files.txt', 'r') as f:
         if 'output_' in row[0]:
             compress_file = row[0].split(' ')[-1].split('output_')[0] + 'output_' + row[0].split(' ')[-1].split('output_')[1].split('/')[0]
             tar_gz = compress_file+'.tar.gz'
-            if
+            #check if mdss save dir is exists
+            mdss_save_dir = 'rlk100/Zoom_in_simulations/Sink_' + compress_file.split('Sink_')[-1].split('data')[0]
+            import pdb
+            pdb.set_trace()
+            #check if tar in mdss
             shellcmd = 'tar -czvf ' + tar_gz + ' ' + compress_file
             result = subprocess.run(shellcmd, stdout=subprocess.PIPE, shell=True, capture_output=True, text=True)
             output_string = result.stdout
             print("Made tar of", compress_file)
             
-            mdss_save_dir = 'rlk100/Zoom_in_simulations/Sink_' + compress_file.split('Sink_')[-1].split('data')[0]
+            
             #check is sim dir exists:
             shellcmd = 'mdss ls' + mdss_save_dir
             result = subprocess.run(shellcmd, stdout=subprocess.PIPE, shell=True, capture_output=True, text=True)
             output_string = result.stdout
-            import pdb
-            pdb.set_trace()
             proc = subprocess.Popen(shellcmd, stdout=subprocess.PIPE, shell=True)
             shellcmd = 'mdss put '+ compress_file+'.tar.gz '+mdss_save_dir
             proc = subprocess.Popen(shellcmd, stdout=subprocess.PIPE, shell=True)
