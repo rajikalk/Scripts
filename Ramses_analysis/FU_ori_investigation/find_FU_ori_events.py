a02728e66a975f05c3619328e80804f776a3adff
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-07-21 12:31:05 +1000

    updating events for ApJL

--- Ramses_analysis/FU_ori_investigation/find_FU_ori_events.py
+++ Ramses_analysis/FU_ori_investigation/find_FU_ori_events.py
@@ -352,11 +352,10 @@ if rank == 0:
         ax1.tick_params(axis='both', labelsize=font_size, labelfontfamily='sans-serif')
         ax2.tick_params(axis='both', labelsize=font_size, labelfontfamily='sans-serif')
         
-        if plot_it == 36:
-            ax1.legend(bbox_to_anchor=(1, 1))
+        ax1.legend(bbox_to_anchor=(1, 1))
             
         useable_times = useable_times/1000
-        Cand_string = "Cand. "+str(plot_it)
+        Cand_string = "Cand. "+str(plot_it+1)
         Cand_string_raw = r"{}".format(Cand_string)
         Cand_text = ax1.text(np.max(useable_times), 0.15, Cand_string_raw, va="center", ha="right", color='k', fontsize=font_size)
         
