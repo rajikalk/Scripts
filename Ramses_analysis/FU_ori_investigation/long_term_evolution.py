c85dcdfc6179f9013b66489779153a07732feb8f
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-08-24 17:31:12 +1000

    working on long term evolution plots

--- Ramses_analysis/FU_ori_investigation/long_term_evolution.py
+++ Ramses_analysis/FU_ori_investigation/long_term_evolution.py
@@ -229,7 +229,7 @@ for sink_id in top_clean:
         axs.flatten()[0].set_xlim([time_bounds[0], time_bounds[1]])
         plot_glob = np.where((glob_time_wrt_sink>time_bounds[0])&(glob_time_wrt_sink<time_bounds[1]))[0]
         for glob_it in plot_glob:
-            axs.flatten()[0].axvline(x=glob_time_wrt_sink[glob_it])
+            axs.flatten()[0].axvline(x=glob_time_wrt_sink[glob_it], c='r')
             ax0.text(glob_time_wrt_sink[glob_it], 900, str(glob_it))
         axs.flatten()[0].set_ylabel("L$_{tot}$ (L$_\odot$)")
         axs.flatten()[0].tick_params(axis='both', direction='in', top=True)
@@ -274,7 +274,7 @@ for sink_id in top_clean:
         ax1 = axs.flatten()[1].twinx()
         plot_glob = np.where((glob_time_wrt_sink>time_bounds[1])&(glob_time_wrt_sink<time_bounds[2]))[0]
         for glob_it in plot_glob:
-            axs.flatten()[0].axvline(x=glob_time_wrt_sink[glob_it])
+            axs.flatten()[0].axvline(x=glob_time_wrt_sink[glob_it], c='r')
             ax1.text(glob_time_wrt_sink[glob_it], 900, str(glob_it))
         ax1.semilogy(particle_data['time'][:end_it], particle_data['separation'][:end_it], color='k', ls="--", alpha=0.25)
         ax1.axhline(y=16.6, color='k', ls='-.')
@@ -314,7 +314,7 @@ for sink_id in top_clean:
         ax2 = axs.flatten()[2].twinx()
         plot_glob = np.where((glob_time_wrt_sink>time_bounds[2])&(glob_time_wrt_sink<time_bounds[3]))[0]
         for glob_it in plot_glob:
-            axs.flatten()[0].axvline(x=glob_time_wrt_sink[glob_it])
+            axs.flatten()[0].axvline(x=glob_time_wrt_sink[glob_it], c='r')
             ax2.text(glob_time_wrt_sink[glob_it], 900, str(glob_it))
         ax2.semilogy(particle_data['time'][:end_it], particle_data['separation'][:end_it], color='k', ls="--", alpha=0.25)
         ax2.axhline(y=16.6, color='k', ls='-.')
@@ -354,7 +354,7 @@ for sink_id in top_clean:
         ax3 = axs.flatten()[3].twinx()
         plot_glob = np.where((glob_time_wrt_sink>time_bounds[3])&(glob_time_wrt_sink<time_bounds[4]))[0]
         for glob_it in plot_glob:
-            axs.flatten()[0].axvline(x=glob_time_wrt_sink[glob_it])
+            axs.flatten()[0].axvline(x=glob_time_wrt_sink[glob_it], c='r')
             ax3.text(glob_time_wrt_sink[glob_it], 900, str(glob_it))
         ax3.semilogy(particle_data['time'][:end_it], particle_data['separation'][:end_it], color='k', ls="--", alpha=0.25)
         ax3.axhline(y=16.6, color='k', ls='-.')
@@ -394,7 +394,7 @@ for sink_id in top_clean:
         ax4 = axs.flatten()[4].twinx()
         plot_glob = np.where((glob_time_wrt_sink>time_bounds[4])&(glob_time_wrt_sink<time_bounds[5]))[0]
         for glob_it in plot_glob:
-            axs.flatten()[0].axvline(x=glob_time_wrt_sink[glob_it])
+            axs.flatten()[0].axvline(x=glob_time_wrt_sink[glob_it], c='r')
             ax4.text(glob_time_wrt_sink[glob_it], 900, str(glob_it))
         ax4.semilogy(particle_data['time'][:end_it], particle_data['separation'][:end_it], color='k', ls="--", alpha=0.25)
         ax4.axhline(y=16.6, color='k', ls='-.')
