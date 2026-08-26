00ad422acb9f05ea72d57520d0823c44ce96fe53
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-08-26 11:48:40 +1000

    added mass to long term evolution plots

--- Ramses_analysis/FU_ori_investigation/simulation_separation_progress_plot.py
+++ Ramses_analysis/FU_ori_investigation/simulation_separation_progress_plot.py
@@ -27,8 +27,12 @@ loaded_sink_data = rsink(datadir='data/', all=True)
     
 particle_data = {}
 particle_data.update({'time':np.array([])})
+particle_data.update({'mass':np.array([])})
+particle_data.update({'mdot':np.array([])})
 particle_data.update({'separation':np.array([])})
 particle_data.update({'closest_sink':np.array([])})
+particle_data.update({'closest_mass':np.array([])})
+particle_data.update({'closest_mdot':np.array([])})
         
 counter = 0
 sink_form_time = 0
@@ -42,6 +46,16 @@ while len(loaded_sink_data)>0:
         sink_form_time = sink_data['tcreate'][sink_ind]*units['time_unit'].in_units('yr')
     time_val = sink_data['snapshot_time']*units['time_unit'].in_units('yr') - sink_form_time
     particle_data['time'] = np.append(particle_data['time'], time_val)
+    
+    particle_mass = yt.YTArray(sink_data['m'][np.array([sink_ind])]*units['mass_unit'].in_units('msun'), 'msun')
+    particle_data['mass'] = np.append(particle_data['mass'], particle_mass)
+    
+    d_mass = sink_data['dm'][np.array([sink_ind])]*units['mass_unit'].in_units('msun')
+    d_time = (sink_data['snapshot_time'] - sink_data['tflush'])*units['time_unit'].in_units('yr')
+    acc_val = d_mass/d_time
+    if acc_val == 0:
+        acc_val =1.e-12
+    particle_data['mdot'] = np.append(particle_data['mdot'], acc_val)
 
     position = yt.YTArray(np.array([sink_data['x'][sink_ind], sink_data['y'][sink_ind], sink_data['z'][sink_ind]])*units['length_unit'].in_units('au'), 'au')
     dx = sink_data['x']*units['length_unit'].in_units('au') - position[0]
@@ -52,3 +66,31 @@ while len(loaded_sink_data)>0:
     particle_data['closest_sink'] = np.append(particle_data['closest_sink'], closest_ind)
     separation = sep[closest_ind]
     particle_data['separation'] = np.append(particle_data['separation'], separation)
+    
+    other_mass = yt.YTArray(sink_data['m'][np.array([closest_ind])]*units['mass_unit'].in_units('msun'), 'msun')
+    d_mass = sink_data['dm'][np.array([closest_ind])]*units['mass_unit'].in_units('msun')
+    d_time = (sink_data['snapshot_time'] - sink_data['tflush'])*units['time_unit'].in_units('yr')
+    acc_val = d_mass/d_time
+    if acc_val == 0:
+        acc_val =1.e-12
+    particle_data['closest_mass'] = np.append(particle_data['closest_mass'], other_mass)
+    particle_data['closest_mdot'] = np.append(particle_data['closest_mdot'], acc_val)
+    
+plt.clf()
+plt.clf()
+fig, axs = plt.subplots(ncols=1, nrows=3, figsize=(two_col_width, single_col_width*1.7), sharex=True)#, sharey=True)
+iter_range = range(0, len(pickle_files))
+plt.subplots_adjust(wspace=0.0)
+plt.subplots_adjust(hspace=0.0)
+axs.flatten()[0].plot(particle_data['time'], particle_data['mass'])
+axs.flatten()[0].plot(particle_data['time'], particle_data['closest_mass'])
+axs.flatten()[0].set_ylabel('Mass (M$_\odot$)')
+axs.flatten()[0].set_ylim(bottom=0)
+axs.flatten()[1].semilogy(particle_data['time'], particle_data['mdot'])
+axs.flatten()[1].semilogy(particle_data['time'], particle_data['closest_mdot'])
+axs.flatten()[1].set_ylabel('Accretion rate (M$_\odot$/yr)')
+axs.flatten()[2].semilogy(particle_data['time'], particle_data['separation'])
+axs.flatten()[2].set_ylabel('Separation (AU)')
+axs.flatten()[2].set_xlabel('Time (yr)', size=font_size)
+axs.flatten()[2].set_xlim([particle_data['time'][0], particle_data['time'][-1]])
+plt.savefig('progress_plot.png')
