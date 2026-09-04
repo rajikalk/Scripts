f074d2a2a5e35840122e908d7701f4f04581d563
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-09-04 12:08:34 +1000

    All the print lines

--- Ramses_analysis/FU_ori_investigation/BHL_accretion_calculation.py
+++ Ramses_analysis/FU_ori_investigation/BHL_accretion_calculation.py
@@ -75,7 +75,7 @@ elif os.path.exists('BHL_accretion_0.pkl'):
     time_arr = np.array([])
     BHL_Acc_acc_low = np.array([])
     BHL_Acc_acc_high = np.array([])
-    for rit in range(size):
+    for rit in range(pickle_files):
         file_open = open('BHL_accretion_'+str(rit)+'.pkl', 'rb')
         time_arr_r, BHL_Acc_acc_low_r, BHL_Acc_acc_high_r = pickle.load(file_open)
         file_open.close()
