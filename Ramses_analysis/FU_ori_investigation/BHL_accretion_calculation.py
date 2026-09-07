8af5be179fa793b6bb198bd0778e37c27574ec47
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-09-07 10:48:20 +1000

    Figuring out parallelisation

--- Ramses_analysis/FU_ori_investigation/BHL_accretion_calculation.py
+++ Ramses_analysis/FU_ori_investigation/BHL_accretion_calculation.py
@@ -105,8 +105,8 @@ CW.Barrier()
 
 if len(files)>0:
     para_div = 4
-    for fn in yt.parallel_objects(files, njobs=int(size/para_div)):
-        proj_root_rank = int(rank/(size/para_div))
+    for fn in yt.parallel_objects(files):
+        #proj_root_rank = int(rank/(size/para_div))
         print('Reading file', fn, 'on rank', rank)
         sys.stdout.flush()
         ds = yt.load(fn, units_override=units_override)
