2bacd6e1a42f0bb1056dd3a416b4933622de7542
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-09-14 17:21:25 +1000

    make multiplt of BHL accretion

--- Ramses_analysis/FU_ori_investigation/Keplerian_mass_measure.py
+++ Ramses_analysis/FU_ori_investigation/Keplerian_mass_measure.py
@@ -74,7 +74,7 @@ CW.Barrier()
 gc.collect()
 
 if len(files)>0:
-    para_div = 28
+    para_div = 4
     for fn in yt.parallel_objects(files, njobs=int(size/para_div)):
         proj_root_rank = int(rank/para_div)
         print('Reading file', fn, 'on rank', rank)
