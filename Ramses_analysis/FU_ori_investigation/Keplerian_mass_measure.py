90a276d56826b91a1298db571b748d63e79e8ed4
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-09-14 13:55:34 +1000

    make multiplt of BHL accretion

--- Ramses_analysis/FU_ori_investigation/Keplerian_mass_measure.py
+++ Ramses_analysis/FU_ori_investigation/Keplerian_mass_measure.py
@@ -53,9 +53,9 @@ elif os.path.exists('Kep_mass_0.pkl'):
         file_open.close()
         for key in save_dict_r.keys():
             try:
-                save_dict[key] = np.append(save_dict[key], save_dict_r[key], axis=1)
+                save_dict[key] = np.append(save_dict[key], [save_dict_r[key]], axis=1)
             except:
-                save_dict[key] = np.append(save_dict[key], save_dict_r[key], axis=0)
+                save_dict[key] = np.append(save_dict[key], [save_dict_r[key]], axis=0)
     del save_dict_r
     gc.collect()
     sorted_inds = np.argsort(save_dict["Time"])
