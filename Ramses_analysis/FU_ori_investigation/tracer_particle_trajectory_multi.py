79ca6f3a861f0c3007e00dde33821b4999c89bef
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-09-16 16:52:58 +1000

    Adjusting frames plots to shows suppression event and long term evolution

--- Ramses_analysis/FU_ori_investigation/tracer_particle_trajectory_multi.py
+++ Ramses_analysis/FU_ori_investigation/tracer_particle_trajectory_multi.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import matplotlib as mpl
-from mpl_toolkits.axes_grid1 import AxesGrid
+
 
 mpl.rcParams['mathtext.fontset'] = 'stixsans'
 mpl.rcParams['mathtext.it'] = 'Arial:italic'
@@ -31,7 +31,7 @@ linewidth = 0.8
 
 Traj_pickles = ['/home/100/rlk100/rlk/RAMSES/Analysis/Tracer_particle_analysis/Event_2/tracer_trajectory.pkl', '/home/100/rlk100/rlk/RAMSES/Analysis/Tracer_particle_analysis/Event_3/tracer_trajectory.pkl', '/home/100/rlk100/rlk/RAMSES/Analysis/Tracer_particle_analysis/Event_4/tracer_trajectory.pkl', '/home/100/rlk100/rlk/RAMSES/Analysis/Tracer_particle_analysis/Event_5/tracer_trajectory.pkl']
 
-fig, axs = plt.subplots(figsize=(two_col_width, 0.8*single_col_width))
+fig, axs = plt.subplots(ncols=4, nrows=1, figsize=(two_col_width, 0.8*single_col_width), sharex=True, sharey=True)
 plt.subplots_adjust(hspace=-0.61)
 plt.subplots_adjust(wspace=0.0)
 
@@ -52,7 +52,7 @@ for Traj_pickle in Traj_pickles:
         Time_norm = Time_array/t_event
         
         ax = axs.flatten()[Traj_pickles.index(Traj_pickle)]
-    
+
         for tracer_it in range(len(Tracer_parallel)):
             start_sep = np.sqrt(Tracer_parallel[tracer_it][0]**2 +Tracer_perpendicular[tracer_it][0]**2)
             end_sep = np.sqrt(Tracer_parallel[tracer_it][-1]**2 +Tracer_perpendicular[tracer_it][-1]**2)
@@ -92,9 +92,7 @@ for Traj_pickle in Traj_pickles:
         plt.setp(xticklabels[-1], visible=False)
         
         plt.savefig("XY_tracer_traj_ref.pdf", bbox_inches='tight', pad_inches=0.02)
-#import pdb
-#pdb.set_trace()
-cax = fig.add_axes([0.90, 0.30, 0.01, 0.35])
+#cax = fig.add_axes([0.90, 0.30, 0.01, 0.35])
 cbar = ax.colorbar(sm) #plt.colorbar(sm, cax=cax)
 cbar.set_label(r"Time Normalised", rotation=270, labelpad=13, fontsize=font_size)
 plt.savefig("XY_tracer_traj_ref.pdf", bbox_inches='tight', pad_inches=0.03)
