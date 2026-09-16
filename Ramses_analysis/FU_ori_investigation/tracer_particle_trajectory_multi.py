85ac18de0ed4a1c1de9086866f6d8d8c2fc7a74e
Rajika Lakmali Kuruwita <reggie@Reggies-MacBook-Pro-2.local>
2026-09-16 17:00:36 +1000

    Adjusting frames plots to shows suppression event and long term evolution

--- Ramses_analysis/FU_ori_investigation/tracer_particle_trajectory_multi.py
+++ Ramses_analysis/FU_ori_investigation/tracer_particle_trajectory_multi.py
@@ -92,7 +92,9 @@ for Traj_pickle in Traj_pickles:
         plt.setp(xticklabels[-1], visible=False)
         
         plt.savefig("XY_tracer_traj_ref.pdf", bbox_inches='tight', pad_inches=0.02)
-#cax = fig.add_axes([0.90, 0.30, 0.01, 0.35])
-cbar = fig.colorbar(sm, ax=ax, label=r"Time Normalised", shrink=0.5) #plt.colorbar(sm, cax=cax)
+import pdb
+pdb.set_trace()
+cax = fig.add_axes([0.90, 0.30, 0.01, 0.35])
+cbar = plt.colorbar(sm, cax=cax)
 cbar.set_label(r"Time Normalised", rotation=270, labelpad=13, fontsize=font_size)
 plt.savefig("XY_tracer_traj_ref.pdf", bbox_inches='tight', pad_inches=0.03)
