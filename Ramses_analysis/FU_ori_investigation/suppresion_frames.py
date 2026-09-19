2d4c32854d7ea99bf52f6a1a0e310ea8f808a3b8
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-09-19 13:18:42 +1000

    adjusting thew supression event plot

--- Ramses_analysis/FU_ori_investigation/suppresion_frames.py
+++ Ramses_analysis/FU_ori_investigation/suppresion_frames.py
@@ -45,7 +45,7 @@ mym.set_global_font_size(font_size)
 #time_bounds = [[3800, 4900],[5575, 5700], [6580, 6720], [7295, 7365], [7850, 7900]]
 time_bounds = [[3800, 4900+500],[5575, 5700+500], [6580, 6720+500], [7295, 7365+500], [7850, 7900+500]]
 burst_bounds = [[], [5575, 5635], [6580, 6625], [7295, 7309], [7850, 7858]]
-cbar_lims_all = [[], [1.e-15, 1.e-13], [1.e-15, 1.e-14], [1.e-15, 1.e-13], [2.e-15, 2.e-13]]
+cbar_lims_all = [[], [1.e-15, 1.e-13], [1.e-15, 5.e-14], [1.e-15, 1.e-13], [2.e-15, 2.e-13]]
 cmap=plt.cm.gist_heat
 
 #Start by loading pickel data and then deleting what we don't need
