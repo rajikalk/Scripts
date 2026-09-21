92dfc496bd27f147092203263813a099855ba6c2
Rajika Lakmali Kuruwita <rajikakuruwita@gmail.com>
2026-09-21 17:26:34 -0400

    remaking plot of all accretion histories

--- Ramses_analysis/CF_analysis/sink_accretion_superplot.py
+++ Ramses_analysis/CF_analysis/sink_accretion_superplot.py
@@ -32,7 +32,8 @@ Weird = [33, 71, 86, 91, 93, 98, 111, 114, 124, 133, 135, 143, 149, 177, 178, 18
 rank = CW.Get_rank()
 size = CW.Get_size()
 
-global_data_pickle_file = '/groups/astro/rlk/rlk/Analysis_plots/Ramses/Global/G100/512/stars_red_512.pkl'
+#global_data_pickle_file = '/groups/astro/rlk/rlk/Analysis_plots/Ramses/Global/G100/512/stars_red_512.pkl'
+global_data_pickle_file = '/home/100/rlk100/gdata/RAMSES/Global/G100/512_Resolution/stars_red_512.pkl'
 
 units_override = {"length_unit":(4.0,"pc"), "velocity_unit":(0.18, "km/s"), "time_unit":(685706129102738.9, "s")}
 
